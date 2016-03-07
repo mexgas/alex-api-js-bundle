f9267d97d3281095343806da799e340ffb235ef6
mmedinap <mperez@nuxiba.com>
2016-03-07 13:32:41 -0600

    ## 2528 se agregan archivos cproj y se cambia version

--- Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
+++ Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.01.118.06")]
-[assembly: AssemblyFileVersion("2016.01.118.06")]
+[assembly: AssemblyVersion("2016.03.119.01")]
+[assembly: AssemblyFileVersion("2016.03.119.01")]

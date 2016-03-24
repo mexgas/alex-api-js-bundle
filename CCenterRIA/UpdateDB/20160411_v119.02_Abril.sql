9931ff191feb7709b6283983b54ec22c8a434b44
Raul Moctezuma <rmoctezuma@nuxiba.com>
2016-03-24 13:36:55 -0600

    #2790 Se agregan los menús para los reportes

--- CCenterRIA/UpdateDB/20160411_v119.02_Abril.sql
+++ CCenterRIA/UpdateDB/20160411_v119.02_Abril.sql
@@ -44,9 +44,12 @@ if @actualVersion = @version and actualVersionFix = versionfix-1
 		begin tran
 		begin try
 
-
-
-		set @process = ''
+		set @process = 'INSERT -------- ccMenus'
+		set @sql ='if not exists(select * from ccmenus where type=3 and menu_id in(4220, 4230, 4240)) begin
+						insert into ccMenus(menu_id, menu_descrip, parent, Nivel, ordengral, [type], HelpSWF, release ) values (4220, ''Reporte de teléfonos por estado de la república|Telephone report ordered by republic states'', 4000, ''B'', 4, 3, '''', ''60b188045ce43b6a1d77f7a81f67767fc90fbef71d57e4498d362c5c67a3c097d076f2f127f0f7af01beda4ac36008993c52871865dfbcc8d37183f0a429089f59ae97a60b9d269449063e6d38f93222a414d69d2a3fb7b721155619d8e6b4e2'')
+						insert into ccMenus(menu_id, menu_descrip, parent, Nivel, ordengral, [type], HelpSWF, release ) values (4230, ''Reporte de Número de Teléfonos por Registro por Lista|Telephone Report per Registry List'',  4000, ''B'', 4, 3, '''',''074a6cc91623e51a5020bb702fad033d304b666112d87ff90249fb8676ec86395611c16cf4e162ebabf8555caeea9365241fede8508032e858587c216da52aa9fea8e00d5e2f12006bb1564615249179bb0886e83a3aa729313cdd7fcd79cf35'')
+						insert into ccMenus(menu_id, menu_descrip, parent, Nivel, ordengral, [type], HelpSWF, release ) values (4240, ''Reporte de resultados de marcación|Dialing Result Report'', 4000, ''B'', 4, 3, '''', ''9cf7679f1b10838b63e4eae2368159813ae5d3eecaf4bccecfb21a247080897dc3e3d80988c85c0931f5a2fe77da619d446f04bcc6ff01e8247b5531a00ded6b'')
+				   end'
 		EXEC(@sql)
 
 		set @process = ''

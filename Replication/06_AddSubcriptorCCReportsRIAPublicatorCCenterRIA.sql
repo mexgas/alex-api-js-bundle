6c3f5b67ce009cab447620055cd66d23997f784d
agallardoNuxiba <agallardo@nuxiba.com>
2016-04-14 15:00:34 -0500

    #fix se agrega publicacion MAil

--- Replication/06_AddSubcriptorCCReportsRIAPublicatorCCenterRIA.sql
+++ Replication/06_AddSubcriptorCCReportsRIAPublicatorCCenterRIA.sql
@@ -173,8 +173,8 @@ if @Version_Actual >= @Version
 		--nuevo agregado
 
 		if not exists(select * FROM dbo.sysmergesubscriptions  WHERE db_name = 'ccReportsRia'AND pubid = (select pubid FROM dbo.sysmergepublications WHERE
-			name = 'Conversationtweet' and UPPER(publisher)=UPPER(publishingservername()) and publisher_db=db_name()) AND status <>2 and subscription_type <> 2 and subscription_type <> 3) begin
-				exec sp_addmergesubscription @publication = N'Conversationtweet', @subscriber = @subscriptionServerReportsRia, @subscriber_db = N'ccReportsRia', @subscription_type = N'pull', @subscriber_type = N'local', @subscription_priority = 0, @sync_type = N'Automatic'
+			name = 'ConversationMail' and UPPER(publisher)=UPPER(publishingservername()) and publisher_db=db_name()) AND status <>2 and subscription_type <> 2 and subscription_type <> 3) begin
+				exec sp_addmergesubscription @publication = N'ConversationMail', @subscriber = @subscriptionServerReportsRia, @subscriber_db = N'ccReportsRia', @subscription_type = N'pull', @subscriber_type = N'local', @subscription_priority = 0, @sync_type = N'Automatic'
 		end
 
 		if not exists(select * FROM dbo.sysmergesubscriptions  WHERE db_name = 'ccReportsRia'AND pubid = (select pubid FROM dbo.sysmergepublications WHERE

3a60a2ff03732a190e61d2887dab2497207a6db4
lambrosionuxiba <lambrosio@nuxiba.com>
2016-05-06 12:41:09 -0500

    Revert "cambios"
    This reverts commit cf3078f32769af6524e6bf9e9e449adcab97e606.

--- CCenterRIA/UpdateDB/20160510_v119.03_Mayo.sql
+++ CCenterRIA/UpdateDB/20160510_v119.03_Mayo.sql
@@ -146,118 +146,103 @@ if @actualVersion = @version and @actualVersionFix = @versionfix-1
 		values (4160,''Calificaciones por hora|Dispositions by hour'', 4000, ''B'', 4, 3, '''', ''54d108e6439d9428e2b8fd3e9f91fdee5f952d3162915efc1d9a9ceeb67ab3264e6299676240becbca206e8e78bdf663'')'
 		EXEC(@sql)
 
-		set @process = 'insert into -- ccMenus 80'
-		set @sql='if not exists(select * from ccMenus where menu_id=80)
-		    insert into ccMenus (menu_id, menu_descrip, parent, Nivel, ordengral, type, release) 
-		    values (86, ''Encuentas|Surveys'', 80, ''B'', 85, 1, ''d1fcffe459da1ffcf96b682518980981f372441edaa94120af5f7e10fe1c5069'')'
-		EXEC(@sql)
 
 		set @process = 'CREATE PROCEDURE -- ccspSurveyIVR'
 		set @sql='CREATE procedure [dbo].[ccspSurveyIVR]
-@action as tinyint,@surveyId int =0,@description varchar(80) = null,@scriptId int=null,@isActive bit=null,  
+@action as tinyint,@surveyId int =0,@description varchar(80) = null,@IVRId int=null,@isActive bit=null,
 @questionId int =0,@answerId int=0,@digit tinyint=null,@ids varchar(400)=null,@orden varchar(400)=null
-AS  
-  
+AS
+
 declare @sql nvarchar(max)
-declare @coma varchar(10)='',''  
-declare @id int=0  
-if @action = 1 begin --INSERT and Update Survey   
- select @id=surveyId from Survey where description=@description  
- if @id > 0 and @id<>@surveyId begin  
-  select -1 as surveyId  
-  return (0)  
- end  
- if @surveyId=0 begin     
-  insert into Survey(description,scriptId,active) values(@description,@scriptId,1)  
-  select @surveyId=IDENT_CURRENT(''Survey'')    
- end  
- else begin     
-  update Survey set description=isnull(@description,description),scriptId=isnull(@scriptId,scriptId),active=isnull(@isActive,active) where surveyId=@surveyId     
- end  
- select @surveyId   
- return 0  
-end  
-else if @action = 2 begin --INSERT and Update SurveyQuestion  
- select @id=questionId from SurveyQuestion where description=@description  
- if @id > 0 and @id<>@questionId begin  
-  select -1 as questionId  
-  return (0)  
- end  
- if @questionId=0 begin    
-  insert into SurveyQuestion(description,active) values(@description,1)  
-  select @questionId=IDENT_CURRENT(''SurveyQuestion'')    
- end  
- else begin  
-  update SurveyQuestion set description=isnull(@description,description),active=isnull(@isActive,active) where questionId=@questionId     
- end  
- select @questionId   
- return 0  
-end  
-else if @action = 3 begin --INSERT and Update SurveyAnswer  
- select @id=answerId from SurveyAnswer where description=@description  
- if @id > 0 and @id<>@answerId begin  
-  select -1 as questionId  
-  return (0)  
- end  
- if @answerId=0 begin  
-  insert into SurveyAnswer(description,active,digit) values(@description,1,@digit)  
-  select @answerId=IDENT_CURRENT(''SurveyAnswer'')
- end  
- else begin  
-  update SurveyAnswer set description=isnull(@description,description),active=isnull(@isActive,active),digit=isnull(@digit,digit) where answerId=@answerId     
- end  
- select @answerId   
- return 0  
-end  
-else if @action = 4 begin --insert relationSurveyQuestion   
- set @sql =''insert into relationSurveyQuestion(surveyId,questionId,orden)  
- select ''+convert(nvarchar(max),@surveyId)+'',A.Value,C.Value from dbo.fn_RIASplitDelimited(''''''+@ids+'''''',''''''+@coma+'''''') A  
-left join relationSurveyQuestion B on A.Value=B.questionId and B.surveyId=''+convert(nvarchar(max),@surveyId)+'' 
-left join dbo.fn_RIASplitDelimited(''''''+@orden+'''''',''''''+@coma+'''''') C on C.Id=A.Id
-where B.questionId is null''  
-exec (@sql)   
-end  
-else if @action = 5 begin --insert relationQuestionAnswer  
- set @sql =''insert into relationQuestionAnswer(surveyId,questionId,answerId)  
- select ''+convert(nvarchar(max),@surveyId)+'',''+convert(nvarchar(max),@questionId)+'',A.Value from dbo.fn_RIASplitDelimited(''''''+@ids+'''''',''''''+@coma+'''''') A  
-left join relationQuestionAnswer B on A.Value=B.answerId and B.questionId=''+convert(nvarchar(max),@questionId)+''  
-where B.answerId is null''   
- exec(@sql)   
-end  
-else if @action = 6 begin --delete relationSurveyQuestion   
- set @sql =''delete from relationSurveyQuestion where questionId in(''+@ids+'') and surveyId=''+convert(nvarchar(max),@surveyId)  
- exec(@sql)   
-end  
-else if @action = 7 begin --delete relationQuestionAnswer   
- set @sql =''delete from relationQuestionAnswer where surveyId=''  
- +convert(nvarchar(max),@surveyId)+'' and questionId=''+convert(nvarchar(max),@questionId) +  
- '' and answerId in(''+@ids+'') ''  
- exec(@sql)  
-end  
-else if @action = 8 begin    
- select surveyId,description,scriptId from Survey where active=1  
-end  
-else if @action = 9 begin   
- select questionId,description from SurveyQuestion where active=1  
-end  
-else if @action = 10 begin   
- select answerId,description,digit from SurveyAnswer where active=1  
-end  
-else if @action = 11 begin   
- select a.questionId,b.description,A.orden from relationSurveyQuestion A left join SurveyQuestion B on a.questionId = b.questionId where a.surveyId=@surveyId and B.active=1  order by A.orden
-end  
-else if @action = 12 begin   
- select A.answerId,B.description,B.digit from relationQuestionAnswer A left join SurveyAnswer B on A.answerId=B.answerId  where a.surveyId=@surveyId  and a.questionId=@questionId
+declare @coma varchar(10)='',''
+declare @id int=0
+if @action = 1 begin --INSERT and Update Survey
+ 	select @id=surveyId from Survey where description=@description
+ 	if @id > 0 and @id<>@surveyId begin
+  		select -1 as surveyId
+  		return (0)
+ 	end
+	if @surveyId=0 begin
+  		insert into Survey(description,IVR_id,active) values(@description,@IVRId,1)
+  		select @surveyId=IDENT_CURRENT(''Survey'')
+ 	end
+ 	else begin
+  		update Survey set description=isnull(@description,description),IVR_id=isnull(@IVRId,IVR_id),active=isnull(@isActive,active) where surveyId=@surveyId
+ 	end
+ 	select @surveyId
+ 	return 0
 end
-else if @action = 13 begin   
- select S.scriptId,S.description,a.questionId,b.description,isnull(rQA.answerId,0),isnull(SA.description,''),isnull(SA.digit,-1)
-	from relationSurveyQuestion A 
-	inner join SurveyQuestion B on a.questionId = b.questionId 
-	inner join Survey S on S.surveyId=A.surveyId
-	left join relationQuestionAnswer rQA on rQA.questionId=A.questionId
-	left join SurveyAnswer SA on SA.answerId=rQA.answerId
-	where a.surveyId=@surveyId and B.active=1  
-	order by A.orden
+else if @action = 2 begin --INSERT and Update SurveyQuestion
+ 	select @id=questionId from SurveyQuestion where description=@description
+ 	if @id > 0 and @id<>@questionId begin
+  		select -1 as questionId
+  		return (0)
+ 	end
+ 	if @questionId=0 begin
+  		insert into SurveyQuestion(description,active) values(@description,1)
+  		select @questionId=IDENT_CURRENT(''SurveyQuestion'')
+ 	end
+ 	else begin
+  		update SurveyQuestion set description=isnull(@description,description),active=isnull(@isActive,active) where questionId=@questionId
+ 	end
+ 	select @questionId
+ 	return 0
+end
+else if @action = 3 begin --INSERT and Update SurveyAnswer
+ 	select @id=answerId from SurveyAnswer where description=@description
+ 	if @id > 0 and @id<>@answerId begin
+  		select -1 as questionId
+  		return (0)
+ 	end
+ 	if @answerId=0 begin
+  		insert into SurveyAnswer(description,active,digit) values(@description,1,@digit)
+  		select @answerId=IDENT_CURRENT(''SurveyAnswer'')
+ 	end
+ 	else begin
+  		update SurveyAnswer set description=isnull(@description,description),active=isnull(@isActive,active),digit=isnull(@digit,digit) where answerId=@answerId
+ 	end
+ 	select @answerId
+	return 0
+end
+else if @action = 4 begin --insert relationSurveyQuestion
+ 	set @sql =''insert into relationSurveyQuestion(surveyId,questionId,orden)
+ 	select ''+convert(nvarchar(max),@surveyId)+'',A.Value,C.Value from dbo.fn_RIASplitDelimited(''''''+@ids+'''''',''''''+@coma+'''''') A
+	left join relationSurveyQuestion B on A.Value=B.questionId and B.surveyId=''+convert(nvarchar(max),@surveyId)+''
+	left join dbo.fn_RIASplitDelimited(''''''+@orden+'''''',''''''+@coma+'''''') C on C.Id=A.Id
+	where B.questionId is null''
+		exec (@sql)
+end
+else if @action = 5 begin --insert relationQuestionAnswer
+ 	set @sql =''insert into relationQuestionAnswer(surveyId,questionId,answerId)
+ 	select ''+convert(nvarchar(max),@surveyId)+'',''+convert(nvarchar(max),@questionId)+'',A.Value from dbo.fn_RIASplitDelimited(''''''+@ids+'''''',''''''+@coma+'''''') A
+	left join relationQuestionAnswer B on A.Value=B.answerId and B.questionId=''+convert(nvarchar(max),@questionId)+''
+	where B.answerId is null''
+ 		exec(@sql)
+end
+else if @action = 6 begin --delete relationSurveyQuestion
+ 	set @sql =''delete from relationSurveyQuestion where questionId in(''+@ids+'') and surveyId=''+convert(nvarchar(max),@surveyId)
+ 	exec(@sql)
+end
+else if @action = 7 begin --delete relationQuestionAnswer
+ 	set @sql =''delete from relationQuestionAnswer where surveyId=''
+ 	+convert(nvarchar(max),@surveyId)+'' and questionId=''+convert(nvarchar(max),@questionId) +
+ 	'' and answerId in(''+@ids+'') ''
+ 	exec(@sql)
+end
+else if @action = 8 begin
+ 	select surveyId,description,IVR_id from Survey where active=1
+end
+else if @action = 9 begin
+ 	select questionId,description from SurveyQuestion where active=1
+end
+else if @action = 10 begin
+	select answerId,description,digit from SurveyAnswer where active=1
+end
+else if @action = 11 begin
+	select a.questionId,b.description,A.orden from relationSurveyQuestion A left join SurveyQuestion B on a.questionId = b.questionId where a.surveyId=@surveyId and B.active=1  order by A.orden
+end
+else if @action = 12 begin
+	select A.answerId,B.description,B.digit from relationQuestionAnswer A left join SurveyAnswer B on A.answerId=B.answerId  where a.surveyId=@surveyId  and a.questionId=@questionId
 end'
 		EXEC(@sql)
 

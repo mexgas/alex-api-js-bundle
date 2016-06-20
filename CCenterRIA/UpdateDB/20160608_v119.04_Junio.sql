c189274f727baba1417a117e9a7dd2eed9323531
dmirandanuxiba <dmiranda@nuxiba.com>
2016-06-20 16:40:06 -0500

    DIMC 119.03 bancomer fix

--- CCenterRIA/UpdateDB/20160608_v119.04_Junio.sql
+++ CCenterRIA/UpdateDB/20160608_v119.04_Junio.sql
@@ -484,160 +484,7 @@ if @actualVersion = @version and @actualVersionFix = @versionfix begin
 	begin tran
 	begin try
 
-		
-		    set @process = 'ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp] -----------'
-    set @sql='ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp]  
-@User_id smallint   
-AS   
-set nocount on  
- select a1.cam_id, cam_Descripcion  
-  , cam_tNotas, cast(cam_ocupado as int) as cam_ocupado, cam_noInt_ocupado, cam_inter_ocupado, cast(cam_nocontesto as int) as cam_nocontesto  
-  , cam_noInt_nocontesto, cam_inter_nocontesto, cast(cam_fax as int) as cam_fax, cam_noInt_fax, cam_inter_fax  
-  , cast(cam_modomanual as int) as cam_modomanual, ANI, cam_ShowCalifWnd, cam_StartTimerOnHangUp, editableCallKey, cam_tNoContesta, iTipoDial  
-  , detectAnswerMachine, detectVoiceMail, compliance, cam_inter_graba, cam_noint_graba, cast(progDial as tinyint)progDial  
-  , cast(excCallBack as tinyint)excCallBack, dialOrder, dialPrefix, dialPrefixMan, dialPrefixXfe, listenManualCall  
-  , stopRecording, cast(abandonCallback as tinyint)abandonCallback, a3.frame, a1.t_autoCB, a1.id_anilist, a1.tDialonWrapUp, dbo.fn_viewMode(@User_id, 10) viewMode, cam_maxqueue as queSize,  
-  DNCScrub, callerIdDesc, timeZoneRule, callsBySurvey, ivrScript, surveyPctg, isnull(a1.call_record,1) as call_record  
-     ,cast (startStopRecording as tinyint)startStopRecording, leaveRecMessage, manualCallOnChat  
-  ,callBackSurveyAgent,callBackSurveyClient,case when surveycamid is null or surveycamid = 0 then 0 else 1 end isRelationSurvey, a1.funcEspDtmf  
-  from ccCamps a1 inner join ccRIACampsGraph a2 on (a1.cam_id=a2.cam_id)  
-  inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)   
-  where a1.cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))  
-  order by cam_descripcion  
- return(0)  
- set nocount off'
-		EXEC(@sql)
-		
-				    set @process = 'ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig] -----------'
-    set @sql='ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig]  
-@cam_id smallint,  
-@cam_descripcion varchar(40) = null,  
-@cam_tnotas smallint = null,  
-@cam_ocupado tinyint = null,  
-@cam_NoInt_ocupado tinyint = null,  
-@cam_inter_ocupado smallint = null,  
-@cam_nocontesto tinyint = null,  
-@cam_NoInt_nocontesto tinyint = null,  
-@cam_inter_nocontesto smallint = null,  
-@cam_fax tinyint = null,  
-@cam_NoInt_fax tinyint = null,  
-@cam_inter_fax smallint = null,  
-@cam_ModoManual tinyint= null,  
-@ANI varchar(15) = null,  
-@cam_ShowCalifWnd bit = null,  
-@cam_StartTimerOnHangUp bit = null,  
-@editableCallKey bit = null,   
-@cam_tNoContesta tinyint = null,  
-@cam_intensive_dialing tinyint = null,   
-@detectAnswerMachine smallint = null, -- defualt 0 | nivel de confianza: 1 rapido, pero no tan exacto | 2 normal | 3 menos rapido, mas exacto  
-@detectVoiceMail TinyInt = null, -- permitidos 0,1 (bandera para activar)  
-@compliance TinyInt = null,   
-@cam_inter_graba smallint = null,  
-@cam_NoInt_graba tinyint = null,  
-@progDial smallint = null,  
-@excCallBack Tinyint = null,  
-@dialOrder Tinyint = null,  
-@dialPrefix varchar(10) = null,  
-@dialPrefixMan varchar(10) = null,  
-@dialPrefixXfe varchar(10) = null,  
-@listenManualCall bit = null,  
-@stopRecording bit = null,  
-@abandonCallback bit = null,  
-@autoCB smallint = null,  
-@id_listAni int = null,  
-@tDialonWrapUp smallint = null,  
-@quesize smallint=null,  
-@DNCScrub int=null,  
-@callerIdDesc varchar(15)=null,  
-@timeZoneRule int=null,  
-@callsBySurvey int=null,  
-@ivrScript int=null,  
-@surveyPctg int=null,  
-@call_record tinyint=null,  
-@dRestrictPlay bit = null,  
-@leaveRecMessage bit = null,  
-@manualCallOnChat bit = null,  
-@callBackSurveyClient bit = null,  
-@callBackSurveyAgent bit = null,
-@funcEspDtmf int =null  
-as  
-set nocount on  
-UPDATE ccCamps SET   
- cam_descripcion = isnull(@cam_descripcion,cam_descripcion),  
- cam_tnotas = isnull(@cam_tnotas,cam_tnotas),  
- cam_ocupado = isnull(@cam_ocupado,cam_ocupado),  
- cam_NoInt_ocupado = isnull(@cam_NoInt_ocupado,cam_NoInt_ocupado),  
- cam_inter_ocupado = isnull(@cam_inter_ocupado,cam_inter_ocupado),  
- cam_nocontesto = isnull(@cam_nocontesto,cam_nocontesto),  
- cam_NoInt_nocontesto = isnull(@cam_NoInt_nocontesto,cam_NoInt_nocontesto),  
- cam_inter_nocontesto = isnull(@cam_inter_nocontesto,cam_inter_nocontesto),  
- cam_fax = isnull(@cam_fax,cam_fax),  
- cam_NoInt_fax = isnull(@cam_NoInt_fax,cam_NoInt_fax),  
- cam_inter_fax = isnull(@cam_inter_fax, cam_inter_fax),  
- cam_ModoManual = isnull(@cam_ModoManual, cam_ModoManual),  
- ANI = isnull(@ANI,ANI),  
- cam_StartTimerOnHangUp = isnull(@cam_StartTimerOnHangUp,cam_StartTimerOnHangUp),  
- editableCallKey = isnull(@editableCallKey, editableCallKey),  
- cam_tNoContesta = isnull(@cam_tNoContesta, cam_tNoContesta),  
- iTipoDial = isnull(@cam_intensive_dialing, iTipoDial),   
- detectAnswerMachine = isnull(@detectAnswerMachine, detectAnswerMachine),   
- detectVoiceMail = isnull(@detectVoiceMail, detectVoiceMail),  
- compliance = isnull(@compliance, compliance),  
- cam_inter_graba = isnull(@cam_inter_graba, cam_inter_graba),  
- cam_NoInt_graba = isnull(@cam_NoInt_graba, cam_NoInt_graba),  
- cam_graba = isnull(convert(bit, @cam_NoInt_graba), cam_graba),  
- progDial = isnull(@progDial, progDial),  
- excCallBack = isnull(@excCallBack,excCallBack),  
- dialOrder = isnull(@dialOrder, dialOrder),  
- dialPrefix = isnull(@dialPrefix, dialPrefix),  
- dialPrefixMan = isnull(@dialPrefixMan, dialPrefixMan),  
- dialPrefixXfe = isnull(@dialPrefixXfe, dialPrefixXfe),  
- listenManualCall = isnull(@listenManualCall, listenManualCall),  
- stopRecording = isnull(@stopRecording, stopRecording),  
- abandonCallback = isnull(@abandonCallback, abandonCallback),  
- t_autoCB = isnull(@autoCB,t_autoCB),  
- id_anilist = isnull(@id_listAni,id_anilist),  
- tDialonWrapUp = case when @cam_tnotas<@tDialonWrapUp and @cam_tnotas<>-1 then @cam_tnotas else isnull(@tDialonWrapUp,tDialonWrapUp) end,  
- cam_fDialOnWU = case @tDialonWrapUp when 0 then 0 else 2 end,  
- cam_maxqueue = isnull(@quesize,cam_maxqueue),  
- DNCScrub = isnull(@DNCScrub,DNCScrub),  
- callerIdDesc = isnull(@callerIdDesc,callerIdDesc),  
- timeZoneRule = isnull(@timeZoneRule,timeZoneRule),  
- callsBySurvey = isnull(@callsBySurvey,callsBySurvey),  
- ivrScript = isnull(@ivrScript,ivrScript),  
- surveyPctg = isnull(@surveyPctg,surveyPctg),  
- call_record = isnull(@call_record,call_record),  
- startStopRecording = isnull(@dRestrictPlay, startStopRecording),  
- leaveRecMessage = isnull(@leaveRecMessage, leaveRecMessage),  
- manualCallOnChat = isnull(@manualCallOnChat, manualCallOnChat),  
- callBackSurveyClient = isnull(@callBackSurveyClient, callBackSurveyClient),  
- callBackSurveyAgent = isnull(@callBackSurveyAgent , callBackSurveyAgent ), 
- funcEspDtmf =  isnull(@funcEspDtmf , funcEspDtmf ) 
-Where cam_id = @cam_id   
-  
-if @cam_ShowCalifWnd = 1  
- begin  
- If not exists(select cam_id from ccCalifCamp where cam_id = @cam_id and tipo = 1)  
-  begin  
-  select 0  
-  return(0)  
-  end  
-    
- UPDATE ccCamps SET cam_ShowCalifWnd = isnull(@cam_ShowCalifWnd, cam_ShowCalifWnd)  
- where cam_id = @cam_id  
- select 1  
- return(0)  
-  end  
-  
---else  
-UPDATE ccCamps SET   
-cam_ShowCalifWnd = isnull(@cam_ShowCalifWnd,cam_ShowCalifWnd)  
-where cam_id = @cam_id  
-return(0)  
-set nocount off'
-		EXEC(@sql)
-		
-    	 set @process = 'create table optionIVR -----------'
+		set @process = 'create table optionIVR -----------'
     set @sql='if not exists (select * from sys.tables where name = N''optionIVR'')
     begin
 create table optionIVR
@@ -647,23 +494,176 @@ camID int,
 type int)
 end'
 		EXEC(@sql)
-		
-			 set @process = 'CREATE UNIQUE INDEX AK_optionIVR_dtmf_tag_camID-----------'
+
+    set @process = 'CREATE UNIQUE INDEX AK_optionIVR_dtmf_tag_camID-----------'
     set @sql='if not exists (select * from sys.indexes where name = N''AK_optionIVR_dtmf_tag_camID'' and object_id = OBJECT_ID(N''optionIVR''))
     begin
-    CREATE UNIQUE INDEX AK_optionIVR_dtmf_tag_camID  
+    CREATE UNIQUE INDEX AK_optionIVR_dtmf_tag_camID
 ON optionIVR (dtmf, tag, camID)
     end'
 		EXEC(@sql)
-		
-		
-		
-			 set @process = 'alter table ccCamps -----------'
+
+
+
+    set @process = 'alter table ccCamps -----------'
     set @sql='if not exists (select * from sys.columns where name = N''funcEspDtmf'' and Object_ID = Object_ID(N''ccCamps''))
     begin
     alter table ccCamps
 add funcEspDtmf int default(0)
     end'
+		EXEC(@sql)
+
+
+	set @process = 'ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp] -----------'
+    set @sql='ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp]
+@User_id smallint
+AS
+set nocount on
+ select a1.cam_id, cam_Descripcion
+  , cam_tNotas, cast(cam_ocupado as int) as cam_ocupado, cam_noInt_ocupado, cam_inter_ocupado, cast(cam_nocontesto as int) as cam_nocontesto
+  , cam_noInt_nocontesto, cam_inter_nocontesto, cast(cam_fax as int) as cam_fax, cam_noInt_fax, cam_inter_fax
+  , cast(cam_modomanual as int) as cam_modomanual, ANI, cam_ShowCalifWnd, cam_StartTimerOnHangUp, editableCallKey, cam_tNoContesta, iTipoDial
+  , detectAnswerMachine, detectVoiceMail, compliance, cam_inter_graba, cam_noint_graba, cast(progDial as tinyint)progDial
+  , cast(excCallBack as tinyint)excCallBack, dialOrder, dialPrefix, dialPrefixMan, dialPrefixXfe, listenManualCall
+  , stopRecording, cast(abandonCallback as tinyint)abandonCallback, a3.frame, a1.t_autoCB, a1.id_anilist, a1.tDialonWrapUp, dbo.fn_viewMode(@User_id, 10) viewMode, cam_maxqueue as queSize,
+  DNCScrub, callerIdDesc, timeZoneRule, callsBySurvey, ivrScript, surveyPctg, isnull(a1.call_record,1) as call_record
+     ,cast (startStopRecording as tinyint)startStopRecording, leaveRecMessage, manualCallOnChat
+  ,callBackSurveyAgent,callBackSurveyClient,case when surveycamid is null or surveycamid = 0 then 0 else 1 end isRelationSurvey, a1.funcEspDtmf
+  from ccCamps a1 inner join ccRIACampsGraph a2 on (a1.cam_id=a2.cam_id)
+  inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)
+  where a1.cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+  order by cam_descripcion
+ return(0)
+ set nocount off'
+		EXEC(@sql)
+
+	set @process = 'ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig] -----------'
+    set @sql='ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig]
+@cam_id smallint,
+@cam_descripcion varchar(40) = null,
+@cam_tnotas smallint = null,
+@cam_ocupado tinyint = null,
+@cam_NoInt_ocupado tinyint = null,
+@cam_inter_ocupado smallint = null,
+@cam_nocontesto tinyint = null,
+@cam_NoInt_nocontesto tinyint = null,
+@cam_inter_nocontesto smallint = null,
+@cam_fax tinyint = null,
+@cam_NoInt_fax tinyint = null,
+@cam_inter_fax smallint = null,
+@cam_ModoManual tinyint= null,
+@ANI varchar(15) = null,
+@cam_ShowCalifWnd bit = null,
+@cam_StartTimerOnHangUp bit = null,
+@editableCallKey bit = null,
+@cam_tNoContesta tinyint = null,
+@cam_intensive_dialing tinyint = null,
+@detectAnswerMachine smallint = null, -- defualt 0 | nivel de confianza: 1 rapido, pero no tan exacto | 2 normal | 3 menos rapido, mas exacto
+@detectVoiceMail TinyInt = null, -- permitidos 0,1 (bandera para activar)
+@compliance TinyInt = null,
+@cam_inter_graba smallint = null,
+@cam_NoInt_graba tinyint = null,
+@progDial smallint = null,
+@excCallBack Tinyint = null,
+@dialOrder Tinyint = null,
+@dialPrefix varchar(10) = null,
+@dialPrefixMan varchar(10) = null,
+@dialPrefixXfe varchar(10) = null,
+@listenManualCall bit = null,
+@stopRecording bit = null,
+@abandonCallback bit = null,
+@autoCB smallint = null,
+@id_listAni int = null,
+@tDialonWrapUp smallint = null,
+@quesize smallint=null,
+@DNCScrub int=null,
+@callerIdDesc varchar(15)=null,
+@timeZoneRule int=null,
+@callsBySurvey int=null,
+@ivrScript int=null,
+@surveyPctg int=null,
+@call_record tinyint=null,
+@dRestrictPlay bit = null,
+@leaveRecMessage bit = null,
+@manualCallOnChat bit = null,
+@callBackSurveyClient bit = null,
+@callBackSurveyAgent bit = null,
+@funcEspDtmf int =null
+as
+set nocount on
+UPDATE ccCamps SET
+ cam_descripcion = isnull(@cam_descripcion,cam_descripcion),
+ cam_tnotas = isnull(@cam_tnotas,cam_tnotas),
+ cam_ocupado = isnull(@cam_ocupado,cam_ocupado),
+ cam_NoInt_ocupado = isnull(@cam_NoInt_ocupado,cam_NoInt_ocupado),
+ cam_inter_ocupado = isnull(@cam_inter_ocupado,cam_inter_ocupado),
+ cam_nocontesto = isnull(@cam_nocontesto,cam_nocontesto),
+ cam_NoInt_nocontesto = isnull(@cam_NoInt_nocontesto,cam_NoInt_nocontesto),
+ cam_inter_nocontesto = isnull(@cam_inter_nocontesto,cam_inter_nocontesto),
+ cam_fax = isnull(@cam_fax,cam_fax),
+ cam_NoInt_fax = isnull(@cam_NoInt_fax,cam_NoInt_fax),
+ cam_inter_fax = isnull(@cam_inter_fax, cam_inter_fax),
+ cam_ModoManual = isnull(@cam_ModoManual, cam_ModoManual),
+ ANI = isnull(@ANI,ANI),
+ cam_StartTimerOnHangUp = isnull(@cam_StartTimerOnHangUp,cam_StartTimerOnHangUp),
+ editableCallKey = isnull(@editableCallKey, editableCallKey),
+ cam_tNoContesta = isnull(@cam_tNoContesta, cam_tNoContesta),
+ iTipoDial = isnull(@cam_intensive_dialing, iTipoDial),
+ detectAnswerMachine = isnull(@detectAnswerMachine, detectAnswerMachine),
+ detectVoiceMail = isnull(@detectVoiceMail, detectVoiceMail),
+ compliance = isnull(@compliance, compliance),
+ cam_inter_graba = isnull(@cam_inter_graba, cam_inter_graba),
+ cam_NoInt_graba = isnull(@cam_NoInt_graba, cam_NoInt_graba),
+ cam_graba = isnull(convert(bit, @cam_NoInt_graba), cam_graba),
+ progDial = isnull(@progDial, progDial),
+ excCallBack = isnull(@excCallBack,excCallBack),
+ dialOrder = isnull(@dialOrder, dialOrder),
+ dialPrefix = isnull(@dialPrefix, dialPrefix),
+ dialPrefixMan = isnull(@dialPrefixMan, dialPrefixMan),
+ dialPrefixXfe = isnull(@dialPrefixXfe, dialPrefixXfe),
+ listenManualCall = isnull(@listenManualCall, listenManualCall),
+ stopRecording = isnull(@stopRecording, stopRecording),
+ abandonCallback = isnull(@abandonCallback, abandonCallback),
+ t_autoCB = isnull(@autoCB,t_autoCB),
+ id_anilist = isnull(@id_listAni,id_anilist),
+ tDialonWrapUp = case when @cam_tnotas<@tDialonWrapUp and @cam_tnotas<>-1 then @cam_tnotas else isnull(@tDialonWrapUp,tDialonWrapUp) end,
+ cam_fDialOnWU = case @tDialonWrapUp when 0 then 0 else 2 end,
+ cam_maxqueue = isnull(@quesize,cam_maxqueue),
+ DNCScrub = isnull(@DNCScrub,DNCScrub),
+ callerIdDesc = isnull(@callerIdDesc,callerIdDesc),
+ timeZoneRule = isnull(@timeZoneRule,timeZoneRule),
+ callsBySurvey = isnull(@callsBySurvey,callsBySurvey),
+ ivrScript = isnull(@ivrScript,ivrScript),
+ surveyPctg = isnull(@surveyPctg,surveyPctg),
+ call_record = isnull(@call_record,call_record),
+ startStopRecording = isnull(@dRestrictPlay, startStopRecording),
+ leaveRecMessage = isnull(@leaveRecMessage, leaveRecMessage),
+ manualCallOnChat = isnull(@manualCallOnChat, manualCallOnChat),
+ callBackSurveyClient = isnull(@callBackSurveyClient, callBackSurveyClient),
+ callBackSurveyAgent = isnull(@callBackSurveyAgent , callBackSurveyAgent ),
+ funcEspDtmf =  isnull(@funcEspDtmf , funcEspDtmf )
+Where cam_id = @cam_id
+
+if @cam_ShowCalifWnd = 1
+ begin
+ If not exists(select cam_id from ccCalifCamp where cam_id = @cam_id and tipo = 1)
+  begin
+  select 0
+  return(0)
+  end
+
+ UPDATE ccCamps SET cam_ShowCalifWnd = isnull(@cam_ShowCalifWnd, cam_ShowCalifWnd)
+ where cam_id = @cam_id
+ select 1
+ return(0)
+  end
+
+--else
+UPDATE ccCamps SET
+cam_ShowCalifWnd = isnull(@cam_ShowCalifWnd,cam_ShowCalifWnd)
+where cam_id = @cam_id
+return(0)
+set nocount off'
 		EXEC(@sql)
 		
 	commit tran

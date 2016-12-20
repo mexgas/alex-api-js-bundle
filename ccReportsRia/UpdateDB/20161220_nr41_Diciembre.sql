547cea30f42401f901fbf0a3a73f9dd8fc171cba
Jesus Antonio Gallardo <agallardo@nuxiba.com>
2016-12-20 11:36:04 -0600

    Se quita la parte del report Master PRocess por que solo es para MCA

--- ccReportsRia/UpdateDB/20161220_nr41_Diciembre.sql
+++ ccReportsRia/UpdateDB/20161220_nr41_Diciembre.sql
@@ -380,7 +380,7 @@ on A.user_id=B.user_id and convert(varchar(19), A.login,121) =B.login and conver
 SELECT TOP 0 * INTO #temp_ccGenSession FROM #tempccGenSession
 
 INSERT INTO #temp_ccGenSession (Fila,[user_id], extension, login, logout)
-select ROW_NUMBER() OVER(ORDER BY login) Fila+1000,
+select ROW_NUMBER() OVER(ORDER BY login)+1000 Fila,
 user_id, ext, login, logout
 from(select a.user_id, max(Extension) as ext, a.fecha as ''logout'',
 		(select isnull(max(Fecha),getdate())
@@ -1103,118 +1103,6 @@ if @action=1 begin
 	drop table #sessionTimeMayores;
 end'
 	EXEC(@sql)
-
-
-	set @process = ''
-	set @sql='USE [msdb]
-
-
-/****** Object:  Job [ReportsMasterProcess]    Script Date: 12/14/2016 11:02:27 ******/
-IF  EXISTS (SELECT job_id FROM msdb.dbo.sysjobs_view WHERE name = N''ReportsMasterProcess'')
-EXEC msdb.dbo.sp_delete_job @job_name=N''ReportsMasterProcess'', @delete_unused_schedule=1
-
-
-/****** Object:  Job [ReportsMasterProcess]    Script Date: 12/14/2016 11:02:27 ******/
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]]    Script Date: 12/14/2016 11:02:27 ******/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''[Uncategorized (Local)]'' AND category_class=1)
-BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''[Uncategorized (Local)]''
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
-END
-
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterProcess'',
-		@enabled=1,
-		@notify_level_eventlog=0,
-		@notify_level_email=0,
-		@notify_level_netsend=0,
-		@notify_level_page=0,
-		@delete_level=0,
-		@description=N''ReportsMasterProcess'',
-		@category_name=N''[Uncategorized (Local)]'',
-		@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Generate Reports]    Script Date: 12/14/2016 11:02:27 ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''Generate Reports'',
-		@step_id=1,
-		@cmdexec_success_code=0,
-		@on_success_action=3,
-		@on_success_step_id=0,
-		@on_fail_action=2,
-		@on_fail_step_id=0,
-		@retry_attempts=0,
-		@retry_interval=0,
-		@os_run_priority=0, @subsystem=N''TSQL'',
-		@command=N''EXEC ReportsMasterProcess'',
-		@database_name=N''ccReportsRia'',
-		@flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [ValidaRepAgentGI]    Script Date: 12/14/2016 11:02:27 ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''ValidaRepAgentGI'',
-		@step_id=2,
-		@cmdexec_success_code=0,
-		@on_success_action=1,
-		@on_success_step_id=0,
-		@on_fail_action=2,
-		@on_fail_step_id=0,
-		@retry_attempts=0,
-		@retry_interval=0,
-		@os_run_priority=0, @subsystem=N''TSQL'',
-		@command=N''declare @dateStart  datetime,@dateEnd datetime
-
-select @dateStart = convert(datetime,convert(varchar(11),getdate())),@dateEnd=getdate()
-
-delete from ValidaRepAgentGI where date >= @dateStart AND date < @dateEnd
-
-insert ValidaRepAgentGI
-SELECT  userid,convert(smalldatetime,convert(varchar(14),min([date]),121) + (CASE WHEN min(minutes) < 15 THEN ''''00'''' WHEN min(minutes) < 30 THEN ''''15'''' WHEN min(minutes) < 45 THEN ''''30'''' ELSE ''''45'''' END)) as [date] ,
-max([user]) AS [user] , max([login]) AS [login] , sum([tlog]) AS [tlog]
-
-,sum(tdialogin) as tdialogin,sum(tnotesin) as tnotesin,sum(tringin) as tringin,sum(txferin) as txferin,sum(tdialogout) as tdialogout,sum(tnotesout) as tnotesout,sum(tringout) as tringout,sum(txferout) as txferout,sum(tunknown) as tunknown,sum(tnotav) as tnotav,sum(tav) as tav,sum(tother) as tother,sum(tprob) as tprob,sum(tundefined) as tundefined,sum(tChatting) as tChatting
-
-,(sum(tdialogin)+sum(tnotesin)+sum(tringin)+sum(txferin)+sum(tdialogout)+sum(tnotesout)+sum(tringout)+sum(txferout)+sum(tunknown)+sum(tnotav)+sum(tav)+sum(tother)+sum(tprob)+sum(tnotavg)+sum(tundefined)+sum(tChatting)) as Total
-FROM RepAgentGI WITH(NOLOCK)
-WHERE date >= @dateStart AND date < @dateEnd
-GROUP BY [year], [month], [day], [hour], (CASE WHEN minutes < 15 THEN 1 WHEN minutes <30 THEN 2 WHEN minutes <45 THEN 3 ELSE 4 END), [userId]
-having sum(tlog)=0 or (sum(tdialogin)+sum(tnotesin)+sum(tringin)+sum(txferin)+sum(tdialogout)+sum(tnotesout)+sum(tringout)+sum(txferout)+sum(tunknown)+sum(tnotav)+sum(tav)+sum(tother)+sum(tprob)+sum(tundefined)+sum(tChatting))>900
-order by date
-'',
-		@database_name=N''ccReportsRia'',
-		@flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''RepotsMasterProcess'',
-		@enabled=1,
-		@freq_type=4,
-		@freq_interval=1,
-		@freq_subday_type=4,
-		@freq_subday_interval=22,
-		@freq_relative_interval=0,
-		@freq_recurrence_factor=0,
-		@active_start_date=20130912,
-		@active_end_date=99991231,
-		@active_start_time=0,
-		@active_end_time=235959
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:'
-	EXEC(@sql)
-
-
-	set @process = ''
-	set @sql=''
-	EXEC(@sql)
-
 
 
 	commit tran

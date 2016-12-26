361004a409362e420d0bca6444558c14a4f20fbf
erick_munoz <emunoz@nuxiba.com>
2016-12-26 12:04:32 -0600

    Se unifican cambios realizados sobre CCRecorderRIA

--- RecorderRIA/UpdateDB/20161220_v38_Diciembre.sql
+++ RecorderRIA/UpdateDB/20161220_v38_Diciembre.sql
@@ -95,24 +95,24 @@ END'
 
 				IF @avrs_enviroment = 2
 					BEGIN
-						SET @SQL = 'SELECT * FROM
+						SET @SQL = ''SELECT * FROM
 						(SELECT par_valor,par_id,par_descripcion FROM TREC_PARAMETROS
 						WHERE par_id in (2,15,29,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,57,60,62,63,65,66,67)
 						UNION
 						SELECT CONVERT(VARCHAR(MAX),MAX(grab_id)),66,''''
 						FROM RIA_GRABACION)x
-						ORDER BY x.par_id'
+						ORDER BY x.par_id''
 					END
 				ELSE
 					BEGIN
 
-						SET @SQL = 'SELECT * FROM
+						SET @SQL = ''SELECT * FROM
 						(SELECT par_valor,par_id FROM TREC_PARAMETROS
 						WHERE par_id in (2,15,29,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,57,60,62,63,65,66,67)
 						UNION
 						SELECT CONVERT(VARCHAR(MAX),MAX(grab_id)),66
 						FROM TREC_GRABACION)x
-						ORDER BY x.par_id'
+						ORDER BY x.par_id''
 					END
 					EXEC sp_executesql @SQL
 			END'

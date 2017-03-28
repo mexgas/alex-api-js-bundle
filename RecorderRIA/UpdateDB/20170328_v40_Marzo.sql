3ac6eed1b1f84ecd8358e13e08e2417235e6462e
Daniel Alberto Vega Corona <dvega@nuxiba.com>
2017-03-28 16:26:20 -0600

    Se agrega validacion para que si la repica elimina el indice IX_ccRIAWorkGroupUsersConsulta2  se cree nuevamente

---
+++ RecorderRIA/UpdateDB/20170328_v40_Marzo.sql
@@ -0,0 +1,185 @@
+/*
+Autor: Jesus Gallardo
+Fecha: 2016/12/20
+Descripcion:
+
+Version requerida: 35
+*/
+set nocount on
+declare @Version int
+declare @Version_Actual int
+
+declare @Sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+---------------- VERSION ----------------
+	Set @Version = 40
+	Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
+
+if @Version_Actual = @Version -1 -- Aqui poner numero de nueva version
+	 begin
+	begin tran
+	begin try
+
+---------------- inicio SCRIPT @Sql ----------------
+
+	--Index
+
+	--Tables
+
+	--Functions
+
+  	--SP
+
+  	  	
+	set @process = 'create Index IX_ccRIAWorkGroupUsersConsulta2'
+ 	set @sql ='
+	if exists (select * from sys.procedures where name = N''ReportsMasterProcessAVRS'')
+    begin
+ 	ALTER procedure [dbo].[ReportsMasterProcessAVRS] as
+
+declare @dateStart datetime
+declare @replicationName nvarchar(100)
+declare @numOfReplications int
+declare @repDelay int
+declare @repStrDelay nvarchar(8)
+declare @minReplication int
+
+set nocount on
+
+set @dateStart = getdate()
+set @replicationName = ''
+set @numOfReplications = 0
+set @repDelay = 0
+set @repStrDelay = ''
+set @minReplication = 600
+
+create table #replications ([name] nvarchar(100), flag bit)
+
+insert into #replications
+select [name], 0 as flag from msdb.dbo.sysjobs where [name] like ''%CCRecorderRIA- 0%'' and [name] like ''%CCenterRia%'' order by [name]
+
+select @numOfReplications = count(*)
+from #replications with(nolock)
+
+--set @repDelay = floor(cast(@minReplication as decimal) / cast(@numOfReplications as decimal))
+
+--set @repStrDelay =CONVERT(char(8), DATEADD(second, @repDelay, ''0:00:00''), 108)
+
+while(select count(*) from #replications with(nolock) where flag = 0) > 0
+begin
+	set rowcount 1
+		select @replicationName = [name]
+		from #replications with(nolock)
+		where flag = 0
+	set rowcount 0
+
+	exec msdb.dbo.sp_start_job @job_name = @replicationName
+
+	update #replications with(rowlock) 	set flag = 1	where [name] = @replicationName
+
+	WAITFOR DELAY ''00:00:01''
+
+	while(
+		SELECT count(*) FROM msdb.dbo.sysjobactivity ja
+		LEFT JOIN msdb.dbo.sysjobhistory jh ON ja.job_history_id = jh.instance_id
+		INNER JOIN msdb.dbo.sysjobs j ON ja.job_id = j.job_id
+		INNER JOIN msdb.dbo.sysjobsteps js ON ja.job_id = js.job_id AND ISNULL(ja.last_executed_step_id,0)+1 = js.step_id
+		WHERE ja.session_id = (SELECT TOP 1 session_id FROM msdb.dbo.syssessions   ORDER BY agent_start_date DESC)
+		AND start_execution_date is not null AND stop_execution_date is null and j.name=@replicationName
+	) > 0
+	begin
+		WAITFOR DELAY ''00:00:01''
+	end
+
+	--waitfor delay @repStrDelay
+end
+
+drop table #replications
+-------------------------Para busquedas en finder 
+
+if not exists (select * from sys.indexes where name = N''IX_ccRIAWorkGroupUsersConsulta2'' and object_id = OBJECT_ID(N''ccRIAWorkGroupUsersConsulta))
+begin
+	CREATE NONCLUSTERED INDEX [IX_ccRIAWorkGroupUsersConsulta2] ON [dbo].[ccRIAWorkGroupUsersConsulta]
+		(
+			[IDWG] ASC,
+			[User_id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
+end
+-----------------------
+
+
+declare @lastTenMinuteFirst datetime
+declare @lastTenMinuteSecond datetime
+declare @id int
+declare @publisher_reinit nvarchar(max)
+declare @publisher_db_reinit nvarchar(max)
+declare @publication_reinit nvarchar(max)
+declare @upload_first_reinit nvarchar(max)
+
+set @lastTenMinuteFirst = dateadd(minute,-10,dateadd(minute, datepart(minute, getdate()) / 10 * 10, dateadd(hour, datediff(hour, 0,getdate()), 0)))
+set @lastTenMinuteSecond = dateadd(minute,10,@lastTenMinuteFirst)
+
+create table #reinitmergepullsubscription(
+id int not null identity,
+publisher nvarchar(max) not null,
+publisher_db nvarchar(max)not null,
+publication nvarchar(max) not null,
+upload_first nvarchar(max) not null,
+[status] bit not null
+)
+
+insert into #reinitmergepullsubscription
+select s.name, ma.publisher_db, ma.publication, ''false'', 0
+from distribution.dbo.MSmerge_history mh
+left outer join distribution.dbo.MSrepl_errors me
+on (mh.error_id = me.id)
+left outer join distribution.dbo.MSmerge_agents ma
+on (mh.agent_id = ma.id)
+left outer join master.sys.servers s
+on (ma.publisher_id = s.server_id)
+where mh.comments like ''%You must reinitialize the subscription (without upload)%''
+and me.error_code = -2147199402
+and mh.time >= @lastTenMinuteFirst
+and mh.time < @lastTenMinuteSecond
+and ma.subscriber_db = ''CCRecorderRIA''
+order by mh.time desc
+
+while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
+	begin
+		set rowcount 1
+		select @id = id, @publisher_reinit = publisher, @publisher_db_reinit = publisher_db, @publication_reinit = publication, @upload_first_reinit = upload_first
+		from #reinitmergepullsubscription
+		where [status] = 0
+		set rowcount 0
+
+		EXEC sp_reinitmergepullsubscription @publisher = @publisher_reinit, @publisher_db = @publisher_db_reinit, @publication = @publication_reinit, @upload_first = @upload_first_reinit
+
+		update #reinitmergepullsubscription
+		set [status] = 1
+		where id = @id
+	end
+
+drop table #reinitmergepullsubscription
+end'  	
+EXEC(@sql)
+
+
+
+------------------ fin SCRIPT @Sql ------------------
+
+	-- Updating DB Version
+
+ 	update trec_parametros set par_valor = @Version where par_id = 30
+ 	set @Version_Actual=@Version_Actual+1
+
+	commit tran
+
+	end try
+	begin catch
+		select @errorGenerated = 'DB Script Version: ' + cast(@Version as nvarchar) + ' Error Process: ' + @process + ' Line: ' + cast(error_line() as nvarchar) + ' Number: ' + cast(@@error as nvarchar) + ' Message: ' + error_message()
+		RAISERROR(@errorGenerated, 11, 1)
+	rollback tran
+	end catch
+ end
+

8ffb665804a2a9074624048e17a43b365b0d2775
Sonia Rodríguez Alvarado <srodriguez@nuxiba.com>
2017-03-30 13:50:14 -0600

    CW-397 se agrega validación al store ccsptelefonosTransferencia cuando se realiza el select de telefonos de transferencia

--- CCenterRIA/UpdateDB/20161220_v119.06_Diciembre.sql
+++ CCenterRIA/UpdateDB/20161220_v119.06_Diciembre.sql
@@ -674,30 +674,28 @@ EXEC(@sql)
 
 		set @process = 'Create SP -- ccsptelefonosTransferencia'
 		set @sql='CREATE PROCEDURE [dbo].[ccsptelefonosTransferencia]
-		@userID INT
-		as
-		set nocount on
+@userID INT
+as
+set nocount on
 
-		BEGIN
-		declare @value bit
-		declare @IDArea int
-		set @value = 0
-		set @IDArea =1
-		select @value = case when valor=''1'' then 1 else 0 end from ccSettings where setting_id = 191
+BEGIN
+declare @value bit
+declare @IDArea int
+set @value = 0
+set @IDArea =1
+select @value = case when valor=''1'' then 1 else 0 end from ccSettings where setting_id = 191
 
-		select @IDArea =IDArea from ccUsers where User_id = @userID
+select @IDArea =IDArea from ccUsers where User_id = @userID
 
-		if @value = 1
-			begin	
-			if @value = 1 begin	
-				select numtra_id id, nombre name, tel number, isnull(IDArea,@IDArea) from telefonosTransferencia where idarea= @IDArea or IDArea is null order by nombre	
-			end
-			else
-				begin
-					select numtra_id id, nombre +'' ''+cast(numtra_id as varchar(20) )  name, tel number, isnull(IDArea,@IDArea) from telefonosTransferencia  order by nombre
-				end
-			end
-		END'
+if @value = 1
+	begin	
+		select numtra_id id, nombre name, tel number, isnull(IDArea,@IDArea) from telefonosTransferencia where idarea= @IDArea or IDArea is null order by nombre	
+	end
+	else
+	begin
+		select numtra_id id, cast(IDArea as varchar(20) )+'' - ''+  nombre name, tel number, isnull(IDArea,@IDArea) from telefonosTransferencia  order by nombre
+	end
+END'
 		EXEC(@sql)
 
 

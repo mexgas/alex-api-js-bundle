088056497bb31ec55252c3930183e4e94443299e
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2017-08-21 15:03:29 -0500

    CW-1058 Programar notReady en la maquina de estados

--- CCenterRIA/UpdateDB/20170821_v119.81.sql
+++ CCenterRIA/UpdateDB/20170821_v119.81.sql
@@ -7,6 +7,7 @@ Author: Armando Rodriguez
 Date: 2017/04/26
 Description:
 	CW-1087 Setting de Cola mensajeria
+	CW-1058 Programar notReady en la maquina de estados
 
 Database: CCenterRia
 Required version: 119.74
@@ -51,7 +52,49 @@ if @actualVersion = @version and (@actualVersionFix = 7 or @actualVersionFix = @
 end'
     EXEC(@Sql)
 
-     set @process = ''
+    set @process = 'CW-1058 Programar notReady en la maquina de estados'
+    set @Sql= 'ALTER procedure [dbo].[ccsp_RIAGetSelectedNotReady]
+@user_id int = 0,
+@tipoNR int
+
+AS
+
+-- Para horarios depues de las 12 de la noche
+declare @inicioTurno integer
+declare @fecha smalldatetime
+declare @fStart datetime
+declare @fEnd datetime
+declare @AcumTime int
+
+set @inicioTurno = 2 --Cambio de dia a las 2 de la mañana
+set @fecha = getdate()
+if datepart( hh,  @fecha ) > @inicioTurno - 1
+begin	
+	set @fStart = convert( datetime, convert(varchar(11), @fecha, 121) + cast(@inicioTurno as varchar) +'':00'', 121)
+	set  @fEnd = dateadd( d,1, @fstart )
+end
+else
+begin
+	set @fEnd = convert( datetime, convert(varchar(11), @fecha, 121) + cast(@inicioTurno as varchar) +'':00'', 121)
+	set  @fStart = dateadd( d,-1, @fEnd )
+end
+
+select @AcumTime =isnull( sum(tStatus),0)  from ccRIALogAgentesNotReady where fecha between @fStart and @fEnd and tiponotready_id=@tipoNR and (user_id = @user_id)
+
+select a1.tiponotready_id,descripcion, frame, time_acum, time_xev,  pas_sup, nextstatus, @AcumTime as AcumTime, 
+dbo.NeventsNRdisp(@user_id, nextstatus, getdate())  as NeventsNRdisp
+ from cctiponotready a1
+inner join ccRIAnotreadyGraph a2 on (a1.tiponotready_id=a2.tiponotready_id)
+inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)
+where a1.tiponotready_id=@tipoNR
+'
+    EXEC(@Sql)
+
+    set @process = ''
+    set @Sql= ''
+    EXEC(@Sql)
+
+    set @process = ''
     set @Sql= ''
     EXEC(@Sql)
 

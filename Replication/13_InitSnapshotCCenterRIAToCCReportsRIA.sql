4d591b43824161c9dcf06106e1c4babea307718a
Daniel10 <dvega@nuxiba.com>
2017-08-22 09:56:47 -0500

    Se mueve al script 13 el reordenamiento de la tabla migration que la vuelve independiente del id que se ingrese y evita el error humano

--- Replication/13_InitSnapshotCCenterRIAToCCReportsRIA.sql
+++ Replication/13_InitSnapshotCCenterRIAToCCReportsRIA.sql
@@ -64,7 +64,18 @@ if @Version_Actual >= @Version
 	alter table migration alter column status int	
 			insert into migration values (115 , ''ConversationMail'', 0, '''', '''', '''')
 			insert into migration values (116 , ''Conversationtweet'', 0, '''', '''', '''')
-	END'
+	END
+
+	
+
+	--Para asegurar el orden correcto en que toma los publicadores y evitar el error humano en los id que hacen tronar las replicas de Twitter.
+	truncate table migration;
+
+	insert into migration 
+	select 99+ ROW_NUMBER() OVER(ORDER BY description ASC) AS Id,name as [description],0 status, 'error','1900-01-01 00:00:00.000' dateStart,'1900-01-01 00:00:00.000' dateEnd 
+	from dbo.sysmergepublications where publisher_db='CCenterRia'
+
+	'
 
 		EXEC(@Sql)
 

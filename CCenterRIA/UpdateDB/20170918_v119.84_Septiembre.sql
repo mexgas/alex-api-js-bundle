8fee066de660e0523113f88a8d15abc501d4d758
Hugo Longoria <hlongoria@nuxiba.com>
2017-10-02 19:04:26 -0500

    CW-965_Time_between_redials_of_a_canceled_call + SipHeaders

--- CCenterRIA/UpdateDB/20170918_v119.84_Septiembre.sql
+++ CCenterRIA/UpdateDB/20170918_v119.84_Septiembre.sql
@@ -1590,26 +1590,27 @@ end --Termina Mexico
     	
     	    set @process = 'ALTER PROCEDURE ccsp_RIAConfCamp  -- CW-965_Time_between_redials_of_a_canceled_call'
 	set @Sql= 'ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp]
-			@User_id smallint
-			AS
-			set nocount on
-			 select a1.cam_id, cam_Descripcion
-			  , cam_tNotas, cast(cam_ocupado as int) as cam_ocupado, cam_noInt_ocupado, cam_inter_ocupado, cast(cam_nocontesto as int) as cam_nocontesto
-			  , cam_noInt_nocontesto, cam_inter_nocontesto, cast(cam_fax as int) as cam_fax, cam_noInt_fax, cam_inter_fax
-			  , cast(cam_modomanual as int) as cam_modomanual, ANI, cam_ShowCalifWnd, cam_StartTimerOnHangUp, editableCallKey, cam_tNoContesta, iTipoDial
-			  , detectAnswerMachine, detectVoiceMail, compliance, cam_inter_graba, cam_noint_graba, cast(progDial as tinyint)progDial
-			  , cast(excCallBack as tinyint)excCallBack, dialOrder, dialPrefix, dialPrefixMan, dialPrefixXfe, listenManualCall
-			  , stopRecording, cast(abandonCallback as tinyint)abandonCallback, a3.frame, a1.t_autoCB, a1.id_anilist, a1.tDialonWrapUp, dbo.fn_viewMode(@User_id, 10) viewMode, cam_maxqueue as queSize,
-			  DNCScrub, callerIdDesc, timeZoneRule, callsBySurvey, ivrScript, surveyPctg, isnull(a1.call_record,1) as call_record
-				 ,cast (startStopRecording as tinyint)startStopRecording, leaveRecMessage, manualCallOnChat
-			  ,callBackSurveyAgent,callBackSurveyClient,case when surveycamid is null or surveycamid = 0 then 0 else 1 end isRelationSurvey,isnull(a1.funcEspDtmf,0)
-			  ,cam_inter_cancelled
-			  from ccCamps a1 inner join ccRIACampsGraph a2 on (a1.cam_id=a2.cam_id)
-			  inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)
-			  where a1.cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
-			  order by cam_descripcion
-			 return(0)
-			 set nocount off'
+				@User_id smallint
+				AS
+				set nocount on
+				 select a1.cam_id, cam_Descripcion
+				  , cam_tNotas, cast(cam_ocupado as int) as cam_ocupado, cam_noInt_ocupado, cam_inter_ocupado, cast(cam_nocontesto as int) as cam_nocontesto
+				  , cam_noInt_nocontesto, cam_inter_nocontesto, cast(cam_fax as int) as cam_fax, cam_noInt_fax, cam_inter_fax
+				  , cast(cam_modomanual as int) as cam_modomanual, ANI, cam_ShowCalifWnd, cam_StartTimerOnHangUp, editableCallKey, cam_tNoContesta, iTipoDial
+				  , detectAnswerMachine, detectVoiceMail, compliance, cam_inter_graba, cam_noint_graba, cast(progDial as tinyint)progDial
+				  , cast(excCallBack as tinyint)excCallBack, dialOrder, dialPrefix, dialPrefixMan, dialPrefixXfe, listenManualCall
+				  , stopRecording, cast(abandonCallback as tinyint)abandonCallback, a3.frame, a1.t_autoCB, a1.id_anilist, a1.tDialonWrapUp, dbo.fn_viewMode(@User_id, 10) viewMode, cam_maxqueue as queSize,
+				  DNCScrub, callerIdDesc, timeZoneRule, callsBySurvey, ivrScript, surveyPctg, isnull(a1.call_record,1) as call_record
+					 ,cast (startStopRecording as tinyint)startStopRecording, leaveRecMessage, manualCallOnChat
+				  ,callBackSurveyAgent,callBackSurveyClient,case when surveycamid is null or surveycamid = 0 then 0 else 1 end isRelationSurvey,isnull(a1.funcEspDtmf,0)
+				  ,isnull(sipHdrFormat, '') sipHdrFormat
+				  ,cam_inter_cancelled
+				  from ccCamps a1 inner join ccRIACampsGraph a2 on (a1.cam_id=a2.cam_id)
+				  inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)
+				  where a1.cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+				  order by cam_descripcion
+				 return(0)
+				 set nocount off'
     	EXEC(@Sql)
     	
     	    set @process = 'ALTER PROCEDURE ccsp_RIAUpdateCamConfig  -- CW-965_Time_between_redials_of_a_canceled_call'
@@ -1664,6 +1665,7 @@ end --Termina Mexico
 				@callBackSurveyClient bit = null,
 				@callBackSurveyAgent bit = null,
 				@funcEspDtmf int =null,
+				@sipHdrsCfg varchar(255) = null,
 				@cam_inter_cancelled smallint = null
 				as
 				set nocount on
@@ -1718,7 +1720,8 @@ end --Termina Mexico
 				 manualCallOnChat = isnull(@manualCallOnChat, manualCallOnChat),
 				 callBackSurveyClient = isnull(@callBackSurveyClient, callBackSurveyClient),
 				 callBackSurveyAgent = isnull(@callBackSurveyAgent , callBackSurveyAgent ),
-				 funcEspDtmf =  isnull(@funcEspDtmf , funcEspDtmf )
+				 funcEspDtmf =  isnull(@funcEspDtmf , funcEspDtmf ),
+				 sipHdrFormat = isnull(@sipHdrsCfg, sipHdrFormat)
 				Where cam_id = @cam_id
 
 				if @cam_ShowCalifWnd = 1

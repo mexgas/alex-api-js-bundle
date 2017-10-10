1c87c7fbb4af0cb7b031e06a486ba9e3324e5e96
Mike Trejo <mtrejo@nuxiba.com>
2017-10-10 14:01:35 -0500

    se agrega un update en la tabla ccSettings para cambiar el valor por default a 3( 3 es el valor   de la opcion "dinamico" que hace que seleccione el reproductor dependiendo de que navegador se abra)  *se agrega otro update para cambiar la informacion dentro de la columna detalle

---
+++ CCenterRIA/UpdateDB/20171010_v119.92_Octubre.sql
@@ -0,0 +1,81 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+
+/*
+Author: Miguel Trejo
+Date: 2017/10/10
+Description:
+	*se agrega un update en la tabla ccSettings para cambiar el valor por default a 3( 3 es el valor
+	 de la opcion "dinamico" que hace que seleccione el reproductor dependiendo de que navegador se abra)
+	*se agrega otro update para cambiar la informacion dentro de la columna detalle
+
+Database: CCenterRia
+Required version: 119.09-1
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+set nocount on
+
+declare @version int,@versionFix int
+declare @actualVersion int,@actualVersionFix int
+declare @sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+declare @versionALL varchar(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+
+set @version = 119--**********actualizar a 119 sin fix
+set @versionfix = 92
+--select * from ccsettings where setting_id=77
+--
+/* Actual version (use your own script to do it)*/
+exec @actualVersion = ccsp_getVersion 'BD'
+
+select @versionALL = valor from ccsettings where setting_id=77;
+select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
+
+if @actualVersion = @version and (@actualVersionFix = 84)
+	begin
+		begin tran
+		begin try
+
+		set @process = 'UPDATE TABLE CCSETINGS   -- CW-944, 611'
+		set @Sql= '
+			update ccSettings set valor=3 where setting_id=182;
+			update ccSettings set detalle = ''0/Jplayer 1/Applet 2/Wavesurfer 3/Dinamico'' where setting_id=182;
+		'
+    	EXEC(@Sql)
+    	
+    	set @process = ''
+		set @Sql= ''
+    	EXEC(@Sql)
+		
+    	
+    	set @process = ''
+		set @Sql= ''
+    	EXEC(@Sql)
+
+    	/* End script release */
+
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		exec ccsp_getVersion 'BDF', @versionFix
+
+		commit tran
+		end try
+
+		begin catch
+
+			/* Error generated based on sintax */
+			select @errorGenerated = 'DB script version: ' + cast(@version as nvarchar) + '''.''' + cast(@versionfix as nvarchar) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() as nvarchar) + ''' Number: ''' + cast(@@error as nvarchar) + ''' Message: '''+ error_message()
+			RAISERROR(@errorGenerated, 11, 1)
+
+		rollback tran
+		end catch
+	end

8df45f81a4285706bf80da00c1cb397489d59c0e
Mike Trejo <mtrejo@nuxiba.com>
2017-10-17 13:11:40 -0500

    SP Load Graphics modificado sin etiqueta "1,2,3"

--- CCenterRIA/UpdateDB/20170821_v119_Galatea.sql
+++ CCenterRIA/UpdateDB/20170821_v119_Galatea.sql
@@ -48,33 +48,39 @@ if @actualVersion = @version --and (@actualVersionFix = 7 or @actualVersionFix =
 		begin tran
 		begin try	
 
-    set @process = 'CW-1162 -- Drop SP ccsp_AgentDataACD'
-    set @Sql= 'if exists (select * from sys.procedures where name = N''ccsp_AgentDataACD'')
+    set @process = 'CW-1162 -- Drop SP ccsp_LoadGraphics'
+    set @Sql= 'if exists (select * from sys.procedures where name = N''ccsp_LoadGraphics'')
     begin
-        DROP PROCEDURE ccsp_AgentDataACD;
+        DROP PROCEDURE ccsp_LoadGraphics;
     end
 '
     EXEC(@Sql)
 
-	set @process = 'CW-1162 -- Create SP ccsp_AgentDataACD'
-    set @Sql= 'CREATE PROCEDURE ccsp_AgentDataACD
-@inboundId as smallint
+	set @process = 'CW-1162 -- Create SP ccsp_LoadGraphics'
+    set @Sql= 'CREATE PROCEDURE [dbo].[ccsp_LoadGraphics]
+@inboundId as smallint,
+@callType as smallint
 AS
 BEGIN
+	
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
+	
+	if (@callType=1)
+	begin
+		select a1.Inbound_id, a2.descripcion, a1.graphic_id, a3.type_id, a3.frame, a2.EditableCallKey, 0 as leaveRecMessage ,
+		case when isnull(a4.callsBySurvey,0) > 0 then 1 else 0 end isRelationSurvey ,
+		isnull(a2.callBackSurveyAgent,1) callBackSurveyAgent,isnull(a2.callBackSurveyClient,1) callBackSurveyClient 
+		from ccRIAInboundGraph a1 
+		inner join ccInbound a2 on (a1.inbound_id=a2.inbound_id)
+		 inner join ccRIAGraphics a3 on (a1.graphic_id=a3.graphic_id) 
+		 left join ccCamps a4 on a4.cam_id=a2.cam_id  where a1.inbound_id=@inboundId and type_id in(''1,2,3'') order by type_id
+		
+	 end
 
-    select a1.Inbound_id, a2.descripcion, a1.graphic_id, a3.type_id, a3.frame, a2.EditableCallKey, 0 as leaveRecMessage ,
-	case when isnull(a4.callsBySurvey,0) > 0 then 1 else 0 end isRelationSurvey ,
-	isnull(a2.callBackSurveyAgent,1) callBackSurveyAgent,isnull(a2.callBackSurveyClient,1) callBackSurveyClient 
-	from ccRIAInboundGraph a1 
-	inner join ccInbound a2 on (a1.inbound_id=a2.inbound_id)
-	 inner join ccRIAGraphics a3 on (a1.graphic_id=a3.graphic_id) 
-	 left join ccCamps a4 on a4.cam_id=a2.cam_id  where a1.inbound_id=@inboundId and type_id in(1,2,3) order by type_id
-
-END
-'
+	
+END'
     EXEC(@Sql)
 
 	set @process = 'CW-1148 -- Alter column ccLogAgentesDia.tStatus float '

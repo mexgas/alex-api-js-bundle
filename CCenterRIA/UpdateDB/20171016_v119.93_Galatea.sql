3f93146e57bf49792c7fd31a34055f57fbfb4891
Mike Trejo <mtrejo@nuxiba.com>
2017-10-18 16:15:19 -0500

    variable tipo tabla, se agragadon dos columnas para el sp loadGraphics

--- CCenterRIA/UpdateDB/20171016_v119.93_Galatea.sql
+++ CCenterRIA/UpdateDB/20171016_v119.93_Galatea.sql
@@ -58,27 +58,36 @@ if @actualVersion = @version and (@actualVersionFix >= 92)
 
 	set @process = 'CW-1162 -- Create SP ccsp_LoadGraphics'
     set @Sql= 'CREATE PROCEDURE [dbo].[ccsp_LoadGraphics]
-@inboundId as smallint,
-@callType as smallint
+@Id as smallint,
+@callType as smallint,
+@UserId as smallint
 AS
 BEGIN
 	
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+	SET NOCOUNT ON
+	DECLARE @AuthorizationPlayStopRec TABLE(value bit)
+	DECLARE @realValue bit
+ 
+	INSERT INTO @AuthorizationPlayStopRec 
+	exec ccsp_AgentGetStartStopPermission @age_id=@UserId, @cam_id=@Id, @call_type=@callType
+
+	select @realValue=value from @AuthorizationPlayStopRec
+
 	
 	if (@callType=1)
 	begin
 		select a1.Inbound_id, a2.descripcion, a1.graphic_id, a3.type_id, a3.frame, a2.EditableCallKey, 0 as leaveRecMessage ,
 		case when isnull(a4.callsBySurvey,0) > 0 then 1 else 0 end isRelationSurvey ,
-		isnull(a2.callBackSurveyAgent,1) callBackSurveyAgent,isnull(a2.callBackSurveyClient,1) callBackSurveyClient 
+		isnull(a2.callBackSurveyAgent,1) callBackSurveyAgent,isnull(a2.callBackSurveyClient,1) callBackSurveyClient,
+		a2.ShowCalifWnd as ShowDisposition, a2.StartTimerOnHangUp,@realValue as isStartStopRecording
 		from ccRIAInboundGraph a1 
 		inner join ccInbound a2 on (a1.inbound_id=a2.inbound_id)
 		 inner join ccRIAGraphics a3 on (a1.graphic_id=a3.graphic_id) 
-		 left join ccCamps a4 on a4.cam_id=a2.cam_id  where a1.inbound_id=@inboundId and type_id in(''1,2,3'') order by type_id
+		 left join ccCamps a4 on a4.cam_id=a2.cam_id  where a1.inbound_id=@Id and type_id in(1,2,3) order by type_id
 		
 	 end
-
 	
 END'
     EXEC(@Sql)

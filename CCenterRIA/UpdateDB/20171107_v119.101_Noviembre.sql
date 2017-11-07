c464a75327dc607ac92c326c7a77d7dbe0e8d800
Hugo Longoria <hlongoria@nuxiba.com>
2017-11-07 10:19:35 -0600

    cambio 101

--- CCenterRIA/UpdateDB/20171107_v119.101_Noviembre.sql
+++ CCenterRIA/UpdateDB/20171107_v119.101_Noviembre.sql
@@ -30,7 +30,7 @@ set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
 
 set @version = 119--**********actualizar a 119 sin fix
-set @versionfix = 110
+set @versionfix = 101
 --select * from ccsettings where setting_id=77
 --
 /* Actual version (use your own script to do it)*/

e1b414e832e44f08773efc8b802b24ef3dc638ac
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2017-11-10 14:23:10 -0600

    Se modifica para realizar agrega el setting de cola de mensajeria

---
+++ CCenterRIA/UpdateDB/20171110_v119.102_Noviembre.sql
@@ -0,0 +1,72 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+
+/*
+Author: Jesus Gallardo 
+Date: 2017/11/07
+Description:
+
+
+
+Database: CCenterRia
+Required version: 119.09-4
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+set nocount on
+
+declare @version int,@versionFix int
+declare @actualVersion int,@actualVersionFix int
+declare @sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+declare @versionALL varchar(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+
+set @version = 119--**********actualizar a 119 sin fix
+set @versionfix = 102
+--select * from ccsettings where setting_id=77
+--
+/* Actual version (use your own script to do it)*/
+exec @actualVersion = ccsp_getVersion 'BD'
+
+select @versionALL = valor from ccsettings where setting_id=77;
+select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
+
+if  @actualVersion = @version and @actualVersionFix = 101 
+	begin
+		begin tran
+		begin try
+		
+		 set @process = 'CW-1087 CW-940 Setting de Cola mensajeria'
+    set @Sql= 'if not exists(select * from ccsettings where setting_id = 199) begin
+	insert ccsettings(setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate)
+	values(199,''127.0.0.1|0|/|adminNuxiba|Nuxiba2017|5000'',''Parámetros para cola mensajería'',1,''AGT'',
+		''Configuracion rabbit IP|Port|VirtualHost|User|Password|Tiempo expiracion mensaje)'',''Parameters for messenger queue'',0,''.*'')
+end'
+    EXEC(@Sql)
+
+    	/* End script release */
+
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		exec ccsp_getVersion 'BDF', @versionFix
+
+		commit tran
+		end try
+
+		begin catch
+
+			/* Error generated based on sintax */
+			select @errorGenerated = 'DB script version: ' + cast(@version as nvarchar) + '''.''' + cast(@versionfix as nvarchar) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() as nvarchar) + ''' Number: ''' + cast(@@error as nvarchar) + ''' Message: '''+ error_message()
+			RAISERROR(@errorGenerated, 11, 1)
+
+		rollback tran
+		end catch
+	end

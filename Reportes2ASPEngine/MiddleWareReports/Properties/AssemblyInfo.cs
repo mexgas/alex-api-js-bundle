171362fc6f4a60734bb1bd4834efec21174fb559
Charls <cechavez@nuxiba.com>
2018-01-11 11:24:57 -0600

    Cambio de versión 119.104 -> 119.112

--- Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
+++ Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.1207.119.104")]
-[assembly: AssemblyFileVersion("2017.1207.119.104")]
+[assembly: AssemblyVersion("2018.109.119.112")]
+[assembly: AssemblyFileVersion("2018.109.119.112")]

88d8f19bf680401f36d54aba8d1135d11bb97e3f
Alan Minor <aleyva@nuxiba.com>
2018-02-08 12:14:49 -0600

    Cambios en Base

---
+++ CCenterRIA/UpdateDB/20180130_v119.121_Enero_StunDefault.sql
@@ -0,0 +1,71 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+
+/*
+Author: Alan Minor 
+Date: 2018/08/02
+Description:
+
+
+
+Database: CCenterRia
+Required version: 119.10.2
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+set nocount on
+
+declare @version int,@versionFix int
+declare @actualVersion int,@actualVersionFix int
+declare @sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+declare @versionALL varchar(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+
+set @version = 119--**********actualizar a 119 sin fix
+set @versionfix = 121
+--select * from ccsettings where setting_id=77
+--
+/* Actual version (use your own script to do it)*/
+exec @actualVersion = ccsp_getVersion 'BD'
+exec @actualVersionFix = ccsp_getVersion 'BDF'
+
+select @versionALL = valor from ccsettings where setting_id=77;
+select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
+
+if  @actualVersion = @version and ( @actualVersionFix >= 114)
+	begin
+		begin tran
+		begin try
+
+
+		set @process = 'CW-1330 -- Update en tabla ccSettings. Valor de setting 183'
+    	set @Sql= 'UPDATE CCSettings set valor=''0|ws://192.168.1.246:10080|#UserIP#|sip:#UserIP#@#UserIP#|||"stun.l.google.com:19302"|true|true|3|4|root|false|5||true|true|3'' where setting_id=183'
+    	EXEC(@Sql)
+
+
+	/* End script release */
+
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		exec ccsp_getVersion 'BDF', @versionFix
+
+		commit tran
+		end try
+
+		begin catch
+
+			/* Error generated based on sintax */
+			select @errorGenerated = 'DB script version: ' + cast(@version as nvarchar) + '''.''' + cast(@versionfix as nvarchar) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() as nvarchar) + ''' Number: ''' + cast(@@error as nvarchar) + ''' Message: '''+ error_message()
+			RAISERROR(@errorGenerated, 11, 1)
+
+		rollback tran
+		end catch
+	end

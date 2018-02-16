91bc5d55471cf8c5a3515a351e8749db32f9578b
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2018-02-16 15:49:28 -0600

    Se modifica para historial de llamadas

--- CCenterRIA/UpdateDB/CW-1382.sql
+++ CCenterRIA/UpdateDB/CW-1382.sql
@@ -112,6 +112,49 @@ if not exists(select * from ccAVRSTransfer where cal_id=@cal_id and tipo= @tipo-
 end'
     	EXEC(@Sql)
 
+
+		set @process = 'CW-1382 Version 119.114 -- Alter SP ccspAgent_GetLastCalls'
+    	set @Sql= 'ALTER PROCEDURE [dbo].[ccspAgent_GetLastCalls] @user_id int AS
+set nocount on
+
+select top 10 c.cal_id as id, ''IN'' as Tipo, convert(varchar(10), cal_inicio, 108) as Hora, cal_ani as Telefono, descripcion as EspCamp, 
+isnull(cal.Description, '''') as Calificacion, 
+convert(varchar(14), dateadd(second, cal_tDialog-cal_tMoh,0), 108) Duracion,
+'''' as CallBack, cal_key, c.inbound_id as IDCampEsp
+from ccCallsIn c with(nolock index(IX_ccCallsIn_4)) 
+inner join ccInbound i on c.inbound_id = i.inbound_id
+left join ccTipoCalif cal on c.calif_id = cal.calif_id
+left join 
+(select cal_id,tipo,sum(tAntesXfer) as tAntesXfer,sum(tDespuesXfer) as tDespuesXfer  from ccLogTransfers where tipo=1  group by cal_id,tipo ) as t  
+ on c.cal_id=t.cal_id 
+
+where user_id = @user_id
+and cal_inicio > dateadd(hh, -3, getdate())
+
+
+Union
+
+select top 10 c.cal_id as id, ''OUT'' as Tipo,convert(varchar(10), cal_inicio, 108) as Hora,cal_telefono as Telefono,cam_descripcion as EspCamp, 
+isnull(cal.Description, '''') as Calificacion, 
+ CONVERT(varchar(8), DATEADD(ss, 
+	cal_tDialog -	 case when t.tAntesXfer is null then cal_tMoh else cal_tMoh-t.tAntesXfer end 	+ isnull( t.tDespuesXfer ,0)	
+	, 0), 114)  as Duracion,
+ 	isnull(convert(varchar(16), cal_fcallback, 121) ,'''') as CallBack, cal_key, c.cam_id as IDCampEsp
+from ccoCallsOut c
+inner join ccCamps o on c.cam_id = o.cam_id
+left join ccTipoCalifOut cal on c.calif_id = cal.calif_id
+left join  
+(select cal_id,tipo,sum(tAntesXfer) as tAntesXfer,sum(tDespuesXfer) as tDespuesXfer from ccLogTransfers where tipo=2 group by cal_id,tipo) as t  
+on c.cal_id=t.cal_id 
+
+where user_id = @user_id
+and cal_inicio > dateadd(hh, -3, getdate())
+
+order by hora desc
+
+set nocount off '
+    	EXEC(@Sql)
+
 		
 	/* End script release */
 

5caabb1f4eac2661382548b4524af3a3ae5a578b
Alan Minor <aleyva@nuxiba.com>
2018-02-23 15:20:30 -0600

    Se modifico Para el tiempo en conferencia

--- CCenterRIA/UpdateDB/20180216_v119.122_Febrero.sql
+++ CCenterRIA/UpdateDB/20180216_v119.122_Febrero.sql
@@ -150,8 +150,13 @@ declare @totalCall_Time integer
 declare @callout_id int
 
 if @action = 1 begin
-	if @modo = 4
+	if @modo = 4 begin
 		insert into ccLogTransfers(cal_id,tipo,modo,destino,tAntesXfer,tDespuesXfer,fechaFin)  values ( @cal_id, @tipo, @modo, @destino, @tantes, @tdespues, getdate() )
+		if @tdespues > 0 begin
+				select @totalCall_Time = ISNULL((select totalCall_Time from ccoCallsOut where cal_id = @cal_id), 0) + @tdespues
+				update ccoCallsOut set totalCall_Time = @totalCall_Time where cal_id = @cal_id
+		end
+	end
 	else begin
 		if not exists (select * from ccLogTransfers where cal_id = @cal_id and tipo = @tipo)
 			insert into ccLogTransfers(cal_id,tipo,modo,destino,tAntesXfer,tDespuesXfer,fechaFin) values ( @cal_id, @tipo, @modo, @destino, 0, @tantes, getdate() )

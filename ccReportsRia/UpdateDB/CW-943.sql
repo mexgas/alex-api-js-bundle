2cd9b78e90865bf1f917d2460b5540efdb53e5bf
Mike Trejo <mtrejo@nuxiba.com>
2018-03-20 15:26:06 -0600

    version

--- ccReportsRia/UpdateDB/CW-943.sql
+++ ccReportsRia/UpdateDB/CW-943.sql
@@ -26,7 +26,7 @@ declare @errorGenerated varchar(max)
 declare @process varchar(max)
 
 /* Version to release (use the version of your own databse)*/
-set @version =49
+set @version =50
 /* Actual version (use your own script to do it) */
 exec @actualVersion = ccsp_getVersion 'BD'
 

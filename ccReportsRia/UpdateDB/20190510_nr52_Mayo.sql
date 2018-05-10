492a7cd130b88fe733be91025902f56069dcd54a
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2018-05-10 11:44:21 -0500

    Se modifica el nombre de la version para liberacion

--- ccReportsRia/UpdateDB/20190510_nr52_Mayo.sql
+++ ccReportsRia/UpdateDB/20190510_nr52_Mayo.sql
@@ -26,7 +26,7 @@ declare @errorGenerated varchar(max)
 declare @process varchar(max)
 
 /* Version to release (use the version of your own databse)*/
-set @version =51
+set @version =52
 /* Actual version (use your own script to do it) */
 exec @actualVersion = ccsp_getVersion 'BD'
 

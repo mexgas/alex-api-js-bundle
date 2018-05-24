264e5227a933d7f5e17851de77cdaa270dbf8a7f
Erick Muñoz <emunoz@nuxiba.com>
2018-05-24 10:41:14 -0500

    Se agrega version de script y condiciones

--- CCenterRIA/UpdateDB/20180509_v120.11_Galatea.sql
+++ CCenterRIA/UpdateDB/20180509_v120.11_Galatea.sql
@@ -32,24 +32,23 @@ Importante:la variable @version puede tener 2 valores dependiendo la necesidad q
 set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
 
-set @version = 120--**********actualizar a 129 sin fix
-set @versionfix = 134
+set @version = 120--**********actualizar a 119 sin fix
+set @versionfix = 11
 --select * from ccsettings where setting_id=77
 --
 /* Actual version (use your own script to do it)*/
 exec @actualVersion = ccsp_getVersion 'BD'
+exec @actualVersionFix = ccsp_getVersion 'BDF'
 
 select @versionALL = valor from ccsettings where setting_id=77;
 select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
 
-select @actualVersion,@actualVersionFix,@versionfix
-
-if @actualVersion in(@version ,@version-1)
+if  @actualVersion = @version-1 and  @actualVersionFix = 135
 	begin
 		begin tran
 		begin try	
 
-	set @process = 'CW-718 -- ALTER SP ccspAgent_GetLastCalls'
+	set @process = 'CW-718 -- Create SP ccspAgent_GetLastCalls'
     set @Sql= 'ALTER PROCEDURE [dbo].[ccspAgent_GetLastCalls] @user_id int AS
 set nocount on
 

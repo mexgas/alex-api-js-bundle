ea24a2457286bdca7d0d3386c0a51b7b60887417
Karen Rodríguez López <krodriguez@nuxiba.com>
2018-06-04 15:20:03 -0500

    Se modifico el nombre de RepMKTDiario a RepViewMKTDiario

--- Reportes2ASPEngine/MiddleWareReports/reports/specialreports/RepMKTAgentes.cs
+++ Reportes2ASPEngine/MiddleWareReports/reports/specialreports/RepMKTAgentes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MiddleWareReports
+namespace MiddleWareReports.reports.specialreports
 {
     class RepMKTAgentes : GenericReport
     {

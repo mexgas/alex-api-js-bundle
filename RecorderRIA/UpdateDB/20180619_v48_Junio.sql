8f27806776fb6fc5e35a8bbf8ddb678bcafc35ae
Daniel Vega <dvega@nuxiba.com>
2018-06-22 08:27:16 -0500

    Se agregan validaciones de la existencia de algunas columnas

--- RecorderRIA/UpdateDB/20180619_v48_Junio.sql
+++ RecorderRIA/UpdateDB/20180619_v48_Junio.sql
@@ -38,15 +38,22 @@ if @Version_Actual = @Version -1 -- Aqui poner numero de nueva version
 
 set @process = 'CW - 1702 SP -- Alter ria_grabacion para saber si la grabacion a sido renombrada'
 	set @Sql= '
-alter table ria_grabacion add 
-HasBeenToRename int null
+
+	if not exists (select * from sys.columns where name = N''HasBeenToRename'' and Object_ID = Object_ID(N''ria_grabacion''))
+	    begin
+			alter table ria_grabacion add HasBeenToRename int null
+		end
+
 '
 EXEC(@sql)
 
 
 set @process = 'CW - 1702 Se agrega el campo prefijo a ria_grabacion '
 	set @Sql= '
-alter table ria_grabacion add  Prefijo varchar(max) null
+		if not exists (select * from sys.columns where name = N''Prefijo'' and Object_ID = Object_ID(N''ria_grabacion''))
+	    begin
+			alter table ria_grabacion add  Prefijo varchar(max) null
+		end
 '
 EXEC(@sql)
 
@@ -57,10 +64,23 @@ set @process = 'DISABLE TRIGGER MSmerge_tr_altertable'
 		end'
 	EXEC(@sql)
 
-set @process = 'CW - 1702 Se agrega prefijo a las tablas ccCamps y ccInbound de la base CCRecorderRIA'
+set @process = 'CW - 1702 Se agrega prefijo a la tabla ccInbound de CCRecorderRIA'
 	set @Sql= '
-alter table ccInbound ADD prefijo varchar(40) null
-alter table ccCamps ADD prefijo varchar(40) null
+	if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccInbound''))
+	     begin
+		    alter table ccInbound ADD prefijo varchar(40) null
+	end
+'
+EXEC(@sql)
+
+
+
+set @process = 'CW - 1702 Se agrega prefijo a la tabla ccCamps de CCRecorderRIA'
+	set @Sql= '
+if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccCamps''))
+	begin
+	    alter table ccCamps ADD prefijo varchar(40) null
+	end
 '
 EXEC(@sql)
 
@@ -69,7 +89,7 @@ EXEC(@sql)
 	set @process = 'ENABLE TRIGGER MSmerge_tr_altertable'
 	set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
 			begin
-			ENABLE TRIGGER MSmerge_tr_altertable ON DATABASE
+				ENABLE TRIGGER MSmerge_tr_altertable ON DATABASE
 			end'
 	EXEC(@sql)
 
@@ -137,6 +157,7 @@ begin
   C24-----> Supervisor Name
   C25-----> Score Template
   C26-----> graphic_id
+  C27----->Prefix recording
  */
 
  select @language= valor from ccSettings where setting_id = 27

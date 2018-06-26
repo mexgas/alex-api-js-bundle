c49e85f6996e41ddaac6798915149acaff37771c
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2018-06-26 08:19:26 -0500

    Se modifica para buscar reinicalizar las replicas

--- RecorderRIA/UpdateDB/20180619_v48_Junio.sql
+++ RecorderRIA/UpdateDB/20180619_v48_Junio.sql
@@ -36,6 +36,125 @@ if @Version_Actual = @Version -1 -- Aqui poner numero de nueva version
 	    end'
         EXEC(@Sql)
 
+
+    set @process = 'CW-  VERSION 120.11 JOb '
+    set @Sql= 'ALTER procedure [dbo].[ReportsMasterProcessAVRS] as
+
+declare @replicationName nvarchar(100)
+declare @numOfReplications int
+declare @minReplication int
+
+set nocount on
+
+set @replicationName = ''''
+set @numOfReplications = 0
+set @minReplication = 600
+
+create table #replications ([name] nvarchar(100), flag bit)
+
+insert into #replications
+select [name], 0 as flag from msdb.dbo.sysjobs where [name] like ''%CCRecorderRIA- 0%'' and [name] like ''%CCenterRia%'' order by [name]
+
+select @numOfReplications = count(*)
+from #replications with(nolock)
+
+while(select count(*) from #replications with(nolock) where flag = 0) > 0
+begin
+	set rowcount 1
+		select @replicationName = [name]
+		from #replications with(nolock)
+		where flag = 0
+	set rowcount 0
+
+	exec msdb.dbo.sp_start_job @job_name = @replicationName
+
+	update #replications with(rowlock) 	set flag = 1	where [name] = @replicationName
+
+	WAITFOR DELAY ''00:00:01''
+
+	while(
+		SELECT count(*) FROM msdb.dbo.sysjobactivity ja
+		LEFT JOIN msdb.dbo.sysjobhistory jh ON ja.job_history_id = jh.instance_id
+		INNER JOIN msdb.dbo.sysjobs j ON ja.job_id = j.job_id
+		INNER JOIN msdb.dbo.sysjobsteps js ON ja.job_id = js.job_id AND ISNULL(ja.last_executed_step_id,0)+1 = js.step_id
+		WHERE ja.session_id = (SELECT TOP 1 session_id FROM msdb.dbo.syssessions   ORDER BY agent_start_date DESC)
+		AND start_execution_date is not null AND stop_execution_date is null and j.name=@replicationName
+	) > 0
+	begin
+		WAITFOR DELAY ''00:00:01''
+	end
+end
+
+drop table #replications
+-------------------------Para busquedas en finder
+
+if not exists (select * from sys.indexes where name = N''IX_ccRIAWorkGroupUsersConsulta2'' and object_id = OBJECT_ID(N''ccRIAWorkGroupUsersConsulta''))
+begin
+	CREATE NONCLUSTERED INDEX [IX_ccRIAWorkGroupUsersConsulta2] ON [dbo].[ccRIAWorkGroupUsersConsulta]
+		(
+			[IDWG] ASC,
+			[User_id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
+end
+-----------------------
+
+
+declare @lastTenMinuteFirst datetime
+declare @lastTenMinuteSecond datetime
+declare @id int
+declare @publisher_reinit nvarchar(max)
+declare @publisher_db_reinit nvarchar(max)
+declare @publication_reinit nvarchar(max)
+declare @upload_first_reinit nvarchar(max)
+
+set @lastTenMinuteFirst = dateadd(minute,-10,dateadd(minute, datepart(minute, getdate()) / 10 * 10, dateadd(hour, datediff(hour, 0,getdate()), 0)))
+set @lastTenMinuteSecond = dateadd(minute,10,@lastTenMinuteFirst)
+
+create table #reinitmergepullsubscription(
+id int not null identity,
+publisher nvarchar(max) not null,
+publisher_db nvarchar(max)not null,
+publication nvarchar(max) not null,
+upload_first nvarchar(max) not null,
+[status] bit not null
+)
+
+insert into #reinitmergepullsubscription
+select s.name, ma.publisher_db, ma.publication, ''false'', 0
+from distribution.dbo.MSmerge_history mh
+left outer join distribution.dbo.MSrepl_errors me
+on (mh.error_id = me.id)
+left outer join distribution.dbo.MSmerge_agents ma
+on (mh.agent_id = ma.id)
+left outer join master.sys.servers s
+on (ma.publisher_id = s.server_id)
+where 
+(mh.comments like ''%You must reinitialize the subscription (without upload)%'' or
+mh.comments like  ''%Start the Snapshot Agent to generate the snapshot for this publication%'')
+and me.error_code = -2147199402
+and mh.time >= @lastTenMinuteFirst
+and mh.time < @lastTenMinuteSecond
+and ma.subscriber_db = ''CCRecorderRIA''
+order by mh.time desc
+
+while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
+	begin
+		set rowcount 1
+		select @id = id, @publisher_reinit = publisher, @publisher_db_reinit = publisher_db, @publication_reinit = publication, @upload_first_reinit = upload_first
+		from #reinitmergepullsubscription
+		where [status] = 0
+		set rowcount 0
+
+		EXEC sp_reinitmergepullsubscription @publisher = @publisher_reinit, @publisher_db = @publisher_db_reinit, @publication = @publication_reinit, @upload_first = @upload_first_reinit
+
+		update #reinitmergepullsubscription
+		set [status] = 1
+		where id = @id
+	end
+
+drop table #reinitmergepullsubscription'
+    EXEC(@Sql)
+
 set @process = 'CW - 1702 SP -- Alter ria_grabacion para saber si la grabacion a sido renombrada'
 	set @Sql= '
 
@@ -1280,6 +1399,143 @@ SET NOCOUNT ON
 END
 '
 EXEC(@sql)
+
+set @process = 'CW-  VERSION 120.11 JOb [AVRSReports Merge Replication] '
+    set @Sql= 'USE [msdb]
+
+/****** Object:  Job [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+EXEC msdb.dbo.sp_delete_job @job_name=N''AVRSReports Merge Replication'', @delete_unused_schedule=1
+
+/****** Object:  Job [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''[Uncategorized (Local)]'' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''[Uncategorized (Local)]''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''AVRSReports Merge Replication'', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N''No description available.'', 
+		@category_name=N''[Uncategorized (Local)]'', 
+		@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''AVRSReports Merge Replication'', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N''TSQL'', 
+		@command=N''
+declare @id int,@i int,@count int
+declare @publicationName varchar(max)
+declare @dateStart datetime,@dateNow datetime
+declare @status int
+declare @maxId int,@minId int
+
+--delete from MigrationAVRSReports
+
+select @maxId=isnull(max(id),99),@dateNow =getdate(),@i=0 FROM MigrationAVRSReports
+
+insert into MigrationAVRSReports
+SELECT ROW_NUMBER() OVER(ORDER BY name desc)+@maxId AS id, P.name as [description],0 as status,'''''''' as error,''''1901-01-01'''' as dateStart,''''1901-01-01'''' as dateEnd FROM dbo.sysmergepublications P
+left join MigrationAVRSReports M on P.name=M.[description]
+where  P.publisher_db=''''CCRecorderRIA'''' and M.[description] is null 
+
+select @minId=ISNULL(min(id),99), @maxId=isnull(max(id),99),@count=COUNT(*) FROM MigrationAVRSReports
+
+select * FROM MigrationAVRSReports
+
+if exists(select * FROM MigrationAVRSReports where status in(0,1)) begin
+
+	while @i<@count and DATEDIFF(ss,@dateNow,getdate())<59 begin
+		select @publicationName=[description],  @dateStart  = dateStart, @status = status from MigrationAVRSReports  with (nolock) where id=@minId+@i
+		
+		if @status = 2 begin		 
+		 set @i=@i+1
+		 continue;
+		end
+
+		if (not exists( select * from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+			where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+			and a.runstatus = 2 and b.publication = @publicationName and a.start_time < convert(datetime,convert(varchar(10),@dateStart,121)))	   
+			) begin
+				 update MigrationAVRSReports with (rowlock) set [status] = 1, [dateStart] = getdate() where id=@minId+@i     
+				 
+				 exec sp_startpublication_snapshot @publication = @publicationName
+
+				 WAITFOR DELAY ''''00:00:01''''
+
+				while not exists(
+					select *
+					from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+					where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+					and a.runstatus = 2
+					and b.publication = @publicationName and a.start_time > convert(datetime,convert(varchar(10),@dateStart,121))	
+				)
+				begin
+					WAITFOR DELAY ''''00:00:01''''
+					if DATEDIFF(ss,@dateNow,getdate())>=59 begin
+						break
+					end
+				end
+				update MigrationAVRSReports with (rowlock) set [status] = 2, [dateEnd] = getdate() where id=@minId+@i
+		end
+		else if(@status = 1 and
+				exists( select * from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+						where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+						and a.runstatus = 2 and b.publication = @publicationName and a.start_time > convert(datetime,convert(varchar(10),@dateStart,121))
+						)
+			) begin 	
+			update MigrationAVRSReports with (rowlock) set [status] = 2, [dateEnd] = getdate() where id=@minId+@i  and status=1
+		end		
+		set @i=@i+1
+		
+	end
+
+end
+'', 
+		@database_name=N''CCRecorderRIA'', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''replication'', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=1, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20130625, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:'
+    EXEC(@Sql)
 	
 	
 ------------------ fin SCRIPT @Sql ------------------

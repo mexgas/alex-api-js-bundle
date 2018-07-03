a7f02fa2f88c6e350d53125277c23c5166e9c3a1
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2018-07-02 19:37:03 -0500

    Se modifica para poder agregar las columnas si es que la replica no se a ejecutado

--- RecorderRIA/UpdateDB/20180619_v48_Junio.sql
+++ RecorderRIA/UpdateDB/20180619_v48_Junio.sql
@@ -57,24 +57,32 @@ if @Version_Actual = @Version -1 -- Aqui poner numero de nueva version
 '
 EXEC(@sql)
 
+	set @process = 'DISABLE TRIGGER MSmerge_tr_altertable'
+	set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
+		begin
+		DISABLE TRIGGER MSmerge_tr_altertable ON DATABASE
+		end'
+	EXEC(@sql)
+
 	set @process = 'CW - 1702 Se agrega prefijo a la tabla ccInbound de CCRecorderRIA, tabla ccCamps de CCRecorderRIA'
-	set @sql='if not exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'') begin
-			if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccInbound'')) begin
-			    alter table ccInbound ADD prefijo varchar(40) null
-			end
-			if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccCamps'')) begin
-	    		alter table ccCamps ADD prefijo varchar(40) null
-			end
-		end		
-		'
+	set @sql='if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccCamps'')) begin
+			    alter table ccCamps ADD prefijo varchar(40) null
+			end'
 	EXEC(@sql)	
 
-	--set @process = 'ENABLE TRIGGER MSmerge_tr_altertable'
-	--set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
-	--		begin
-	--			exec ReportsMasterProcessAVRS
-	--		end'
-	--EXEC(@sql)
+	set @process = 'CW - 1702 Se agrega prefijo a la tabla ccInbound de CCRecorderRIA, tabla ccCamps de CCRecorderRIA'
+	set @sql='if not exists (select * from sys.columns where name = N''prefijo'' and Object_ID = Object_ID(N''ccInbound'')) begin
+			    alter table ccInbound ADD prefijo varchar(40) null
+			end'
+	EXEC(@sql)
+
+	set @process = 'ENABLE TRIGGER MSmerge_tr_altertable'
+	set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
+			begin
+			ENABLE TRIGGER MSmerge_tr_altertable ON DATABASE
+			end'
+	EXEC(@sql)
+	
 
 	set @process = 'CW - 1702 UPDATE column HasBeenToRename'
 	set @Sql= 'update RIA_GRABACION set HasBeenToRename = 0'
@@ -1249,6 +1257,134 @@ SET NOCOUNT ON
 END
 '
 EXEC(@sql)
+
+	set @process = 'Alter SP ReportsMasterProcessAVRS'
+	set @Sql= 'ALTER procedure [dbo].[ReportsMasterProcessAVRS] as
+
+declare @replicationName nvarchar(100)
+declare @numOfReplications int
+declare @minReplication int
+
+set nocount on
+
+set @replicationName = ''''
+set @numOfReplications = 0
+set @minReplication = 600
+
+create table #replications ([name] nvarchar(100), flag bit)
+
+insert into #replications
+select [name], 0 as flag from msdb.dbo.sysjobs where [name] like ''%CCRecorderRIA- 0%'' and [name] like ''%CCenterRia%'' order by [name]
+
+select @numOfReplications = count(*)
+from #replications with(nolock)
+
+while(select count(*) from #replications with(nolock) where flag = 0) > 0
+begin
+	set rowcount 1
+		select @replicationName = [name]
+		from #replications with(nolock)
+		where flag = 0
+	set rowcount 0
+	
+	if not exists(
+		SELECT * FROM msdb.dbo.sysjobactivity ja
+		LEFT JOIN msdb.dbo.sysjobhistory jh ON ja.job_history_id = jh.instance_id
+		INNER JOIN msdb.dbo.sysjobs j ON ja.job_id = j.job_id
+		INNER JOIN msdb.dbo.sysjobsteps js ON ja.job_id = js.job_id AND ISNULL(ja.last_executed_step_id,0)+1 = js.step_id
+		WHERE ja.session_id = (SELECT TOP 1 session_id FROM msdb.dbo.syssessions   ORDER BY agent_start_date DESC)
+		AND start_execution_date is not null AND stop_execution_date is null and j.name=@replicationName
+	) 
+
+	exec msdb.dbo.sp_start_job @job_name = @replicationName
+
+	update #replications with(rowlock) 	set flag = 1	where [name] = @replicationName
+
+	WAITFOR DELAY ''00:00:01''
+
+	while exists(
+		SELECT * FROM msdb.dbo.sysjobactivity ja
+		LEFT JOIN msdb.dbo.sysjobhistory jh ON ja.job_history_id = jh.instance_id
+		INNER JOIN msdb.dbo.sysjobs j ON ja.job_id = j.job_id
+		INNER JOIN msdb.dbo.sysjobsteps js ON ja.job_id = js.job_id AND ISNULL(ja.last_executed_step_id,0)+1 = js.step_id
+		WHERE ja.session_id = (SELECT TOP 1 session_id FROM msdb.dbo.syssessions   ORDER BY agent_start_date DESC)
+		AND start_execution_date is not null AND stop_execution_date is null and j.name=@replicationName
+	) 
+	begin
+		WAITFOR DELAY ''00:00:01''
+	end
+end
+
+drop table #replications
+-------------------------Para busquedas en finder
+
+if not exists (select * from sys.indexes where name = N''IX_ccRIAWorkGroupUsersConsulta2'' and object_id = OBJECT_ID(N''ccRIAWorkGroupUsersConsulta''))
+begin
+	CREATE NONCLUSTERED INDEX [IX_ccRIAWorkGroupUsersConsulta2] ON [dbo].[ccRIAWorkGroupUsersConsulta]
+		(
+			[IDWG] ASC,
+			[User_id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
+end
+-----------------------
+
+
+declare @lastTenMinuteFirst datetime
+declare @lastTenMinuteSecond datetime
+declare @id int
+declare @publisher_reinit nvarchar(max)
+declare @publisher_db_reinit nvarchar(max)
+declare @publication_reinit nvarchar(max)
+declare @upload_first_reinit nvarchar(max)
+
+set @lastTenMinuteFirst = dateadd(minute,-120,dateadd(minute, datepart(minute, getdate()) / 10 * 10, dateadd(hour, datediff(hour, 0,getdate()), 0)))
+set @lastTenMinuteSecond = dateadd(minute,120,@lastTenMinuteFirst)
+
+create table #reinitmergepullsubscription(
+id int not null identity,
+publisher nvarchar(max) not null,
+publisher_db nvarchar(max)not null,
+publication nvarchar(max) not null,
+upload_first nvarchar(max) not null,
+[status] bit not null
+)
+
+insert into #reinitmergepullsubscription
+select distinct s.name, ma.publisher_db, ma.publication, ''true'', 0
+from distribution.dbo.MSmerge_history mh
+left outer join distribution.dbo.MSrepl_errors me
+on (mh.error_id = me.id)
+left outer join distribution.dbo.MSmerge_agents ma
+on (mh.agent_id = ma.id)
+left outer join master.sys.servers s
+on (ma.publisher_id = s.server_id)
+where 
+(mh.comments like ''%You must reinitialize the subscription (without upload)%'' or
+mh.comments like  ''%Start the Snapshot Agent to generate the snapshot for this publication%'')
+--and me.error_code = -2147199402
+and mh.time >= @lastTenMinuteFirst
+and mh.time < @lastTenMinuteSecond
+and ma.subscriber_db = ''CCRecorderRIA''
+--order by mh.time desc
+
+while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
+	begin
+		set rowcount 1
+		select @id = id, @publisher_reinit = publisher, @publisher_db_reinit = publisher_db, @publication_reinit = publication, @upload_first_reinit = upload_first
+		from #reinitmergepullsubscription
+		where [status] = 0
+		set rowcount 0
+
+		--EXEC sp_reinitmergepullsubscription @publisher = @publisher_reinit, @publisher_db = @publisher_db_reinit, @publication = @publication_reinit, @upload_first = @upload_first_reinit
+		EXEC sp_reinitmergesubscription @publication = @publication_reinit, @subscriber = @publisher_reinit, @subscriber_db = ''CCRecorderRIA'', @upload_first = @upload_first_reinit
+
+		update #reinitmergepullsubscription
+		set [status] = 1
+		where id = @id
+	end
+
+drop table #reinitmergepullsubscription'
+	EXEC(@sql)
 	
 	
 ------------------ fin SCRIPT @Sql ------------------

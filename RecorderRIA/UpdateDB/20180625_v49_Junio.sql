08a58d3e8c072e1e4e16a622053120dcc52e4583
Jesus Antonio Gallardo Sanchez <agallardo@nuxiba.com>
2018-07-02 19:37:50 -0500

    Se modifica para la ejecucion de replicas y eliminacion de process que bloquean las replicas

--- RecorderRIA/UpdateDB/20180625_v49_Junio.sql
+++ RecorderRIA/UpdateDB/20180625_v49_Junio.sql
@@ -21,51 +21,121 @@ if @Version_Actual = @Version -1 -- Aqui poner numero de nueva version
 	begin tran
 	begin try
 
-	set @process = 'CW-  VERSION 120.11 JOb '
-    set @Sql= 'ALTER procedure [dbo].[ReportsMasterProcessAVRS] as
+	set @process = 'Alter SP ReportsMasterProcessAVRS'
+	set @Sql= 'ALTER procedure [dbo].[ReportsMasterProcessAVRS] as
 
 declare @replicationName nvarchar(100)
-declare @numOfReplications int
-declare @minReplication int
-
+declare @timeSch int
+declare @now datetime
 set nocount on
 
 set @replicationName = ''''
-set @numOfReplications = 0
-set @minReplication = 600
+set @now=getdate()
+
+
+
+
+declare @sessionKIll table(id int, sessionId int)
+declare @i int,@count int
+declare @sessionId int
+DECLARE @SQL nvarchar(1000)
+
+while exists(SELECT	s.session_id AS SessionID		
+	from [master].sys.dm_exec_sessions  as s 
+	LEFT OUTER JOIN [master].sys.sysprocesses p	ON s.session_id = p.spid
+	where s.session_id in(
+	select distinct r.blocking_session_id
+	FROM [master].sys.dm_exec_sessions AS s
+	INNER JOIN [master].sys.dm_exec_requests AS r ON r.session_id = s.session_id
+	WHERE    r.session_id != @@SPID and  r.blocking_session_id   <>0
+	)
+	and s.[program_name] like ''%Replication Merge Agent%''	
+	and DB_NAME(p.dbid)=''CCRecorderRIA''	
+) begin
+	insert into @sessionKIll(id,sessionId)
+	
+	SELECT	ROW_NUMBER() OVER(ORDER BY s.session_id) AS Row#, s.session_id AS SessionID		
+	from [master].sys.dm_exec_sessions  as s 
+	LEFT OUTER JOIN [master].sys.sysprocesses p	ON s.session_id = p.spid
+	where s.session_id in(
+	select distinct r.blocking_session_id
+	FROM [master].sys.dm_exec_sessions AS s
+	INNER JOIN [master].sys.dm_exec_requests AS r ON r.session_id = s.session_id
+	WHERE    r.session_id != @@SPID and  r.blocking_session_id   <>0
+	)
+	and s.[program_name] like ''%Replication Merge Agent%''
+	and DB_NAME(p.dbid)=''CCRecorderRIA''
+
+	select * from @sessionKIll
+
+	select @i=1,@count =COUNT(*) from @sessionKIll
+	while @i<=@count begin
+		select @sessionId=sessionId from @sessionKIll where id=@i
+		SET @SQL = ''KILL '' + CAST(@sessionId as varchar(max))
+		begin try
+			EXEC (@SQL)
+		end try
+		begin catch
+			print @SQL+ '' is proccess end''
+		end catch
+		set @i=@i+1
+	end
+	delete from @sessionKIll
+end
+
+
+
 
 create table #replications ([name] nvarchar(100), flag bit)
 
 insert into #replications
 select [name], 0 as flag from msdb.dbo.sysjobs where [name] like ''%CCRecorderRIA- 0%'' and [name] like ''%CCenterRia%'' order by [name]
 
-select @numOfReplications = count(*)
-from #replications with(nolock)
+select @count=count(*),@timeSch=600 from #replications
 
-while(select count(*) from #replications with(nolock) where flag = 0) > 0
+while(select count(*) from #replications with(nolock) where flag = 0) > 0 and datediff(ss,@now,getdate())<@timeSch
 begin
 	set rowcount 1
 		select @replicationName = [name]
 		from #replications with(nolock)
 		where flag = 0
 	set rowcount 0
-
-	exec msdb.dbo.sp_start_job @job_name = @replicationName
+	
+	if (
+		SELECT top 1 sjh.run_status
+	  FROM msdb.dbo.sysjobhistory                sjh  
+	  inner join msdb.dbo.sysjobs j on j.job_id=sjh.job_id
+	  inner join msdb.dbo.sysjobs_view sj  on  (sj.job_id = sjh.job_id)  
+	  WHERE
+	  j.name = @replicationName
+	  order by sjh.instance_id desc		
+	) <>4 begin
+		exec msdb.dbo.sp_start_job @job_name = @replicationName
+		print ''sp_start_job ''+@replicationName
+	end
+	else begin
+		print ''Job is Init ''+@replicationName
+	end
 
 	update #replications with(rowlock) 	set flag = 1	where [name] = @replicationName
 
-	WAITFOR DELAY ''00:00:01''
+	WAITFOR DELAY ''00:00:01''		
 
-	while(
-		SELECT count(*) FROM msdb.dbo.sysjobactivity ja
-		LEFT JOIN msdb.dbo.sysjobhistory jh ON ja.job_history_id = jh.instance_id
-		INNER JOIN msdb.dbo.sysjobs j ON ja.job_id = j.job_id
-		INNER JOIN msdb.dbo.sysjobsteps js ON ja.job_id = js.job_id AND ISNULL(ja.last_executed_step_id,0)+1 = js.step_id
-		WHERE ja.session_id = (SELECT TOP 1 session_id FROM msdb.dbo.syssessions   ORDER BY agent_start_date DESC)
-		AND start_execution_date is not null AND stop_execution_date is null and j.name=@replicationName
-	) > 0
-	begin
+	while (
+		SELECT top 1 sjh.run_status
+	  FROM msdb.dbo.sysjobhistory                sjh  
+	  inner join msdb.dbo.sysjobs j on j.job_id=sjh.job_id
+	  inner join msdb.dbo.sysjobs_view sj  on  (sj.job_id = sjh.job_id)  
+	  WHERE
+	  j.name = @replicationName
+	  order by sjh.instance_id desc		
+	) = 4
+	begin	
 		WAITFOR DELAY ''00:00:01''
+		if datediff(ss,@now,getdate())>(@timeSch/@count) begin
+			print ''Stop Job in ReplicationName: ''+@replicationName
+			break 
+		end
 	end
 end
 
@@ -91,8 +161,8 @@ declare @publisher_db_reinit nvarchar(max)
 declare @publication_reinit nvarchar(max)
 declare @upload_first_reinit nvarchar(max)
 
-set @lastTenMinuteFirst = dateadd(minute,-10,dateadd(minute, datepart(minute, getdate()) / 10 * 10, dateadd(hour, datediff(hour, 0,getdate()), 0)))
-set @lastTenMinuteSecond = dateadd(minute,10,@lastTenMinuteFirst)
+set @lastTenMinuteFirst = dateadd(minute,-120,dateadd(minute, datepart(minute, getdate()) / 10 * 10, dateadd(hour, datediff(hour, 0,getdate()), 0)))
+set @lastTenMinuteSecond = dateadd(minute,120,@lastTenMinuteFirst)
 
 create table #reinitmergepullsubscription(
 id int not null identity,
@@ -104,7 +174,7 @@ upload_first nvarchar(max) not null,
 )
 
 insert into #reinitmergepullsubscription
-select s.name, ma.publisher_db, ma.publication, ''false'', 0
+select distinct s.name, ma.publisher_db, ma.publication, ''false'', 0
 from distribution.dbo.MSmerge_history mh
 left outer join distribution.dbo.MSrepl_errors me
 on (mh.error_id = me.id)
@@ -115,11 +185,13 @@ on (ma.publisher_id = s.server_id)
 where 
 (mh.comments like ''%You must reinitialize the subscription (without upload)%'' or
 mh.comments like  ''%Start the Snapshot Agent to generate the snapshot for this publication%'')
-and me.error_code = -2147199402
+--and me.error_code = -2147199402
 and mh.time >= @lastTenMinuteFirst
-and mh.time < @lastTenMinuteSecond
+--and mh.time < @lastTenMinuteSecond
 and ma.subscriber_db = ''CCRecorderRIA''
-order by mh.time desc
+--order by mh.time desc
+
+select * from #reinitmergepullsubscription
 
 while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
 	begin
@@ -129,7 +201,8 @@ while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
 		where [status] = 0
 		set rowcount 0
 
-		EXEC sp_reinitmergepullsubscription @publisher = @publisher_reinit, @publisher_db = @publisher_db_reinit, @publication = @publication_reinit, @upload_first = @upload_first_reinit
+		--EXEC sp_reinitmergepullsubscription @publisher = @publisher_reinit, @publisher_db = @publisher_db_reinit, @publication = @publication_reinit, @upload_first = @upload_first_reinit
+		EXEC sp_reinitmergesubscription @publication = @publication_reinit, @subscriber = @publisher_reinit, @subscriber_db = ''CCRecorderRIA'', @upload_first = @upload_first_reinit
 
 		update #reinitmergepullsubscription
 		set [status] = 1
@@ -137,7 +210,8 @@ while (select count(*) from #reinitmergepullsubscription where [status] = 0) > 0
 	end
 
 drop table #reinitmergepullsubscription'
-    EXEC(@Sql)
+	EXEC(@sql)
+	
 
 
  	set @process = 'CW-  VERSION 120.11 Alter Table  MigrationAVRSReports.status'
@@ -850,7 +924,7 @@ QuitWithRollback:
 EndSave:'
     EXEC(@Sql)
 
-    set @process = 'CW-  VERSION 120.11 JOb '
+    set @process = 'CW-  VERSION 120.11 JOb ReportsMasterProcessAVRS'
     set @Sql= 'USE [msdb]
 
 /****** Object:  Job [ReportsMasterProcessAVRS]    Script Date: 23/06/2018 11:26:05 a.m. ******/

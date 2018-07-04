8ab7ba167f99bd746f3743414792a32d59297bbb
Vic Gonzalez <vgonzalez@nuxiba.com>
2018-07-04 12:33:05 -0500

    Se modifica el tipo de setting del 204 de x a AGT

--- CCenterRIA/UpdateDB/20180704_v120.12_Galatea.sql
+++ CCenterRIA/UpdateDB/20180704_v120.12_Galatea.sql
@@ -55,7 +55,7 @@ if  @actualVersion = @version and  @actualVersionFix = @versionfix-1
 	set @process = 'Setting_id 204 Configuration of Galatea integration service'
     set @Sql= 'IF not exists (SELECT * FROM ccSettings WHERE setting_id = 204)
 	INSERT INTO ccSettings (setting_id, valor, descripcion,	Status,	Tipo,detalle,description,bLoadSettings,	validate)
-	VALUES	(204, ''0.0.0.0|1337|1338'', ''Configuración de Galatea integration service'',	1,''x'',
+	VALUES	(204, ''0.0.0.0|1337|1338'', ''Configuración de Galatea integration service'',	1,''AGT'',
 			''IP|WebSocketServerPort|SocketServerPort'',
 			'' Galatea Integration Service configuration'',	1, ''.*'')'
     EXEC(@Sql)

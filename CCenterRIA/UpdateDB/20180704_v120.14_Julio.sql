5760a35ae94e2f17c1f100bad073f587d40081fc
Mike Trejo <mtrejo@nuxiba.com>
2018-07-06 11:26:26 -0500

    se agrega condicion para que acepte el valor 2 que es el idioma portugues y se cambia el rango en el setting 27

--- CCenterRIA/UpdateDB/20180704_v120.14_Julio.sql
+++ CCenterRIA/UpdateDB/20180704_v120.14_Julio.sql
@@ -1182,6 +1182,59 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:'
 		EXEC(@Sql)	
+
+		set @process = 'CW-2017 -- Agente XION en Portugués'
+		set @Sql= 'update ccSettings set detalle = ''Idioma en que apareceran tanto agente como admin RIA.  (0 español - 1 inglés - 2 portugués)'' where setting_id=27
+update ccSettings set validate = ''^[0-2]$'' where setting_id=27'
+		EXEC(@Sql)
+				
+
+		set @process = 'CW-2017 -- Agente XION en Portugués'
+		set @Sql= 'ALTER PROCEDURE [dbo].[ccsp_RIAccSettingsConfig]
+			@command tinyint,
+			@setting_id smallint = null,
+			@value varchar(200) = null
+			AS
+			set nocount on
+
+			declare @idioma tinyint
+			declare @activeChat tinyint
+
+			select @idioma=valor from ccSettings where setting_id=27
+			Select @activeChat=valor from ccSettings where setting_id=145
+
+			if @command=0
+			 begin
+				SELECT case @idioma when 0 then descripcion else [description] end descripcion
+				FROM ccSettings WITH(NOLOCK, index(PK_ccSettings)) WHERE setting_id=@setting_id
+				order by descripcion
+				return(0)
+			 end
+
+			if @command=1
+			 begin
+				Select setting_id, case @idioma when 0 then descripcion else [description] end descripcion, valor, tipo,validate
+				from ccSettings WITH(NOLOCK, index(PK_ccSettings)) where tipo in (''AGT'',''ADM'',''GRL'',''REP'',''SV'')
+				and (setting_id not in (139,140,141)
+				or   setting_id     in (139,140,141) and @activeChat > 0)
+				order by tipo, descripcion
+				return(0)
+			 end
+
+			if @command=2
+			 begin
+				if @setting_id = 27 and @value not in(''0'',''1'',''2'') begin
+					set @value = 0
+				end
+				else if @setting_id = 104 and @value not in(''1'',''2'',''3'',''4'',''5'',''6'',''7'',''8'',''9'',''10'',''11'',''12'',''13'',''14'',''15'',''16'') begin
+					set @value = 1
+				end
+				update ccSettings set valor=@value where setting_id = @setting_id
+				return(0)
+			 end
+
+			set nocount off'
+		EXEC(@Sql)
 				
 		/* End script release */
 

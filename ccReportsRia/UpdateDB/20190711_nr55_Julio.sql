2d13f4003ca2a951a600525bf256c4e5dd65820e
Hugo Longoria <hlongoria@nuxiba.com>
2018-07-11 15:08:56 -0500

    script final

--- ccReportsRia/UpdateDB/20190711_nr55_Julio.sql
+++ ccReportsRia/UpdateDB/20190711_nr55_Julio.sql
@@ -32,111 +32,213 @@ if @actualVersion  in(@version,@version - 1) begin
 	begin tran
 	begin try
 	
-	set @process = 'DISABLE TRIGGER MSmerge_tr_altertable'
-	set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
-		begin
-		DISABLE TRIGGER MSmerge_tr_altertable ON DATABASE
-		end'
-	EXEC(@sql)
-
-	set @process = 'CW-1974 -- Columna cal_tMsg en ccocallsout'
-	set @Sql= 'if not exists (select * from sys.columns where name = N''cal_tMsg'' and Object_ID = Object_ID(N''ccocallsout''))
-		begin
-			alter table ccocallsout ADD cal_tMsg smallint null
-		end'
-	EXEC(@Sql)
-	
-	set @process = 'ENABLE TRIGGER MSmerge_tr_altertable'
-	set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
+		set @process = 'DISABLE TRIGGER MSmerge_tr_altertable'
+		set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
 			begin
-			ENABLE TRIGGER MSmerge_tr_altertable ON DATABASE
+			DISABLE TRIGGER MSmerge_tr_altertable ON DATABASE
 			end'
-	EXEC(@sql)
-	
-	set @process = 'CW-1974 -- SP ccspRepOutAnswAndXferCalls'
-	set @sql='ALTER PROCEDURE [dbo].[ccspRepOutAnswAndXferCalls]
-		@action as tinyint,
-		@from as datetime = null,
-		@to as datetime = null
-		AS
+		EXEC(@sql)
 
-		if @from is null
-			select @from = convert(datetime,convert(varchar(11),getdate()))
-		select @to = getdate()
+		set @process = 'CW-1974 -- Columna cal_tMsg en ccocallsout'
+		set @Sql= 'if not exists (select * from sys.columns where name = N''cal_tMsg'' and Object_ID = Object_ID(N''ccocallsout''))
+			begin
+				alter table ccocallsout ADD cal_tMsg smallint null
+			end'
+		EXEC(@Sql)
+		
+		set @process = 'ENABLE TRIGGER MSmerge_tr_altertable'
+		set @sql='if exists(select * from sys.triggers where name = N''MSmerge_tr_altertable'')
+				begin
+				ENABLE TRIGGER MSmerge_tr_altertable ON DATABASE
+				end'
+		EXEC(@sql)
+		
+		set @process = 'CW-1974 -- SP ccspRepOutAnswAndXferCalls'
+		set @sql='ALTER PROCEDURE [dbo].[ccspRepOutAnswAndXferCalls]
+			@action as tinyint,
+			@from as datetime = null,
+			@to as datetime = null
+			AS
 
-		declare @IVA INT
-		declare @country as tinyint
+			if @from is null
+				select @from = convert(datetime,convert(varchar(11),getdate()))
+			select @to = getdate()
+
+			declare @IVA INT
+			declare @country as tinyint
 
 
-		select @IVA = convert(int,isnull(valor,0)) from ccsettings where setting_id = 25
-		select @country = convert(tinyint,isnull(valor,1)) from ccsettings where setting_id = 104
+			select @IVA = convert(int,isnull(valor,0)) from ccsettings where setting_id = 25
+			select @country = convert(tinyint,isnull(valor,1)) from ccsettings where setting_id = 104
 
 
-		if @country is null set @country = 1
+			if @country is null set @country = 1
 
 
-		if @action = 1
-		begin
-			--Borrar lo que esta para no repetir
-			delete from RepOutAnswAndXferCalls with(rowlock) where date >= @from AND date < @to
+			if @action = 1
+			begin
+				--Borrar lo que esta para no repetir
+				delete from RepOutAnswAndXferCalls with(rowlock) where date >= @from AND date < @to
 
-			insert into RepOutAnswAndXferCalls
-			select Call.cal_inicio as [date],
-			Call.cal_id as [callid],
-			camps.cam_id as [campaignId],
-			ISNULL(camps.cam_descripcion, ''systemTranslated_NoCampaign'') as [campaign],
-			Usr.user_id as [userId],
-			ISNULL(Usr.ApellidoPaterno + '' '' + ISNULL(Usr.ApellidoMaterno, '''') + '' '' + Usr.Nombres, '''') as [Agent],
-			ISNULL(Call.totalCall_Time, 0)+isnull(cal_tMsg,0) as [dialog],
-			Call.cal_telefono as [telephone],
-			Call.cal_manual as [dialId],
-			(select [description] from dialType where dialId = Call.cal_manual) as [dialType],
-			ISNULL(tl.descrip, ''systemTranslated_Indefinite'') as [CallTypes],
-			dbo.fnGetCstoTarifa(Call.tipoLlamada_id, Call.provedor_id, isnull(Call.totalCall_Time,0)+isnull(cal_tMsg,0)) as [ncost],
-			@IVA as iva,
-			convert(decimal(10,2),ISNULL(dbo.fnGetCstoTarifa(Call.tipoLlamada_id, Call.provedor_id, isnull(Call.totalCall_Time,0)+isnull(cal_tMsg,0)),0.00) * (1 + (@IVA / 100.00))) as total
-			from ccoCallsOut Call
-			LEFT JOIN ccCamps camps ON camps.[cam_id] = Call.[cam_id]
-			INNER JOIN ccUsers Usr ON Usr.[user_id] = Call.[user_id] -- User_id IS NOT NULL
-			LEFT JOIN cstoTipoLlamada tl ON (tl.[tipoLlamada_id] = Call.[tipoLlamada_id] and tl.Country_id = @country)
-			INNER JOIN ccoLogDials ccld on ccld.cal_id = Call.cal_id and ccld.answerbit = 1
-			where Call.cal_inicio >= @from
-			and Call.cal_inicio < @to
-			order by date
+				insert into RepOutAnswAndXferCalls
+				select Call.cal_inicio as [date],
+				Call.cal_id as [callid],
+				camps.cam_id as [campaignId],
+				ISNULL(camps.cam_descripcion, ''systemTranslated_NoCampaign'') as [campaign],
+				Usr.user_id as [userId],
+				ISNULL(Usr.ApellidoPaterno + '' '' + ISNULL(Usr.ApellidoMaterno, '''') + '' '' + Usr.Nombres, '''') as [Agent],
+				ISNULL(Call.totalCall_Time, 0)+isnull(cal_tMsg,0) as [dialog],
+				Call.cal_telefono as [telephone],
+				Call.cal_manual as [dialId],
+				(select [description] from dialType where dialId = Call.cal_manual) as [dialType],
+				ISNULL(tl.descrip, ''systemTranslated_Indefinite'') as [CallTypes],
+				dbo.fnGetCstoTarifa(Call.tipoLlamada_id, Call.provedor_id, isnull(Call.totalCall_Time,0)+isnull(cal_tMsg,0)) as [ncost],
+				@IVA as iva,
+				convert(decimal(10,2),ISNULL(dbo.fnGetCstoTarifa(Call.tipoLlamada_id, Call.provedor_id, isnull(Call.totalCall_Time,0)+isnull(cal_tMsg,0)),0.00) * (1 + (@IVA / 100.00))) as total
+				from ccoCallsOut Call
+				LEFT JOIN ccCamps camps ON camps.[cam_id] = Call.[cam_id]
+				INNER JOIN ccUsers Usr ON Usr.[user_id] = Call.[user_id] -- User_id IS NOT NULL
+				LEFT JOIN cstoTipoLlamada tl ON (tl.[tipoLlamada_id] = Call.[tipoLlamada_id] and tl.Country_id = @country)
+				INNER JOIN ccoLogDials ccld on ccld.cal_id = Call.cal_id and ccld.answerbit = 1
+				where Call.cal_inicio >= @from
+				and Call.cal_inicio < @to
+				order by date
 
-			insert into RepOutAnswAndXferCalls
-			select dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) as [date],
-			clt.cal_id as [callid],
-			'''' as [campaignId],
-			'''' as [campaign],
-			(case tipo when 1 then ci.User_id else co.User_id end) as [userId],
-			isnull((select nombres + '' '' + apellidopaterno + '' '' + apellidomaterno from ccusers nolock where user_id = 
-			(case tipo when 1 then ci.User_id else co.User_id end)),''systemTranslated_NoName'') as [Agent],
-			ISNULL(clt.tAntesXfer,0) + ISNULL(clt.tDespuesXfer,0) as [dialog],
-			case when modo = 0 then isnull((select top 1 tel from telefonosTransferencia where tel = clt.destino),clt.destino)  
-			when modo = 3 then isnull((select tel from telefonosConferencia where tel = clt.destino),clt.destino) 
-			when modo = 4 then isnull((select top 1 tel from telefonosTransferencia where tel = clt.destino),clt.destino) 
-			when modo = 5 then isnull((select Computer from ccposicion where pos_id = abs(clt.destino)),clt.destino) end as [telephone],
-			3 as [dialId],
-			(select [description] from dialType where dialId = 3) as [dialType],
-			ISNULL(tl.descrip, ''systemTranslated_Indefinite'') as [CallTypes],
-			ISNULL(dbo.fnGetCstoTarifa(dbo.fnGetTipoLlamada(clt.destino), channel.proveedorId, clt.tAntesXfer+clt.tDespuesXfer), 0) as [ncost],
-			@IVA as iva,
-			convert(decimal(10,2),ISNULL(dbo.fnGetCstoTarifa(dbo.fnGetTipoLlamada(clt.destino), channel.proveedorId, clt.tAntesXfer+clt.tDespuesXfer),0.00) * (1 + (@IVA / 100.00))) as [total]
-			from cclogtransfers clt
-			LEFT JOIN cccallsin ci (nolock) on ci.cal_id=clt.cal_id and tipo=1
-			LEFT JOIN ccocallsout co (nolock) on co.cal_id=clt.cal_id and tipo=2 
-			LEFT JOIN ccChannelTransfer channel on clt.pbxId=channel.pbxId and clt.channel between channel.startChannel and channel.endChannel
-			LEFT JOIN cstoTarifa tarifa on tarifa.provedor_id=channel.proveedorId and tarifa.tipoLlamada_id=dbo.fnGetTipoLlamada(clt.destino)
-			LEFT JOIN cstoTipoLlamada tl ON (tl.[tipoLlamada_id] = dbo.fnGetTipoLlamada(clt.destino) and tl.Country_id = @country)
-			where clt.modo not in (1,2) 
-			and (clt.tAntesXfer > 0 or clt.tDespuesXfer > 0)
-			and dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) >= @from
-			and dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) < @to
-			order by date 
+				insert into RepOutAnswAndXferCalls
+				select dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) as [date],
+				clt.cal_id as [callid],
+				'''' as [campaignId],
+				'''' as [campaign],
+				(case tipo when 1 then ci.User_id else co.User_id end) as [userId],
+				isnull((select nombres + '' '' + apellidopaterno + '' '' + apellidomaterno from ccusers nolock where user_id = 
+				(case tipo when 1 then ci.User_id else co.User_id end)),''systemTranslated_NoName'') as [Agent],
+				ISNULL(clt.tAntesXfer,0) + ISNULL(clt.tDespuesXfer,0) as [dialog],
+				case when modo = 0 then isnull((select top 1 tel from telefonosTransferencia where tel = clt.destino),clt.destino)  
+				when modo = 3 then isnull((select tel from telefonosConferencia where tel = clt.destino),clt.destino) 
+				when modo = 4 then isnull((select top 1 tel from telefonosTransferencia where tel = clt.destino),clt.destino) 
+				when modo = 5 then isnull((select Computer from ccposicion where pos_id = abs(clt.destino)),clt.destino) end as [telephone],
+				3 as [dialId],
+				(select [description] from dialType where dialId = 3) as [dialType],
+				ISNULL(tl.descrip, ''systemTranslated_Indefinite'') as [CallTypes],
+				ISNULL(dbo.fnGetCstoTarifa(dbo.fnGetTipoLlamada(clt.destino), channel.proveedorId, clt.tAntesXfer+clt.tDespuesXfer), 0) as [ncost],
+				@IVA as iva,
+				convert(decimal(10,2),ISNULL(dbo.fnGetCstoTarifa(dbo.fnGetTipoLlamada(clt.destino), channel.proveedorId, clt.tAntesXfer+clt.tDespuesXfer),0.00) * (1 + (@IVA / 100.00))) as [total]
+				from cclogtransfers clt
+				LEFT JOIN cccallsin ci (nolock) on ci.cal_id=clt.cal_id and tipo=1
+				LEFT JOIN ccocallsout co (nolock) on co.cal_id=clt.cal_id and tipo=2 
+				LEFT JOIN ccChannelTransfer channel on clt.pbxId=channel.pbxId and clt.channel between channel.startChannel and channel.endChannel
+				LEFT JOIN cstoTarifa tarifa on tarifa.provedor_id=channel.proveedorId and tarifa.tipoLlamada_id=dbo.fnGetTipoLlamada(clt.destino)
+				LEFT JOIN cstoTipoLlamada tl ON (tl.[tipoLlamada_id] = dbo.fnGetTipoLlamada(clt.destino) and tl.Country_id = @country)
+				where clt.modo not in (1,2) 
+				and (clt.tAntesXfer > 0 or clt.tDespuesXfer > 0)
+				and dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) >= @from
+				and dateadd(ss,-(clt.tAntesXfer + clt.tDespuesXfer),clt.fechaFin) < @to
+				order by date 
 
-		end'
-	EXEC(@sql)
+			end'
+		EXEC(@sql)
+		
+		set @process = 'CW-1290 -- Columna userName en RepInCallsDetail'
+		set @Sql= 'EXEC sp_RENAME ''RepInCallsDetail.username'', ''userName'', ''COLUMN'''
+		EXEC(@Sql)
+		
+		set @process = 'CW-1290 -- SP ccspRepInCallsDetail'
+		set @Sql= 'ALTER PROCEDURE [dbo].[ccspRepInCallsDetail]
+			@action as tinyint,
+			@from as datetime = null,
+			@to as datetime = null
+			AS
+
+			DECLARE @callId as int
+
+			if @from is null
+				select @from = convert(datetime,convert(varchar(11),getdate()))
+			select @to = getdate()
+
+			if @action = 1
+			begin
+				
+				declare @tab table(callId int primary key, [Dato1] varchar(255),[Dato2] varchar(255),[Dato3] varchar(255),[Dato4] varchar(255),[Dato5] varchar(255))
+
+				insert into @tab
+				select callId,[Dato 1],[Dato 2],[Dato 3],[Dato 4],[Dato 5]
+				from
+				(select A.CallId,[Data],[Description] from DataCallIn A
+				inner join ccCallsIn B on A.CallId=B.cal_id
+				where b.cal_Inicio >= @from AND b.cal_Inicio < @to
+				) as SourceTable
+				pivot
+				(
+				max([Data])
+				for [Description] in ([Dato 1],[Dato 2],[Dato 3],[Dato 4],[Dato 5])
+				)as pvt
+
+
+				--Borrar lo que esta para no repetir
+				delete from RepInCallsDetail with(rowlock) where date >= @from AND date < @to
+
+				insert into RepInCallsDetail
+				select cal_inicio, Inbound_id, '''' as Inbound, statusCall_id, '''' as statusCall, calif_id, '''' as calif, isnull(califSub_id,0), '''' as califSub,
+				dni_id, '''' as dni, user_id, '''' as agentName,
+				isnull(cal_key,''''), cal_ANI, cal_tWait, cal_tXfer, cal_tRing, cal_tDialog, cal_extension, '''',
+				case when a.cal_whoHung = 0 then ''systemTranslated_Client''
+				when a.cal_whoHung = 1 then ''systemTranslated_Agent''
+				else ''systemTranslated_AgentSurvey'' end [whoHangUp]
+				, cal_tMoh, datepart(yyyy,cal_inicio), datepart(mm,cal_inicio), datepart(dd,cal_inicio)
+				, datepart(hh,cal_inicio), datepart(mi,cal_inicio)
+				,di.provedor_id,prov.descrip [Proveedor],a.cal_puerto
+				,case when a.file_moved = 1 then ''systemTranslated_Remoto'' else ''Local'' end as file_Moved
+				,cal_tNotas,AverageHandleTime= cal_tNotas+cal_tDialog
+				,ISNULL(tab.Dato1,'''') as Dato1
+				,ISNULL(tab.Dato2,'''') as Dato2
+				,ISNULL(tab.Dato3,'''') as Dato3
+				,ISNULL(tab.Dato4,'''') as Dato4
+				,ISNULL(tab.Dato5,'''') as Dato5
+				from cccallsin a
+				left join ccoDialers di on di.dialer_id = a.cal_puerto
+				left join cstoProvedor prov on di.provedor_id = prov.provedor_id
+				left join @tab tab on tab.callId=a.cal_id
+				where cal_inicio >= @from AND cal_inicio < @to
+				
+				update a set acdGroup = isnull(descripcion,'''')
+				from RepInCallsDetail a
+				left join ccInbound b
+				on a.inboundId = b.Inbound_id
+				where [date] >= @from AND [date] < @to
+
+				update a set callStatus = isnull(descripcion,'''')
+				from RepInCallsDetail a
+				left join ccstatusllamada b
+				on a.callStatusId = b.statusCall_id
+				where [date] >= @from AND [date] < @to
+
+				update a set disposition = isnull(description,'''')
+				from RepInCallsDetail a
+				left join cctipocalif b
+				on a.dispositionId = b.calif_id
+				where [date] >= @from AND [date] < @to
+
+				update a set subDisposition = isnull(califSubDesc,'''')
+				from RepInCallsDetail a
+				left join cctipocalifsub b
+				on a.subDispositionId = b.califSub_id
+				where [date] >= @from AND [date] < @to
+
+				update a set dnis = isnull(dni_numero,'''')
+				from RepInCallsDetail a
+				left join ccdnis b
+				on a.dnisId = b.dni_id
+				where [date] >= @from AND [date] < @to
+
+				update a set userName = isnull(login,''''),
+				agentName = isnull(Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMaterno,'''')
+				from RepInCallsDetail a
+				left join ccusers b
+				on a.userId = b.user_id
+				where [date] >= @from AND [date] < @to
+
+			end'
+		EXEC(@Sql)
 
 
 		if @actualVersion  = @version - 1

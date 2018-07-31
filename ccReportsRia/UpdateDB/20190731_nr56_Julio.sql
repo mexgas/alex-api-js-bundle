a2bc5119ed98aa632992d7ff6dacca4acb29d05a
Jesus Gallardo <agallardo@nuxiba.com>
2018-07-31 08:34:32 -0500

    Se modifica el nombre del archivo

--- ccReportsRia/UpdateDB/20190731_nr56_Julio.sql
+++ ccReportsRia/UpdateDB/20190731_nr56_Julio.sql
@@ -24,7 +24,7 @@ declare @errorGenerated varchar(max)
 declare @process varchar(max)
 
 /* Version to release (use the version of your own databse)*/
-set @version =55
+set @version =56
 /* Actual version (use your own script to do it) */
 exec @actualVersion = ccsp_getVersion 'BD'
 

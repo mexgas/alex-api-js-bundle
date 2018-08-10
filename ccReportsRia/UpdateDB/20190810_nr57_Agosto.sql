998f89a78e7d89afb16a2278657027049f726a63
Armando Rodriguez Ortega <arodriguez@nuxiba.com>
2018-08-10 16:12:13 -0500

    se agregan los cambios al archivo del release

--- ccReportsRia/UpdateDB/20190810_nr57_Agosto.sql
+++ ccReportsRia/UpdateDB/20190810_nr57_Agosto.sql
@@ -10,6 +10,7 @@ Description:
 CW-2156 Valores negativos en reporte especiale de MKT
 CW-1860 Alter SP ReportMasterProcess
 CW-1975 Tiempo completo de llamadas en reporte de detalle
+CW-2020 Geller agregar columna a rep. detalle de llamadas ARO
 
 Database: ccReportsRia
 Required version: 56
@@ -41,6 +42,14 @@ if @actualVersion  in(@version,@version - 1) begin
 				begin
 					ALTER TABLE RepOutCallsDetail
       						ADD MessageTime smallint null
+				end'
+		EXEC(@sql)
+		
+		set @process = 'CW-2020 Geller agregar columna a rep. detalle de llamadas ARO'
+		set @sql='if not exists (select * from sys.columns where name = N''disconnectCause'' and Object_ID = Object_ID(N''RepOutDialDetail''))
+				begin
+					alter table RepOutDialDetail
+						add disconnectCause varchar(250) null
 				end'
 		EXEC(@sql)
 				
@@ -129,6 +138,43 @@ if @action = 1
 
     end
 '
+		EXEC(@sql)
+		
+		set @process = 'CW-2020 Geller agregar columna a rep. detalle de llamadas ARO'
+				set @sql='ALTER PROCEDURE [dbo].[ccspRepOutDialDetail]  
+				@action as tinyint,  
+				@from as datetime = null,  
+				@to as datetime = null  
+				AS  
+				if @from is null  
+				select @from = convert(datetime,convert(varchar(11),getdate()))  
+				select @to = getdate()  
+				if @action = 1  begin  
+					--Borrar lo que esta para no repetir  
+					delete from RepOutDialDetail with(rowlock)  
+					where date >= @from AND date < @to  
+
+					--Inserta información de reporte  
+					insert into RepOutDialDetail  
+					SELECT fecha,isnull(isnull(dials.cal_key,cs.cal_key),'''') cal_key, telefono, dials.tiporesdial_id, isnull(descripcion,'''') as resultado,  
+					dials.[cam_id],ISNULL(rtrim(ltrim(camps.cam_descripcion)), ''systemTranslated_NoCampaign'') as campa, dials.tbusy as Msgtime,  
+					datepart(yyyy,fecha), datepart(mm,fecha), datepart(dd,fecha), datepart(hh,fecha), datepart(mi,fecha), isnull(rl.name,'''')  
+					,case when answerbit = 1 then ''systemTranslated_Charged'' else ''systemTranslated_NotCharged'' end as billed, 
+					isnull(cs.Dato1,'''') as data1, isnull(cs.Dato2,'''') as data2, isnull(cs.Dato3,'''') as data3, isnull(cs.Dato4,'''') as data4, isnull(cs.Dato5,'''') as data5
+					,case when dials.[file_moved] = 1 then ''systemTranslated_Remoto'' else ''Local'' end as file_Moved, dials.disconnectCause
+					FROM 
+					(select dial.logDial_id,dial.callout_id,dial.cam_id,dial.tipoResDial_id,dial.Telefono,dial.Puerto,dial.fecha,dial.tDialing,  
+						dial.tBusy,dial.answerbit,dial.canceledNoAgents,dial.cal_id,dial.disconnectCause, co.cal_key, co.file_moved 
+						FROM ccoLogDials dial (nolock)
+						left join ccocallsout co (nolock) on dial.cal_id=co.cal_id
+						WHERE fecha >= @from AND fecha < @to) dials  
+					LEFT JOIN ccoCallsOutSource cs (nolock) ON dials.callout_id = cs.callout_id  
+					LEFT JOIN cctipoResultadoDial tr ON dials.tiporesdial_id=tr.tiporesdial_id  
+					LEFT JOIN ccCamps camps ON camps.[cam_id] = dials.[cam_id]  
+					LEFT JOIN ccRIARegistryLists rl ON cs.list_id = rl.list_id  
+					WHERE fecha >= @from AND fecha < @to  
+					order by fecha  
+				 end'
 		EXEC(@sql)
 	
 	

958bd839e18a479551aeefb80229538d3df01ff8
Charls <cechavez@nuxiba.com>
2018-08-21 09:53:40 -0500

    CW-1682 Permiso Exportacion Finder

---
+++ CCenterRIA/UpdateDB/20180821_v120.24_Agosto.sql
@@ -0,0 +1,73 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+
+/*
+Author: 
+		
+Date: 2018/08/21
+Description:
+
+Database: CCenterRia
+Required version: 120.14
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+set nocount on
+
+declare @version int,@versionFix int
+declare @actualVersion int,@actualVersionFix int
+declare @sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+declare @versionALL varchar(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+
+set @version = 120
+set @versionfix = 24
+
+
+/* Actual version (use your own script to do it)*/
+exec @actualVersion = ccsp_getVersion 'BD'
+exec @actualVersionFix = ccsp_getVersion 'BDF'
+
+select @versionALL = valor from ccsettings where setting_id=77;
+select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
+
+if  @actualVersion = @version and  @actualVersionFix >= 15
+	begin
+		begin tran
+		begin try
+
+		set @process = 'CW-1682 Permiso Exportación Finder'
+		set @Sql= 'if not exists(select * from ccRIACat_AdminPermissions where per_id = 11 ) 
+		begin
+			insert into ccRIACat_AdminPermissions(per_desc,bStatus,release) 
+			values(''Enviar y extraer archivos|Email and transfer files'',1,''f497d327698765762eea6d10a29f4b5168ff1469c6135b2a88aa48ea7404aeada5a285c1899dcea00a01033056a22f68effdc7bfba4357cc070ea32c48287a9c'')
+		end'
+		EXEC(@Sql)
+
+				
+		/* End script release */
+
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		exec ccsp_getVersion 'BDF', @versionFix 
+
+		commit tran
+		end try
+
+		begin catch
+
+			/* Error generated based on sintax */
+			select @errorGenerated = 'DB script version: ' + cast(@version as nvarchar) + '''.''' + cast(@versionfix as nvarchar) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() as nvarchar) + ''' Number: ''' + cast(@@error as nvarchar) + ''' Message: '''+ error_message()
+			RAISERROR(@errorGenerated, 11, 1)
+
+		rollback tran
+		end catch
+	end

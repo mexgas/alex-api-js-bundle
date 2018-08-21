b3cdf63572e13cbbe4e6ceb5eb272014ef59ce26
Hugo Longoria <hlongoria@nuxiba.com>
2018-08-21 12:29:01 -0500

    cambio version

--- Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
+++ Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.0810.120.23")]
-[assembly: AssemblyFileVersion("2018.0810.120.23")]
+[assembly: AssemblyVersion("2018.0821.120.24")]
+[assembly: AssemblyFileVersion("2018.0821.120.24")]

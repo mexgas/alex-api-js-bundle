fc1e6d492704f24f06af7d67a5e30bc82058ad23
Daniel Vega <dvega@nuxiba.com>
2018-09-20 12:01:53 -0500

    archivo resource.designer.cs

--- Reportes2ASPEngine/MiddleWareReports/language/Resources.Designer.cs
+++ Reportes2ASPEngine/MiddleWareReports/language/Resources.Designer.cs
@@ -4183,16 +4183,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Origen de transferencia.
-        /// </summary>
-        public static string Origin {
-            get {
-                return ResourceManager.GetString("Origin", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Fecha Original.
+        ///   Looks up a localized string similar to Fecha Original.
         /// </summary>
         public static string originalDate {
             get {
@@ -5380,16 +5371,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Celular.
-        /// </summary>
-        public static string systemTranslated_cellPhone {
-            get {
-                return ResourceManager.GetString("systemTranslated_cellPhone", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Celular Minutos Facturados.
+        ///   Looks up a localized string similar to Celular Minutos Facturados.
         /// </summary>
         public static string systemTranslated_CelMinBilled_Count {
             get {
@@ -5587,16 +5569,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Fijo.
-        /// </summary>
-        public static string systemTranslated_fijo {
-            get {
-                return ResourceManager.GetString("systemTranslated_fijo", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Inbound.
+        ///   Looks up a localized string similar to Inbound.
         /// </summary>
         public static string systemTranslated_inbound {
             get {
@@ -5614,16 +5587,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Interno.
-        /// </summary>
-        public static string systemTranslated_interno {
-            get {
-                return ResourceManager.GetString("systemTranslated_interno", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Opción inválida.
+        ///   Looks up a localized string similar to Opción inválida.
         /// </summary>
         public static string systemTranslated_Invalid {
             get {
@@ -7279,16 +7243,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Tipo de teléfono.
-        /// </summary>
-        public static string TipoTel {
-            get {
-                return ResourceManager.GetString("TipoTel", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Sesión.
+        ///   Looks up a localized string similar to Sesión.
         /// </summary>
         public static string tlog {
             get {
@@ -7513,16 +7468,7 @@ namespace MiddleWareReports {
         }
         
         /// <summary>
-        ///   Busca una cadena traducida similar a Duración de transferencia.
-        /// </summary>
-        public static string TotalTimeDuration {
-            get {
-                return ResourceManager.GetString("TotalTimeDuration", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Busca una cadena traducida similar a Total de tuits por grupo ACD.
+        ///   Looks up a localized string similar to Total de tuits por grupo ACD.
         /// </summary>
         public static string TotalTweetMessagesbyACDGroup {
             get {

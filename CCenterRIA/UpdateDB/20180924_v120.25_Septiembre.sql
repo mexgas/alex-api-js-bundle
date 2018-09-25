c1b90223a01262c682a4459794840737cd7ae622
Daniel Vega <dvega@nuxiba.com>
2018-09-25 16:12:52 -0500

    correcion de version

--- CCenterRIA/UpdateDB/20180924_v120.25_Septiembre.sql
+++ CCenterRIA/UpdateDB/20180924_v120.25_Septiembre.sql
@@ -29,7 +29,7 @@ set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
 
 set @version = 120--**********actualizar a 119 sin fix
-set @versionfix = 22
+set @versionfix = 25
 --select * from ccsettings where setting_id=77
 --
 /* Actual version (use your own script to do it)*/
@@ -39,7 +39,7 @@ exec @actualVersionFix = ccsp_getVersion 'BDF'
 select @versionALL = valor from ccsettings where setting_id=77;
 select @actualVersionFix=cast(isnull(max(value),'0') as int) from dbo.fn_RIASplitDelimited(@versionALL,'.') where id=4;
 
-if  @actualVersion = @version and  @actualVersionFix >= 15
+if  @actualVersion = @version and  @actualVersionFix >= 24
 	begin
 		begin tran
 		begin try

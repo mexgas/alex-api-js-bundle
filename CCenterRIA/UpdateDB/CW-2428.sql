2935a371bdf3e0a4e9f2956fd73e7ff16f217269
Jesus Gallardo <agallardo@nuxiba.com>
2018-10-30 08:22:29 -0600

    Se modifica para poder agregar en el finder la fecha donde se realizo la ultima interaccion

--- CCenterRIA/UpdateDB/CW-2428.sql
+++ CCenterRIA/UpdateDB/CW-2428.sql
@@ -44,9 +44,118 @@ if  @actualVersion = @version and  @actualVersionFix = 34
 		begin tran
 		begin try
 
-	set @process = 'CW-2428 Alter Lenght Column conversation.mailClient'
-        set @Sql= 'alter table conversation alter column mailClient varchar(255) not null'
-        EXEC(@Sql)       
+	   set @process = 'CW-2428 Alter Lenght Column conversation.mailClient'
+       set @Sql= 'alter table conversation alter column mailClient varchar(255) not null'
+       EXEC(@Sql)
+
+       set @process = 'CW-2428 Alter SP ccsp_CreateNodeMultimedia'
+       set @Sql= 'ALTER PROCEDURE [dbo].[ccsp_CreateNodeMultimedia]
+@conversationId bigint,
+@xml xml OUTPUT,
+@supervisor varchar(255)='''',
+@template varchar (255)='''',
+@ScoreTemplate int=0,
+@type int =1--1 EMAIL , 2 Twitter
+AS
+BEGIN
+declare @info varchar(255)
+declare @infoEscape varchar(max)
+declare @charEscape varchar(255),@charReplace varchar(max)
+set @charEscape=''"|''''''''|<|>|&''
+set @charReplace=''&quot;|&apos;|&lt;|&gt;|&amp;''
+
+declare @existAttached bit,@numInteracion smallint
+if @type=0 begin--CHAT
+
+    select @xml = convert(xml,''<R01 CDATE="''+rtrim(ltrim(convert(varchar(23), isNull(chatDate,requestDate), 126))) +
+    ''" C01="''+convert(varchar(max),chatId) +
+    ''" C02="''+convert(varchar(max),isnull(ccinbound.descripcion,'''')) +
+    ''" C03="''+convert(varchar(max),domain) +
+    ''" C04="''+convert(varchar(max), Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMAterno ) +
+    ''" C05="''+convert(varchar(max),tchatting) +
+    ''" C06="''+convert(varchar(max),isnull(cctipocalif.[Description],''N/A'')) +
+    ''" C07="''+convert(varchar(max),isnull(cctipocalifsub.califSubdesc,''N/A'')) +
+    ''" C08="''+convert(varchar(max),clientname) +
+    ''" C09="''+rtrim(ltrim(convert(varchar(23), chatDate, 126))) +
+    ''" C10="''+convert(varchar(max),isnull(@supervisor,'''') ) +
+    ''" C11="''+convert(varchar(max),isnull(@template,'''') )  +
+    ''" C12="''+convert(varchar(max),isnull(@ScoreTemplate,0)) +
+    ''" C13="''+convert(varchar(max),isnull(ccusers.[Login],'''')) + ''"/>'')
+    from ccRIAChats
+    left outer join ccinbound on ccinbound.inbound_id = ccRIAChats.inboundid
+    left outer join ccusers on ccusers.user_id = ccRIAChats.userid
+    left outer join cctipocalif on cctipocalif.calif_id = ccRIAChats.disposition
+    left outer join cctipocalifsub on cctipocalifsub.califsub_id = ccRIAChats.subdisposition and ccRIAChats.subdisposition <> 0
+    where chatId = @conversationId and chatStatus = 4 and requestDate is not null and chatDate is not null
+
+end
+else if @type=1 begin--EMAIL
+    SELECT @existAttached = case when count(*)>0 then 1 else 0 end
+    from attached where messageId in (select messageId from message where conversationId=@conversationId)
+    select @numInteracion = count(*) from message where conversationId=@conversationId
+    --Replaza los caracteres por los comunes
+    select @info=info from conversation where conversationId=@conversationId
+    select @info=replace(@info,A.Value,B.Value) from dbo.fn_RIASplitDelimited(@charEscape,''|'') A
+    inner join dbo.fn_RIASplitDelimited(@charReplace,''|'') B on A.Id=B.Id
+
+
+    select @xml = convert(xml,''<R03 CDATE="''+ rtrim(ltrim(convert(varchar(23), isnull(max(b.tsend), getdate()), 126))) +
+    ''" C01="''+ convert(varchar(max),a.conversationId) +
+    ''" C02="''+ rtrim(ltrim(convert(varchar(23), isnull(max(b.tsend), getdate()), 126))) +
+    ''" C03="''+ convert(varchar(max),max(c.descripcion)) +
+    ''" C04="''+ convert(varchar,max(isnull(Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMAterno,''''))) +
+    ''" C05="''+ convert(varchar,max(isnull(cctipocalif.[Description],''N/A''))) +
+    ''" C06="''+ convert(varchar,max(replace(replace(a.mailClient,''<'','' ''),''>'','' ''))) +
+    ''" C07="''+ convert(varchar(max),sum(b.tRetention+b.tResponse+b.tWrapup)) +
+    ''" C08="''+ convert(varchar(max),min(isnull(@info,''''))) +
+    ''" C09="''+ convert(varchar(max),max(b.messageStatusid) ) +''" C10="''+  convert(varchar(max), isnull(@numInteracion,0)) +
+    ''" C11="''+ convert(varchar(max),@existAttached) +''" C12="''+ convert(varchar(max),isnull(@supervisor,'''') ) +
+    ''" C13="''+ convert(varchar(max),isnull(@template,'''') )  +''" C14="''+convert(varchar(max),isnull(@ScoreTemplate,0)) +
+    ''" C15="''+ convert(varchar,max(isnull(cctipocalifsub.califSubdesc,''N/A''))) +
+    ''" C16="''+ convert(varchar(max),isnull(max(d.[Login]),'''')) + ''"/>'')
+    from conversation a
+    inner join message b on a.conversationid=b.conversationid
+    left outer join ccinbound c on c.inbound_id = a.inboundid
+    left outer join ccusers d on d.user_id = b.userid
+    left outer join relationmessageDisposition e on e.messageId=b.messageId
+    left outer join cctipocalif on cctipocalif.calif_id = e.dispositionId
+    left outer join cctipocalifsub on cctipocalifsub.califsub_id = e.subdispositionId and e.subdispositionId <> 0
+    where a.conversationId=@conversationId
+    group by a.conversationId,a.inboundid
+
+end
+else if @type=2 begin--Twitter
+    select @numInteracion = sum(ninteration) from messageOutTwitter where conversationTwitterId=@conversationId
+
+    select @xml = convert(xml,''<R04 CDATE="''+rtrim(ltrim(convert(varchar(23), min(b.date), 126))) +
+    ''" C01="''+convert(varchar(max),a.conversationTwitterId) +
+    ''" C02="''+rtrim(ltrim(convert(varchar(23), min(b.date), 126))) +
+    ''" C03="''+convert(varchar(max),max(c.descripcion)) +
+    ''" C04="''+ convert(varchar,max(isnull(Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMAterno,''''))) +
+    ''" C05="''+convert(varchar,max(isnull(cctipocalif.[Description],''N/A''))) +
+    ''" C06="''+ max(a.screenNameClient) +
+    ''" C07="''+convert(varchar(max),sum(b.tRetention+b.tResponse+b.tWrapup)) +
+    ''" C08="''+ max(a.screenNameInbound) +
+    ''" C09="''+convert(varchar(max),max(b.messageStatusid) ) +
+    ''" C10="''+  convert(varchar(max), isnull(@numInteracion,0)) +
+    ''" C11="''+ convert(varchar(max),isnull(@supervisor,'''') ) +
+    ''" C12="''+convert(varchar(max),isnull(@template,''''))  +
+    ''" C13="''+convert(varchar(max),isnull(@ScoreTemplate,0)) +
+    ''" C14="''+ convert(varchar,max(isnull(cctipocalifsub.califSubdesc,''N/A''))) +
+    ''" C15="''+convert(varchar(max),isnull(max(d.[Login]),'''')) + ''"/>'')
+    from conversationTwitter a
+    inner join messageOutTwitter b on a.conversationTwitterId=b.conversationTwitterId
+    left outer join ccinbound c on c.inbound_id = a.inboundid
+    left outer join ccusers d on d.user_id = b.userid
+    left outer join relationMessageDispositionTwit e on e.messageOutTwitterId=b.messageOutTwitterId
+    left outer join cctipocalif on cctipocalif.calif_id = e.dispositionId
+    left outer join cctipocalifsub on cctipocalifsub.califsub_id = e.subdispositionId and e.subdispositionId <> 0
+    where a.conversationTwitterId=@conversationId
+    group by a.conversationTwitterId,a.inboundid
+end
+
+END'
+       EXEC(@Sql)
 
 		set @process = 'CW-2428 Alter SP ccsp_MailSave'
         set @Sql= 'ALTER PROCEDURE [dbo].[ccsp_MailSave]
@@ -61,7 +170,7 @@ if  @actualVersion = @version and  @actualVersionFix = 34
 @messageId int =null,
 @timeAtt int = 0,
 @pathFile varchar(255)= null,
-@mailClient varchar(255)= null,
+@mailClient varchar(60)= null,
 @mailACD varchar(60)= null,
 @isSender bit=0,
 @isUser bit = 0,
@@ -71,10 +180,8 @@ if  @actualVersion = @version and  @actualVersionFix = 34
 @tWrapUp int =0,
 @tRetention int = 0,
 @email varchar(255) = null,
-
 ---Finder
-@supervisor varchar(100)='''' ,@template varchar (100)='''',@ScoreTemplate int =0,
-@top int=30
+@supervisor varchar(100)='''' ,@template varchar (100)='''',@ScoreTemplate int =0
 AS
 BEGIN
 
@@ -179,10 +286,10 @@ else if @action = 7 BEGIN --Cambia el status del mensaje
     --Answered,Send,CLose Conversation system or agent
     if @messageStatusId in (5,6,10,11)  begin
         exec ccsp_CreateNodeMultimedia @type=1, @conversationId=@conversationId, @xml = @xmlnode OUTPUT
-		if exists(select * from ccEmailNodeHistory where emailId=@conversationId) begin
+        if exists(select * from ccEmailNodeHistory where emailId=@conversationId) begin
             update ccEmailNodeHistory set node=@xmlnode,status=2 where emailId=@conversationId
         end
-        if  exists(select * from ccEmailNode where emailId=@conversationId) begin
+        else if exists(select * from ccEmailNode where emailId=@conversationId) begin
             update ccEmailNode set node=@xmlnode,status=2 where emailId=@conversationId
         end
         else begin
@@ -332,8 +439,7 @@ end
 else if @action = 22 begin
    
    declare @correo varchar(255)
-   select  @correo = mailClient from conversation where conversationId = @conversationId   
-   --insert into emailSpam (inboundId,agentId,conversationId,correo) values (@inboundId,@userId,@conversationId,@correo)    
+   select  @correo = mailClient from conversation where conversationId = @conversationId
   insert into emailSpam (inboundId,agentId,conversationId,correo,fecha) values (@inboundId,@userId,@conversationId,@correo,getDate())   
 
    update Conversation set isFinished = 1 where mailClient = @correo

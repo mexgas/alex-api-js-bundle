f1888016cbcc02d942d0e75eecb3db178ac04afb
Jesus Gallardo <agallardo@nuxiba.com>
2018-11-21 11:18:13 -0600

    Se cambia la version

--- CCenterRIA/UpdateDB/20181121_v120.36_Noviembre.sql
+++ CCenterRIA/UpdateDB/20181121_v120.36_Noviembre.sql
@@ -5,11 +5,11 @@
 /*
 Author: 
 		
-Date: 2018/05/15
+Date: 2018/11/21
 Description:
 
 Database: CCenterRia
-Required version: 120.24
+Required version: 120.35
 
 IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
 */

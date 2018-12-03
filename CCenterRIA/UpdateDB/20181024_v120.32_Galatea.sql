e19aca1bdf1a014cb0ce873b6ba6fd7537d218aa
Erick Muñoz <emunoz@nuxiba.com>
2018-12-03 12:43:17 -0600

    se juntan escripts para garantizar ejecución consecutiva

--- CCenterRIA/UpdateDB/20181024_v120.32_Galatea.sql
+++ CCenterRIA/UpdateDB/20181024_v120.32_Galatea.sql
@@ -73,12 +73,9 @@ if  @actualVersion = @version and  @actualVersionFix = @versionfix - 1
 				 SELECT @stringToSplit
 
 				 RETURN
-				END'
-    EXEC(@Sql)
+				END
 
-
-	set @process = 'CW-2419 Deshardcodear conexión segura'
-    set @Sql= ' DECLARE @setting VARCHAR(MAX)
+				DECLARE @setting VARCHAR(MAX)
 				DECLARE @MQIP VARCHAR(MAX)
 				DECLARE @pos INT
 				IF EXISTS (SELECT *

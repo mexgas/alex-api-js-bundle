2c5dd72213840ba377ecf1bf74f8ed6899a07406
Hugo Longoria <hlongoria@nuxiba.com>
2019-01-11 17:50:55 -0600

    agregar filtros default

--- Reportes2ASPEngine/MiddleWareReports/reports/ivrreports/RepIVRSurveys.cs
+++ Reportes2ASPEngine/MiddleWareReports/reports/ivrreports/RepIVRSurveys.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 
 namespace MiddleWareReports
 {
    public class RepIVRSurveys : GenericReport
     {
+       protected override DataTable GetDefaultFilters(int type, DataTable catalog)
+       {
+           switch (type)
+           {
+               case 1:
+               case 7:
+                   DataRow row = catalog.NewRow();
+                   row["dbColumn"] = type == 1 ? "campaignId" : "inboundId";
+                   row["description"] = type == 1 ? "systemTranslated_NoCampaign" : "systemTranslated_NoACDGroup";
+                   row["id"] = 0;
+                   catalog.Rows.Add(row);
+                   return catalog;
+               default:
+                   return base.GetDefaultFilters(type, catalog);
+           }
+       }
     }
 }

a48819e2e525d48a6e6d9b275a17219957e3099d
Jesus Gallardo <agallardo@nuxiba.com>
2019-02-14 12:50:47 -0600

    Se modifica el nombre de la version

--- RecorderRIA/UpdateDB/20190214_v57_Feberero.sql
+++ RecorderRIA/UpdateDB/20190214_v57_Feberero.sql
@@ -3,7 +3,7 @@ Autor: Jesus Gallardo
 Descripcion: Optimization BaseX
 
 
-Version requerida: 55
+Version requerida: 56
 */
 set nocount on
 declare @Version int
@@ -13,7 +13,7 @@ declare @Sql varchar(max)
 declare @errorGenerated varchar(max)
 declare @process varchar(max)
 ---------------- VERSION ----------------
-	Set @Version = 56
+	Set @Version = 57
 	Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
 
 if @Version_Actual in(@Version, @Version -1) -- Aqui poner numero de nueva version

6d5de579221195cd50c4c37427e5f641176173b2
Hugo Longoria <hlongoria@nuxiba.com>
2019-03-20 11:49:53 -0600

    cambios lenguaje Liz

--- CCenterRIA/UpdateDB/20180419_v119.133_Abril.sql
+++ CCenterRIA/UpdateDB/20180419_v119.133_Abril.sql
@@ -413,6 +413,12 @@ set nocount off'
     	EXEC(@Sql)
 
 		
+		set @process = 'Cambios lenguaje'
+		set @Sql= 'update ccRIALog_Module set descripcion=''RECORDING SERVER|RECORDING SERVER'' where module_id=59
+					update ccRIALog_Module set descripcion=''RECORDINGS MANAGER|RECORDINGS MANAGER'' where module_id=57
+					update ccRIALog_Operation set descripcion=''ADJUNTAR EN EMAIL|EMAIL FILE''where operationType=172'
+		EXEC(@Sql)
+		
 	
 		/* End script release */
 

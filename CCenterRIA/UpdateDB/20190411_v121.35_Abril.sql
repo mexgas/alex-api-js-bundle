6a22c3754ba21e2086de9c58cfc76c0ff3578605
Jesus Gallardo <agallardo@nuxiba.com>
2019-05-02 11:39:58 -0500

    Correcion de lista de agentes duplicados

--- CCenterRIA/UpdateDB/20190411_v121.35_Abril.sql
+++ CCenterRIA/UpdateDB/20190411_v121.35_Abril.sql
@@ -824,7 +824,7 @@ END
 '
 		EXEC (@Sql)
 
-		SET @process = 'CW-2770 Alter SP -- Version 121.34'
+		SET @process = 'CW-2770 Alter SP ccsp_RIAABCChat-- Version 121.34'
 		SET @Sql = 'ALTER Procedure [dbo].[ccsp_RIAABCChat]
 @OperationType tinyint = 0, -- 0:Select | 1:Insert | 2:Select Excel | 3:DateRange | 4:Admins | 5:Agents | 6:GalateaAdmin
 @TipoMsgChat tinyint = null,
@@ -991,6 +991,41 @@ if @OperationType=5
  end
 select 0
 set nocount off'
+		EXEC (@Sql)
+
+		SET @process = 'CW-XXXX Alter SP ccsp_Multimedia2 --Acd DUplicate'
+		SET @Sql = 'ALTER PROCEDURE [dbo].[ccsp_Multimedia2] @action INT, @inboundId INT = NULL, @userId INT = NULL, @senderId INT = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF @action = 1
+	BEGIN --Lista  ACD
+		SELECT DISTINCT A.inbound_id AS Id, A.chat AS Mode, C.maxMails MaxMails, cast(isnull(C.maxTweets, 3) AS TINYINT) AS MaxTweets, A.IDArea AS AreaId
+		FROM ccInbound A
+		INNER JOIN ccRIACat_Areas C ON A.IDArea = C.IDArea
+		WHERE @inboundId IS NULL OR @inboundId = A.Inbound_id
+	END
+	ELSE IF @action = 2
+	BEGIN --Lista Agentes  
+		SELECT DISTINCT A.User_id AS [Id], C.idCampEsp AcdId, isnull(skill, 8) Skill
+		FROM ccRIAWorkGroupUsers A
+		INNER JOIN ccusers B ON A.User_id = B.User_id
+		INNER JOIN ccRIACampEspWG C ON C.IDWG = A.IDWG AND C.Tipo = 0
+		INNER JOIN ccInbound D ON C.idCampEsp = D.inbound_id
+		LEFT JOIN ccskills S ON S.inbound_id = D.inbound_id AND S.user_id = B.user_id
+		WHERE B.TipoUser_id = 1 AND (@userId IS NULL OR @userId = A.User_id)
+		ORDER BY A.User_id
+	END
+	ELSE IF @action = 3
+	BEGIN --List Sender Mail
+		SELECT A.contactMeanOutId AS Id, ISNULL(R.inboundId, 0) AS AcdId, A.isActive AS IsActive
+		FROM contactMeanOut A
+		LEFT JOIN relationContactMeanOutInbound R ON A.contactMeanOutId = R.contactMeanOutId
+		WHERE @senderId IS NULL OR @senderId = A.contactMeanOutId
+	END
+END
+'
 		EXEC (@Sql)
 		
 	

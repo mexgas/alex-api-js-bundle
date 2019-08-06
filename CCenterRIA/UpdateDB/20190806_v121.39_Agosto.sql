2ed252ca90e288765556631b9275507caff39334
Jesus Gallardo <agallardo@nuxiba.com>
2019-08-06 10:24:51 -0500

    Se crea el archivo de la version 121.39

--- CCenterRIA/UpdateDB/20190806_v121.39_Agosto.sql
+++ CCenterRIA/UpdateDB/20190806_v121.39_Agosto.sql
@@ -9,15 +9,7 @@ Date: 2019/04/11
 Description: 
 
 Database: CCenterRia
-Required version: 121.37
-
-Se agrega la tarea
-cw-2915
-cw-3001
-CW-3201
-CW-3032
-CW-3045
-CW-3199
+Required version: 121.38
 
 IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
 */
@@ -36,7 +28,7 @@ Importante:la variable @version puede tener 2 valores dependiendo la necesidad q
 set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
 SET @version = 121 --**********actualizar a 119 sin fix
-SET @versionfix = 38
+SET @versionfix = 39
 /* Actual version (use your own script to do it)*/
 EXEC @actualVersion = ccsp_getVersion 'BD'
 

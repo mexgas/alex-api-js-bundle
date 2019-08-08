e1bfeb9843bdc96678f39306904047b0a839914b
Charls <cechavez@nuxiba.com>
2019-08-08 13:25:38 -0500

    Actualizacion en funcion TelAni

--- CCenterRIA/UpdateDB/CW-2611.sql
+++ CCenterRIA/UpdateDB/CW-2611.sql
@@ -56,6 +56,7 @@ RETURNS varchar(32)
 AS
 BEGIN
 declare @cldLocal varchar(10), @pais tinyint, @lon tinyint, @ret as varchar(10)
+declare @telResp varchar(32) = ''''
 
 select  @cldLocal = valor from ccsettings where setting_id = 17
 select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
@@ -67,23 +68,23 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 	if @pais = 1 begin --Empieza Mexico
 		select @lon = len(@tel)
 		if @lon >= 10 begin
-			select TOP 1 @tel = telani from ccEstadosAni WITH (NOLOCK) where id_anilist = @lista and (
+			select TOP 1 @telResp = telani from ccEstadosAni WITH (NOLOCK) where id_anilist = @lista and (
 				( left(right(@tel, 10), 3) = area and len(area) = 3 )
 				or
 				( left(right(@tel, 10), 2) = area and len(area) = 2 ))
 			AND telani <> ''''
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Mexico
 
 	if @pais = 2 begin  -- Empieza Argentina
 		select @lon = len(@tel)
 		if @lon >= 6 and @lon <=13 begin
-			select @tel = telAni from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telAni from ccEstadosAni where id_anilist = @lista and
 				(( @lon = 6 and left(@tel,4) = area and len(area) = 4 )
 				or
 				( @lon = 7 and left(@tel,3) = area and len(area) = 3 )
@@ -103,15 +104,15 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 				( @lon = 13 and substring(@tel, 2, 4) = area and len(area) = 4 ))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-			return @tel
+			return @telResp
 	end  --Termina Argentina
 
 	if @pais = 3 begin  --Empieza Colombia
 		select @lon = len(@tel)
 		if @lon >= 6 and @lon <=13 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 				(( len(@tel) = 7 and @cldlocal = area )
 				or
 				( len(@tel) = 8 and left(@tel,5) = area )
@@ -119,9 +120,9 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 				( len(@tel) in(10,11) and (left(@tel,1) = ''3'' or substring(@tel,2,1) = ''3'')))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-		return @tel
+		return @telResp
 	end  --Termina Colombia
 
 	if @pais = 4 begin --Empieza USA
@@ -131,18 +132,18 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 				set @tel = @cldLocal + @tel
 			end
 			set @tel = right(@tel, 10)
-			select @tel = telani from ccEstadosAni where area = left(@tel,3) and id_anilist = @lista
+			select @telResp = telani from ccEstadosAni where area = left(@tel,3) and id_anilist = @lista
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-		return @tel
+		return @telResp
 	end --Termina USA
 
 	if @pais = 5 begin -- Empieza Chile
 		select @lon = len(@tel)
 		if @lon >= 6 and @lon <=15 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 			(( len(@tel) = 6 and @cldlocal = area )
 			or
 			( len(@tel) = 7 and @cldlocal = area )
@@ -156,23 +157,23 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 			( len(@tel) = 10 and substring(@tel,3,1) = area and left(@tel,2) = ''09'' ))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-		return @tel
+		return @telResp
 	end --Termina Chile
 
 	if @pais = 6 begin -- Venezuela
 		select @lon = len(@tel)
 		if @lon >= 7 and @lon <=11 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 				(len(@tel) = 7 and left(@tel,3) = area or
 				len(@tel) = 11 and substring(@tel,2,3) = area)
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Venezuela
 
 	if @pais = 7 begin -- Empieza UK
@@ -182,7 +183,7 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 		end
 
 		if @lon >= 9 and @lon <=11 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 			(( len(@tel) = 10 and substring(@tel,1,5) = area )
 			or
 			( len(@tel) = 10 and substring(@tel,1,4) = area )
@@ -196,15 +197,15 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 			( len(@tel) = 9 and substring(@tel,1,4) = area ) )
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-		return @tel
+		return @telResp
 	end --Termina UK
 
 	if @pais = 8 begin --Empieza Arabia Saudita
 		select @lon = len(@tel)
 		if @lon >= 7 and @lon <=13 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 				(len(@tel) = 7 and ''0''+@cldlocal + ''-''+ substring(@tel,1,1) + ''00'' = area or
 				len(@tel) = 9 and substring(@tel,1,3) + ''00'' = replace(area,''-'','''') or
 				len(@tel) = 10 and substring(@tel,1,4) + ''00'' = replace(area,''-'','''') or
@@ -212,10 +213,10 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 				len(@tel) = 11 and substring(@tel,1,5) = replace(area,''-'',''''))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Arabia Saudita
 
 	if @pais = 9 --Empieza Australia
@@ -223,23 +224,23 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 			select @lon = len(@tel)
 			if @lon >= 8 and @lon <=10
 				begin
-					select @tel = telani from ccEstadosAni where id_anilist = @lista
+					select @telResp = telani from ccEstadosAni where id_anilist = @lista
 					and (len(@tel) = 8 and @cldLocal + substring(@tel,1,2) = area or
 							len(@tel) = 9 and ''0'' + substring(@tel,1,3) = area or
 							len(@tel) = 10 and substring(@tel,1,4) = area)
 				end
 			else
 				begin
-					select @tel = ''''
+					select @telResp = ''''
 				end
 
-			return @tel
+			return @telResp
 		end --Termina Australia
 
 	if @pais = 10 begin -- Empieza Brasil
 		select @lon = len(@tel)
 		if @lon >= 8 and @lon <=15 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and (
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and (
 				((@lon       = 8        )                                    and             @cldlocal = area) or
 				((@lon       = 9        ) and substring(@tel, 1, 1) = ''9''    and             @cldlocal = area) or
 				((@lon between 10 and 11)                                    and substring(@tel, 1, 2) = area) or
@@ -249,71 +250,71 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 				((@lon       = 14       ) and substring(@tel, 1, 1) = ''0''    and substring(@tel, 4, 2) = area))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
 
-		return @tel
+		return @telResp
 	end -- Termina Brasil
 
 	if @pais = 11 begin --Empieza Guatemala
 		if len(@tel) = 8  begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Guatemala
 
 	if @pais = 12 begin --Empieza Costa Rica
 		if len(@tel) = 8  begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
 		end
 		else if len(@tel) = 10 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 3) = area
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 3) = area
 		end
 		else begin
 			if charindex(substring(@tel,1,2),''00,08'') <= 0
-				select @tel = ''''
+				select @telResp = ''''
 			else
-				select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
+				select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Costa Rica
 
 	if @pais = 13 begin --Empieza Salvador
 		if len(@tel) = 8  begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
 		end
 		else begin
 			if charindex(substring(@tel,1,2),''00'') <= 0
-				select @tel = ''''
+				select @telResp = ''''
 			else
-				select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
+				select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Salvador
 
 	if @pais = 14 begin --Empieza Espa?a
 		if len(@tel) = 9  begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 			((substring(@tel, 1, 1) = area) or
 			(substring(@tel, 1, 2) = area) or
 			(substring(@tel, 1, 3) = area))
 		end
 		else
-			select @tel = ''''
+			select @telResp = ''''
 
-		return @tel
+		return @telResp
 	end --Termina Espa?a
 
 	if @pais = 15 begin -- Empieza Peru
 		select @lon = len(@tel)
 		if @lon >= 6 and @lon <=9 begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and
 			(( len(@tel) = 6 and @cldlocal = area )
 			or
 			( len(@tel) = 7 and @cldlocal = area )
@@ -321,23 +322,23 @@ select @pais = valor, @ret = '''' from ccSettings where setting_id = 104
 			( len(@tel) = 9 and left(@tel,1)=''0'' and substring(@tel,2,len(@cldlocal)) = area ))
 		end
 		else begin
-			select @tel = ''''
+			select @telResp = ''''
 		end
-		return @tel
+		return @telResp
 	end --Termina Peru
 
 	if @pais = 16 begin --Empieza Panama
 		if len(@tel) = 7  begin
-			select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
+			select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 1) = area
 		end
 		else begin
 			if charindex(substring(@tel,1,2),''00'') <= 0
-				select @tel = ''''
+				select @telResp = ''''
 			else
-				select @tel = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
+				select @telResp = telani from ccEstadosAni where id_anilist = @lista and substring(@tel, 1, 2) = area
 		end
 
-		return @tel
+		return @telResp
 	end --Termina Panama
 
 	return @ret

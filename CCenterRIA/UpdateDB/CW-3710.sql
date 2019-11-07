e0642a9c6d6995ff005adb99ef7607fb585f2b36
Erick Muñoz <emunoz@nuxiba.com>
2019-11-07 11:55:42 -0600

    CW-3610 Se modifica ruta a la nueva donde se encuentran los certificados

--- CCenterRIA/UpdateDB/CW-3710.sql
+++ CCenterRIA/UpdateDB/CW-3710.sql
@@ -3,7 +3,7 @@ set @process = 'CW-3610 Setting_ID 217 Información del certificado'
 	IF not exists (SELECT * FROM ccSettings WHERE setting_id = 217)
 	INSERT INTO ccSettings (setting_id, valor, descripcion,	Status,	Tipo,detalle,description,bLoadSettings,	validate)
 	VALUES	(217, 
-			''D:\Centerware\Sites\certs\nuxiba_pfx.pfx|e7451896fd98715c9e67f110351b5719'',
+			''D:\Centerware\certs\nuxiba_pfx.pfx|e7451896fd98715c9e67f110351b5719'',
 			''Parámetros del Certificado de seguridad (.PFX)'',	
 			1,
 			''X'',

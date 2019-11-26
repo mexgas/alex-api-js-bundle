d5f3f9f17523aab1d372b3e1e8990e8ebd3373b8
Erick Muñoz <emunoz@nuxiba.com>
2019-11-26 08:53:42 -0600

    CW-3701 Se agrega store para el borrado de pendientes y nuevos

---
+++ CCenterRIA/UpdateDB/CW-3701.sql
@@ -0,0 +1,22 @@
+USE [CCenterRia]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[ccsp_GalateaDeleteRegistryLoad]
+@loadID INT
+AS
+
+IF (@loadID IS NOT NULL AND EXISTS(SELECT * FROM ccRIARegistryLists WHERE list_id = @loadID and status <> 0))
+BEGIN
+	UPDATE ccoCallsOutSource SET cal_status = '' WHERE list_id = @loadID
+	DELETE FROM ccoWorkingTable WHERE list_id = @loadID 
+	exec ccsp_RIARegistryLists @action=6, @list_id = @loadID 
+END
+ELSE
+BEGIN
+	--Si el id de carga es nulo o no se encuentra registro de dicha carga o esta ya ha sido borrada
+	raiserror('ERROR. No existe una carga el id especificado', 18, 1)
+END				
+	

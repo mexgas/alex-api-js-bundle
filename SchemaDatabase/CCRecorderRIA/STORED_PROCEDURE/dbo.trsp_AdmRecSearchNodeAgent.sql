bac6ffa26ab2fe69137828acc72daf91c6de97df
Mr_Jenkins <mrjenkins@nuxiba.com>
2019-11-29 02:28:45 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCRecorderRIA/STORED_PROCEDURE/dbo.trsp_AdmRecSearchNodeAgent.sql
+++ SchemaDatabase/CCRecorderRIA/STORED_PROCEDURE/dbo.trsp_AdmRecSearchNodeAgent.sql
@@ -5,14 +5,20 @@ CREATE PROCEDURE [dbo].[trsp_AdmRecSearchNodeAgent]
             BEGIN
 
             SET NOCOUNT ON;
-
-            select a.User_id, a.Nombres + ' ' +  a.ApellidoPaterno+ ' ' +  a.ApellidoMaterno as [Nombres], b.IDWG from ccUsers a
-            inner join ccRIAWorkGroupUsersConsulta b
-            on b.IDWG = @Workgroup
-            where a.User_id = b.User_id and a.TipoUser_id = 1
-            union 
-            select a.User_id, a.Nombres + ' ' +  a.ApellidoPaterno+ ' ' +  a.ApellidoMaterno as [Nombres], b.IDWG from ccUsers a
-            inner join ccRIAWorkGroupUsers b
-            on b.IDWG = @Workgroup
-            where a.User_id = b.User_id and a.TipoUser_id = 1
+			if @Workgroup = 0
+			begin
+				select a.User_id, a.Nombres + ' ' +  a.ApellidoPaterno+ ' ' +  a.ApellidoMaterno as [Nombres], 0 IDWG from ccUsers a
+			end
+			else
+			begin
+				select a.User_id, a.Nombres + ' ' +  a.ApellidoPaterno+ ' ' +  a.ApellidoMaterno as [Nombres], b.IDWG from ccUsers a
+				inner join ccRIAWorkGroupUsersConsulta b
+				on b.IDWG = @Workgroup
+				where a.User_id = b.User_id and a.TipoUser_id = 1
+				union 
+				select a.User_id, a.Nombres + ' ' +  a.ApellidoPaterno+ ' ' +  a.ApellidoMaterno as [Nombres], b.IDWG from ccUsers a
+				inner join ccRIAWorkGroupUsers b
+				on b.IDWG = @Workgroup
+				where a.User_id = b.User_id and a.TipoUser_id = 1
+			end
            END
--- SchemaDatabase/CCRecorderRIA/STORED_PROCEDURE/dbo.trsp_AdmRecSearchNodeCalif.sql
+++ SchemaDatabase/CCRecorderRIA/STORED_PROCEDURE/dbo.trsp_AdmRecSearchNodeCalif.sql
@@ -7,19 +7,31 @@ CREATE PROCEDURE [dbo].[trsp_AdmRecSearchNodeCalif]
 
 								IF @CallType = 0 
 								BEGIN
-
-									select a.calif_id, b.Description from ccCalifCamp a
-									inner join ccTipoCalif b 
-									on b.calif_id = a.calif_id and b.Calif_Status = 1
-									where cam_id = @CamACD_id and a.tipo = 0
+									if @CamACD_id = 0
+									begin
+										select calif_id, Description from ccTipoCalif
+									end
+									else
+									begin
+										select a.calif_id, b.Description from ccCalifCamp a
+										inner join ccTipoCalif b 
+										on b.calif_id = a.calif_id and b.Calif_Status = 1
+										where cam_id = @CamACD_id and a.tipo = 0
+									end
 								END
 								ELSE IF @CallType = 1 
 									BEGIN 
-
-										select a.calif_id, b.Description from ccCalifCamp a
-										inner join ccTipoCalifOUT b 
-										on b.calif_id = a.calif_id and b.CalifOUT_Status = 1
-										where cam_id = @CamACD_id and a.tipo = 1
+										if @CamACD_id = 0
+										begin
+											select calif_id, Description from ccTipoCalifOUT
+										end
+										else
+										begin
+											select a.calif_id, b.Description from ccCalifCamp a
+											inner join ccTipoCalifOUT b 
+											on b.calif_id = a.calif_id and b.CalifOUT_Status = 1
+											where cam_id = @CamACD_id and a.tipo = 1
+										end
 									END
 
 							END

d35b23f2c9001c47068f3c6142ca748b0f1de0cd
Erick Muñoz <emunoz@nuxiba.com>
2019-12-05 16:06:51 -0600

    CW-3701 Se agrega store que consulta el último id de carga dado el cam_id

--- CCenterRIA/UpdateDB/CW-3701.sql
+++ CCenterRIA/UpdateDB/CW-3701.sql
@@ -10,7 +10,7 @@ AS
 
 IF (@loadID IS NOT NULL AND EXISTS(SELECT * FROM ccRIARegistryLists WHERE list_id = @loadID and status <> 0))
 BEGIN
-	UPDATE ccoCallsOutSource SET cal_status = '' WHERE list_id = @loadID
+	UPDATE ccoCallsOutSource SET cal_status = '5' WHERE list_id = @loadID
 	DELETE FROM ccoWorkingTable WHERE list_id = @loadID 
 	exec ccsp_RIARegistryLists @action=6, @list_id = @loadID 
 END
@@ -19,4 +19,24 @@ BEGIN
 	--Si el id de carga es nulo o no se encuentra registro de dicha carga o esta ya ha sido borrada
 	raiserror('ERROR. No existe una carga el id especificado', 18, 1)
 END				
+
+USE [CCenterRia]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ccsp_GalateaGetRegistryListID]
+@camID INT
+AS
+
+IF (@camID IS NOT NULL AND EXISTS(SELECT * FROM cccamps WHERE cam_id = @camID))
+BEGIN
+	SELECT TOP 1 list_id FROM ccRIARegistryLists WHERE cam_id = @camID AND status = 2 ORDER BY list_id DESC
+END
+ELSE
+BEGIN
+	--Si el id de carga es nulo o no se encuentra registro de dicha carga o esta ya ha sido borrada
+	raiserror('ERROR. No existe una campaña con el id especificado', 18, 1)
+END					
 	

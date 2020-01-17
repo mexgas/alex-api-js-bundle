85bc7bd26e48407453e9e7a32c461b69bd24b79a
Jose Luis Jimenez <jlhernandez@nuxiba.com>
2020-01-17 11:21:19 -0600

    no message

--- CCenterRIA/UpdateDB/20200106_v122.13_Enero.sql
+++ CCenterRIA/UpdateDB/20200106_v122.13_Enero.sql
@@ -1516,7 +1516,7 @@ BEGIN
 			SELECT Cam_id
 			FROM CW_CenterScript..Campaign
 			)
-		AND IDArea > 0 and chat = 0
+		AND IDArea > 0 
 END
 
 IF (@action = 2)
@@ -1529,7 +1529,7 @@ BEGIN
 			SELECT Inbound_id
 			FROM CW_CenterScript..ACD
 			)
-		AND IDArea > 0 
+		AND IDArea > 0 and chat = 0
 END
 
 IF (@action = 3)

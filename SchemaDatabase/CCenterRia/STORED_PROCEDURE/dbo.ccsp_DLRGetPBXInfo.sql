b13d631b3a606ebd3b5c31779632c9746a00caaa
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-02-20 02:33:22 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_DLRGetPBXInfo.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_DLRGetPBXInfo.sql
@@ -6,8 +6,9 @@ CREATE procedure [dbo].[ccsp_DLRGetPBXInfo]
 				declare @port varchar(5), @remotes varchar(300)
 				select @port = valor from ccsettings where setting_id=119
 				select @remotes = valor from ccsettings where setting_id=143
-				select 
-				case when charindex(':',pbxIp)>0 then pbxIp else pbxIp + ':' + @port end pbxUri, @port port
+				select
+				case when charindex(':',pbxIp)>0 then substring(pbxIp, 0, charindex(':',pbxIp)) else pbxIp end pbxUri,
+				case when charindex(':',pbxIp)>0 then substring(pbxIp, charindex(':',pbxIp)+1, 5) else @port end port
 				from
 				(select
 				substring(value,0,charindex('|',value)) pbxId,

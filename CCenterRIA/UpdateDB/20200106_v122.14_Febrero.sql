7cccf9d0cc430c7ee4791e3508fba9d1a684c234
Jose Luis Jimenez <jlhernandez@nuxiba.com>
2020-02-20 16:07:39 -0600

    cw_Centerscript  -> agrega apellido a usuario

--- CCenterRIA/UpdateDB/20200106_v122.14_Febrero.sql
+++ CCenterRIA/UpdateDB/20200106_v122.14_Febrero.sql
@@ -70,6 +70,74 @@ BEGIN
 				set nocount off'
 		EXEC(@sql)
 
+		set @process = '(CenterScript) Alter getadminprops sp'
+		set @sql = '
+						ALTER PROCEDURE [dbo].[CS_GetAdminProps] @admin_id INT
+AS
+SET NOCOUNT ON;
+
+SELECT convert(int,user_id) as [user_id], Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMaterno as [name]
+	,Upper(left(nombres, 1) + left(apellidopaterno, 1)) AS [initials]
+FROM ccUsers a
+WHERE TipoUser_id = 2
+	AND User_id = @admin_id
+		'
+
+		exec(@sql)
+
+		set @process = '(Centerscript) alter cs_getAcdCampList'
+		set @sql = '
+			ALTER PROCEDURE [dbo].[cs_GetACDCampaignList] @action AS SMALLINT
+AS
+IF (@action = 1)
+BEGIN
+	SELECT cam_id AS [cam_id]
+		,cam_descripcion AS [name]
+	FROM ccCamps
+	WHERE cam_activo = 1
+		AND cam_id NOT IN (
+			SELECT Cam_id
+			FROM CW_CenterScript..Campaign
+			)
+		AND IDArea > 0 
+END
+
+IF (@action = 2)
+BEGIN
+	SELECT inbound_id
+		,descripcion AS [name]
+	FROM ccInbound
+	WHERE STATUS = 1
+		AND Inbound_id NOT IN (
+			SELECT Inbound_id
+			FROM CW_CenterScript..Inbound_Campaign
+
+			)
+		AND IDArea > 0 and chat = 0
+END
+
+IF (@action = 3)
+BEGIN
+	SELECT cam_id AS [cam_id]
+		,cam_descripcion AS [name]
+	FROM ccCamps
+	WHERE cam_activo = 1
+		AND IDArea > 0
+END
+
+IF (@action = 4)
+BEGIN
+	SELECT inbound_id
+		,descripcion AS [name]
+	FROM ccInbound
+	WHERE STATUS = 1
+		AND IDArea > 0
+		AND chat = 0
+END
+
+		'
+exec (@sql)
+
 
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

3f75100105229b5398a647580b0012868205a139
Vic González <vgonzalez@nuxiba.com>
2020-03-31 12:31:00 -0600

    Se agrega ccsp_GalateaAdminGetAgentCounters

--- CCenterRIA/UpdateDB/20200306_v122.15_Marzo.sql
+++ CCenterRIA/UpdateDB/20200306_v122.15_Marzo.sql
@@ -503,6 +503,89 @@ AS
  END'
 		EXEC(@sql)		
 
+	set @process = 'CW-3928 Drop if exists ccsp_GalateaAdminGetAgentCounters'
+		set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaAdminGetAgentCounters'')
+	    begin
+	        DROP PROCEDURE ccsp_GalateaAdminGetAgentCounters;
+	    end'
+	    exec (@sql)
+
+			set @process = 'CW-3928 Refactorizacion AM'
+		set @sql = '
+CREATE PROCEDURE [dbo].[ccsp_GalateaAdminGetAgentCounters] @type AS     INT, 
+                                                    @sup_id AS   INT = 0, 
+                                                    @agent_id AS INT = 0, 
+                                                    @WG AS       INT = 0
+AS
+     SET NOCOUNT ON;
+     IF @type = 1
+         BEGIN
+             WITH TableUserAgent(userId)
+                  AS (SELECT DISTINCT 
+                           wgAgt.User_id  AS Id --,usr.login 
+                      FROM ccriaworkgroupusers wgAdmin
+                           INNER JOIN ccriaworkgroupusers wgAgt ON wgAdmin.IDWG = wgAgt.IDWG
+                           INNER JOIN ccUsers usr ON usr.User_id = wgAgt.User_id
+                                                     AND usr.TipoUser_id = 1
+                      WHERE wgAdmin.User_id = @sup_id)
+                  SELECT CAST(a.User_id AS INT) Id, 
+                         a.login AS Username, 
+                         a.Nombres + '' '' + a.ApellidoPaterno + '' '' + a.ApellidoMaterno AS Name
+                  FROM ccusers a(NOLOCK)--, ccGenViewRelsSupsAgent b
+                       INNER JOIN TableUserAgent b ON a.User_id = b.userId
+                  ORDER BY a.Login ASC;
+     END;
+     IF @type = 2
+         BEGIN
+             SELECT CAST(User_id AS INT) Id,
+					Login Username, 
+                    Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMaterno Name
+             FROM ccUsers
+             WHERE User_id = @agent_id;
+     END;
+     IF @type = 3 --Agents by supervisor and WG
+         BEGIN
+             DECLARE @table2 TABLE
+             (userId INT
+              PRIMARY KEY NOT NULL
+             );
+             INSERT INTO @table2
+                    SELECT DISTINCT 
+                           wg.User_id
+                    FROM ccRIAWorkGroupUsers wg
+                         LEFT JOIN ccUsers us ON wg.User_id = us.User_id
+                    WHERE us.TipoUser_id = 1
+                          AND wg.IDWG IN
+                    (
+                        SELECT IDWG
+                        FROM ccRIAWorkGroupUsers
+                        WHERE User_id = @sup_id
+                              AND IDWG <> @WG
+                    );
+             SELECT CAST(B.User_id AS int) AS Id
+             FROM @table2 A
+                  RIGHT JOIN
+             (
+                 SELECT DISTINCT 
+                        wg.User_id
+                 FROM ccRIAWorkGroupUsers wg
+                      LEFT JOIN ccUsers us ON wg.User_id = us.User_id
+                 WHERE wg.IDWG = @WG
+                       AND us.TipoUser_id = 1
+             ) B ON A.userId = B.User_id
+             WHERE A.userId IS NULL;
+     END;
+
+	 IF @type = 4 --Agents IDs by WG
+     BEGIN
+		SELECT  CAST(wg.User_id AS INT) Id  
+		FROM ccRIAWorkGroupUsers wg
+		JOIN CCUsers u on u.user_id = wg.user_id AND u.TipoUser_id = 1
+		where IDWG = @WG
+     END;
+     SET NOCOUNT ON;
+'
+	    exec (@sql)
 		
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

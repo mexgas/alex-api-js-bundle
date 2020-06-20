c8dd7677dceaed3bd56e519527cc1cc10a658357
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-06-20 02:32:22 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaGetCampaignOutDialingStats.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaGetCampaignOutDialingStats.sql
@@ -32,12 +32,13 @@ CREATE PROCEDURE [dbo].[ccsp_GalateaGetCampaignOutDialingStats]
 
 						select A.*, C.Xfer,
 						((A.Abandon *100.0)/ A.Answer) as AbandonRate,
-						(A.Answer - A.Abandon - A.Canceled) as Attended
+						(A.Answer - A.Abandon - A.Canceled) as Attended,
+                        B.AggressionFactor
 						
 						from @table as A
 
 						left join(
-							select ccC.cam_id, ccC.aggressionFactor
+							select ccC.cam_id, ccC.AggressionFactor
 							from ccCamps as ccC
 						)B ON A.cam_id = B.cam_id
 

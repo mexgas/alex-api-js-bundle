f60445bd997db84e519a7159f9c2405d5f723acf
uespinosa01 <uespinosa@nuxiba.com>
2020-06-23 17:44:32 -0500

    Se agrega el rol predefinido de root

--- CCenterRIA/UpdateDB/20200610_v122.18_Junio.sql
+++ CCenterRIA/UpdateDB/20200610_v122.18_Junio.sql
@@ -590,10 +590,29 @@ insert into ccroles values (''Admin'',''translate_admin'',GetDate(),1,NEWID(),2)
 insert into ccroles values (''Supervisor'',''translate_supervisor'',GetDate(),1,NEWID(),3)
 insert into ccroles values (''Monitor'',''translate_monitor'',GetDate(),1,NEWID(),4)
 
-insert into ccPermissions values(10001,''Iniciar y detener campañas|Start and stop Campaign'',''RolesPermissionStartStop'',0,0,0,''N/A'',1,NEWID())
-insert into ccPermissions values(10002,''Carga de base de datos|Data Import'',''RolesPermissionDataImport'',9,1,26,''644f3f9a7013f33219aae30ca25565240c0234b9a50a88494c16bb33bf9d3303b9cccff75b50ddb09b2242c5b3dbaf78'',1,NEWID())
-insert into ccPermissions values(10003,''Sólo Monitoreo|Only Monitoring'',''RolesPermissionMonitoring'',0,0,0,''N/A'',1,NEWID())
-insert into ccPermissions values(10004,''CenterScript|CenterScript'',''RolesPermissionCenterScript'',0,0,0,''N/A'',1,NEWID())'
+insert into ccPermissions values(10001,''Iniciar y detener campañas|Start and stop Campaign'',''translate_start_stop_camp'',0,0,0,''N/A'',1,NEWID())
+insert into ccPermissions values(10002,''Carga de base de datos|Data Import'',''translate_data_import'',9,1,26,''644f3f9a7013f33219aae30ca25565240c0234b9a50a88494c16bb33bf9d3303b9cccff75b50ddb09b2242c5b3dbaf78'',1,NEWID())
+insert into ccPermissions values(10003,''Sólo Monitoreo|Only Monitoring'',''translate_monitoring'',0,0,0,''N/A'',1,NEWID())
+insert into ccPermissions values(10004,''CenterScript|CenterScript'',''translate_centerScript'',0,0,0,''N/A'',1,NEWID())
+
+insert into ccRoles_Permissions values (1,10001)
+insert into ccRoles_Permissions values (1,10002)
+insert into ccRoles_Permissions values (1,10003)
+insert into ccRoles_Permissions values (1,10004)
+
+insert into ccRoles_Permissions values (2,10001)
+insert into ccRoles_Permissions values (2,10002)
+insert into ccRoles_Permissions values (2,10003)
+insert into ccRoles_Permissions values (2,10004)
+
+
+insert into ccRoles_Permissions values (3,10002)
+insert into ccRoles_Permissions values (3,10003)
+
+insert into ccRoles_Permissions values (4,10003)
+
+insert into ccUsers_Roles values (1,1)
+'
 		EXEC(@sql)
 
 		set @process = 'CW-4083 if exists sp ccsp_GalateaAdminRolesManagement drop '

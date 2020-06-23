5b84a36dec96497b59bbcbcbe22a0859c0abdb7a
uespinosa01 <uespinosa@nuxiba.com>
2020-06-23 15:11:20 -0500

    SE agrega el regreso de los usuarios afectados po la eliminacion del rol

--- CCenterRIA/UpdateDB/20200610_v122.18_Junio.sql
+++ CCenterRIA/UpdateDB/20200610_v122.18_Junio.sql
@@ -609,7 +609,7 @@ insert into ccPermissions values(10004,''CenterScript|CenterScript'',''RolesPerm
 ---- Create date: 15/05/2020
 ---- Description:	Role management and permissions in galatea Admin
 ---- =============================================
-CREATE PROCEDURE [dbo].[ccsp_GalateaAdminRolesManagement]
+ALTER PROCEDURE [dbo].[ccsp_GalateaAdminRolesManagement]
 	@action SMALLINT,
 	@User_id VARCHAR(MAX)= '''',
 	@subaction VARCHAR(50)= '''',
@@ -627,15 +627,15 @@ AS
 --	@description VARCHAR(250)= ''aa'',
 --	@keyJson VARCHAR(250)= '''',
 --	@active BIT= 1,
---	@Roles_id VARCHAR(50)= ''1070'',
+--	@Roles_id VARCHAR(50)= ''1051'',
 --	@Permissions_Id VARCHAR(MAX)= ''1,2'',
---	@menus_id VARCHAR(250)= ''''
+--	@menus_id VARCHAR(250)= '''';
 BEGIN TRY
-    BEGIN TRANSACTION-- Inicia el bloque de la transaccion
-	DECLARE @resultado int
-	DECLARE @returnValue SMALLINT
+    BEGIN TRANSACTION;-- Inicia el bloque de la transaccion
+	DECLARE @resultado varchar(50) = '''';
+	DECLARE @returnValue SMALLINT;
     BEGIN
-	 IF @action = 1 -- @subaction = Permissions Show permissions aviables -- @subaction = Permissions Show Roles aviables -- @subaction = Permissions Show Users aviables
+	 IF @action = 1
         BEGIN
         IF @subaction = ''Permissions''
             BEGIN
@@ -646,8 +646,8 @@ BEGIN TRY
                        OrderGrl
                 FROM ccPermissions
                 ORDER BY OrderGrl, 
-                         Parent
-        END
+                         Parent;
+        END;
         IF @subaction = ''Roles''
             BEGIN
                 SELECT r.Rol_id AS RolId, 
@@ -677,17 +677,17 @@ BEGIN TRY
                 FROM ccRoles r
                      LEFT JOIN ccUsers_Roles ur WITH(NOLOCK) ON r.Rol_id = ur.Rol_id
                                                                 AND ur.User_id = @User_id
-        END
+        END;
         IF @subaction = ''Users''
             BEGIN
                 SELECT User_id, 
                        Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMaterno AS Names
                 FROM ccUsers
-                WHERE TipoUser_id = 2 AND User_id > 1
-        END
-    END
-	END
-    IF @action = 2 -- Show relationship between role and user permissions
+                WHERE TipoUser_id = 2 AND User_id > 1;
+        END;
+    END;
+	END;
+    IF @action = 2
         BEGIN
             SELECT p.Permissions_id, 
                    Parent, 
@@ -699,8 +699,8 @@ BEGIN TRY
                  INNER JOIN ccPermissions p WITH(NOLOCK) ON p.Permissions_id = rp.Permissions_id
             WHERE ur.User_Id = @User_id
                   AND r.Active = 1
-                  AND p.Active = 1
-    END
+                  AND p.Active = 1;
+    END;
     IF @action = 3 -- assign roles to user
         BEGIN
             IF OBJECT_ID(''tempdb..#Users_Ids'') IS NOT NULL DROP TABLE #Users_Ids
@@ -720,11 +720,13 @@ BEGIN TRY
 			FROM ccUsers_Roles
 			WHERE User_id in (SELECT value FROM #Users_split)
 
-
-            IF EXISTS( select top 1 * from #Users_Ids)
-                BEGIN
-                    DELETE ccUsers_Roles
-                    WHERE User_id IN (select * from #Users_Ids)
+			IF @subaction = ''NewRelate''
+			BEGIN
+				IF EXISTS( select top 1 * from #Users_Ids)
+					BEGIN
+						DELETE ccUsers_Roles
+						WHERE User_id IN (select * from #Users_Ids);
+					END
 				END
 			IF @Roles_id <> ''''
 			BEGIN
@@ -734,102 +736,59 @@ BEGIN TRY
 
 			END
 			SET @returnValue = (select top 1 * from  #Users_split)
-    END
-    
-	IF @action = 4 -- Delete Roles
-		BEGIN
-			SELECT STUFF(
-			(
-				SELECT '', 
-					   '' + CAST(ur.User_id AS VARCHAR)
-				FROM dbo.ccUsers_Roles ur
-					 INNER JOIN dbo.ccRoles C ON ur.Rol_id = C.Rol_id
-				WHERE c.Rol_id IN
-				(
-					SELECT fn_RIASplitDelimited.[value]
-					FROM dbo.fn_RIASplitDelimited(@Roles_id, '', '')
-				) FOR XML PATH('''')
-			), 1, 2, '''') AS Users_Ids;
-			IF OBJECT_ID('' tempdb..#roles_permissions '') IS NOT NULL
-				DROP TABLE #roles_permissions;
-			IF OBJECT_ID('' tempdb..#Users_Roles '') IS NOT NULL
-				DROP TABLE #Users_Roles;
-			SELECT DISTINCT
-				   (a.Rol_id)
+    END;
+    IF @action = 4 -- Delete Roles
+        BEGIN
+			IF OBJECT_ID(''tempdb..#UsersIds'') IS NOT NULL DROP TABLE #UsersIds
+			SET @resultado = STUFF(
+					(SELECT Distinct('', '' + CAST(ur.User_id AS varchar))
+					FROM ccUsers_Roles ur
+					INNER JOIN ccRoles C ON ur.Rol_id = C.Rol_id
+					WHERE c.Rol_id in (SELECT value FROM fn_RIASplitDelimited(@Roles_id, '',''))
+					FOR XML PATH ('''')),
+				1,2,'''')
+            IF OBJECT_ID(''tempdb..#roles_permissions'') IS NOT NULL DROP TABLE #roles_permissions
+			IF OBJECT_ID(''tempdb..#Users_Roles'') IS NOT NULL DROP TABLE #Users_Roles
+
+			SELECT DISTINCT(Rol_id)
 			INTO #roles_permissions
-			FROM dbo.ccroles_permissions a
-				 INNER JOIN
-			(
-				SELECT fn_RIASplitDelimited.[value]
-				FROM dbo.fn_RIASplitDelimited(@Roles_id, '', '')
-			) b ON b.[value] = a.Rol_Id;
-			SELECT DISTINCT
-				   (a.[value]) AS Rol_id
-			INTO #Users_Roles
-			FROM dbo.fn_RIASplitDelimited(@Roles_id, '', '') a
-				 INNER JOIN dbo.ccUsers_Roles b ON b.Rol_id = a.[Value]
-			WHERE b.Rol_id IS NOT NULL;
-			IF EXISTS
-			(
-				SELECT TOP 1 *
-				FROM #roles_permissions
-			)
-				BEGIN
-					--select * from #roles_permissions
-					DELETE dbo.ccroles_permissions
-					WHERE ccroles_permissions.Rol_id IN
-					(
-						SELECT ccroles_permissions.Rol_id
-						FROM #roles_permissions
-					);
-					SET @returnValue = 1;
-			END;
-			IF EXISTS
-			(
-				SELECT TOP 1 *
-				FROM #Users_Roles
-			)
-				BEGIN
-					--select * from #Users_Roles
-					DELETE dbo.ccUsers_Roles
-					WHERE ccUsers_Roles.Rol_id IN
-					(
-						SELECT #Users_Roles.Rol_id
-						FROM #Users_Roles
-					);
-					SET @returnValue = 1;
-			END;
-			IF EXISTS
-			(
-				SELECT TOP 1 ccRoles.Rol_id
-				FROM dbo.ccRoles
-				WHERE ccRoles.Rol_id IN
+				FROM ccroles_permissions a
+						INNER JOIN
 				(
-					SELECT DISTINCT
-						   (fn_RIASplitDelimited.[value])
-					FROM dbo.fn_RIASplitDelimited(@Roles_id, '', '')
-				)
-			)
-				BEGIN
-					--select * from ccRoles where Rol_id in (SELECT  DISTINCT(value) FROM fn_RIASplitDelimited(@Roles_id, ','))
-					DELETE dbo.ccRoles
-					WHERE ccRoles.Rol_id IN
-					(
-						SELECT DISTINCT
-							   (fn_RIASplitDelimited.[value])
-						FROM dbo.fn_RIASplitDelimited(@Roles_id, '', '')
-					);
-					SET @returnValue = 1;
-			END;
-	END;
+					SELECT value
+					FROM fn_RIASplitDelimited(@Roles_id, '','')
+				) b ON b.value = a.Rol_Id
+
+			SELECT  DISTINCT(value) AS Rol_id
+			INTO #Users_Roles
+			FROM fn_RIASplitDelimited(@Roles_id, '','') a
+					INNER JOIN ccUsers_Roles b ON b.Rol_id = a.Value
+			WHERE b.Rol_id IS NOT NULL
+			IF EXISTS(SELECT TOP 1 * FROM #roles_permissions)
+			BEGIN
+				--select * from #roles_permissions
+				DELETE ccroles_permissions WHERE Rol_id in (select Rol_id from #roles_permissions )
+			END
+			IF EXISTS(SELECT TOP 1 * FROM #Users_Roles)
+			BEGIN
+				--select * from #Users_Roles
+				DELETE ccUsers_Roles WHERE Rol_id in (select Rol_id from #Users_Roles )
+			END
+			IF EXISTS(select top 1 Rol_id from ccRoles where Rol_id in (SELECT  DISTINCT(value) FROM fn_RIASplitDelimited(@Roles_id, '','')))
+			BEGIN
+				--select * from ccRoles where Rol_id in (SELECT  DISTINCT(value) FROM fn_RIASplitDelimited(@Roles_id, '',''))
+				DELETE ccRoles WHERE Rol_id in (SELECT  DISTINCT(value) FROM fn_RIASplitDelimited(@Roles_id, '',''))
+				IF(@resultado IS NULL OR @resultado = '''') SET @resultado = ''1''
+			END
+		END;
     IF @action = 5 -- New Role
         BEGIN
             IF @menus_id <> ''''
                OR @Permissions_Id <> ''''
                 BEGIN
-                    DECLARE @exists BIT
-                    SET @returnValue = 0
-                    SET @exists = 1
+                    DECLARE @exists BIT;
+                    SET @returnValue = 0;
+                    SET @exists = 1;
 
 					/*IF @menus_id <> '''' --Check if role with same menus exists
 						BEGIN
@@ -867,17 +826,16 @@ BEGIN TRY
                                     FROM fn_RIASplitDelimited(@Permissions_Id, '','')
                                 )
                             )
-
-                                SET @exists = 0
-                    END
+                                SET @exists = 0;
+                    END;
                     IF @exists = 0 -- IF not exist role with same menus and permissions create
                         BEGIN
                             SELECT @exists = COUNT(*)
                             FROM ccRoles
-                            WHERE Description = @description
+                            WHERE Description = @description;
                             IF @exists = 0
                                 BEGIN
-                                    DECLARE @newRoleId INT
+                                    DECLARE @newRoleId INT;
                                     INSERT INTO ccroles
                                     (Description, 
                                      KeyJson, 
@@ -893,8 +851,8 @@ BEGIN TRY
                                      NEWID(), 
                                      @active,
 									 1001
-                                    )
-                                    SELECT @newRoleId = SCOPE_IDENTITY()
+                                    );
+                                    SELECT @newRoleId = SCOPE_IDENTITY();
                                     IF @Permissions_Id <> ''''
                                         BEGIN
                                             INSERT INTO ccroles_permissions
@@ -902,30 +860,37 @@ BEGIN TRY
                                                           value
                                                    FROM fn_RIASplitDelimited(@Permissions_Id, '','') AS a
                                                         INNER JOIN ccPermissions b ON a.value = b.Permissions_Id
-                                                   GROUP BY value
-                                    END
-                                    SET @returnValue = @newRoleId --  if new role was created return Role_id
-                            END
+                                                   GROUP BY value;
+                                    END;
+                                    SET @returnValue = @newRoleId; --  if new role was created return Role_id
+                            END;
                                 ELSE
                                 BEGIN
-                                    SET @returnValue = -1
-                            END-- else if role name exists, return -1
-                    END
-                    --SELECT @returnValue --  else if exists role with same menus & permissions, return 0
-            END
-    END
-    COMMIT TRANSACTION
+                                    SET @returnValue = -1;
+                            END;-- else if role name exists, return -1
+                    END;
+                    --SELECT @returnValue; --  else if exists role with same menus & permissions, return 0
+            END;
+    END;
+    COMMIT TRANSACTION;
+	if @resultado <>''''
+	begin
+		select @resultado
+	end
+	else
+	begin
     -- Indica que la operación se efectuo correctamente
-    SELECT @returnValue
+		SELECT @returnValue
+	end
 END TRY
 
 /* Manejo de error de la transacción */
 
 BEGIN CATCH
-	SET @returnValue = -1
+	SET @returnValue = -1;
     SELECT @returnValue
-    ROLLBACK TRANSACTION
-END CATCH'
+    ROLLBACK TRANSACTION;
+END CATCH;'
 		EXEC(@sql)
 		
 

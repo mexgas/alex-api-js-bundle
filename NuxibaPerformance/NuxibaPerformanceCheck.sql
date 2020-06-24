044b06f8fedcecf2b7fec41a9d30e9da1a38d250
Jesus Gallardo <agallardo@nuxiba.com>
2020-06-24 12:36:42 -0500

    Se modifica para se mas facil las busquedas

--- NuxibaPerformance/NuxibaPerformanceCheck.sql
+++ NuxibaPerformance/NuxibaPerformanceCheck.sql
@@ -1,7 +1,7 @@
-select A.nameProcess,B.nameProcess,C.nameProcess, A.IndividualQuery,A.ParentQuery,A.DatabaseName
+select CONVERT(date,A.dateStart) dateStart, A.nameProcess,B.nameProcess,C.nameProcess, A.IndividualQuery,A.ParentQuery,A.DatabaseName
 from Performance A 
 left join (select nameProcess,IndividualQuery,ParentQuery,DatabaseName from Performance where nameProcess='E/S') B
 on A.ParentQuery=B.ParentQuery
 left join (select nameProcess,IndividualQuery,ParentQuery,DatabaseName from Performance where nameProcess='CPU') C
 on A.ParentQuery=C.ParentQuery
-where A.nameProcess='Block'
+where A.nameProcess='Block' and (B.nameProcess is not null or C.nameProcess is not null) order by dateStart desc

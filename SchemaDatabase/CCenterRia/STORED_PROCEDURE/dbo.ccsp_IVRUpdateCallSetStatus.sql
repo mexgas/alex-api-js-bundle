2d0963df8a731598e8ca6c20ab35dadd7d721e58
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-06-25 02:30:11 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_IVRUpdateCallSetStatus.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_IVRUpdateCallSetStatus.sql
@@ -1,11 +1,15 @@
-CREATE procedure dbo.ccsp_IVRUpdateCallSetStatus
+CREATE procedure [dbo].[ccsp_IVRUpdateCallSetStatus]
 @cal_id int,
-@nStatus tinyint
+@nStatus tinyint,
+@userId int=0
 as
 set nocount on
 
 Update ccCallsIn SET cal_que=case @nStatus when 5 -- En Espera
-then 1 else cal_que end, statusCall_id=@nStatus where cal_id=@cal_id
+then 1 else cal_que end, statusCall_id=@nStatus
+,User_id= case when @userId >0 and User_id=0  then @userId else User_id end
+
+where cal_id=@cal_id
 
 exec ccsp_RIAUpdateCallBack_Abandon @cal_id, @nStatus
 

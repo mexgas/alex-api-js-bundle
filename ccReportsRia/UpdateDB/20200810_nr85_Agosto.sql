9a71256f283a8ac10d115373c8ac1b5e73d86264
rodSalazar <rsalazar@nuxiba.com>
2020-08-11 13:57:55 -0500

    Se cambia versión

--- ccReportsRia/UpdateDB/20200810_nr85_Agosto.sql
+++ ccReportsRia/UpdateDB/20200810_nr85_Agosto.sql
@@ -7,7 +7,7 @@ DECLARE @errorGenerated VARCHAR(max)
 DECLARE @process VARCHAR(max)
 
 /* Version to release (use the version of your own databse)*/
-SET @version = 84
+SET @version = 85
 
 /* Actual version (use your own script to do it) */
 EXEC @actualVersion = ccsp_getVersion 'BD'

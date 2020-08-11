0888a3f28bafa2e1044d7af40bbafadd384bd99a
ivanmartinenciso <imartin@nuxiba.com>
2020-08-11 13:10:10 -0500

    Se modifican sps para Twitter

--- CCenterRIA/UpdateDB/20200806_v122.21_Agosto.sql
+++ CCenterRIA/UpdateDB/20200806_v122.21_Agosto.sql
@@ -407,10 +407,172 @@ set nocount off
 		return(0)'
 		EXEC(@sql)
 
+		set @process = 'Twitter drop sp ccspADMaddConversationTweet'
+		set @sql = 'if exists (select * from sys.procedures where name = N''ccspADMaddConversationTweet'')
+		    begin
+		        DROP PROCEDURE ccspADMaddConversationTweet;
+		    end'
+		EXEC(@sql)
+
+		set @process = 'Modificación a sp ccspADMaddConversationTweet para regresar nombres correctos'
+		set @sql = 'CREATE PROCEDURE [dbo].[ccspADMaddConversationTweet]
+		@action int,
+		@inboundId int = null,
+		@clientId varchar(255)= null,
+		@isFinished bit = 0,
+		@screenNameClient varchar(100) = null,
+		@screenNameInbound varchar(100) = null,
+		@meanContactTypeId smallint = null,
+		@twitId varchar(255) = null,
+		@conversationId bigint = null,
+		@date datetime=null,
+		@replayId varchar(255)=null,
+		@tipoTwitId tinyint=1,
+		@messageId bigint = null,
+		@dispositionId smallint=0,
+		@subDispositionId smallint=0,
+		@tWrapUp int =0
+
+		as
+		set nocount on
+
+		declare @ninteration int ,@messageOutTwitterId bigint
+		declare @userId int
+		declare @isEndConversation bit
 
 
+		if @action = 1 begin --Revisa que exista la conversacion
+			select @conversationId =  isnull(max(conversationTwitterId),0) from conversationTwitter where isFinished = 0 and meanContactTypeId = 2 and ClientId = @clientId and inboundId=@inboundId
+			if @conversationId = 0
+				select cast(0 as bigint) as Id
+			else begin
+				declare @closeConversation tinyint
+				declare @tRsponse datetime
+				select @tRsponse = isnull(max(tSend),getdate()) from messageOutTwitter where conversationTwitterId = @conversationId
+				select @closeConversation = closeConversationTime from contactMeanIn where inboundId=@inboundId
+				 if datediff(dd,getdate(),@tRsponse ) > @closeConversation
+					select  cast(0 as bigint)  as Id
+				else
+					select @conversationId as Id
+			end
+		    return 0
+		end
+		else if @action = 2 begin --Nueva conversacion y mensaje entrada y salida
+		    --agregar tabla de messagetwit fecha de descarga
+			if @replayId is null or @replayId=''''
+				set @replayId= ''0''
+		    if NOT EXISTS (select * from messageInTwitter where twitId = @twitId) 
+			BEGIN
+				insert into conversationTwitter (inboundId,ClientId,isFinished,screenNameClient,screenNameInbound,meanContactTypeId,replayId)
+				values(@inboundId,@clientId,@isFinished,@screenNameClient,@screenNameInbound,@meanContactTypeId,@replayId)
+				set  @conversationId  = SCOPE_IDENTITY()
+			
+					insert into messageInTwitter(conversationTwitterId,tipoTwitId,twitId,[date]) values(@conversationId,@tipoTwitId,@twitId,@date)
+					set @messageId=SCOPE_IDENTITY()
+			
+				insert into messageOutTwitter(conversationTwitterId,messageStatusId,tipoTwitId,userId,[date],ninteration,messageInTwitterIdIni,messageInTwitterIdEnd)
+				values(@conversationId,1,@tipoTwitId,0,@date,1,@messageId,@messageId)
 
+			END
+		    select 0 as LastUserId,@conversationId as Id, @messageId as MessageId
+		    return 0
+		end
+		else if @action = 3 begin --Nuevo mensaje Entrada
+			---Revisa que no se contesto el twitt
+			select @messageOutTwitterId=max(A.messageOutTwitterId),@ninteration= count(B.messageInTwitterId)
+			from messageOutTwitter A inner join messageInTwitter B on A.conversationTwitterId=B.conversationTwitterId
+			where A.conversationTwitterId=@conversationId and A.messageStatusId not in (5,6,7,8,9,10,11)
+			
+			SELECT TOP 1  @messageId=messageInTwitterId from messageInTwitter where twitId = @twitId
+
+			IF @messageId is null 
+			BEGIN
+				insert into messageInTwitter(conversationTwitterId,tipoTwitId,twitId,[date]) values(@conversationId,@tipoTwitId,@twitId,@date)
+				set @messageId=SCOPE_IDENTITY()
+
+				if  @messageOutTwitterId is null begin
+					insert into messageOutTwitter(conversationTwitterId,messageStatusId,tipoTwitId,userId,[date],ninteration,messageInTwitterIdIni,messageInTwitterIdEnd)
+					values(@conversationId,1,@tipoTwitId,0,@date,1,@messageId,@messageId)
+					set @messageOutTwitterId=SCOPE_IDENTITY()
+				end
+				else begin
+					update messageOutTwitter set messageInTwitterIdEnd=@messageId,[date]=@date,ninteration=@ninteration
+					where messageOutTwitterId=@messageOutTwitterId
+				end
+			end
+			select @userId = userId  from messageOutTwitter with(nolock) where messageOutTwitterId=@messageOutTwitterId
+			select @userId as LastUserId,@conversationId as Id, @messageId as MessageId
+			return 0
+		end
+		else if @action = 4 begin --Obtiene el maximo messageOutTwitterId por conversacion
+		    select @messageOutTwitterId=max(messageOutTwitterId) from [messageOutTwitter] with(nolock) where conversationTwitterId=@conversationId
+			select @replayId=replayId from conversationTwitter where conversationTwitterId=@conversationId
+			select @messageOutTwitterId as messageOutTwitterId,@replayId as replayId
+			return 0
+		end
+		else if @action = 5 begin --Ultimo mensaje en por ACD
+		    select cast(isnull(max(twitId),0)as bigint) as Id, max(date) as Date from messageInTwitter as A
+			inner join conversationTwitter as B on A.conversationTwitterId=B.conversationTwitterId
+			where B.inboundId=@inboundId
+			return 0
+		end
+		else if @action = 6 begin --Obtiene conversación dependiendo del replayId
+			select @conversationId=conversationTwitterId  from messageOutTwitter where twitId=@replayId
+			if @conversationId is not null begin
+				select @replayId=replayId from conversationTwitter where conversationTwitterId=@conversationId
+			end
+			else begin
+				select 0 as conversationId,''0'' as replayId
+			end
+			select @conversationId as conversationId,@replayId as replayId
+			return 0
+		end
+
+		set nocount off'
+		EXEC(@sql)
+
+		set @process = 'Twitter drop COLUMN maxDownloadTweetsNumber'
+		set @sql = 'if not exists (select * from sys.columns where name = N''maxDownloadTweetsNumber'' and Object_ID = Object_ID(N''ccInbound''))
+	    begin
+	    	ALTER TABLE ccInbound ADD maxDownloadTweetsNumber INT DEFAULT 20;
+	    end'
+		EXEC(@sql)
 		
+		set @process = 'Twitter modify COLUMN maxDownloadTweetsNumber'
+		set @sql = 'if exists (select * from sys.columns where name = N''maxDownloadTweetsNumber'' and Object_ID = Object_ID(N''ccInbound''))
+	    begin
+			update ccInbound set maxDownloadTweetsNumber = 20  where addDataCallBackReminder = 0
+	    end'
+		EXEC(@sql)
+
+		set @process = 'Twitter drop sp ccspTwitterConfiguration'
+		set @sql = 'if exists (select * from sys.procedures where name = N''ccspTwitterConfiguration'')
+		    begin
+		        DROP PROCEDURE ccspTwitterConfiguration;
+		    end'
+		EXEC(@sql)
+
+		set @process = 'Twitter add sp ccspTwitterConfiguration'
+		set @sql = 'CREATE PROCEDURE [dbo].[ccspTwitterConfiguration]     @Option AS SMALLINT,
+												   @InboundId as INT
+		AS
+		BEGIN
+			set nocount on
+			IF @Option = 1   -- Get Campaigns Ids List Per Workgroup and Campaign Type 
+			BEGIN
+				IF @InboundId IS NOT NULL
+					BEGIN
+						SELECT maxDownloadTweetsNumber AS maxDownloadTweetsNumber FROM ccInbound WHERE Inbound_id = @InboundId 
+					END
+				ELSE
+					BEGIN
+						raiserror(''ERROR. No existe una campa?a de salida con el id especificado'', 18, 1)
+					END	
+			END
+		END'
+		EXEC(@sql)
+
+
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		-- exec ccsp_getVersion 'BD', @version

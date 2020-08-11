023d9f70681bd38a9dcfa8c17436dcae18faaef7
GabyGuevara <eguevara@nuxiba.com>
2020-08-11 10:17:01 -0500

    Se agrega tarea 4245

--- CCenterRIA/UpdateDB/20200806_v122.21_Agosto.sql
+++ CCenterRIA/UpdateDB/20200806_v122.21_Agosto.sql
@@ -407,7 +407,116 @@ set nocount off
 		return(0)'
 		EXEC(@sql)
 
+		set @process = 'CW-4245 -- Actualiza SP ccsp_RIAConfCamp'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAConfCamp]
+@User_id smallint
+AS
+set nocount on
+	declare @tableExistsRec table (camId int primary key,existRec bit)
 
+	insert into @tableExistsRec
+	select B.cam_id,case when count(A.cal_id) >0 then 1 else 0 end as existRec 
+	from dbo.fGet_CampAcd_Area (@User_id, 1) B
+	left join ccoCallsOut A on A.cam_id=B.cam_id
+	group by B.cam_id
+
+	select a1.cam_id, cam_Descripcion
+	, cam_tNotas, cast(cam_ocupado as int) as cam_ocupado, cam_noInt_ocupado, cam_inter_ocupado, cast(cam_nocontesto as int) as cam_nocontesto
+	, cam_noInt_nocontesto, cam_inter_nocontesto, cast(cam_fax as int) as cam_fax, cam_noInt_fax, cam_inter_fax
+	, cast(cam_modomanual as int) as cam_modomanual, ANI, cam_ShowCalifWnd, cam_StartTimerOnHangUp, editableCallKey, cam_tNoContesta, iTipoDial
+	, detectAnswerMachine, detectVoiceMail, compliance, cam_inter_graba, cam_noint_graba, cast(progDial as tinyint)progDial
+	, cast(excCallBack as tinyint)excCallBack, dialOrder, dialPrefix, dialPrefixMan, dialPrefixXfe, listenManualCall
+	, stopRecording, cast(abandonCallback as tinyint)abandonCallback, a3.frame, a1.t_autoCB, a1.id_anilist, a1.tDialonWrapUp, dbo.fn_viewMode(@User_id, 10) viewMode, 
+	cam_maxqueue as queSize,
+	DNCScrub, callerIdDesc, timeZoneRule, callsBySurvey, ivrScript, surveyPctg, isnull(a1.call_record,1) as call_record
+		,cast (startStopRecording as tinyint)startStopRecording, leaveRecMessage, manualCallOnChat
+	,callBackSurveyAgent,callBackSurveyClient,case when surveycamid is null or surveycamid = 0 then 0 else 1 end isRelationSurvey,isnull(a1.funcEspDtmf,0)
+	,isnull(sipHdrFormat, '''') sipHdrFormat
+	,cam_inter_cancelled
+	,prefijo,	enbleprefix = case when existRec = 0 then 1 else 0 end
+	from ccCamps a1 inner join ccRIACampsGraph a2 on (a1.cam_id=a2.cam_id)
+	inner join ccRIAGraphics a3 on (a2.graphic_id=a3.graphic_id)
+	inner join @tableExistsRec a4 on a1.cam_id=a4.camId
+	--where a1.cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+	order by cam_descripcion
+	return(0)
+	set nocount off'
+		EXEC(@sql)
+        
+        set @process = 'CW-4245 -- Actualiza SP ccsp_RIAConfEspec'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAConfEspec]
+@User_id int
+AS
+set nocount on
+/****
+Conexion Info Email In
+  protocol|server|ssl|port|cleanMail|revisionTime
+Conexion Info Email Out
+  serverOut|portOut|tls|sslOut
+Conexion Info Twitter
+  usuarioID|token|tokenSecret|time|daysTwitterRecord
+***/
+declare @tableExistsRec table (camId int primary key,existRec bit)
+
+insert into @tableExistsRec
+select I.cam_id,case when count(O.cal_id) >0 then 1 else 0 end as existRec 
+from dbo.fGet_CampAcd_Area (@User_id, 4) I
+left join ccCallsIn O on I.cam_id=O.inbound_id
+group by I.cam_id
+
+select  A.inbound_id, A.Descripcion, A.Status, A.tNotas,
+A.tMaxWaitCall, A.nMaxQue,tel_maxwait, A.tel_MaxQueue, A.tel_outservice, A.tel_noct, A.ShowCalifWnd,
+A.StartTimerOnHangUp, A.editableCallKey, A.queuePosition, A.tMaxQueueCallBack, A.stopRecording, A.dialPrefixOverflow,
+A.OpriorityT, A.callerIdDesc, A.chat mode, A.inactiveChatTime, A.maxChats, isnull(A.chatDomain,'''') chatDomain, A.chatQueueOverflow, A.chatTimeOverflow,
+isnull(A.startStopRecording,0) startStopRecording
+,isnull(B.name,'''') as nameMail,isnull(B.conexionInfo,'''') as conexionInfo,isnull(B.connUser,'''') as connUser,
+isnull(B.ConnPass,'''') as connPass,isnull(B.numMessages,3) as numMessages,isnull(B.timeAlertMessage,10)  as timeAlertMessage,
+isnull(B.IsActive,0) as Active, isnull(B.answerTimeOut,0) as answerTimeOut,
+case when A.cam_id > 0   and C.callsBySurvey>0 then A.callBackSurveyAgent else 0 end callBackSurveyAgent,
+case when A.cam_id > 0  and C.callsBySurvey>0 then A.callBackSurveyClient else 0 end callBackSurveyClient,
+case when A.cam_id > 0  and C.callsBySurvey>0 then 1 else 0 end isRelationSurvey,
+isnull(A.agts_notavailable,'''') as agts_notavailable,
+isnull(nameTwitter,'''') nameTwitter,isnull(userTwitter,'''') userTwitter,isnull(numMessagesTwitter,3) numMessagesTwitter,
+isnull(timeAlertMessageTwitter,10) timeAlertMessageTwitter,isnull(ActiveTwitter,0) ActiveTwitter,isnull(answerTimeOutTwitter,10) answerTimeOutTwitter,
+--usuarioID|token|tokenSecret|time|daysTwitterRecord
+isnull(conexionInfoTwitter,''usuarioID|token|tokenSecret|1|0'') conexionInfoTwitter
+,isnull(closeConversationTimeTwitter,3) closeConversationTimeTwitter,isnull(closeConversationTime,3) closeConversationTimeEmail
+,isnull(A.editableDtmf,0) as editableDtmf
+,isnull(gra.graphic_id,1) as frame
+,isnull(A.prefijo,'''') as prefijo
+,isnull(A.addDataCallBackReminder,0) as addDataCallBackReminder
+,isnull(Conv.hasMessage,0) as hasMessageMail
+, enbleprefix = case when R.existRec = 0 then 1 else 0 end
+from ccInbound A
+left join ccRIAInboundGraph gra on gra.Inbound_id=A.Inbound_id
+left join ContactMeanIn B on A.inbound_id=B.inboundId and B.meanContactTypeId=1
+left join ccCamps C on C.cam_id=A.cam_id
+left join(
+
+select GP.inboundId,case when count(*)>0 then 1 else 0 end hasMessage 
+ from (
+  select A.inboundId, A.conversationId, max(B.messageId) messageId  from conversation A 
+  inner join message B  on A.conversationId = B.conversationId  where A.isFinished=0
+    GROUP BY A.inboundId,A.conversationId
+  ) GP
+inner join message M on GP.messageId=M.messageId and messageStatusId not in(6,10,11,12,13)
+group by inboundId
+
+)  Conv on Conv.inboundId=A.inbound_id
+
+left join (
+select D.inboundId,
+D.name as nameTwitter,D.connUser as userTwitter,D.numMessages as numMessagesTwitter,
+D.timeAlertMessage as timeAlertMessageTwitter,
+D.IsActive as ActiveTwitter, D.answerTimeOut as answerTimeOutTwitter,D.conexionInfo as conexionInfoTwitter,
+closeConversationTime as  closeConversationTimeTwitter
+from ContactMeanIn D
+where D.meanContactTypeId=2) D on A.Inbound_id=D.inboundId
+inner join @tableExistsRec R on A.inbound_id=R.camId
+--where A.inbound_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 4))
+return(0)
+set nocount off'
+		EXEC(@sql)
 
 
 		

4a339787fbbaedb66fc21f5a97a7ad0325089d0a
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-08-26 02:30:30 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepInNotTransferred.sql
+++ SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepInNotTransferred.sql
@@ -10,42 +10,27 @@ select @to = getdate()
 
 if @action = 1
 begin
-
+	--1 as wgId
 	--Borrar lo que esta para no repetir
 	delete from RepInNotTransferred with(rowlock)
 	where date >= @from AND date < @to
 
 	insert into RepInNotTransferred
 	select a.cal_Inicio as [date], a.Inbound_id, 
-	'' as acd, statusCall_id, '' as statusCall,'' as statusCallCount,1 as [count],  b.IDArea, 
-	'' as area, 1 as wgId, 'systemTranslated_WorkGroup' as wg
+	b.descripcion as acd, a.statusCall_id, isnull(d.descripcion,'') as statusCall,isnull(d.descripcion,'')  + '_Count' as statusCallCount,1 as [count],  b.IDArea, 
+	isnull(Area.AreaName, '') as area, c.IDWG as wgId, isnull(c.WGName,'systemTranslated_WorkGroup') as wg
 	,datepart(yyyy,cal_inicio) as [year]
 	,datepart(mm,cal_inicio) as [mount]
 	,datepart(dd,cal_inicio) as [day]
 	,datepart(hh,cal_inicio) as [hour]
 	,datepart(mi,cal_inicio) as [minutes]
-	,0 as cal_id,isnull(a.cal_Ani,'') as phone_in
+	,a.cal_id as cal_id,isnull(a.cal_Ani,'') as phone_in
 	from cccallsin a 		
-	left join ccInbound b
-	on	b.Inbound_id = a.Inbound_id
-	where cal_inicio >= @from AND cal_inicio < @to and statuscall_id in (1,2,3,4,6,7,8)
-	and  b.IDArea is not null	
-
-	update a set acdGroup = isnull(descripcion,'')
-	from RepInNotTransferred a
-	left join ccInbound b 
-	on a.inboundId = b.Inbound_id
-	where [date] >= @from AND [date] < @to
-
-	update a set callStatus = isnull(descripcion,''), callStatus_Count = isnull(descripcion,'') + '_Count'
-	from RepInNotTransferred a
-	left join ccstatusllamada b 
-	on a.callStatusId = b.statusCall_id
-	where [date] >= @from AND [date] < @to
-
-	update a set area = isnull(AreaName,'')
-	from RepInNotTransferred a
-	left join ccRIACat_Areas b 
-	on a.areaId = b.IDArea
-	where [date] >= @from AND [date] < @to
+	inner join ccWgByAcdView b on a.Inbound_id=b.Inbound_id
+	left join ccRIACat_WorkGroup c on c.IDWG=b.IDWG
+	left join ccstatusllamada d	on a.statusCall_id = d.statusCall_id
+	left join ccRIACat_Areas Area on Area.IDArea=b.IDArea
+	where cal_inicio >= @from AND cal_inicio < @to and 
+	a.statuscall_id in (1,2,3,4,6,7,8)
+	and  b.IDArea is not null
 end

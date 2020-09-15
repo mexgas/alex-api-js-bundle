62b5225fab1bfe6981521c5bc2279abc0562fffc
GabyGuevara <eguevara@nuxiba.com>
2020-09-15 12:39:51 -0500

    Se modifican sp

--- CCenterRIA/UpdateDB/20200903_v122.22_Septiembre.sql
+++ CCenterRIA/UpdateDB/20200903_v122.22_Septiembre.sql
@@ -1300,6 +1300,278 @@ BEGIN CATCH
 END CATCH;'
 		EXEC(@sql)
 
+set @process = 'CW-4372 se modifica sp ccsp_IVRUpdateCallEndNew'
+		set @sql = 'ALTER procEDURE [dbo].[ccsp_IVRUpdateCallEndNew]
+@cal_id int,
+@cal_tIVRCallDuration smallint,
+@statuscal_id tinyint, 
+-- Aqui solo se Aceptan Edos Terminales 2(Fuera de Horario), 3(Fuera de Servicio), 4(NoAgentesFirmados), 7(TimeOut), 8(DesbordeQue),
+@cal_opciones varchar(10),
+@cal_colgada tinyint,
+@User_id smallint,
+@cal_extension varchar(7),
+@tWait smallint
+AS
+set nocount on
+
+
+
+Update ccCallsIn SET statusCall_id = case when @statuscal_id in (2, 3, 4, 7, 8) then @statuscal_id else case when statusCall_id = 5 then 6 else statuscall_id end end, 
+ user_id= case when user_id=0 and @User_id>0 then @User_id else user_id end, cal_extension= case when cal_extension=0 and @cal_extension>0 then @cal_extension else cal_extension end, cal_tWait=@tWait where cal_id=@cal_id
+
+ 
+
+exec ccsp_RIAUpdateCallBack_Abandon @cal_id, @statuscal_id
+
+--Actualizar tiempo total de llamada
+exec ccsp_EngineLogTransfers 2, @cal_id, 2, 2, null, @tWait, @cal_tIVRCallDuration
+
+set nocount off'
+		EXEC(@sql)
+
+
+set @process = 'CW-4372 Se modifica sp ccsp_IVRAfterXferAge'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_IVRAfterXferAge]
+@cal_id int,
+@User_id smallint,
+@cal_extension varchar(7),
+@tWait smallint
+AS
+set nocount on
+-- 2005-11-15 por ODC
+-- colocar como asignada despues de transferir
+Update ccCallsIn SET user_id= case when user_id=0 and @User_id>0 then @User_id else user_id end, cal_extension= case when cal_extension=0 and @cal_extension>0 then @cal_extension else cal_extension end,-- cal_tWait=@tWait,
+cal_xfer=getdate(), statusCall_id=11  -- 11=Asignada
+where cal_id=@cal_id
+
+update ccRIAWorkGroup_Calid set user_id=@User_id where cal_id = @cal_id and tipo = 0
+
+return(0)
+set nocount off'
+		EXEC(@sql)
+
+
+set @process = 'CW-4372 se modifica sp ccsp_AgentSetCallStatus'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_AgentSetCallStatus] 
+	@callout_id INT, 
+    @cal_id     INT, 
+    @TipoCall   TINYINT, -- 1= IN,  2=Out
+    @TipoMov    TINYINT, -- 4 OnDialog, 7=OFFHook_OnXfer, 9=CallNoAnswered
+    @cal_tXfer  TINYINT    = 0, 
+    @cal_tring  SMALLINT   = 0, 
+    @user_id    SMALLINT   = 0, 
+    @extension  VARCHAR(5) = '''', 
+    @isChatCall BIT        = 0
+AS
+     SET NOCOUNT ON
+     DECLARE @RecicleSIC TINYINT
+
+     SELECT @RecicleSIC = ISNULL(valor, 0)
+     FROM ccSettings
+     WHERE setting_id = 60
+
+     DECLARE @ANI_x VARCHAR(19)
+     DECLARE @cal_inicio DATETIME
+     DECLARE @callout_id_IN INT
+     DECLARE @cal_key VARCHAR(20)
+     DECLARE @cam_id INT
+     DECLARE @cal_telefono VARCHAR(30)
+     DECLARE @surveycamid INT
+     DECLARE @inbound_id INT
+     IF @TipoMov = 4 OR @TipoMov = 14 -- DIALOG OnDialog
+         BEGIN
+             IF @TipoCall = 2
+                 BEGIN
+                     IF @TipoMov = 4
+                         BEGIN
+                             UPDATE ccoCallsOUT WITH(ROWLOCK)
+                               SET cal_Inicio = GETDATE(), 
+                                   statusCall_id = 13, 
+                                   cal_manual = CASE
+                                                    WHEN @isChatCall = 1
+                                                    THEN 3
+                                                    ELSE cal_manual
+                                                END
+                             WHERE cal_id = @cal_id
+                     END
+                         ELSE
+                         IF @TipoMov = 14
+                             UPDATE ccoCallsOUT WITH(ROWLOCK)
+                               SET statusCall_id = 13, 
+                                   cal_tRing = @cal_tring, 
+                                   user_id = case when user_id=0 and @user_id>0 then @user_id else user_id end, 
+                                   cal_extension = case when cal_extension=0 and @extension>0 then @extension else cal_extension end
+                             WHERE cal_id = @cal_id
+                     IF @RecicleSIC = 0
+                         BEGIN
+                             DELETE ccoWorkingTable WITH(ROWLOCK)
+                             WHERE callout_id = @callout_id
+
+                             DELETE ccoCallPriorityOrder WITH(ROWLOCK)
+                             WHERE callout_id = @callout_id
+                     END
+                     UPDATE ccoCallBacks
+                       SET [status] = 1, 
+                           schedulerStatus = 1, 
+                           cal_fcallback = cal_inicio
+                     FROM ccoCallBacks a WITH (INDEX(IX_ccoCallBacks), NOLOCK), ccoCallsOUT b WITH (INDEX(IX_ccoCallsOut_11), NOLOCK)
+                     WHERE a.callout_id = b.callout_id
+                           AND b.callout_id = @callout_id
+                           AND b.cal_id = @cal_id
+                           AND [status] = 0
+                           AND statusCall_id = 13
+
+                     -- calcula el costo de la llamada
+                     EXEC ccsp_CstoCalculaCosto @cal_id
+
+                     RETURN(0)
+             END
+             IF @TipoMov = 4
+                 UPDATE ccCallsIN WITH(ROWLOCK)
+                   SET statusCall_id = 13
+                 WHERE cal_id = @cal_id
+
+                 ELSE
+                 IF @TipoMov = 14
+                     UPDATE ccCallsIN WITH(ROWLOCK)
+                       SET statusCall_id = 13, 
+                           cal_tRing = @cal_tring, 
+                           user_id = case when user_id=0 and @user_id>0 then @user_id else user_id end, 
+                           cal_extension = case when cal_extension=0 and @extension>0 then @extension else cal_extension end
+                     WHERE cal_id = @cal_id
+
+             -- Elimina callback generado por abandono
+             SELECT @ANI_x = cal_ani, 
+                    @cal_inicio = cal_inicio
+             FROM cccallsin WITH (INDEX(PK_ccCallsIn))
+             WHERE cal_id = @cal_id
+
+             SELECT @callout_id_IN = callout_id
+             FROM ccRIAUpdateCallBack_Abandon
+             WHERE cal_ani = @ANI_x
+
+             UPDATE ccoCallBacks WITH(ROWLOCK)
+               SET [status] = 1, 
+                   schedulerStatus = 1, 
+                   cal_fcallback = @cal_inicio
+             WHERE callout_id = @callout_id_IN
+                   AND [status] = 0
+
+             DELETE ccoWorkingTable WITH(ROWLOCK)
+             WHERE callout_id IN
+             (
+                 SELECT DISTINCT
+                        (callout_id)
+                 FROM ccRIAUpdateCallBack_Abandon WITH(ROWLOCK)
+                 WHERE cal_ani = @ANI_x
+             )
+
+             DELETE ccRIAUpdateCallBack_Abandon WITH(ROWLOCK)
+             WHERE cal_ANI = @ANI_x
+
+             RETURN(0)
+     END
+     IF @TipoMov = 7 --OTHER OFFHook_OnXfer
+         BEGIN
+             IF @cal_id <= 0
+                 RETURN(0)
+             IF @TipoCall = 2
+                 BEGIN
+                     UPDATE ccoCallsOUT WITH(ROWLOCK)
+                       SET statusCall_id = 16
+                     WHERE cal_id = @cal_id
+                     -- calcula el costo de la llamada
+
+                     UPDATE ccoCallBacks
+                       SET [status] = 2, 
+                           schedulerStatus = 1, 
+                           cal_fcallback = cal_inicio
+                     FROM ccoCallBacks a WITH (INDEX(IX_ccoCallBacks), NOLOCK), ccoCallsOUT b WITH (INDEX(IX_ccoCallsOut_11), NOLOCK)
+                     WHERE a.callout_id = b.callout_id
+                           AND b.callout_id = @callout_id
+                           AND b.cal_id = @cal_id
+                           AND [status] = 0
+                           AND statusCall_id = 16
+
+                     EXEC ccsp_CstoCalculaCosto 
+                          @cal_id
+
+                     RETURN(0)
+             END
+             UPDATE ccCallsIN WITH(ROWLOCK)
+               SET statusCall_id = 16
+             WHERE cal_id = @cal_id
+
+             SELECT @ANI_x = cal_ani, 
+                    @cal_inicio = cal_inicio
+             FROM cccallsin WITH (INDEX(PK_ccCallsIn))
+             WHERE cal_id = @cal_id
+
+             SELECT @callout_id_IN
+             FROM ccRIAUpdateCallBack_Abandon
+             WHERE cal_ani = @ANI_x
+
+             UPDATE ccoCallBacks WITH(ROWLOCK)
+               SET [status] = 2, 
+                   schedulerStatus = 1, 
+                   cal_fcallback = @cal_inicio
+             WHERE callout_id = @callout_id_IN
+                   AND [status] = 0
+
+             RETURN(0)
+     END
+     IF @TipoMov = 9 --RING CallNoAnswered
+         BEGIN
+             IF @TipoCall = 2
+                 BEGIN
+                     UPDATE ccoCallsOUT WITH(ROWLOCK)
+                       SET statusCall_id = 15, 
+                           cal_tXFer = @cal_txFer, 
+                           cal_tRing = @cal_tring
+                     WHERE cal_id = @cal_id
+
+                     -- calcula el costo de la llamada
+
+                     UPDATE ccoCallBacks
+                       SET [status] = 2, 
+                           schedulerStatus = 1, 
+                           cal_fcallback = cal_inicio
+                     FROM ccoCallBacks a WITH (INDEX(IX_ccoCallBacks), NOLOCK), ccoCallsOUT b WITH (INDEX(IX_ccoCallsOut_11), NOLOCK)
+                     WHERE a.callout_id = b.callout_id
+                           AND b.callout_id = @callout_id
+                           AND b.cal_id = @cal_id
+                           AND [status] = 0
+                           AND statusCall_id = 15
+
+                     EXEC ccsp_CstoCalculaCosto @cal_id
+             END
+
+             UPDATE ccCallsIN WITH(ROWLOCK)
+               SET statusCall_id = 15, 
+                   cal_tXFer = @cal_txFer, 
+                   cal_tRing = @cal_tring
+             WHERE cal_id = @cal_id
+
+             SELECT @ANI_x = cal_ani, 
+                    @cal_inicio = cal_inicio
+             FROM cccallsin WITH (INDEX(PK_ccCallsIn))
+             WHERE cal_id = @cal_id
+
+             SELECT @callout_id_IN
+             FROM ccRIAUpdateCallBack_Abandon
+             WHERE cal_ani = @ANI_x
+
+             UPDATE ccoCallBacks WITH(ROWLOCK)
+               SET [status] = 2, 
+                   schedulerStatus = 1, 
+                   cal_fcallback = @cal_inicio
+             WHERE callout_id = @callout_id_IN
+                   AND [status] = 0
+
+             RETURN(0)
+     END
+     SET NOCOUNT OFF'
+		EXEC(@sql)
 
 
 

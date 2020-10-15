678b62240459c8852c5b0b12e4d70ecf9f7ecc46
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-10-15 02:30:10 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccspAgent_GetLastCalls.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccspAgent_GetLastCalls.sql
@@ -1,61 +1,94 @@
-CREATE PROCEDURE [dbo].[ccspAgent_GetLastCalls] @user_id int AS
-set nocount on
-
-declare @lastCallAgt table(
-id int not null,
-tipo varchar(10) not null,
-Hora varchar(10) not null,
-Telefono varchar(55) not null,
-EspCamp varchar(55) not null,
-Calificacion varchar(60),
-Duracion varchar(10) not null,
-CallBack varchar(60),
-cal_key varchar(20),
-IDCampEsp smallint not null,
-prefijo varchar(maX) null,
-GraphicID int 
-)
-insert into @lastCallAgt
-select top 10 c.cal_id as id, 'IN' as Tipo, convert(varchar(10), cal_inicio, 108) as Hora, cal_ani as Telefono, descripcion as EspCamp, 
-isnull(cal.Description, '') as Calificacion, 
-convert(varchar(14), dateadd(second, 
-cal_tDialog - cal_tMoh 
-    +  case when stopRecording=0 then isnull( t.tDespuesXfer ,0) else 0 end
-,0), 108) Duracion,
-'' as CallBack, cal_key, c.inbound_id as IDCampEsp,ISNULL(ccInbound.prefijo,'') Prefijo,
-graph.graphic_id GraphicID
-from ccCallsIn c with(nolock index(IX_ccCallsIn_4)) 
-join ccRIAInboundGraph graph on graph.Inbound_id = c.Inbound_id
-inner join ccInbound on ccInbound.Inbound_id=c.Inbound_id
-left join ccTipoCalif cal on c.calif_id = cal.calif_id
-left join 
-(select cal_id,tipo,sum(tAntesXfer) as tAntesXfer,sum(tDespuesXfer) as tDespuesXfer  from ccLogTransfers where tipo=1  group by cal_id,tipo ) as t  
- on c.cal_id=t.cal_id 
-where user_id = @user_id and cal_inicio > dateadd(hh, -3, getdate())
-order by c.cal_inicio desc
-
-
-insert into @lastCallAgt
-select top 10 c.cal_id as id, 'OUT' as Tipo,convert(varchar(10), cal_inicio, 108) as Hora,cal_telefono as Telefono,cam_descripcion as EspCamp, 
-isnull(cal.Description, '') as Calificacion, 
- CONVERT(varchar(8), DATEADD(ss, 
-    cal_tDialog - cal_tMoh 
-    +  case when stopRecording=0 then isnull( t.tDespuesXfer ,0) else 0 end
-    , 0), 114)  as Duracion,
-    isnull(convert(varchar(16), cal_fcallback, 121) ,'') as CallBack, cal_key, c.cam_id as IDCampEsp , ISNULL(ccCamps.prefijo,'') Prefijo,
-	graph.graphic_id GraphicID
-from ccoCallsOut c
-inner join ccCamps on ccCamps.cam_id=c.cam_id
-left join ccRIACampsGraph graph on graph.cam_id = c.cam_id
-left join ccTipoCalifOut cal on c.calif_id = cal.calif_id
-left join  
-(select cal_id,tipo,sum(tAntesXfer) as tAntesXfer,sum(tDespuesXfer) as tDespuesXfer from ccLogTransfers where tipo=2 group by cal_id,tipo) as t  
-on c.cal_id=t.cal_id 
-
-where user_id = @user_id and cal_inicio > dateadd(hh, -3, getdate())
-order by c.cal_inicio desc
-
-select * from @lastCallAgt
-order by hora desc
-
-set nocount off
+CREATE PROCEDURE [dbo].[ccspAgent_GetLastCalls] @user_id INT
+AS
+     SET NOCOUNT ON
+     DECLARE @lastCallAgt TABLE
+     (id           INT NOT NULL, 
+      tipo         VARCHAR(10) NOT NULL, 
+      Hora         VARCHAR(10) NOT NULL, 
+      Telefono     VARCHAR(55) NOT NULL, 
+      EspCamp      VARCHAR(55) NOT NULL, 
+      Calificacion VARCHAR(60), 
+      Duracion     VARCHAR(10) NOT NULL, 
+      CallBack     VARCHAR(60), 
+      cal_key      VARCHAR(20), 
+      IDCampEsp    SMALLINT NOT NULL, 
+      prefijo      VARCHAR(MAX) NULL, 
+      GraphicID    INT,
+	  HidePhone	   bit
+     )
+     INSERT INTO @lastCallAgt
+            SELECT TOP 10 c.cal_id AS id, 
+                          'IN' AS Tipo, 
+                          CONVERT(VARCHAR(10), cal_inicio, 108) AS Hora, 
+                          cal_ani AS Telefono, 
+                          descripcion AS EspCamp, 
+                          ISNULL(cal.Description, '') AS Calificacion, 
+                          CONVERT(VARCHAR(14), DATEADD(second, cal_tDialog - cal_tMoh + CASE
+                                                                                            WHEN stopRecording = 0
+                                                                                            THEN ISNULL(t.tDespuesXfer, 0)
+                                                                                            ELSE 0
+                                                                                        END, 0), 108) Duracion, 
+                          '' AS CallBack, 
+                          cal_key, 
+                          c.inbound_id AS IDCampEsp, 
+                          ISNULL(ccInbound.prefijo, '') Prefijo, 
+                          graph.graphic_id GraphicID,
+						  case when (select valor from ccSettings where setting_id = 223) = '0' then 0 else 1 end as HidePhone
+            FROM ccCallsIn c WITH (NOLOCK INDEX(IX_ccCallsIn_4))
+                 JOIN ccRIAInboundGraph graph ON graph.Inbound_id = c.Inbound_id
+                 INNER JOIN ccInbound ON ccInbound.Inbound_id = c.Inbound_id
+                 LEFT JOIN ccTipoCalif cal ON c.calif_id = cal.calif_id
+                 LEFT JOIN
+            (
+                SELECT cal_id, 
+                       tipo, 
+                       SUM(tAntesXfer) AS tAntesXfer, 
+                       SUM(tDespuesXfer) AS tDespuesXfer
+                FROM ccLogTransfers
+                WHERE tipo = 1
+                GROUP BY cal_id, 
+                         tipo
+            ) AS t ON c.cal_id = t.cal_id
+            WHERE user_id = @user_id
+                  AND cal_inicio > DATEADD(hh, -3, GETDATE())
+            ORDER BY c.cal_inicio DESC
+     INSERT INTO @lastCallAgt
+            SELECT TOP 10 c.cal_id AS id, 
+                          'OUT' AS Tipo, 
+                          CONVERT(VARCHAR(10), cal_inicio, 108) AS Hora, 
+                          cal_telefono AS Telefono, 
+                          cam_descripcion AS EspCamp, 
+                          ISNULL(cal.Description, '') AS Calificacion, 
+                          CONVERT(VARCHAR(8), DATEADD(ss, cal_tDialog - cal_tMoh + CASE
+                                                                                       WHEN stopRecording = 0
+                                                                                       THEN ISNULL(t.tDespuesXfer, 0)
+                                                                                       ELSE 0
+                                                                                   END, 0), 114) AS Duracion, 
+                          ISNULL(CONVERT(VARCHAR(16), cal_fcallback, 121), '') AS CallBack, 
+                          cal_key, 
+                          c.cam_id AS IDCampEsp, 
+                          ISNULL(ccCamps.prefijo, '') Prefijo, 
+                          graph.graphic_id GraphicID,
+						  case when (select valor from ccSettings where setting_id = 223) = '0' then 0 else 1 end as HidePhone
+            FROM ccoCallsOut c
+                 INNER JOIN ccCamps ON ccCamps.cam_id = c.cam_id
+                 LEFT JOIN ccRIACampsGraph graph ON graph.cam_id = c.cam_id
+                 LEFT JOIN ccTipoCalifOut cal ON c.calif_id = cal.calif_id
+                 LEFT JOIN
+            (
+                SELECT cal_id, 
+                       tipo, 
+                       SUM(tAntesXfer) AS tAntesXfer, 
+                       SUM(tDespuesXfer) AS tDespuesXfer
+                FROM ccLogTransfers
+                WHERE tipo = 2
+                GROUP BY cal_id, 
+                         tipo
+            ) AS t ON c.cal_id = t.cal_id
+            WHERE user_id = @user_id
+                  AND cal_inicio > DATEADD(hh, -3, GETDATE())
+            ORDER BY c.cal_inicio DESC
+     SELECT *
+     FROM @lastCallAgt
+     ORDER BY hora DESC
+     SET NOCOUNT OFF

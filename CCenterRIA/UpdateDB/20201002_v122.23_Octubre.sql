61a8217783664d47e174e33b8b42f207fa24ff1a
uespinosa01 <uespinosa@nuxiba.com>
2020-10-20 15:15:18 -0500

    Se agegan los permisos para las cubetas de campañas

--- CCenterRIA/UpdateDB/20201002_v122.23_Octubre.sql
+++ CCenterRIA/UpdateDB/20201002_v122.23_Octubre.sql
@@ -679,7 +679,237 @@ AS
      ORDER BY hora DESC
      SET NOCOUNT OFF'
 		EXEC(@sql)
+
+		set @process = 'CW-4256 Agregar permiso cubetas'
+		set @sql = 'IF NOT EXISTS
+(
+    SELECT Permissions_Id
+    FROM ccPermissions
+    WHERE Permissions_Id = 10005
+)
+    BEGIN
+        INSERT INTO ccPermissions
+        VALUES
+        (10005, 
+         ''Eliminar nuevos registros|Delete new records'', 
+         ''RolesPermissionDeleteNews'', 
+         0, 
+         0, 
+         0, 
+         ''N/A'', 
+         1
+        )
+END
+IF NOT EXISTS
+(
+    SELECT Permissions_Id
+    FROM ccPermissions
+    WHERE Permissions_Id = 10006
+)
+    BEGIN
+        INSERT INTO ccPermissions
+        VALUES
+        (10006, 
+         ''Devolucion de llamada|CallBacks'', 
+         ''RolesPermissionCallBacks'', 
+         0, 
+         0, 
+         0, 
+         ''N/A'', 
+         1
+        )
+END
+IF NOT EXISTS
+(
+    SELECT rol_id
+    FROM ccRoles_Permissions
+    WHERE Rol_Id = 1
+          AND Permissions_id = 10005
+)
+    BEGIN
+        INSERT INTO ccRoles_Permissions
+        VALUES
+        (1, 
+         10005
+        )
+END
+IF NOT EXISTS
+(
+    SELECT rol_id
+    FROM ccRoles_Permissions
+    WHERE Rol_Id = 1
+          AND Permissions_id = 10006
+)
+    BEGIN
+        INSERT INTO ccRoles_Permissions
+        VALUES
+        (1, 
+         10006
+        )
+END'
+
+		EXEC(@sql)
+
+		set @process = 'Agregar asignadas y atendidas'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_OUTGetCallsInfo_AllCamps]
+		@Tipo as tinyint= 1,
+		@cam_id as smallint = 0,
+		@sup_id as smallint= 0
+		AS
+
+		declare @mToday as smalldatetime
 		
+		select @mToday = convert(smalldatetime, convert(varchar(11), getdate() ), 101)
+		if @Tipo = 0
+		begin
+		  SELECT cam_id, cam_descripcion,
+		    0 as pContesta,
+		    0 as pOcupado,
+		    0 as pNoContesta,
+		    0 as pFaxModem,
+		    0 as pNoService,
+		    0 as Marcaciones, 0 as Contestan,  0 as Ocupado, 0 as NoContesta, 0 as FaxModem, 0 as NoService
+		  FROM ccCamps
+		  order by cam_id
+		end
+
+		else if @Tipo = 1
+		begin
+		  select L.cam_id, L.Campana,
+		    ((L.Contestan*100)/ L.Marcaciones) as pContesta,
+		    ((L.Ocupado*100)/ L.Marcaciones) as pOcupado,
+		    ((L.NoContesta*100)/ L.Marcaciones) as pNoContesta,
+		    ((L.FaxModem*100)/ L.Marcaciones) as pFaxModem,
+		    ((L.NoService*100)/ L.Marcaciones) as pNoService,
+		    L.Marcaciones, L.Contestan, L.Ocupado, L.NoContesta, L.FaxModem, L.NoService
+		    ,L.Otro,L.Cancelado,L.buzon,L.NoDialTone,L.congestion
+			,isnull(Assigned,0) As Assigned,isnull(Attended,0) As Attended
+		  from (
+		  select cam_id, '''' as Campana,
+		    count(case tipoResDial_id when 1 then 1 else null end) as Contestan,
+		    count(case tipoResDial_id when 2 then 1 else null end) as Ocupado,
+		    count(case tipoResDial_id when 3 then 1 else null end) as NoContesta,
+		    count(case tipoResDial_id when 4 then 1 else null end) as FaxModem,
+		    count(case tipoResDial_id when 10 then 1 else null end) as NoService,
+		    count(*) as Marcaciones
+		    ,count(case when tipoResDial_id= 8  or tipoResDial_id> 13 then 1   else null end) as Otro
+		    ,count(case tipoResDial_id when 13 then 1 else null end) as Cancelado
+		    ,count(case tipoResDial_id when 11 then 1 else null end) as buzon
+		    ,count(case tipoResDial_id when 5 then 1 else null end) as NoDialTone
+		    ,count(case tipoResDial_id when 12 then 1 else null end) as congestion
+
+		  from ccoLogDials with(nolock)
+		  Where fecha >  @mToday
+		  group by cam_id
+		  ) L 
+		  left join (select 
+		    cam_id
+		    ,count(case statuscall_id when 6 then 1 else null end) as Abandon
+		    ,count(*) as Contesta
+			,count(case when statuscall_id in(11, 12,15,16)  then 1 else null end) as [Assigned]
+			,count(case statuscall_id when 13 then 1 else null end) as [Attended]
+		  from ccoCallsOut with(nolock index(IX_ccoCallsOut_2))
+		  where cal_Inicio > @mToday
+		  group by cam_id) callsOut on L.cam_id = callsOut.cam_id
+		  
+		  order by Campana
+
+		end
+
+		else if @Tipo = 2
+		begin
+		  select cam_id, L.Campana,
+		    ((L.Contestan*100)/ L.Marcaciones) as pContesta,
+		    ((L.Ocupado*100)/ L.Marcaciones) as pOcupado,
+		    ((L.NoContesta*100)/ L.Marcaciones) as pNoContesta,
+		    ((L.FaxModem*100)/ L.Marcaciones) as pFaxModem,
+		    ((L.NoService*100)/ L.Marcaciones) as pNoService,
+		    L.Marcaciones, L.Contestan, L.Ocupado, L.NoContesta, L.FaxModem, L.NoService
+		  from (
+		  select C.cam_id as cam_id, cam_descripcion as Campana,
+		    count(case tipoResDial_id when 1 then 1 else null end) as Contestan,
+		    count(case tipoResDial_id when 2 then 1 else null end) as Ocupado,
+		    count(case tipoResDial_id when 3 then 1 else null end) as NoContesta,
+		    count(case tipoResDial_id when 4 then 1 else null end) as FaxModem,
+		    count(case tipoResDial_id when 10 then 1 else null end) as NoService,
+		    count(*) as Marcaciones
+		  from ccoLogDials L with(nolock)
+		  inner join ccCamps C on L.cam_id=C.cam_id
+		  Where fecha >  @mToday
+		  group by C.cam_id, cam_descripcion
+		  ) L order by Campana
+		end
+
+		else if @Tipo = 3 --Busqueda por campaña
+		begin
+		  select L.cam_id,
+		    L.Calls, L.Answer, L.Busy, L.NoAnswer, L.Fax, L.NoService
+		    ,L.Other,L.Canceled,L.Machine,L.NoTone,L.Congestion, isnull(callsOut.Abandon,0) as Abandon
+		  from (
+		  select cam_id,
+		    count(case tipoResDial_id when 1 then 1 else null end) as Answer,
+		    count(case tipoResDial_id when 2 then 1 else null end) as Busy,
+		    count(case tipoResDial_id when 3 then 1 else null end) as NoAnswer,
+		    count(case tipoResDial_id when 4 then 1 else null end) as Fax,
+		    count(case tipoResDial_id when 10 then 1 else null end) as NoService,
+		    count(*) as Calls
+		    ,count(case when tipoResDial_id= 8  or tipoResDial_id> 13 then 1   else null end) as Other
+		    ,count(case tipoResDial_id when 13 then 1 else null end) as Canceled
+		    ,count(case tipoResDial_id when 11 then 1 else null end) as Machine
+		    ,count(case tipoResDial_id when 5 then 1 else null end) as NoTone
+		    ,count(case tipoResDial_id when 12 then 1 else null end) as Congestion
+
+		  from ccoLogDials with(nolock)
+		  Where cam_id = @cam_id
+		  and fecha >  @mToday
+		  group by cam_id
+		  ) L 
+		  left join (select 
+		    cam_id,
+		    count(case statuscall_id when 6 then 1 else null end) as Abandon,
+		    count(*) as Contesta    
+		  from ccoCallsOut with(nolock index(IX_ccoCallsOut_2))
+		  where cal_Inicio > @mToday
+		  group by cam_id) callsOut on L.cam_id = callsOut.cam_id
+
+		end
+
+		else if @Tipo = 4-- Busqueda por campañas asociadas a admin
+		begin
+		  select L.cam_id,
+		    L.Calls, L.Answer, L.Busy, L.NoAnswer, L.Fax, L.NoService
+		    ,L.Other,L.Canceled,L.Machine,L.NoTone,L.Congestion, isnull(callsOut.Abandon,0) as Abandon
+			,isnull(Assigned,0) As Assigned,isnull(Attended,0) As Attended
+		  from (
+		  select logDials.cam_id,
+		    count(case tipoResDial_id when 1 then 1 else null end) as Answer,
+		    count(case tipoResDial_id when 2 then 1 else null end) as Busy,
+		    count(case tipoResDial_id when 3 then 1 else null end) as NoAnswer,
+		    count(case tipoResDial_id when 4 then 1 else null end) as Fax, 
+		    count(case tipoResDial_id when 10 then 1 else null end) as NoService,
+		    count(*) as Calls
+		    ,count(case when tipoResDial_id= 8  or tipoResDial_id> 13 then 1   else null end) as Other
+		    ,count(case tipoResDial_id when 13 then 1 else null end) as Canceled
+		    ,count(case tipoResDial_id when 11 then 1 else null end) as Machine
+		    ,count(case tipoResDial_id when 5 then 1 else null end) as NoTone
+		    ,count(case tipoResDial_id when 12 then 1 else null end) as Congestion
+		  from ccoLogDials logDials with(nolock)
+		  right join (select distinct cam_id from ccSupervisorCam supCam where user_id=@sup_id) B ON logDials.cam_id = B.cam_id
+		  Where fecha >  @mToday
+		  group by logDials.cam_id
+		  ) L 
+		  left join (select 
+		    cam_id
+		    ,count(case statuscall_id when 6 then 1 else null end) as Abandon
+		    ,count(*) as Contesta
+			,count(case when statuscall_id in(11, 12,15,16)  then 1 else null end) as [Assigned]
+			,count(case statuscall_id when 13 then 1 else null end) as [Attended]
+		  from ccoCallsOut with(nolock index(IX_ccoCallsOut_2))
+		  where cal_Inicio > @mToday
+		  group by cam_id) callsOut on L.cam_id = callsOut.cam_id
+		  order by L.cam_id
+		end'
+		EXEC(@sql)
 		
 
 		/* End script release */

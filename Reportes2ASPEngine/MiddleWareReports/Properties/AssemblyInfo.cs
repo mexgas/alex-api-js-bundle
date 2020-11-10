9227ac68c2f90c51d58e3b14e9ae243504a8999e
Jesus Gallardo <agallardo@nuxiba.com>
2020-11-10 10:46:12 -0600

    Se cambia la version 2020.1011.123.1201

--- Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
+++ Reportes2ASPEngine/MiddleWareReports/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.0615.122.1801")]
-[assembly: AssemblyFileVersion("2020.0615.122.1801")]
+[assembly: AssemblyVersion("2020.1011.123.1201")]
+[assembly: AssemblyFileVersion("2020.1011.123.1201")]

d8bb0bf9f9e76b67a109cac240246fe73ced2745
Vic González <vgonzalez@nuxiba.com>
2020-11-27 10:07:54 -0600

    Se modifica ccsp_GalateaCallbacks para envaiar solo informacion del agente que hace la peticion.

--- CCenterRIA/UpdateDB/20201110_v123.12_Noviembre.sql
+++ CCenterRIA/UpdateDB/20201110_v123.12_Noviembre.sql
@@ -2220,6 +2220,31 @@ return(0)
 set nocount off'
 		EXEC(@sql)
 
+
+		set @process = 'CW-4496 ccsp_GalateaCallbacks'
+		set @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaCallbacks]
+@dateCallBack datetime,
+@agentId int = 0
+AS
+-- Returns the total of callbacks by hour on especific day
+IF OBJECT_ID(''tempdb..#CallBackHours'') IS NOT NULL
+BEGIN
+	DROP TABLE #CallBackHours
+END
+
+CREATE TABLE #CallBackHours (Hour int, callback int )
+
+INSERT INTO #CallBackHours
+select  DATEPART(HOUR, cal_fcallback) ''Hour'', 1
+from ccoCallsOut
+where convert(datetime,convert(varchar(10),cal_fcallback,121))  = convert(datetime,convert(varchar(10),@dateCallBack,121))
+and User_id = @agentId
+SELECT  CAST(Hour AS smallint) Hour, SUM(callback) ''CallBacks'' FROM #CallBackHours
+GROUP BY Hour
+ORDER BY Hour
+'
+		EXEC(@sql)
+
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

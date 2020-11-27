b337fb7ca04b21141c674f0da87a685c55d7e7b2
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-11-27 02:29:13 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_IVRUpdateCallSetStatus.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_IVRUpdateCallSetStatus.sql
@@ -6,7 +6,7 @@ as
 set nocount on
 
 Update ccCallsIn SET cal_que=case @nStatus when 5 -- En Espera
-then 1 else cal_que end, statusCall_id=@nStatus
+then 1 else cal_que end, statusCall_id=case when statusCall_id<>13 then @nStatus else statusCall_id end
 ,User_id= case when @userId >0 and User_id=0  then @userId else User_id end
 
 where cal_id=@cal_id

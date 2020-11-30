b10117347136062c6dd1e87e79caf979747de109
Luis Ernesto Rangel <lrangel@nuxiba.com>
2020-11-30 14:24:52 -0600

    Se integra script

--- CCenterRIA/UpdateDB/20201110_v123.12_Noviembre.sql
+++ CCenterRIA/UpdateDB/20201110_v123.12_Noviembre.sql
@@ -2554,6 +2554,96 @@ BEGIN CATCH
 END CATCH;
 		'
 		EXEC(@sql)
+		
+		set @process = 'se quita sp si existe'
+				set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaAdminWorkgroups'')
+				    begin
+					DROP PROCEDURE ccsp_GalateaAdminWorkgroups;
+				    end'
+				EXEC(@sql)
+		
+				set @process = 'se agrega sp ccsp_GalateaAdminWorkgroups'
+				set @sql = '
+					   
+CREATE PROCEDURE [dbo].[ccsp_GalateaAdminWorkgroups] 
+	@Option AS SMALLINT,
+	@AdminId AS INT = 0,
+	@WorkgroupId AS INT = 0,
+	@idArea AS INT = NULL,
+	@Descripcion AS varchar(40) = null
+AS
+BEGIN
+	IF @Option = 1
+	BEGIN 
+		SELECT @AdminId = ISNULL(@AdminId, 0)			
+		
+		SELECT CAST(wg.IDWG AS INT) AS Id, WGName Name, StatusWorkGroup Status  FROM ccRIAWorkGroupUsers wgu
+		JOIN  ccRIACat_WorkGroup wg ON wg.IDWG = wgu.IDWG
+		WHERE User_id = @AdminId
+					
+	END
+	IF @Option = 2
+	BEGIN 
+		SELECT @WorkgroupId = ISNULL(@WorkgroupId, 0)			
+		
+		SELECT CAST(wg.IDWG AS INT) AS Id,
+				WGName Name,
+				StatusWorkGroup Status  
+		FROM 
+		ccRIACat_WorkGroup wg 
+		WHERE IDWG = @WorkgroupId
+					
+	END
+
+	IF @Option = 3 --Lista de wg 
+	BEGIN 
+	
+		SELECT cast(IDWG as int) IDWG,WGName
+		FROM ccRIACat_WorkGroup 
+					
+	END
+
+	IF @Option = 4 --Lista de wg por area
+	BEGIN 
+		SELECT @idArea = ISNULL(@idArea, 0)	
+
+		SELECT CAST(IDWG as int) IDWG 
+		FROM 
+		ccRIAAreaWorkGroup
+		WHERE IDArea = @idArea
+					
+	END
+
+	if @option = 6 -- Verifica si existe el grupo
+		 begin
+		  	select @WorkgroupId = case when exists(select WGName from ccRIACat_WorkGroup where StatusWorkGroup=1 and WGName=@Descripcion)
+			 then 1 else 0 end
+		 
+		 	if isnull(@IDArea,0)=0
+			 begin
+				select @WorkgroupId
+				return(0)
+			 end
+
+		 	if @WorkgroupId=1
+			 begin
+			 set @WorkgroupId = -1
+				select @WorkgroupId
+				return(0)
+			 end
+
+			insert into ccRIACat_WorkGroup (WGName) values (@Descripcion)
+			if @@rowcount = 1
+				select @WorkgroupId = scope_identity()
+
+			insert into ccRIAAreaWorkGroup (IDWG, IDArea) values (@WorkgroupId, @IDArea)
+			select @WorkgroupId
+			return(0)
+		 end
+
+END'
+
+		EXEC(@sql)
 
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

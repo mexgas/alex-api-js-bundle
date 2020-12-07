0fbc5f762508f88792f360b6dcad6a8170415d2e
Erik_Omar_Montes_Castro <emontes@nuxiba.com>
2020-12-07 14:11:22 -0600

    se libera sp ccsp_GalateaUpdateUser para la edicion de los datos de los usuarios

--- CCenterRIA/UpdateDB/20201203_v123.13_Diciembre.sql
+++ CCenterRIA/UpdateDB/20201203_v123.13_Diciembre.sql
@@ -48,6 +48,74 @@ BEGIN
 
 	BEGIN TRY
 
+        set @process = 'se quita sp ccsp_GalateaUpdateUser si existe CW-4593-EOMC-Editar_Agentes_en_Admin_Kolob'
+        set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaUpdateUser'')
+            begin
+          DROP PROCEDURE ccsp_GalateaUpdateUser;
+            end'
+        EXEC(@sql)
+    
+        set @process = 'se agrega sp ccsp_GalateaUpdateUser CW-4593-EOMC-Editar_Agentes_en_Admin_Kolob'
+        set @sql ='Create PROCEDURE ccsp_GalateaUpdateUser
+@UserId int,
+@Login varchar(40),
+@Nombres varchar(45),
+@LastName varchar(45),
+@NombreOpcionalExtra varchar(45),-- para español es el ap materno, para ingles es un segundo nombre y para portugues es el nombre del padre ya que en portugal  va primero el nombre de la madre
+@Password varchar(200),
+@Sexo bit,
+@canChangeStatus bit
+as
+
+Declare @ApellidoMaterno varchar(45)
+Declare @ApellidoPaterno varchar(45)
+Declare @CurrentPass varchar(200)
+--Obtiene el idioma de Centerware
+Declare @lenguageXion varchar
+select @lenguageXion= valor from ccsettings where setting_id=27 --  0 para español, 1 para ingles, 2 para portugues
+
+--se acondiciona los apellidos con el nombre opcional dependiendo del idioma
+	if @lenguageXion= ''0'' or @lenguageXion= ''2'' --para español y portugues
+		begin
+			set @ApellidoPaterno = @LastName
+			set @ApellidoMaterno = @NombreOpcionalExtra
+		end
+	else-- es idioma ingles
+		begin
+			set @ApellidoPaterno = @NombreOpcionalExtra 
+			set @ApellidoMaterno = @LastName
+		end
+
+-- validaciones	
+	if not exists(select Login from ccUsers where Login=@Login and User_id=@UserId)
+		begin
+		select -5 as ResponseCode--,''el usuario no existe''
+		return(0)
+		end
+
+--verificamos si la constrasena ha cambiado
+	select @CurrentPass= Password from ccUsers where User_id=@UserId and Login=@Login
+
+	if @Password <> '''' and @Password <> null and @Password <> @CurrentPass -- si la contraseña si cambio actualizamos en base el fecha de actualizacion de pass
+		begin 
+		Update ccUsers set Password=@Password, LastPasswordChange = GETDATE() where User_id=@UserId
+		end
+
+--update
+	Update ccUsers set 
+	Nombres=@Nombres,
+	ApellidoPaterno=@ApellidoPaterno,
+	ApellidoMaterno=@ApellidoMaterno,
+	Password=case when @Password <> '''' then @Password else Password end,
+	Sexo=@Sexo,
+	canChangeStatus=@canChangeStatus
+	where User_id=@UserId
+
+select 200 as ResponseCode -- indica que se actualizo correctamente el usuario
+
+'
+        EXEC(@sql)
+
 
         set @process = 'CW-4505 Alter sp ccsp_RIAGetAveTimeEspec'
         set @sql = '

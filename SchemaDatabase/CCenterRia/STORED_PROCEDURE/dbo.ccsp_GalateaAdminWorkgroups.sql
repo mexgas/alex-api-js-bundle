f8703617d3408a88735903fe5ea73c3c0af6dd43
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-12-11 02:32:53 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaAdminWorkgroups.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaAdminWorkgroups.sql
@@ -1,110 +1,112 @@
 CREATE PROCEDURE [dbo].[ccsp_GalateaAdminWorkgroups] 
-			@Option AS SMALLINT,
-			@AdminId AS INT = 0,
-			@WorkgroupId AS INT = 0,
-			@idArea AS INT = NULL,
-			@Descripcion AS varchar(40) = null
-		AS
-		declare @users as int
-		declare @camps as int
+	@Option AS SMALLINT,
+	@AdminId AS INT = 0,
+	@WorkgroupId AS INT = 0,
+	@idArea AS INT = NULL,
+	@Descripcion AS varchar(40) = null,
+	@groupList as varchar (MAX) = NULL
 
-		BEGIN
-			IF @Option = 1
-			BEGIN 
-				SELECT @AdminId = ISNULL(@AdminId, 0)			
+AS
+declare @users as int
+declare @camps as int
+declare @sql as varchar(max)
+
+BEGIN
+	IF @Option = 1
+	BEGIN 
+		SELECT @AdminId = ISNULL(@AdminId, 0)			
 		
-				SELECT CAST(wg.IDWG AS INT) AS Id, WGName Name, StatusWorkGroup Status  FROM ccRIAWorkGroupUsers wgu
-				JOIN  ccRIACat_WorkGroup wg ON wg.IDWG = wgu.IDWG
-				WHERE User_id = @AdminId		
-			END
-			IF @Option = 2
-			BEGIN 
-				SELECT @WorkgroupId = ISNULL(@WorkgroupId, 0)			
-		
-				SELECT CAST(wg.IDWG AS INT) AS Id,
-						WGName Name,
-						StatusWorkGroup Status  
-				FROM 
-				ccRIACat_WorkGroup wg 
-				WHERE IDWG = @WorkgroupId
+		SELECT CAST(wg.IDWG AS INT) AS Id, WGName Name, StatusWorkGroup Status  FROM ccRIAWorkGroupUsers wgu
+		JOIN  ccRIACat_WorkGroup wg ON wg.IDWG = wgu.IDWG
+		WHERE User_id = @AdminId
 					
-			END
+	END
+	IF @Option = 2
+	BEGIN 
+		SELECT @WorkgroupId = ISNULL(@WorkgroupId, 0)			
+		
+		SELECT CAST(wg.IDWG AS INT) AS Id,
+				WGName Name,
+				StatusWorkGroup Status  
+		FROM 
+		ccRIACat_WorkGroup wg 
+		WHERE IDWG = @WorkgroupId
+					
+	END
 
-			IF @Option = 3 --Lista de wg 
-			BEGIN 
+	IF @Option = 3 --Lista de wg 
+	BEGIN 
 	
-				SELECT cast(IDWG as int) Id, WGName as Name
-				FROM ccRIACat_WorkGroup 
+		SELECT cast(IDWG as int) Id, WGName as Name
+		FROM ccRIACat_WorkGroup
+		WHERE StatusWorkGroup =1
 					
-			END
+	END
 
-			IF @Option = 4 --Lista de wg por area
-			BEGIN 
-				SELECT @idArea = ISNULL(@idArea, 0)	
+	IF @Option = 4 --Lista de wg por area
+	BEGIN 
+		SELECT @idArea = ISNULL(@idArea, 0)	
 
-				SELECT CAST(IDWG as int) IDWG 
-				FROM 
-				ccRIAAreaWorkGroup
-				WHERE IDArea = @idArea
+		SELECT CAST(IDWG as int) IDWG 
+		FROM 
+		ccRIAAreaWorkGroup
+		WHERE IDArea = @idArea
 					
-			END
+	END
 
-			IF @Option = 5 --Delete WG
-			BEGIN
-				--revisar tablas con relacion de grupos de trabajo
-				SELECT @WorkgroupId = ISNULL(@WorkgroupId, 0)
-				if  @WorkgroupId = 0
-				begin
-					SELECT 0
-					return (0)
-				end
+	IF @Option = 5 --Delete WG
+	BEGIN
+		--revisar tablas con relacion de grupos de trabajo
+		IF OBJECT_ID('tempdb..#WGDelete') IS NOT NULL DROP TABLE #WGDelete;
+		SELECT value As IDwg into #WGDelete FROM fn_RIASplitDelimited(@groupList, ',')
 
-				SELECT @users=count(IdCampEsp) 
-				FROM ccRIACampEspWG 
-				where IDWG= @WorkgroupId
+		SELECT @camps=count(IdCampEsp) 
+		FROM ccRIACampEspWG 
+		where IDWG in  (select IDwg from #WGDelete)
 
-				SELECT @users=count(User_id) 
-				FROM ccRIAWorkGroupUsers 
-				where IDWG= @WorkgroupId
+		SELECT @users=count(User_id) 
+		FROM ccRIAWorkGroupUsers 
+		where IDWG in (select IDwg from #WGDelete)
 
-				if @users>0 or @camps >0 
-				begin
-					select -1
-				end
-				else
-				begin
-					Update ccRIACat_WorkGroup set StatusWorkGroup = 0 where IDWG =@WorkgroupId 
-					select 1
-				end
+		if @users>0 or @camps >0 
+		begin
+			select -1
+		end
+		else
+		begin
+			Delete from ccRIAAreaWorkGroup where IDWG in (select IDwg from #WGDelete)
+			Update ccRIACat_WorkGroup set StatusWorkGroup = 0 where IDWG in (select IDwg from #WGDelete)
+			select 1
+		end
 		
 
-			END
+	END
 
-			if @option = 6 -- Verifica si existe el grupo
-				 begin
-		  			select @WorkgroupId = case when exists(select WGName from ccRIACat_WorkGroup where StatusWorkGroup=1 and WGName=@Descripcion)
-					 then 1 else 0 end
+	if @option = 6 -- Verifica si existe el grupo
+		 begin
+		  	select @WorkgroupId = case when exists(select WGName from ccRIACat_WorkGroup where StatusWorkGroup=1 and WGName=@Descripcion)
+			 then 1 else 0 end
 		 
-		 			if isnull(@IDArea,0)=0
-					 begin
-						select @WorkgroupId
-						return(0)
-					 end
+		 	if isnull(@IDArea,0)=0
+			 begin
+				select @WorkgroupId
+				return(0)
+			 end
 
-		 			if @WorkgroupId=1
-					 begin
-					 set @WorkgroupId = -1
-						select @WorkgroupId
-						return(0)
-					 end
+		 	if @WorkgroupId=1
+			 begin
+			 set @WorkgroupId = -1
+				select @WorkgroupId
+				return(0)
+			 end
 
-					insert into ccRIACat_WorkGroup (WGName) values (@Descripcion)
-					if @@rowcount = 1
-						select @WorkgroupId = scope_identity()
+			insert into ccRIACat_WorkGroup (WGName) values (@Descripcion)
+			if @@rowcount = 1
+				select @WorkgroupId = scope_identity()
 
-					insert into ccRIAAreaWorkGroup (IDWG, IDArea) values (@WorkgroupId, @IDArea)
-					select @WorkgroupId
-					return(0)
-				 end
+			insert into ccRIAAreaWorkGroup (IDWG, IDArea) values (@WorkgroupId, @IDArea)
+			select @WorkgroupId
+			return(0)
+		 end
 
-		END
+END

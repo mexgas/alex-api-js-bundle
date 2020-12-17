5e76d469a9e8ee788b6e7ff10a8480e16985600f
Mr_Jenkins <mrjenkins@nuxiba.com>
2020-12-17 02:29:07 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaLoadUsersForManagement.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaLoadUsersForManagement.sql
@@ -1,7 +1,8 @@
-Create PROCEDURE ccsp_GalateaLoadUsersForManagement
+CREATE PROCEDURE [dbo].[ccsp_GalateaLoadUsersForManagement]
  @option SMALLINT,
  @AreaId SMALLINT,
- @UserType INT
+ @UserType INT,
+ @Username VARCHAR(200)=null
 as
 
 --Obtiene el idioma de de Centerware
@@ -31,6 +32,32 @@ BEGIN
   FROM ccusers
   WHERE isnull(IDArea, 0) = isnull(@AreaId, 0) AND TipoUser_id & 2 = CASE @UserType WHEN 1 THEN 0 ELSE 2 END AND STATUS = 1
   ORDER BY LOGIN, Nombres, ApellidoPaterno,Sexo, User_id
+
+  RETURN (0)
+END
+
+IF @option = 2 -- obtiene Agente o supervisor en base a su nombre de usuario
+BEGIN
+  SELECT  TipoUser_id as UserType,
+  User_id as UserId,
+  LOGIN as Username,
+  Nombres as Names,
+  CASE 
+    WHEN @lenguageXion='1' THEN isnull(' ' + ApellidoMaterno, '')-- El sistema esta en ingles
+    ELSE isnull(' ' + ApellidoPaterno, '')
+  END as LastName,
+
+  CASE 
+    WHEN @lenguageXion='1' THEN isnull(' ' + ApellidoPaterno, '')-- El sistema esta en ingles
+    ELSE isnull(' ' + ApellidoMaterno, '')
+  END as OptionalExtraName,
+
+  Password as Password,
+  Sexo as IsMan,
+  CanChangeStatus as EnableNotReady,
+  isnull(IDArea, 0) as AreaId
+  FROM ccusers
+  WHERE Login=@Username
 
   RETURN (0)
 END

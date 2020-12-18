02222036ed496a3d8aa1168354ce5a478fb8c909
Erik_Omar_Montes_Castro <emontes@nuxiba.com>
2020-12-18 11:43:19 -0600

    se agregan cambios para quitar espacios en blaco en el apellido paterno y materno de los usuarios y tambien para ignorar el password a la hora de actualizar datos

--- CCenterRIA/UpdateDB/20201203_v123.13_Diciembre.sql
+++ CCenterRIA/UpdateDB/20201203_v123.13_Diciembre.sql
@@ -67,13 +67,13 @@ BEGIN
   LOGIN as Username,
   Nombres as Names,
   CASE 
-    WHEN @lenguageXion=''1'' THEN isnull('' '' + ApellidoMaterno, '''')-- El sistema esta en ingles
-    ELSE isnull('' '' + ApellidoPaterno, '''')
+    WHEN @lenguageXion=''1'' THEN isnull(ApellidoMaterno, '''')-- El sistema esta en ingles
+    ELSE isnull(ApellidoPaterno, '''')
   END as LastName,
 
   CASE 
-    WHEN @lenguageXion=''1'' THEN isnull('' '' + ApellidoPaterno, '''')-- El sistema esta en ingles
-    ELSE isnull('' '' + ApellidoMaterno, '''')
+    WHEN @lenguageXion=''1'' THEN isnull(ApellidoPaterno, '''')-- El sistema esta en ingles
+    ELSE isnull(ApellidoMaterno, '''')
   END as OptionalExtraName,
 
   Password as Password,
@@ -94,13 +94,13 @@ BEGIN
   LOGIN as Username,
   Nombres as Names,
   CASE 
-    WHEN @lenguageXion=''1'' THEN isnull('' '' + ApellidoMaterno, '''')-- El sistema esta en ingles
-    ELSE isnull('' '' + ApellidoPaterno, '''')
+    WHEN @lenguageXion=''1'' THEN isnull(ApellidoMaterno, '''')-- El sistema esta en ingles
+    ELSE isnull(ApellidoPaterno, '''')
   END as LastName,
 
   CASE 
-    WHEN @lenguageXion=''1'' THEN isnull('' '' + ApellidoPaterno, '''')-- El sistema esta en ingles
-    ELSE isnull('' '' + ApellidoMaterno, '''')
+    WHEN @lenguageXion=''1'' THEN isnull(ApellidoPaterno, '''')-- El sistema esta en ingles
+    ELSE isnull(ApellidoMaterno, '''')
   END as OptionalExtraName,
 
   Password as Password,
@@ -173,6 +173,67 @@ select @lenguageXion= valor from ccsettings where setting_id=27 --  0 para espa�
 	ApellidoPaterno=@ApellidoPaterno,
 	ApellidoMaterno=@ApellidoMaterno,
 	Password=case when @Password <> '''' then @Password else Password end,
+	Sexo=@Sexo,
+	canChangeStatus=@canChangeStatus
+	where User_id=@UserId
+
+select 200 as ResponseCode -- indica que se actualizo correctamente el usuario
+
+'
+        EXEC(@sql)
+
+        set @process = 'modifica sp ccsp_GalateaUpdateUser CW-4593-EOMC-Editar_Agentes_en_Admin_Kolob sin contraseña'
+        set @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaUpdateUser]
+@UserId int,
+@Login varchar(40),
+@Nombres varchar(45),
+@LastName varchar(45),
+@NombreOpcionalExtra varchar(45),-- para español es el ap materno, para ingles es un segundo nombre y para portugues es el nombre del padre ya que en portugal  va primero el nombre de la madre
+@Password varchar(200),
+@Sexo bit,
+@canChangeStatus bit
+as
+
+Declare @ApellidoMaterno varchar(45)
+Declare @ApellidoPaterno varchar(45)
+Declare @CurrentPass varchar(200)
+--Obtiene el idioma de Centerware
+Declare @lenguageXion varchar
+select @lenguageXion= valor from ccsettings where setting_id=27 --  0 para español, 1 para ingles, 2 para portugues
+
+--se acondiciona los apellidos con el nombre opcional dependiendo del idioma
+	if @lenguageXion= ''0'' or @lenguageXion= ''2'' --para español y portugues
+		begin
+			set @ApellidoPaterno = @LastName
+			set @ApellidoMaterno = @NombreOpcionalExtra
+		end
+	else-- es idioma ingles
+		begin
+			set @ApellidoPaterno = @NombreOpcionalExtra 
+			set @ApellidoMaterno = @LastName
+		end
+
+-- validaciones	
+	if not exists(select Login from ccUsers where Login=@Login and User_id=@UserId)
+		begin
+		select -5 as ResponseCode--,''el usuario no existe''
+		return(0)
+		end
+
+--verificamos si la constrasena ha cambiado
+	--select @CurrentPass= Password from ccUsers where User_id=@UserId and Login=@Login
+
+	--if @Password <> '''' and @Password <> null and @Password <> @CurrentPass -- si la contraseña si cambio actualizamos en base el fecha de actualizacion de pass
+	--	begin 
+	--	Update ccUsers set Password=@Password, LastPasswordChange = GETDATE() where User_id=@UserId
+	--	end
+
+--update
+	Update ccUsers set 
+	Nombres=@Nombres,
+	ApellidoPaterno=@ApellidoPaterno,
+	ApellidoMaterno=@ApellidoMaterno,
+	--Password=case when @Password <> '''' then @Password else Password end,
 	Sexo=@Sexo,
 	canChangeStatus=@canChangeStatus
 	where User_id=@UserId

cbeffd2a69afb738046a688c4f83613b7a073eaa
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-02-06 02:29:54 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCRecorderRIA/TABLE/dbo.RIA_AgentVideo.sql
+++ SchemaDatabase/CCRecorderRIA/TABLE/dbo.RIA_AgentVideo.sql
@@ -1,1 +1,1 @@
-CREATE TABLE [dbo].[RIA_AgentVideo](	  [callId] INT NOT NULL	, [callType] TINYINT NULL	, CONSTRAINT [PK__RIA_Agen__180B0E342CEE136A] PRIMARY KEY ([callId] ASC))
+CREATE TABLE [dbo].[RIA_AgentVideo](	  [callId] INT NOT NULL	, [callType] TINYINT NULL	, CONSTRAINT [PK__RIA_Agen__180B0E340C2409BC] PRIMARY KEY ([callId] ASC))

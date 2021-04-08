b1c9733325150b9245b625c92314531feaf73c6b
Charls <cechavez@nuxiba.com>
2021-04-08 14:25:38 -0500

    Se integran cambios

--- CCenterRIA/UpdateDB/20210406_v123.17_Abril.sql
+++ CCenterRIA/UpdateDB/20210406_v123.17_Abril.sql
@@ -298,6 +298,38 @@ set nocount off
 
 '
 	exec (@sql)
+	
+	set @process = 'CW-5060 Check if exist ccsp_GalateaDispositionRelations'	
+	set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaDispositionRelations'')
+            begin
+          DROP PROCEDURE ccsp_GalateaDispositionRelations;
+            end'
+    EXEC(@sql)
+
+	set @process = 'CW-5060 Create sp ccsp_GalateaDispositionRelations'	
+	set @sql = 'CREATE PROCEDURE [dbo].[ccsp_GalateaDispositionRelations]
+@command int
+AS
+set nocount on
+If @command = 1
+begin
+	select 
+		cast (0 as int) [type], 
+		i.inbound_id as cam_id, 
+		c.calif_id 
+	from ccInbound i inner join ccCalifCamp c on i.inbound_id = c.cam_id and c.tipo = 0
+	inner join ccTipoCalif t on c.calif_id = t.calif_id
+	UNION
+	select 
+		cast (1 as int) [type], 
+		o.cam_id, 
+		c.calif_id 
+	from ccCamps o inner join ccCalifCamp c on o.cam_id = c.cam_id and c.tipo = 1
+	inner join ccTipoCalifOUT co on c.calif_id = co.calif_id
+	order by [type], cam_id, calif_id
+end
+set nocount off'
+    EXEC(@sql)
 
 
 

f0d7d685e05152205e17ea7764edba30a37fb14a
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-04-09 02:31:10 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaADMPermisos.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaADMPermisos.sql
@@ -1,17 +1,40 @@
 CREATE PROCEDURE [dbo].[ccsp_GalateaADMPermisos]
 @users_id varchar(255),
 @Type int, -- 1.- cambia permiso, 2.- obtiene lista de permisos
-@Permit int, -- 1.- AllowChangeDialingMode
-@isActive int
+@Permit int, -- 1.- Permiso de marcacion
+@Value int --Valor para permiso tipo de marcacion
 AS
 set nocount on
 
-If @Type = 1 --1 Update Permission
+If @Type = 1 --1 Update Permission DialingMode
  begin
-	 if @permit = 1   --AllowChangeDialingMode
-	   		UPDATE ccUsers SET AllowChangeDialingMode = @isActive where user_id in (select value from dbo.fn_RIASplitDelimited(@users_id, ','))
-
-	 return(0)
+	 if @permit = 1 --DialingMode/PreviewPro
+	 begin
+		 if @Value = 0
+		 begin
+	   			UPDATE ccUsers SET DialingMode = @Value
+				where user_id in (select value from dbo.fn_RIASplitDelimited(@users_id, ','))
+				return(0)
+		 end
+		 if @Value = 1
+		 begin
+	   			UPDATE ccUsers SET  DialingMode = @Value
+				where user_id in (select value from dbo.fn_RIASplitDelimited(@users_id, ','))
+				return(0)
+		 end
+		 if @Value = 2
+		 begin
+	   			UPDATE ccUsers SET AllowChangeDialingMode = 1
+				where user_id in (select value from dbo.fn_RIASplitDelimited(@users_id, ','))
+				return(0)
+		 end
+		 if @Value = 3
+		 begin
+	   			UPDATE ccUsers SET AllowChangeDialingMode = 0
+				where user_id in (select value from dbo.fn_RIASplitDelimited(@users_id, ','))
+				return(0)
+		 end
+	end
  end
 
 if @Type = 2 --Get Permission

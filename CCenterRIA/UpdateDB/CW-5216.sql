82b095a7dcc0c6454a41158faaccdce848571d86
Charls <cechavez@nuxiba.com>
2021-05-04 12:12:23 -0500

    Se agrega sp ccsp_GalateaAdminSubdispositions

---
+++ CCenterRIA/UpdateDB/CW-5216.sql
@@ -0,0 +1,113 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2021/04/06
+Description:
+
+Database: CCenterRia
+Required version: 123.14
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+SET @version = 123 --**********actualizar a 122 sin fix
+SET @versionfix = 18
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD' 
+
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 4;
+
+
+
+IF @actualVersion = @version and @actualVersionFix >= @versionfix - 1
+BEGIN
+	BEGIN TRAN
+
+	BEGIN TRY
+
+
+	set @process = 'CW-5216 Check if exists ccsp_GalateaAdminSubdispositions'	
+	set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaAdminSubdispositions'')
+            begin
+          DROP PROCEDURE ccsp_GalateaAdminSubdispositions;
+            end'
+    EXEC(@sql)
+
+	set @process = 'CW-5216 Create ccsp_GalateaAdminSubdispositions'	
+	set @sql = 'CREATE PROCEDURE [dbo].[ccsp_GalateaAdminSubdispositions]
+@command int
+AS
+set nocount on
+
+if @command=1 -- Load ccTipoCalifSub
+begin
+  select califSub_id, IsNull(califSubDesc,'''') [califSubDesc], orden, canReprogram, 
+  IsNull(EndConversation,0) EndConversation
+  from ccTipoCalifSub
+  where califSub_Status = 1
+  order by 2
+  return(0)
+end
+
+If @command=2 -- Load ccTipoCalifSubOut
+begin
+  select califSub_id, IsNull(califSubDesc,'''') [califSubDesc],
+  IsNull(canReprogram, 0) [canReprogram],
+  IsNull(orden, 0) [orden],
+  IsNull(keepDial, 0) [keepDial],
+  IsNull(autoCallback, 0) [autoCallback],
+  IsNull(contactOwner, 0) [contactOwner]
+  from ccTipoCalifSubOut
+  where califSubOut_Status = 1
+  order by 2
+  return(0)
+end
+
+set nocount off'
+    EXEC(@sql)
+	
+	
+
+
+		/* End script release */
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		EXEC ccsp_getVersion 'BDF', @versionFix
+
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		/* Error generated based on sintax */
+		SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+
+		RAISERROR (@errorGenerated, 11, 1)
+
+		ROLLBACK TRAN
+	END CATCH
+END

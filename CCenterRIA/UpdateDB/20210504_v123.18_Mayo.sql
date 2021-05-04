43df2d0b964ae39189e9bfba57e2540576627e0e
Charls <cechavez@nuxiba.com>
2021-05-04 13:07:48 -0500

    Se agrega stored procedure

--- CCenterRIA/UpdateDB/20210504_v123.18_Mayo.sql
+++ CCenterRIA/UpdateDB/20210504_v123.18_Mayo.sql
@@ -89,6 +89,38 @@ end
 
 set nocount off'
     EXEC(@sql)
+	
+	set @process = 'CW-5236 Check if exists ccsp_GalateaAdminSubdispositionRelations'	
+	set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaAdminSubdispositionRelations'')
+            begin
+          DROP PROCEDURE ccsp_GalateaAdminSubdispositionRelations;
+            end'
+    EXEC(@sql)
+
+	set @process = 'CW-5236 Create ccsp_GalateaAdminSubdispositionRelations'	
+	set @sql = 'CREATE PROCEDURE [dbo].[ccsp_GalateaAdminSubdispositionRelations]
+@command int
+AS
+set nocount on
+
+If @command = 1
+begin
+	select cast(0 as int) [type],
+		r.calif_id,
+		r.califSub_id
+	from cctipoSubCalifRel r inner join ccTipoCalifSub t on r.califSub_id = t.califSub_id and r.tipoSubRel = 1
+	where t.califSub_Status = 1
+	UNION
+	select cast(1 as int) [type],
+		r.calif_id,
+		r.califSub_id
+	from cctipoSubCalifRel r inner join ccTipoCalifSubOUT t on r.califSub_id = t.califSub_id and r.tipoSubRel = 0
+	where t.califSubOut_Status = 1
+	order by [type], calif_id, califSub_id
+end
+
+set nocount off'
+    EXEC(@sql)
 
 	
 	

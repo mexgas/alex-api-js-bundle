f4ec31f7db423128bb85d8857b90765a10b7db40
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-05-05 02:30:14 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

---
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaAdminSubdispositionRelations.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE [dbo].[ccsp_GalateaAdminSubdispositionRelations]
+@command int
+AS
+set nocount on
+
+If @command = 1
+begin
+	select cast(0 as int) [type],
+		r.calif_id,
+		r.califSub_id
+	from cctipoSubCalifRel r inner join ccTipoCalifSub t on r.califSub_id = t.califSub_id and r.tipoSubRel = 1
+	where t.califSub_Status = 1
+	UNION
+	select cast(1 as int) [type],
+		r.calif_id,
+		r.califSub_id
+	from cctipoSubCalifRel r inner join ccTipoCalifSubOUT t on r.califSub_id = t.califSub_id and r.tipoSubRel = 0
+	where t.califSubOut_Status = 1
+	order by [type], calif_id, califSub_id
+end
+
+set nocount off

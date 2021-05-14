d7221ae054a704ffce00e51a1658908d1110ee62
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-05-14 16:18:39 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepOutSubDispositions.sql
+++ SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepOutSubDispositions.sql
@@ -14,11 +14,13 @@ BEGIN
 	WHERE DATE >= @from AND DATE < @to
 
 	INSERT INTO RepOutSubDispositions
-	SELECT CONVERT(SMALLDATETIME, CONVERT(VARCHAR(13), a.cal_inicio, 121) + ':00', 121) AS dateHour, a.cam_id, '' AS Campaign, isnull(a.califSub_id, 0), '' AS DispName, '', count(calif_id) DispAmount, user_id, '' AS LOGIN, '' AS username, b.IDArea, '' AS areaName, 1 AS wgId, 'systemTranslated_WorkGroup' AS wg, datepart(yyyy, max(cal_inicio)) AS year, datepart(mm, max(cal_inicio)), datepart(dd, max(cal_inicio)), datepart(hh, max(cal_inicio)), datepart(mi, max(cal_inicio))
+	SELECT CONVERT(SMALLDATETIME, CONVERT(VARCHAR(13), a.cal_inicio, 121) + ':00', 121) AS dateHour, a.cam_id, '' AS Campaign, 
+	isnull(a.calif_id, 0), '' AS DispName, '', count(calif_id) DispAmount, user_id, '' AS LOGIN, '' AS username, b.IDArea, '' AS areaName, 1 AS wgId, 'systemTranslated_WorkGroup' AS wg, datepart(yyyy, max(cal_inicio)) AS year,
+	datepart(mm, max(cal_inicio)), datepart(dd, max(cal_inicio)), datepart(hh, max(cal_inicio)), datepart(mi, max(cal_inicio))
 	FROM ccocallsout a(NOLOCK)
 	LEFT JOIN ccCamps b ON b.cam_Id = a.cam_id
 	WHERE cal_inicio >= @from AND cal_inicio < @to AND a.statuscall_id = 13 AND cal_manual IN (0, 2) AND b.IDArea IS NOT NULL
-	GROUP BY CONVERT(SMALLDATETIME, CONVERT(VARCHAR(13), a.cal_inicio, 121) + ':00', 121), a.cam_id, a.califSub_id, user_id, b.IDArea
+	GROUP BY CONVERT(SMALLDATETIME, CONVERT(VARCHAR(13), a.cal_inicio, 121) + ':00', 121), a.cam_id, a.calif_id, user_id, b.IDArea
 
 	UPDATE a
 	SET campaign = isnull(cam_descripcion, '')
@@ -27,9 +29,9 @@ BEGIN
 	WHERE [date] >= @from AND [date] < @to
 
 	UPDATE a
-	SET subDisposition = isnull(califSubDesc, 'systemTranslated_Dispositionless'), subDisposition_count = isnull(califSubDesc, 'systemTranslated_Dispositionless') + '_Count'
+	SET subDisposition = isnull(Description, 'systemTranslated_Dispositionless'), subDisposition_count = isnull(Description, 'systemTranslated_Dispositionless') + '_Count'
 	FROM RepOutSubDispositions a
-	LEFT JOIN cctipocalifsubout b ON a.subDispositionId = b.califSub_id
+	LEFT JOIN cctipocalifout b ON a.subDispositionId = b.calif_id
 	WHERE [date] >= @from AND [date] < @to
 
 	UPDATE a

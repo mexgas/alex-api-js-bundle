1ce7a505ec8be389756bd739c97c4688a9d8b68d
Jesus Gallardo <agallardo@nuxiba.com>
2021-06-15 08:53:52 -0500

    Se coloca en la unidad D para que lo tenga jenkis

--- schemaspy/ReportGenerate.bat
+++ schemaspy/ReportGenerate.bat
@@ -1,16 +1,17 @@
 cls
+set pathReport=D:/schemaspy
 java -jar schemaspy-6.1.0.jar -configFile ./CCenterRia.config.file -norows 
 
-start "" ".\CCenterRia\index.html"
+rem start "" "%pathReport%/CCenterRia/index.html"
 
 java -jar schemaspy-6.1.0.jar -configFile ./CCReportsRIA.config.file -norows 
 
-start "" ".\CCReportsRIA\index.html"
+rem start "" "%pathReport%/CCReportsRIA/index.html"
 
 java -jar schemaspy-6.1.0.jar -configFile ./CCRecorderRIA.config.file -norows 
 
-start "" ".\CCRecorderRIA\index.html"
+rem start "" "%pathReport%/CCRecorderRIA/index.html"
 
 java -jar schemaspy-6.1.0.jar -configFile ./CW_CenterScript.config.file -norows 
 
-start "" ".\CW_CenterScript\index.html"
+rem start "" "/%pathReport%/CW_CenterScript/index.html"

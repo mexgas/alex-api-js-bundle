1e83f1af3108e5cf577d2ac50ac565cb7e45d5a1
Jesus Gallardo <agallardo@nuxiba.com>
2021-06-16 15:42:22 -0500

    Se modifica para pruebas

--- schemaspy/ReportGenerate.bat
+++ schemaspy/ReportGenerate.bat
@@ -1,17 +1,17 @@
 cls
 set pathReport=D:/schemaspy
-java -jar schemaspy-6.1.0.jar -configFile ./CCenterRia.config.file -norows 
+java -jar schemaspy-6.1.0.jar -configFile ./CCenterRia.config.file -norows -o "%pathReport%/CCenterRia"
 
 rem start "" "%pathReport%/CCenterRia/index.html"
 
-java -jar schemaspy-6.1.0.jar -configFile ./CCReportsRIA.config.file -norows 
+java -jar schemaspy-6.1.0.jar -configFile ./CCReportsRIA.config.file -norows -o "%pathReport%/CCReportsRIA"
 
 rem start "" "%pathReport%/CCReportsRIA/index.html"
 
-java -jar schemaspy-6.1.0.jar -configFile ./CCRecorderRIA.config.file -norows 
+java -jar schemaspy-6.1.0.jar -configFile ./CCRecorderRIA.config.file -norows -o "%pathReport%/CCRecorderRIA"
 
 rem start "" "%pathReport%/CCRecorderRIA/index.html"
 
-java -jar schemaspy-6.1.0.jar -configFile ./CW_CenterScript.config.file -norows 
+java -jar schemaspy-6.1.0.jar -configFile ./CW_CenterScript.config.file -norows -o "%pathReport%/CW_CenterScript"
 
 rem start "" "/%pathReport%/CW_CenterScript/index.html"

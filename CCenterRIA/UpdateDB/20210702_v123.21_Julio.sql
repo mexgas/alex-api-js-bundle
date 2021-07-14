d9a4ea509f0ad2e3908f451808085a2b7a8766f0
Vic González <vgonzalez@nuxiba.com>
2021-07-14 08:54:52 -0500

    Se modifica ccsp_RIAUpdateDataCallIn para guardar los datos de las llamadas de entrada de galatea

--- CCenterRIA/UpdateDB/20210702_v123.21_Julio.sql
+++ CCenterRIA/UpdateDB/20210702_v123.21_Julio.sql
@@ -271,6 +271,50 @@ BEGIN
                 SET NOCOUNT OFF'
 	exec (@sql)
 
+
+	set @process = 'CW-55543 No se muestran los datos de llamadas de entrada en reportes'
+    set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_RIAUpdateDataCallIn'')
+            begin
+          DROP PROCEDURE ccsp_RIAUpdateDataCallIn;
+            end'
+    EXEC(@sql)
+
+	set @process = 'CW-55543 No se muestran los datos de llamadas de entrada en reportes'
+    set @sql = 'CREATE procedure [dbo].[ccsp_RIAUpdateDataCallIn]
+@calloutid int,
+@callid int,
+@typecall smallint,
+@data1 varchar(100),
+@data2 varchar(100),
+@data3 varchar(100),
+@data4 varchar(100),
+@data5 varchar(100)
+
+AS
+
+if @typecall = 1 --Inbound 
+begin
+	IF EXISTS (SELECT * FROM DataCallIn WHERE CallId=@callid )
+		DELETE FROM DataCallIn WHERE CallId=@callid
+
+	INSERT INTO DataCallIn (CallId, Data, Description) Values (@callid, @data1, ''Dato 1'' )
+	INSERT INTO DataCallIn (CallId, Data, Description) Values (@callid, @data2, ''Dato 2'' )
+	INSERT INTO DataCallIn (CallId, Data, Description) Values (@callid, @data3, ''Dato 3'' )
+	INSERT INTO DataCallIn (CallId, Data, Description) Values (@callid, @data4, ''Dato 4'' )
+	INSERT INTO DataCallIn (CallId, Data, Description) Values (@callid, @data5, ''Dato 5'' )
+end
+	
+if @typecall = 2 --Outbound 
+begin
+	update ccocallsoutsource set Dato1 = @data1,
+			Dato2 = @data2,
+			Dato3 = @data3,
+			Dato4 = @data4,
+			Dato5 = @data5 where callout_id = @calloutid
+end
+'
+    EXEC(@sql)
+
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

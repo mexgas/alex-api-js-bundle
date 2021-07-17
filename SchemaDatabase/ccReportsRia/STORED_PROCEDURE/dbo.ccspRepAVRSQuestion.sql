9b50a20de8817dd0e51b4a1700e142143f4cd97a
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-07-17 02:30:04 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepAVRSQuestion.sql
+++ SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepAVRSQuestion.sql
@@ -3,61 +3,59 @@ CREATE PROCEDURE  [dbo].[ccspRepAVRSQuestion]
 @from as datetime = null,
 @to as datetime = null
 AS
-set nocount on
-
-
 if @from is null
-	select @from = convert(datetime,convert(varchar(11),getdate()))
+select @from = convert(datetime,convert(varchar(11),getdate()))
 if @to is null
-	select @to = getdate()
-
+select @to = getdate()
 if @action = 1 BEGIN		
----Before insert delete first table dbo.RepAVRSQuestionDetail 
+
+--SELECT * FROM RepAVRSQuestion
 DELETE FROM dbo.RepAVRSQuestion with(rowlock)
 where date >= @from AND date < @to
-
-INSERT INTO dbo.RepAVRSQuestion
-
+;with pregunta as(
+SELECT id_pregunta,enunciado_pregunta FROM dbo.RIA_PREGUNTAS GROUP BY id_pregunta,enunciado_pregunta),
+  dataResume as(
 select
-	DATEADD(dd, 0, DATEDIFF(dd, 0, f.fecha_calif)) AS fecha,
-	a.User_id,
-	a.Login,
-	(a.apellidopaterno+' '+a.apellidomaterno+' '+a.nombres) AS agent, 
-	s.User_id,
-	s.Login,
-	(s.apellidopaterno+' '+s.apellidomaterno+' '+s.nombres) AS Supervisor,
-	t.id_formato,
-	t.nombre,
-	c.id_concepto,
-	c.con_descripcion,
-	p.id_pregunta,
-	p.enunciado_pregunta,
-	r.peso as avgDisposition,
-	r.peso as avgDisposition,
-	r.peso as avgDisposition,
-	f.id_grabacion,
-	case f.tipo 
-		when 1 then 'systemTranslated_Recording' 
-		when 2 then 'systemTranslated_Chat'
-		when 3 then 'systemTranslated_Email'
-		when 3 then 'systemTranslated_Twitter'
-	end as Medio,		
-	f.cam_id as CamId,
-	(CASE WHEN f.tipo_llamada = 2 THEN e.cam_descripcion ELSE u.descripcion END)
-		AS Cam	
+convert(date, f.fecha_calif) as [date]
+    ,f.age_id as userId
+    ,a.Login as [user]
+    ,(a.apellidopaterno+' '+a.apellidomaterno+' '+a.nombres) AS agentName
+    ,f.id_calificador as supervisorId
+    ,s.Login as supervisorUser
+    ,(s.apellidopaterno+' '+s.apellidomaterno+' '+s.nombres) AS Supervisor
+    ,f.id_formato as templateId
+    ,q.nombre as Template
+    ,c.id_concepto as sectionId
+    ,c.con_descripcion as Section
+    ,p.id_pregunta AS questionId
+    ,p.enunciado_pregunta AS Question
+    ,r.peso AS score 	
+    ,f.id_grabacion as mediaId
+    ,case f.tipo 
+        when 1 then 'systemTranslated_Recording' 
+        when 2 then 'systemTranslated_Chat'
+        when 3 then 'systemTranslated_Email'
+        when 3 then 'systemTranslated_Twitter'
+    end as media
+    ,f.cam_id as cam_id
+    ,(CASE WHEN f.tipo_llamada = 2 THEN e.cam_descripcion ELSE u.descripcion END) AS campaignAcd
+
 from RIA_RESULTADOSFORMA r
-INNER JOIN dbo.RIA_FORMACALIF f ON f.id_forma = r.id_forma
-INNER JOIN dbo.ccUserView a ON f.age_id = a.User_id
-INNER JOIN dbo.ccUserView s ON f.id_calificador = s.User_id
-INNER JOIN (SELECT id_formato,nombre,MAX(version)AS version
-							FROM dbo.RIA_FORMATOS
-							WHERE activo = 1 and tipo=1
-							GROUP BY id_formato,nombre) as t ON t.id_formato= f.id_formato
-INNER JOIN RIA_PREGUNTAS p ON r.id_pregunta = p.id_pregunta
-INNER JOIN RIA_CONCEPTOS c ON p.id_concepto = c.id_concepto
-left join cccamps AS e ON f.cam_id = e.cam_id
-left join ccinbound AS u ON f.cam_id = u.Inbound_id
-WHERE f.fecha_calif >= @from AND f.fecha_calif < @to
-				
-set nocount off
+  INNER JOIN dbo.RIA_FORMACALIF f ON f.id_forma = r.id_forma
+  INNER JOIN RIA_FORMATOS q ON q.id_formato = f.id_formato
+  INNER JOIN RIA_PREGUNTAS p ON r.id_pregunta = p.id_pregunta 
+  INNER JOIN RIA_CONCEPTOS c ON p.id_concepto = c.id_concepto
+  INNER JOIN dbo.ccUserView a ON f.age_id = a.User_id
+  INNER JOIN dbo.ccUserView s ON f.id_calificador = s.User_id
+  INNER JOIN cccamps AS e ON f.cam_id = e.cam_id and  f.tipo_llamada=2
+  INNER JOIN ccinbound AS u ON f.cam_id = u.Inbound_id and  f.tipo_llamada=1
+  WHERE f.fecha_calif >= @from AND f.fecha_calif <= @to
+  )
+
+  INSERT INTO dbo.RepAVRSQuestion ([date],userId,[user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section, questionId, Question, avgDisposition,mediaId,media,cam_id,campaignAcd,Dispositions)
+  select [date],userId, [user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section, questionId, Question, avg(score) score, mediaId, media,cam_id,campaignAcd, avg(score) score
+  from dataResume 
+  group by  [date], userId,[user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section, questionId, Question,
+  mediaId,media,cam_id,campaignAcd
+  SELECT * FROM DetailReports
 END
--- SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepAVRSSection.sql
+++ SchemaDatabase/ccReportsRia/STORED_PROCEDURE/dbo.ccspRepAVRSSection.sql
@@ -5,63 +5,67 @@ CREATE PROCEDURE [dbo].[ccspRepAVRSSection]
 AS
 
 if @from is null
-	select @from = convert(datetime,convert(varchar(11),getdate()))
+select @from = convert(datetime,convert(varchar(11),getdate()))
 if @to is null
-	select @to = getdate()
+select @to = getdate()
 
 if @action = 1
 BEGIN
----Before insert delete first  table dbo.RepAVRSSection 
-DELETE FROM dbo.RepAVRSSection with(rowlock)
-where date >= @from AND date < @to
+	DELETE FROM dbo.RepAVRSSection with(rowlock)
+	where date >= @from AND date < @to
 
-INSERT INTO dbo.RepAVRSSection
-				
-select
-	DATEADD(dd, 0, DATEDIFF(dd, 0, f.fecha_calif)) AS fecha,
-	a.User_id,
-	a.Login,
-	(a.apellidopaterno+' '+a.apellidomaterno+' '+a.nombres) AS agent, 
-	s.User_id,
-	s.Login,
-	(s.apellidopaterno+' '+s.apellidomaterno+' '+s.nombres) AS Supervisor,
-	f.id_formato,
-	t.nombre,
-	c.id_concepto,
-	c.con_descripcion,
-	r.peso AS scores, 
-	r.peso as avgDisposition,
-	r.peso as avgDisposition,
-	r.peso as avgDisposition,
-	f.id_grabacion,
-	case f.tipo 
-		when 1 then 'systemTranslated_Recording' 
-		when 2 then 'systemTranslated_Chat'
-		when 3 then 'systemTranslated_Email'
-		when 3 then 'systemTranslated_Twitter'
-	end as Medio,		
-	f.cam_id as CamId,
-	f.tipo_llamada as TipoLlamada,	
-	(CASE WHEN f.tipo_llamada = 2 THEN e.cam_descripcion ELSE u.descripcion END)
-		AS Cam,
-		f.id_forma,		 
-	YEAR(f.fecha_calif) AS [year], 
-	MONTH(f.fecha_calif) AS [month], 
-	DAY(f.fecha_calif) AS [day], 
-	CAST(DATEPART(hour, f.fecha_calif) as varchar(2)) AS [hour], 
-	CAST(DATEPART(minute, f.fecha_calif) as varchar(2)) AS [minute]		
-from RIA_RESULTADOSFORMA r
-INNER JOIN dbo.RIA_FORMACALIF f ON f.id_forma = r.id_forma
-INNER JOIN dbo.ccUserView a ON f.age_id = a.User_id
-INNER JOIN dbo.ccUserView s ON f.id_calificador = s.User_id
-INNER JOIN (SELECT id_formato,nombre,MAX(version)AS version
-							FROM dbo.RIA_FORMATOS
-							WHERE activo = 1 and tipo=1
-							GROUP BY id_formato,nombre) as t ON t.id_formato= f.id_formato
-INNER JOIN RIA_PREGUNTAS p ON r.id_pregunta = p.id_pregunta
-INNER JOIN RIA_CONCEPTOS c ON p.id_concepto = c.id_concepto
-left join cccamps AS e ON f.cam_id = e.cam_id
-left join ccinbound AS u ON f.cam_id = u.Inbound_id
-WHERE f.fecha_calif >= @from AND f.fecha_calif < @to
 
+	;with formato as(
+	SELECT id_formato,nombre,MAX(version)AS version FROM dbo.RIA_FORMATOS WHERE activo = 1 and tipo=1 GROUP BY id_formato,nombre),
+	dataResume as(
+	select convert(date, f.fecha_calif) as [date],
+	f.age_id as userId,
+	a.Login as [user],
+	(a.apellidopaterno+' '+a.apellidomaterno+' '+a.nombres) AS agentName,
+	f.id_calificador as supervisorId,
+	s.Login as supervisorUser,
+	(s.apellidopaterno+' '+s.apellidomaterno+'
+	'+s.nombres) AS Supervisor,
+	f.id_formato as templateId
+	,t.nombre as Template
+	,c.id_concepto as sectionId
+	,c.con_descripcion as Section
+	,r.peso AS score
+	,f.id_grabacion as idMedia
+	,case f.tipo
+	when 1 then ' systemTranslated_Recording '
+	when 2 then ' systemTranslated_Chat '
+	when 3 then ' systemTranslated_Email '
+	when 4 then ' systemTranslated_Twitter
+	'
+	end as media,
+	f.cam_id as cam_id,
+	f.tipo_llamada as tipoLlamada,
+	(CASE WHEN f.tipo_llamada = 2 THEN e.cam_descripcion ELSE u.descripcion END) AS campaignAcd
+	,f.id_forma
+	from RIA_RESULTADOSFORMA r
+	INNER JOIN dbo.RIA_FORMACALIF f ON f.id_forma = r.id_forma
+	INNER JOIN formato t ON t.id_formato= f.id_formato
+	INNER JOIN RIA_PREGUNTAS p ON r.id_pregunta = p.id_pregunta
+	INNER JOIN RIA_CONCEPTOS c ON p.id_concepto = c.id_concepto
+	INNER JOIN dbo.ccUserView a ON f.age_id = a.User_id
+	INNER JOIN dbo.ccUserView s ON f.id_calificador = s.User_id
+	left join cccamps AS e ON f.cam_id = e.cam_id and  f.tipo_llamada=2
+	left join ccinbound AS u ON f.cam_id = u.Inbound_id and  f.tipo_llamada=1
+	WHERE f.fecha_calif >= @from AND f.fecha_calif <= @to
+	)
+
+	INSERT INTO dbo.RepAVRSSection([date],userId,[user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section,avgDisposition,idMedia,media,cam_id,tipoLlamada,campaignAcd,idForma,year,month,day,hour,minutes,Dispositions)
+	select [date],userId, [user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section
+	,avg(score) score,
+	idMedia,media,cam_id,tipoLlamada,campaignAcd,id_forma,
+	YEAR([date]) AS [year],
+	MONTH([date]) AS [month],
+	DAY([date]) AS [day],
+	0 AS [hour],
+	0 AS [minute]
+	,avg(score) score
+	from dataResume
+	group by  [date], userId,[user],agentName,supervisorId,supervisorUser,Supervisor,templateId,Template,sectionId,Section,
+	idMedia,media,cam_id,tipoLlamada,campaignAcd,id_forma
 END

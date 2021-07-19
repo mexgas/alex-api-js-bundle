f37d437cae97dab7e5b101c11de1d04ffde0d888
Agarcia <agarcia@nuxiba.com>
2021-07-19 12:32:56 -0500

    fix CW-5484

--- CCenterRIA/UpdateDB/20210702_v123.21_Julio.sql
+++ CCenterRIA/UpdateDB/20210702_v123.21_Julio.sql
@@ -641,18 +641,11 @@ SET NOCOUNT OFF'
     EXEC(@sql)
 
 	set @process = 'CW-5484 ALTER ccsp_RIAInsertChat SP'
-    set @sql = ' USE [CCenterRIA]
-GO
-/****** Object:  StoredProcedure [dbo].[ccsp_RIAInsertChat]    Script Date: 19/07/2021 09:29:58 a. m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-ALTER PROCEDURE [dbo].[ccsp_RIAInsertChat]
+    set @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAInsertChat]
 @action int,
 @inboundId smallint = 0,
-@domain varchar(50) = '',
-@session varchar(50) = '',
+@domain varchar(50) = '''',
+@session varchar(50) = '''',
 @tTimeout smallint = 0,
 @chatId int = 0,
 @status tinyInt = 0,
@@ -660,7 +653,7 @@ ALTER PROCEDURE [dbo].[ccsp_RIAInsertChat]
 @finished tinyInt = 0,
 @chattingTime int = 0,
 @startTime datetime = null,
-@clientName varchar(50) = '',
+@clientName varchar(50) = '''',
 @firstMessage int = 0,
 @firstMessageTime datetime = null,
 @crmNode xml = null,
@@ -680,7 +673,7 @@ if @action = 1 begin -- Inserta nuevo chat request /*comentario: se recomienda h
 end
 
 else if @action = 2 begin -- Save Initial Info
-update ccRIAChats set inboundId = @inboundId, chatStatus = @status, userId = case when @userId = 0 then userId else @userId end, tTimeout = @tTimeout where chatId = @chatId
+update ccRIAChats set inboundId = @inboundId, chatStatus = @status, userId = @userId, tTimeout = @tTimeout where chatId = @chatId
 end
 
 else if @action = 3 begin -- Update Status
@@ -690,7 +683,7 @@ end
 else if @action = 4 begin -- Save Final Status
 if @firstMessage = 0
        begin
-             update ccRIAChats set finishedBy = @finished, userID =case when @userId = 0 then userId else @userId end where chatId = @chatId
+             update ccRIAChats set finishedBy = @finished, userID = @userId where chatId = @chatId
        end
 else
        begin
@@ -700,11 +693,11 @@ end
 
 else if @action in (5,6) begin -- Save Chatting Time /*comentario: la insercion del nodo (registro final para el finder) se recomiendo en esta action, no olvidar validar status = 4, finishedby != null y validar los tiempos para garantizar el dato final */
        if @action = 5 begin
-             update ccRIAChats set tChatting = @chattingTime, userId = case when @userId = 0 then userId else @userId end, chatDate = @startTime where chatId = @chatId
+             update ccRIAChats set tChatting = @chattingTime, userId = @userId, chatDate = @startTime where chatId = @chatId
        end
 
 	   if @action = 6 begin
-			update ccRIAChats set userId = case when @userId = 0 then userId else @userId end  where chatId = @chatId
+			update ccRIAChats set userId = @userId where chatId = @chatId
 	   end
 
 	   set @crmNode = null
@@ -716,16 +709,15 @@ else if @action in (5,6) begin -- Save Chatting Time /*comentario: la insercion 
              select @crmNode = node from ccCRMNodes where chatId = @chatId
              if @crmNode is not null
              begin
-                    set @sql = N' set @xml.modify(''insert'++CONVERT(NVARCHAR(2000),@crmNode)+' into(/R01)[1]'') '
-                    execute sp_executesql @sql,N'@xml XML Output,@crmNode XML',@xml OUTPUT,@crmNode
+                    set @sql = N'' set @xml.modify(''''insert''++CONVERT(NVARCHAR(2000),@crmNode)+'' into(/R01)[1]'''') ''
+                    execute sp_executesql @sql,N''@xml XML Output,@crmNode XML'',@xml OUTPUT,@crmNode
              end
 
              if not exists(select * from ccChatsNode where chatId=@chatId) begin ---insert finder
                 insert into ccChatsNode (chatId,node, dateIn,[status]) values (@chatId,@xml, getdate(),0)
              end
              else begin ---update finder
-				update ccChatsNode set [status] = 2, node =@xml  where chatId = @chatId
-                --select @chatId
+		     update ccChatsNode set [status] = 2, node =@xml  where chatId = @chatId                
              end
        end
 end'

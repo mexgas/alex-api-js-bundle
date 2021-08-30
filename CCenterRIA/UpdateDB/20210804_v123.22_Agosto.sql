1771d521e4bd209074cfd89c6d1f1217dd6fab3e
Luis Ernesto Rangel <lrangel@nuxiba.com>
2021-08-30 15:16:34 -0500

    se integra script para actualizar conversacion WhatsApp

--- CCenterRIA/UpdateDB/20210804_v123.22_Agosto.sql
+++ CCenterRIA/UpdateDB/20210804_v123.22_Agosto.sql
@@ -1657,6 +1657,76 @@ execute sp_executesql @sql, @params,@Ndate=@date,@Nidtipomov=@idtipomov,@Nteleph
 return(0)'
     EXEC(@sql)
     
+	set @process = 'CW-5697 Valida si existe ccsp_ConversationWASave'
+    set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_ConversationWASave'')
+            begin
+          DROP PROCEDURE ccsp_ConversationWASave;
+            end'
+    EXEC(@sql)
+
+	set @process = 'CW-5697 Crea SP ccsp_ConversationWASave '
+    set @sql = 'CREATE PROCEDURE [dbo].[ccsp_ConversationWASave]
+
+	@action int,
+	@conversationId int=0,
+	@inboundId smallint=null,
+	@phoneACD varchar(50)= null,
+	@clientId varchar(25)= null,
+	@conversationStatus smallint=0,
+	@tChatting smallint=0,
+	@tWrapUp smallint=0,
+	@finishedBy tinyint = 0,
+	@onQueue bit = null,
+	@tQueue smallint = 0,
+	@tTimeout int = 0,
+	@clientName varchar(100)= null,
+	@disposition smallint=0,
+	@subDisposition smallint=0
+
+AS
+BEGIN
+	DECLARE @isEndConversation bit
+	DECLARE @meanContactTypeId smallint
+
+	SET @meanContactTypeId = 1
+SET NOCOUNT ON;
+
+	IF @action = 1 BEGIN --new Conversation
+		IF NOT EXISTS(SELECT A.conversationId conversationId FROM ccWhatsAppConversations A WHERE A.conversationId=@conversationId) BEGIN
+			INSERT INTO [ccWhatsAppConversations](
+												inboundId, phoneACD, clientId, conversationStatus, tChatting, 
+												tWrapUp, finishedBy, onQueue, tQueue, tTimeout, clientName, disposition, subDisposition) values 
+											   (@inboundId, @phoneACD, @clientId, @conversationStatus, @tChatting, 
+												@tWrapUp, @finishedBy, @onQueue, @tQueue, @tTimeout, @clientName, @disposition, @subDisposition)
+			SELECT @conversationId=SCOPE_IDENTITY()
+			SELECT @conversationId as ConversationId
+			RETURN (0)
+		END
+		ELSE BEGIN
+			SELECT 0 AS ConversationId
+			RETURN (0)
+		END
+	END
+
+	IF @action = 2 BEGIN --save conversation Times
+		Update ccWhatsAppConversations 
+		set tChatting = DATEDIFF(ss,conversationDate,getdate()), 
+			conversationStatus = @conversationStatus, finishedBy = 1,
+			tConversation = DATEDIFF(ss,requestDate,getdate()) 
+		where conversationId = @conversationId  
+	END
+	
+	IF @action = 3 BEGIN --save conversation Status
+		Update ccWhatsAppConversations 
+		set conversationDate = getdate(),
+			conversationStatus = @conversationStatus
+		where conversationId = @conversationId  
+	END
+END'
+	
+	EXEC(@sql)
+	
+	
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

5b0b843b343b83e2edee43cef8f17bde765fe3f3
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-09-03 02:29:08 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_CleanNodeBaseX.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_CleanNodeBaseX.sql
@@ -1,69 +1,68 @@
-CREATE PROCEDURE [dbo].[ccsp_CleanNodeBaseX]
-@option int
-AS
-BEGIN
+CREATE Procedure [dbo].[ccsp_CleanNodeBaseX]
 
-declare @percentage int,@setting int
-declare @top int
-declare @table table(id bigint primary key,node xml not null,status	tinyint not null)
-declare @tableNotExists table(id bigint primary key)
+	@option int
+	AS
+	BEGIN
 
-set @percentage=20 --porcentaje de registros que se pasaran esta en funcion del setting 188
+		declare @percentage int,@setting int
+		declare @top int
+		declare @table table(id bigint primary key,node xml not null,dateStart datetime, status	tinyint not null)
+		declare @tableNotExists table(id bigint primary key)
 
+		set @percentage=20 --porcentaje de registros que se pasaran esta en funcion del setting 188
 
-select  @setting  = valor from ccSettings where setting_id = 188
-if @setting is null set @setting = 40000
-set @top=@setting/@percentage
-
+		select  @setting  = valor from ccSettings where setting_id = 188
+		if @setting is null set @setting = 40000
+		set @top=@setting/@percentage
 	
-if @option = 1 begin
+		if @option = 1 begin
 		
-		insert into @table
-		select top (@top)  A.chatId, A.node,status from ccChatsNode A with(nolock) where A.status in(1,3) order by chatId
+				insert into @table
+				select top (@top)  A.chatId, A.node,A.dateIn, status from ccChatsNode A with(nolock) where A.status in(1,3) order by chatId
 		
-		insert into @tableNotExists
-		select A.id from  @table A 
-		left join ccChatsNodeHistory  B with(nolock)  on B.chatId=A.id 
-		where B.chatId is null
+				insert into @tableNotExists
+				select A.id from  @table A 
+				left join ccChatsNodeHistory  B with(nolock)  on B.chatId=A.id 
+				where B.chatId is null
 		
-		insert into ccChatsNodeHistory(chatId,node,dateIn,status)		
-		select  A.id,A.node,GETDATE() as dateIn,A.status from @table A
-		inner join @tableNotExists B on A.id=B.id
+				insert into ccChatsNodeHistory(chatId,node,dateIn,status)		
+				select  A.id,A.node,A.dateStart,A.status from @table A
+				inner join @tableNotExists B on A.id=B.id
 
-		delete from ccChatsNode where chatId in(select id from @table)
+				delete from ccChatsNode where chatId in(select id from @table)
 
-end
-else if @option = 3  begin
+		end
+		else if @option = 3  begin
 	
-	insert into @table
-	select top (@top)  A.emailId, A.node,status from ccEmailNode A with(nolock) where A.status in(1,3) order by emailId
+			insert into @table
+			select top (@top)  A.emailId, A.node,A.dateIn,status from ccEmailNode A with(nolock) where A.status in(1,3) order by emailId
 		
-	insert into @tableNotExists
-	select A.id from  @table A 
-	left join ccEmailNodeHistory  B with(nolock)  on B.emailId=A.id 
-	where B.emailId is null
+			insert into @tableNotExists
+			select A.id from  @table A 
+			left join ccEmailNodeHistory  B with(nolock)  on B.emailId=A.id 
+			where B.emailId is null
 		
-	insert into ccEmailNodeHistory(emailId,node,dateIn,status)		
-	select  A.id,A.node,GETDATE() as dateIn,A.status from @table A
-	inner join @tableNotExists B on A.id=B.id
+			insert into ccEmailNodeHistory(emailId,node,dateIn,status)		
+			select  A.id,A.node,A.dateStart,A.status from @table A
+			inner join @tableNotExists B on A.id=B.id
 
-	delete from ccEmailNode where emailId in(select id from @table)
-end
-else if @option = 4  begin
+			delete from ccEmailNode where emailId in(select id from @table)
+		end
+		else if @option = 4  begin
 	
-	insert into @table
-	select top (@top)  A.conversationTwitterId, A.node,status from ccTwitterNode A with(nolock) where A.status in(1,3) order by conversationTwitterId
+			insert into @table
+			select top (@top)  A.conversationTwitterId, A.node,A.dateIn,status from ccTwitterNode A with(nolock) where A.status in(1,3) order by conversationTwitterId
 		
-	insert into @tableNotExists
-	select A.id from  @table A 
-	left join ccTwitterNodeHistory  B with(nolock)  on B.conversationTwitterId=A.id 
-	where B.conversationTwitterId is null
+			insert into @tableNotExists
+			select A.id from  @table A 
+			left join ccTwitterNodeHistory  B with(nolock)  on B.conversationTwitterId=A.id 
+			where B.conversationTwitterId is null
 		
-	insert into ccTwitterNodeHistory(conversationTwitterId,node,dateIn,status)		
-	select  A.id,A.node,GETDATE() as dateIn,A.status from @table A
-	inner join @tableNotExists B on A.id=B.id
+			insert into ccTwitterNodeHistory(conversationTwitterId,node,dateIn,status)		
+			select  A.id,A.node,A.dateStart,A.status from @table A
+			inner join @tableNotExists B on A.id=B.id
 
-	delete from ccTwitterNode where conversationTwitterId in(select id from @table)
-end
+			delete from ccTwitterNode where conversationTwitterId in(select id from @table)
+		end
 
-END
+	END

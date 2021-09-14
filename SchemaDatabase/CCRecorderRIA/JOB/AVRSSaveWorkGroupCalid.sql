3ff07c5fc27e64d3be34d1a08523abb865f17c55
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-09-14 02:29:52 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCRecorderRIA/JOB/AVRSSaveWorkGroupCalid.sql
+++ SchemaDatabase/CCRecorderRIA/JOB/AVRSSaveWorkGroupCalid.sql
@@ -12,27 +12,27 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 
 END
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'AVRSSaveWorkGroupCalid', 
-		@enabled=1, 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'AVRSSaveWorkGroupCalid',
+		@enabled=1,
 		@notify_level_eventlog=0,
 		@notify_level_email=0,
 		@notify_level_netsend=0,
 		@notify_level_page=0,
 		@delete_level=0,
-		@description=N'This Job allows everyday, save the relations  between ccRIAWorkGroup_Calid and recordings in the new column added in RIAGrabacion (IDWG) so that customers can check the recordings without any problem after the purification process in  ccRIAWorkGroup_Calid', 
-		@category_name=N'Nuxiba', 
+		@description=N'This Job allows everyday, save the relations  between ccRIAWorkGroup_Calid and recordings in the new column added in RIAGrabacion (IDWG) so that customers can check the recordings without any problem after the purification process in  ccRIAWorkGroup_Calid',
+		@category_name=N'Nuxiba',
 		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'/', 
-		@step_id=1, 
-		@cmdexec_success_code=0, 
-		@on_success_action=1, 
-		@on_success_step_id=0, 
-		@on_fail_action=2, 
-		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
-		@os_run_priority=0,  @subsystem=N'TSQL', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'/',
+		@step_id=1,
+		@cmdexec_success_code=0,
+		@on_success_action=1,
+		@on_success_step_id=0,
+		@on_fail_action=2,
+		@on_fail_step_id=0,
+		@retry_attempts=0,
+		@retry_interval=0,
+		@os_run_priority=0,  @subsystem=N'TSQL',
 		@command=N'declare @idWgs nvarchar(max) ,@idWgsCorchetes nvarchar(max) ,@sql nvarchar(max)
 SELECT @idWgsCorchetes=COALESCE(@idWgsCorchetes + '', '', '''') + convert(varchar(max),QUOTENAME(IDWG)) FROM ccRIACat_WorkGroup
 SELECT @idWgs=COALESCE(@idWgs + ''+ '', '''') + ''case when '' + convert(varchar(max),QUOTENAME(IDWG)) + '' is null then '''''''' else cast(''+ convert(varchar(max),QUOTENAME(IDWG)) + '' as nvarchar(max)) + '''','''' end'' FROM ccRIACat_WorkGroup
@@ -73,17 +73,17 @@ exec(@sql)
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'AVRSSaveWorkgroupCalidSchedule', 
-		@enabled=1, 
-		@freq_type=4, 
-		@freq_interval=1, 
-		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=0, 
-		@active_start_date=20150522, 
-		@active_end_date=99991231, 
-		@active_start_time=4000,  
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'AVRSSaveWorkgroupCalidSchedule',
+		@enabled=1,
+		@freq_type=4,
+		@freq_interval=1,
+		@freq_subday_type=1,
+		@freq_subday_interval=0,
+		@freq_relative_interval=0,
+		@freq_recurrence_factor=0,
+		@active_start_date=20150522,
+		@active_end_date=99991231,
+		@active_start_time=4000,
 		@active_end_time=235959
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'

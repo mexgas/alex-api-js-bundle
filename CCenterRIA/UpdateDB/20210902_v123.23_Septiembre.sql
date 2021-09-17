a307d8c590aac2a3862c12031e82bab82d628369
ivanmartinenciso <imartin@nuxiba.com>
2021-09-17 14:51:23 -0500

    Se arregla fix para creacion de campañas de whats

--- CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
+++ CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
@@ -2459,6 +2459,43 @@ end
 	            SET detalle = ''Activo(0:apagado,1:Mensual,2:semanal,3:diario)|# Semana Ejecucion|Dia Ejecucion(1:LU,2:Ma,3:Mi,4:Ju,5:Vi,6:Sa,0:Do)|Hora Inicio(00:00)|Servidor FTP|usuario FTP|contraseña FTP|Ruta de descarga FTP|Tiene SSL (1 si, 0 no)''
 	            WHERE setting_id=228'
     EXEC(@sql)
+
+    set @process = 'CW-5829 Drop procedure ccsp_UpdateACDWhatsappConfig'
+    set @sql = 'IF EXISTS (SELECT * FROM sys.procedures WHERE name = N''ccsp_UpdateACDWhatsappConfig'')
+	            BEGIN
+	          		DROP PROCEDURE ccsp_UpdateACDWhatsappConfig;
+	            END'
+    EXEC(@sql)
+
+    set @process = 'CW-5829 Create procedure ccsp_UpdateACDWhatsappConfig'
+    set @sql = 'CREATE procedure  [dbo].[ccsp_UpdateACDWhatsappConfig]
+
+					@ConexionInfo varchar(400),
+					@inbound_id int,
+					@ConnUser varchar(60),
+					@tNotas int,
+					@closeConversationTime tinyint,
+					@ShowCalifWnd bit 
+
+					AS
+					set nocount on
+					    IF EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId = @inbound_id) 
+					    BEGIN
+					        UPDATE contactMeanIn SET conexionInfo = @conexionInfo, connUser = @connUser, closeConversationTime = @closeConversationTime,
+													 ConnPass = ''N/A'', numMessages = 3, timeAlertMessage = 5, answerTimeOut = 10 					 
+							where inboundId = @inbound_id;
+							UPDATE ccWhatsAppNumbers SET inboundId = @inbound_id WHERE number = @conexionInfo
+					    END;
+
+					    IF EXISTS (SELECT Inbound_id FROM ccInbound WHERE Inbound_id = @inbound_id) 
+					    BEGIN
+					        UPDATE ccInbound SET tNotas = @tNotas, ShowCalifWnd = @ShowCalifWnd where Inbound_id = @inbound_id;
+					    END;
+					SELECT @inbound_id;
+					return(@inbound_id)
+
+					set nocount off'
+    EXEC(@sql)
     
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

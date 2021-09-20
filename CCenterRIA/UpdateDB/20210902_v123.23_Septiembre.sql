ccb0939e311e944c987453dfffe6772653ea34b5
Gerardo_Zumaya <gmorales@nuxiba.com>
2021-09-20 15:14:48 -0500

    Se agrega modificación de sp faltante

--- CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
+++ CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
@@ -2571,6 +2571,42 @@ end
 				END   	
 		END'
     EXEC(@sql)
+
+    set @process = 'CW-5820 Drop procedure ccsp_UpdateACDWhatsappConfig'
+    set @sql = 'IF EXISTS (SELECT * FROM sys.procedures WHERE name = N''ccsp_UpdateACDWhatsappConfig'')
+	            BEGIN
+	          		DROP PROCEDURE ccsp_UpdateACDWhatsappConfig;
+	            END'
+    EXEC(@sql)
+
+    set @process = 'CW-5820 update procedure ccsp_UpdateACDWhatsappConfig'
+    set @sql = 'CREATE procedure  [dbo].[ccsp_UpdateACDWhatsappConfig]
+
+					@ConexionInfo varchar(400),
+					@inbound_id int,
+					@ConnUser varchar(60),
+					@tNotas int,
+					@closeConversationTime tinyint,
+					@ShowCalifWnd bit,
+					@ExitWrapUpDisposition bit
+
+					AS
+					set nocount on
+					    IF EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId = @inbound_id) 
+					    BEGIN
+					        UPDATE contactMeanIn SET conexionInfo = @conexionInfo, connUser = @connUser, closeConversationTime = @closeConversationTime where inboundId = @inbound_id;
+							UPDATE ccWhatsAppNumbers SET inboundId = @inbound_id WHERE number = @conexionInfo
+					    END;
+
+					    IF EXISTS (SELECT Inbound_id FROM ccInbound WHERE Inbound_id = @inbound_id) 
+					    BEGIN
+					        UPDATE ccInbound SET tNotas = @tNotas, ShowCalifWnd = @ShowCalifWnd, ExitWrapUpDisposition = @ExitWrapUpDisposition where Inbound_id = @inbound_id;
+					    END;
+					SELECT @inbound_id;
+					return(@inbound_id)
+
+					set nocount off'
+    EXEC(@sql)
     
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

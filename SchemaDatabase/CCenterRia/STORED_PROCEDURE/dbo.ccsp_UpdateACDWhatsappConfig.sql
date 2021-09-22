35e5cea614e469a86c48646facf257ecfb1e869c
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-09-22 02:30:01 -0500

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_UpdateACDWhatsappConfig.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_UpdateACDWhatsappConfig.sql
@@ -12,7 +12,9 @@ CREATE procedure  [dbo].[ccsp_UpdateACDWhatsappConfig]
 					set nocount on
 					    IF EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId = @inbound_id) 
 					    BEGIN
-					        UPDATE contactMeanIn SET conexionInfo = @conexionInfo, connUser = @connUser, closeConversationTime = @closeConversationTime where inboundId = @inbound_id;
+					        UPDATE contactMeanIn SET conexionInfo = @conexionInfo, connUser = @connUser, closeConversationTime = @closeConversationTime,
+													 ConnPass = 'N/A', numMessages = 3, timeAlertMessage = 5, answerTimeOut = 10 					 
+							where inboundId = @inbound_id;
 							UPDATE ccWhatsAppNumbers SET inboundId = @inbound_id WHERE number = @conexionInfo
 					    END;
 

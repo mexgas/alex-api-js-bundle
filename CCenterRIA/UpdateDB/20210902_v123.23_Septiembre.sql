4c2e85803cbf82e57fea1fa5dc5a7fbb76b4e03e
ivanmartinenciso <imartin@nuxiba.com>
2021-09-22 20:52:39 -0500

    Se agrega ShowCalifWnd en peticion de GetConversationData

--- CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
+++ CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
@@ -2550,7 +2550,8 @@ end
 						cast(cm.closeConversationTime as int) as [TimeOut],
 						cast(cm.answerTimeOut as int) as [TimeOutWarning],
 						i.ExitWrapUpDisposition as [ExitWrapUpDisposition],
-						i.tNotas as [WrapUpTime]
+						i.tNotas as [WrapUpTime],
+						i.ShowCalifWnd
 					FROM  ccInbound i
 						INNER JOIN  contactMeanIn cm  ON i.Inbound_id = cm.inboundId
 						INNER JOIN ccWhatsAppConversations c ON (c.inboundId = i.Inbound_id and c.conversationId = @conversationId)

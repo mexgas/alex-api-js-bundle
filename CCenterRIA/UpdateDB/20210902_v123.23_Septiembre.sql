57b790b905c1d7ddc16927d8740bb399f575bb96
ivanmartinenciso <imartin@nuxiba.com>
2021-09-24 17:34:46 -0500

    Se agrega fila para el tipo de WhatsApp en meanContactType

--- CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
+++ CCenterRIA/UpdateDB/20210902_v123.23_Septiembre.sql
@@ -2610,6 +2610,14 @@ end
 
 					set nocount off'
     EXEC(@sql)
+
+    set @process = 'CW-XX Se agrega fila para el tipo de WhatsApp en meanContactType '
+    set @sql = 'if not exists(select * from meanContactType where meanContactTypeId=5) begin
+	SET IDENTITY_INSERT meanContactType ON
+	insert into meanContactType (meanContactTypeId, name, isActive) values (5, ''WhatsApp'', 1)
+	SET IDENTITY_INSERT meanContactType OFF
+	end'
+    EXEC(@sql)
     
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

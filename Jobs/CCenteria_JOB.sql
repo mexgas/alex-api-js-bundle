8a2553c831cd9d478377f9d3e304708d1fb1773e
Dunzz <bdunzz@nuxiba.com>
2021-10-11 11:09:58 -0500

    Archivo corregido

--- Jobs/CCenteria_JOB.sql
+++ Jobs/CCenteria_JOB.sql
@@ -152,11 +152,11 @@ if exists(select * from  [msdb].[dbo].[sysjobs] AS [sJOB] where [name]=N''CW (Au
 	EXEC msdb.dbo.sp_delete_job @job_name=N''CW (AutoStart),(Callback/abandoned update),(Campaign summary)'', @delete_unused_schedule=1
 end
 
-/****** Object:  Job [CW (AutoStart),(Callback/abandoned update),(Campaign summary)]    Script Date: 30/09/2021 01:30:54 p. m. ******/
+/****** Object:  Job [CW (AutoStart),(Callback/abandoned update),(Campaign summary)]    Script Date: 11/10/2021 11:02:12 a. m. ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Nuxiba]    Script Date: 30/09/2021 01:30:55 p. m. ******/
+/****** Object:  JobCategory [Nuxiba]    Script Date: 11/10/2021 11:02:12 a. m. ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Nuxiba'' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Nuxiba''
@@ -172,11 +172,11 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''CW (AutoStart),(Callback/ab
 		@notify_level_netsend=0, 
 		@notify_level_page=0, 
 		@delete_level=0, 
-		@description=N''Se funcioan los jobs CW Callback/abandoned update y CW Campaign summary'', 
+		@description=N''Se funcioan los jobs CW AutoStart, CW Callback/abandoned update y CW Campaign summary'', 
 		@category_name=N''Nuxiba'', 
 		@owner_login_name=N''replication'', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [CW Callback/abandoned update]    Script Date: 30/09/2021 01:30:56 p. m. ******/
+/****** Object:  Step [CW Callback/abandoned update]    Script Date: 11/10/2021 11:02:13 a. m. ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''CW Callback/abandoned update'', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -188,28 +188,28 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''CW Call
 		@retry_interval=0, 
 		@os_run_priority=0, @subsystem=N''TSQL'', 
 		@command=N''declare @callout_id_array varchar(max), @SQL varchar(max)
-set @callout_id_array=''|''
+set @callout_id_array=''''|''''
 
-select @callout_id_array=@callout_id_array+coalesce('',''+cast(callout_id as varchar(10)), @callout_id_array, '''')
+select @callout_id_array=@callout_id_array+coalesce('''',''''+cast(callout_id as varchar(10)), @callout_id_array, '''''''')
 from ccRIAUpdateCallBack_Abandon where minCallBackAbandonXpire < getdate()
 
 if len(@callout_id_array)>1
  begin
-  select @callout_id_array=replace(@callout_id_array, ''|,'', '''')
+  select @callout_id_array=replace(@callout_id_array, ''''|,'''', '''''''')
 
-  set @SQL=''delete ccoWorkingTable with(rowlock) where callout_id in (''+@callout_id_array+'')''
+  set @SQL=''''delete ccoWorkingTable with(rowlock) where callout_id in (''''+@callout_id_array+'''')''''
   exec(@SQL)
 
-  set @SQL=''delete ccRIAUpdateCallBack_Abandon with(rowlock) where callout_id in (''+@callout_id_array+'')''
+  set @SQL=''''delete ccRIAUpdateCallBack_Abandon with(rowlock) where callout_id in (''''+@callout_id_array+'''')''''
   exec(@SQL)
 
-  set @SQL=''update ccoCallBacks with(rowlock) set [status] = 4, schedulerStatus = 1 where callout_id in (''+@callout_id_array+'') and [status] = 0''
+  set @SQL=''''update ccoCallBacks with(rowlock) set [status] = 4, schedulerStatus = 1 where callout_id in (''''+@callout_id_array+'''') and [status] = 0''''
   exec(@SQL)
  end'', 
 		@database_name=N''CCenterRIA'', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [CW Campaign summary]    Script Date: 30/09/2021 01:30:56 p. m. ******/
+/****** Object:  Step [CW Campaign summary]    Script Date: 11/10/2021 11:02:13 a. m. ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''CW Campaign summary'', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
@@ -237,7 +237,8 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''CW Commo
 		@active_start_date=20151022, 
 		@active_end_date=99991231, 
 		@active_start_time=0, 
-		@active_end_time=235959,
+		@active_end_time=235959, 
+		@schedule_uid=N''e0e3a902-8fb2-48b8-8a14-b5f5d0f24216''
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
@@ -247,10 +248,10 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 '
+EXEC(@sql)
 
-    EXEC(@sql)
     set @process = 'CREATE JOB CW (AutoStart)'
-    set @process = 'USE [msdb]
+    set @sql = 'USE [msdb]
 
 if exists(select * from  [msdb].[dbo].[sysjobs] AS [sJOB] where [name]=N''CW (AutoStart)'') begin
 	EXEC msdb.dbo.sp_delete_job @job_name=N''CW (AutoStart)'', @delete_unused_schedule=1
@@ -316,7 +317,6 @@ QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
 '
-
     EXEC(@sql)
 
     set @process = 'CREATE JOB CW Delete old records'

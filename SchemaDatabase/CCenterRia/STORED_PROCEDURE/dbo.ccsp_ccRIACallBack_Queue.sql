9fc430729ca9c8d30b1d865ba4179c6d0394c994
Mr_Jenkins <mrjenkins@nuxiba.com>
2021-11-05 02:30:02 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_ccRIACallBack_Queue.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_ccRIACallBack_Queue.sql
@@ -1,41 +1,59 @@
 CREATE proc [dbo].[ccsp_ccRIACallBack_Queue]
-@Que_id int = null,
-@cal_id int = null,
-@CAL_ANI varchar(15) = null,
-@callout_id int = null,
-@inbound_id int = null
-as
-set nocount on
+		@Que_id int = null,
+		@cal_id int = null,
+		@CAL_ANI varchar(15) = null,
+		@callout_id int = null,
+		@inbound_id int = null,
+		@retry tinyint = null,
+		@action smallint = null,
+		@xfer_date datetime = null,
+		@call_key varchar(40) = null
+		as
+		set nocount on
 
-if (isnull(@Que_id, '') = '') and (isnull(@cal_id, '') = '' or isnull(@CAL_ANI, '') = '')
- begin
-	select -1
-	return(0)
- end
+		if @action = 2 and @Que_id is not null
+		begin
+			update ccRIACallBack_Queue set xferDate=isnull(@xfer_date,xferDate), status_queue=2 where Que_id=@Que_id
+			return(0)
+		end
 
-declare @ANI_CB varchar(13)
-declare @pais varchar(2)
-declare @ld varchar(5)
+		if @cal_id is null or @CAL_ANI is null or @inbound_id is null
+		 begin
+			select -1
+			return(0)
+		 end
 
-select @pais = valor from ccSettings with(nolock) where setting_id = 104
-select @ld = valor from ccSettings with(nolock) where setting_id = 17
-select @ANI_CB=dbo.completa(@CAL_ANI, @pais, @ld)
+		declare @retries smallint, @custom tinyint
+		declare @ANI_CB varchar(13)
+		declare @pais varchar(2)
+		declare @ld varchar(5)
 
-if isnull(@Que_id, '') <> ''
- begin
-	update ccRIACallBack_Queue set cal_id = isnull(@cal_id,cal_id),
-	 CAL_ANI = isnull(@CAL_ANI,CAL_ANI), callout_id = isnull(@callout_id,callout_id),
-	 inbound_id = isnull(@inbound_id,inbound_id), status_queue = 1
-	where Que_id=@Que_id
+		select @retries=isnull(callBackRetries,0), @custom=isnull(callBackCustomPhone,0) from ccInbound nolock where Inbound_id=@inbound_id
 
-	select 0 Que_id, @ANI_CB ANI_CB
-	return(0)
- end
+		if @custom=0
+		begin
+			select @pais = valor from ccSettings with(nolock) where setting_id = 104
+			select @ld = valor from ccSettings with(nolock) where setting_id = 17
+			select @ANI_CB=dbo.completa(@CAL_ANI, @pais, @ld)
+		end
+		else
+		begin
+			select @ANI_CB=@CAL_ANI
+		end
 
-insert ccRIACallBack_Queue (cal_id, CAL_ANI, callout_id, inbound_id, status_queue, datestamp)
-select @cal_id, @CAL_ANI, @callout_id, @inbound_id, 0, GETDATE()
+		select @retry = count(*)+1 from ccRIACallBack_Queue where cal_id = @cal_id
 
-select @Que_id = SCOPE_IDENTITY()
-select @Que_id Que_id, @ANI_CB ANI_CB
+		insert ccRIACallBack_Queue (cal_id, CAL_ANI, callout_id, inbound_id, status_queue, datestamp, retry)
+		select @cal_id, @ANI_CB, @callout_id, @inbound_id, case when @custom & 4 = 4 then 2 else case when @retry>=@retries then 1 else 0 end end, 
+			GETDATE(), case when @custom & 4 = 4 then 0 else @retry end
 
-set nocount off
+		select @Que_id = SCOPE_IDENTITY()
+
+		if len(@call_key) > 0
+		begin
+			update ccCallsIn set cal_Key=@call_key where cal_id=@cal_id
+		end
+
+		select @Que_id Que_id, @ANI_CB ANI_CB, @retry Retry, @retries Retries
+
+		set nocount off

69aa0898b7c50b995d4196908bed8175a527f9ae
Hugo Longoria <hlongoria@nuxiba.com>
2021-11-22 17:24:43 -0600

    CW-6053

--- CCenterRIA/UpdateDB/20211101_v123.25_Noviembre.sql
+++ CCenterRIA/UpdateDB/20211101_v123.25_Noviembre.sql
@@ -1742,6 +1742,66 @@ set nocount off'
 
 					END'
 		EXEC(@sql)
+
+		set @process = 'CW-6053 Reporte IVR Encuestas ADD IVROptions COLUMNS'
+    set @sql = 'IF NOT EXISTS(SELECT 1 FROM sys.columns 
+						  WHERE Name = N''callType''
+						  AND Object_ID = Object_ID(N''dbo.IVROptions''))
+				BEGIN
+					alter table IVROptions add callType tinyint null
+				END'
+	EXEC(@sql)
+
+	set @process = 'CW-6053 Reporte IVR Encuestas Fix IVROptions'
+    set @sql = 'update IVROptions set callType=2 from IVROptions ivro (nolock) join ccoCallsOut cco (nolock) on cco.cal_Inicio between dateadd(mi,-1,ivro.date) and date and ivro.cal_id=cco.cal_id
+		where cal_Inicio>dateadd(m,-1,getdate())
+		update IVROptions set callType=1 from IVROptions ivro (nolock) join ccCallsIn cci (nolock) on cci.cal_Inicio between dateadd(mi,-1,ivro.date) and date and ivro.cal_id=cci.cal_id
+		where cal_Inicio>dateadd(m,-1,getdate())'
+	EXEC(@sql)
+
+	set @process = 'CW-6053 Reporte IVR Encuestas ALTER SP ccsp_IVRInCalls'
+    set @sql = 'ALTER procedure [dbo].[ccsp_IVRInCalls]
+		@action tinyint = 0 ,
+		@ani varchar(30) = null ,
+		@idIvr int = 0 ,
+		@option varchar(5)= null ,
+		@saveType tinyInt = null,
+		@dnis varchar(50) = null,
+		@name varchar(50) = null,
+		@questionId int = 0,
+		@surveyId int = 0,
+		@calId int = 0,
+		@callout_id int = 0,
+		@ttotalIVR int = 0,
+		@callType tinyint = null
+		-- saveType 1 es menu 2 es dato
+		-- accion 1 siempre @ani  -> @idIvr
+		-- accion 2 siempre @idIvr @opcionDigitada -> nada
+		AS
+		IF @action = 1
+		BEGIN
+			IF @ani IS NOT NULL
+			BEGIN
+				INSERT INTO IVRCallsIn(cal_ani,date,dnis,callout_id) values(@ani,getDate(),isnull(@dnis,''''),@callout_id);
+				Select ''ID''=scope_identity()
+			END
+		END
+		ELSE IF @action = 2
+		BEGIN
+			IF @option IS NOT NULL AND @idIvr IS NOT NULL
+			BEGIN
+				INSERT INTO IVROptions(IVR_id,selectedOption,date,saveType,name, questionId, surveyId, cal_id, callType) values (@idIvr,@option,getDate(),@saveType,@name,isnull(@questionId,0),isnull(@surveyId,0),isnull(@calId,0),isnull(@callType,0))
+				select 0
+			END
+			ELSE select -1
+		END
+		ELSE IF @action = 3
+		BEGIN
+			UPDATE IVRCallsIn set tincall = @ttotalIVR where IVR_id = @idIvr and callout_id = @callout_id
+			if @callout_id > 0
+				exec ccsp_EngineLogTransfers 4, @callout_id, 0, 0, null
+		END'
+	EXEC(@sql)
 	
 	
 		/* End script release */

d35a117f4c378e43cd970a5f356c67b9540a0997
Mr_Jenkins <mrjenkins@nuxiba.com>
2022-01-14 02:30:06 -0600

    Automated commit of Jenkis save Schema 122.01-1_20191106_1

--- SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaUpdateVoiceConfiguration.sql
+++ SchemaDatabase/CCenterRia/STORED_PROCEDURE/dbo.ccsp_GalateaUpdateVoiceConfiguration.sql
@@ -1,5 +1,6 @@
 CREATE PROCEDURE [dbo].[ccsp_GalateaUpdateVoiceConfiguration]
 	@inboundId				smallint,
+	@frame					smallint	= null,
 	@description			varchar(50) = null,
 	@mediaType				tinyint		= null,
 	@status					smallint	= null,
@@ -22,10 +23,10 @@ CREATE PROCEDURE [dbo].[ccsp_GalateaUpdateVoiceConfiguration]
 	@callBackSurveyClient	bit			= null,
 	@editableDtmf			bit			= null,
 	@addDataCallBackReminder bit		= null
-
 AS
 BEGIN
 	SET NOCOUNT ON;
+	DECLARE @graph_id smallint
 
 	UPDATE ccInbound SET
 		descripcion = ISNULL(@description, descripcion),
@@ -51,6 +52,12 @@ BEGIN
 		editableDtmf = ISNULL(@editableDtmf, editableDtmf),
 		addDataCallBackReminder = ISNULL(@addDataCallBackReminder, addDataCallBackReminder)
 	WHERE Inbound_id = @inboundId
+
+	IF @frame IS NOT NULL
+	BEGIN
+		SELECT @graph_id = graphic_id from ccRIAGraphics where frame = @frame and [type_id] = 1
+		UPDATE ccRIAInboundGraph set graphic_id = ISNULL(@graph_id, graphic_id) where inbound_id = @inboundId
+	END
 
 	IF @showCalifWnd = 1
     BEGIN

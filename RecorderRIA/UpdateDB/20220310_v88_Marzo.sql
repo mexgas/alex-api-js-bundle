120862763e1829ca05d0b85aeecc730caef22dcc
Ciro <ccanales@nuxiba.com>
2022-03-11 12:46:01 -0600

    Se sube archivo base

---
+++ RecorderRIA/UpdateDB/20220310_v88_Marzo.sql
@@ -0,0 +1,34 @@
+set nocount on
+declare @Version int
+declare @Version_Actual int
+
+declare @Sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+---------------- VERSION ----------------
+	Set @Version = 88
+	Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
+
+if @Version_Actual in(@Version, @Version -1) -- Aqui poner numero de nueva version
+begin
+	begin tran
+	begin try
+	
+
+ 	update trec_parametros set par_valor = @Version where par_id = 30
+ 	set @Version_Actual=@Version_Actual+1
+
+	select par_valor from trec_parametros where par_id = 30
+
+	commit tran
+
+	end try
+	begin catch
+		select @errorGenerated = 'DB Script Version: ' + cast(@Version as nvarchar) + ' Error Process: ' + @process + ' Line: ' + cast(error_line() as nvarchar) + ' Number: ' + cast(@@error as nvarchar) + ' Message: ' + error_message()
+		RAISERROR(@errorGenerated, 11, 1)
+	rollback tran
+	end catch
+ end
+ else begin
+	select par_valor,'This version is incorrect, need version '+ convert(varchar(max),@Version-1) from trec_parametros where par_id = 30
+ end

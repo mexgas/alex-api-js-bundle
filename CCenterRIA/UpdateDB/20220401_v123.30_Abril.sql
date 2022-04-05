9128ce194ddef89ea9e64a8008d792790c866d16
Gus_Ramirez <gcruz@nuxiba.com>
2022-04-05 16:40:17 -0500

    Se actualiza SP ccsp_OUTGetCB_Distribucion para regresar los callbacks por campaña

--- CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
+++ CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
@@ -230,6 +230,45 @@ BEGIN
     end'
     EXEC(@sql)
 
+    set @process = 'KR020000 Se borra job si existe de callbacks por campaña'
+    set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_OUTGetCB_Distribucion'')
+    begin
+        DROP PROCEDURE ccsp_AgentDataACD;
+    end'
+    EXEC(@sql)
+
+    set @process = 'KR020000 Se actualiza job de callbacks por campaña'
+    set @sql = 'CREATE PROCEDURE [dbo].[ccsp_OUTGetCB_Distribucion]
+    @CAMPID as int,
+    @Tipo int=0
+    as
+    set nocount on
+    declare @start datetime, @end datetime, @final datetime
+
+    select @end=CONVERT(datetime,CONVERT(varchar(11),GETDATE(),121)+''00:00'',121)
+    select @start=DATEADD(d,-1,@end)
+    select @final=DATEADD(d,+1,@end)
+
+    if @Tipo=0
+    begin
+        select count(case when(cal_fechaDial<@end) then 1 else null end) as Antes,
+        count(case when(cal_fechaDial between @end and @final) then 1 else null end) as Hoy,
+        count(case when(cal_fechaDial>@final) then 1 else null end) as Despues,
+        count(callout_id) as Todos
+        from ccoWorkingTable where cam_id = @CAMPID and cal_status=1
+        return(0)
+    end
+
+    --else if @Tipo=1
+    select datepart(hh, cal_fechaDial) as Hora, count(callout_id) as CB
+        from ccoWorkingTable
+        where cam_id=@CAMPID and cal_fechaDial BETWEEN @end AND @final and cal_status=1
+        group by datepart(hh, cal_fechaDial)
+        order by Hora
+    return(0)
+    set nocount off'
+    EXEC(@sql)
+
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

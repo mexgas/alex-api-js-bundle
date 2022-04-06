1fee9a9821b58a53f5850cd47ebfa5840e744303
Gus_Ramirez <gcruz@nuxiba.com>
2022-04-06 08:39:35 -0500

    Se agrega setting 231 tiempo de callbacks por hora

--- CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
+++ CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
@@ -238,35 +238,46 @@ BEGIN
     EXEC(@sql)
 
     set @process = 'KR020000 Se actualiza job de callbacks por campaña'
-    set @sql = 'CREATE PROCEDURE [dbo].[ccsp_OUTGetCB_Distribucion]
-    @CAMPID as int,
-    @Tipo int=0
-    as
-    set nocount on
-    declare @start datetime, @end datetime, @final datetime
+    set @sql = '
+        CREATE PROCEDURE [dbo].[ccsp_OUTGetCB_Distribucion]
+        @CAMPID as int,
+        @Tipo int=0
+        as
+        set nocount on
+        declare @start datetime, @end datetime, @final datetime
 
-    select @end=CONVERT(datetime,CONVERT(varchar(11),GETDATE(),121)+''00:00'',121)
-    select @start=DATEADD(d,-1,@end)
-    select @final=DATEADD(d,+1,@end)
+        select @end=CONVERT(datetime,CONVERT(varchar(11),GETDATE(),121)+''00:00'',121)
+        select @start=DATEADD(d,-1,@end)
+        select @final=DATEADD(d,+1,@end)
 
-    if @Tipo=0
-    begin
-        select count(case when(cal_fechaDial<@end) then 1 else null end) as Antes,
-        count(case when(cal_fechaDial between @end and @final) then 1 else null end) as Hoy,
-        count(case when(cal_fechaDial>@final) then 1 else null end) as Despues,
-        count(callout_id) as Todos
-        from ccoWorkingTable where cam_id = @CAMPID and cal_status=1
+        if @Tipo=0
+        begin
+            select count(case when(cal_fechaDial<@end) then 1 else null end) as Antes,
+            count(case when(cal_fechaDial between @end and @final) then 1 else null end) as Hoy,
+            count(case when(cal_fechaDial>@final) then 1 else null end) as Despues,
+            count(callout_id) as Todos
+            from ccoWorkingTable where cam_id = @CAMPID and cal_status=1
+            return(0)
+        end
+
+        --else if @Tipo=1
+        select datepart(hh, cal_fechaDial) as Hora, count(callout_id) as CB
+            from ccoWorkingTable
+            where cam_id=@CAMPID and cal_fechaDial BETWEEN @end AND @final and cal_status=1
+            group by datepart(hh, cal_fechaDial)
+            order by Hora
         return(0)
-    end
+        set nocount off'
+    EXEC(@sql)
 
-    --else if @Tipo=1
-    select datepart(hh, cal_fechaDial) as Hora, count(callout_id) as CB
-        from ccoWorkingTable
-        where cam_id=@CAMPID and cal_fechaDial BETWEEN @end AND @final and cal_status=1
-        group by datepart(hh, cal_fechaDial)
-        order by Hora
-    return(0)
-    set nocount off'
+    set @process = 'CW-Settings permiso tiempo de consulta de callbacks por hora'
+    set @sql = 'if not exists (select * from ccSettings where setting_id=231)
+    begin
+        insert into ccSettings (setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate) values (231,5,
+        ''Tiempo de consulta Callbacks por hora (mins)'',1,''ADM'',
+        ''Tiempo (mins) para realizar la consulta de callbacks agrupados por hora en el Dashboard del Admin Kolob, default 5 min'',
+        ''Time delay to refresh callbacks information (mins), default 5 min'',1,''.*'')
+    end'
     EXEC(@sql)
 
 		/* End script release */

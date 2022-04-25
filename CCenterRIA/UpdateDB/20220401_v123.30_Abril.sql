b1f6b1fcb0ce7b69d66ab50e7c6dfa93f1f17374
Ulises Espinosa <uespinosa@nuxiba.com>
2022-04-25 10:48:01 -0500

    Se agrega el cambio del sp ccsp_UpdateACDWhatsappConfig para la tarea CW-6328

--- CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
+++ CCenterRIA/UpdateDB/20220401_v123.30_Abril.sql
@@ -1581,6 +1581,46 @@ End
         end
         '
     EXEC(@sql)
+
+	set @process = 'CW-6328 Guardar nueva configuración del tiempo en base'
+    set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_UpdateACDWhatsappConfig'')
+    begin
+        DROP PROCEDURE ccsp_UpdateACDWhatsappConfig;
+    end'
+    EXEC(@sql)
+
+	set @process = ''
+    set @sql = 'CREATE procedure  [dbo].[ccsp_UpdateACDWhatsappConfig]
+
+	@ConexionInfo varchar(400),
+	@inbound_id int,
+	@ConnUser varchar(60),
+	@tNotas int,
+	@closeConversationTime tinyint,
+	@ShowCalifWnd bit,
+	@ExitWrapUpDisposition bit,
+	@MUTimeOutClient tinyint
+
+	AS
+	set nocount on
+		IF EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId = @inbound_id) 
+		BEGIN
+			UPDATE contactMeanIn SET conexionInfo = @conexionInfo, connUser = @connUser, closeConversationTime = @closeConversationTime,
+										ConnPass = ''N/A'', numMessages = 3, timeAlertMessage = 5, answerTimeOut = 10 , answerTimeoutClient = @MUTimeOutClient 					 
+			where inboundId = @inbound_id;
+			UPDATE ccWhatsAppNumbers SET inboundId = @inbound_id WHERE number = @conexionInfo
+		END;
+
+		IF EXISTS (SELECT Inbound_id FROM ccInbound WHERE Inbound_id = @inbound_id) 
+		BEGIN
+			UPDATE ccInbound SET tNotas = @tNotas, ShowCalifWnd = @ShowCalifWnd, ExitWrapUpDisposition = @ExitWrapUpDisposition where Inbound_id = @inbound_id;
+		END;
+	SELECT @inbound_id;
+	return(@inbound_id)
+
+	set nocount off'
+    EXEC(@sql)
+
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

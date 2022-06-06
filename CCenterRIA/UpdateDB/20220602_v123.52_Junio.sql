ff93aa97168f7a926e719cea1e030bfa1bc4cfd7
Jesus Gallardo <agallardo@nuxiba.com>
2022-06-06 14:09:03 -0500

    Se realiza funcion del SP que se modifico en las tareas CW-6946, CW-6984  Se cambia ccsp_GalateaUpdateWhatsAppConfiguration

--- CCenterRIA/UpdateDB/20220602_v123.52_Junio.sql
+++ CCenterRIA/UpdateDB/20220602_v123.52_Junio.sql
@@ -189,8 +189,8 @@ BEGIN
       set nocount off'')'
 	 EXEC(@sql)
 
-	 set @process = 'Se cambia ccsp_GalateaUpdateWhatsAppConfiguration'
-     set @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaUpdateWhatsAppConfiguration]
+	 set @process = 'CW-6946, CW-6984  Se cambia ccsp_GalateaUpdateWhatsAppConfiguration'
+    set @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaUpdateWhatsAppConfiguration]
         @inboundId        smallint,
         @frame          smallint  = null,
         @description      varchar(50) = null,
@@ -277,93 +277,7 @@ BEGIN
       END'
 	 EXEC(@sql)
 		----------------------- End CCC   ---------------------------------------------
-
-		set @process = 'CW-6946 ALTER PROCEDURE [dbo].[ccsp_GalateaUpdateWhatsAppConfiguration]'
-    set @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaUpdateWhatsAppConfiguration]
-  @inboundId        smallint,
-  @frame          smallint  = null,
-  @description      varchar(50) = null,
-  @mediaType        tinyint   = null,
-  @status         smallint  = null,
-  @number         varchar(400)= null,
-  @maxAnswerTime      tinyint   = null,
-  @muTimeOutClient    int     = null,
-  @tNotas         int     = null,
-  @exitWrapUpDisposition  bit     = null,
-  @showCalifWnd     bit     = null
-AS
-BEGIN
-  SET NOCOUNT ON;
-  DECLARE @graph_id smallint
-
-  UPDATE ccInbound SET
-    descripcion = ISNULL(@description, descripcion),
-    chat = ISNULL(@mediaType, chat),
-    Status = ISNULL(@status, Status),
-    tNotas = ISNULL(@tNotas, tNotas),
-    ExitWrapUpDisposition = ISNULL(@exitWrapUpDisposition, ExitWrapUpDisposition)
-  WHERE Inbound_id = @inboundId
-
-  DECLARE @descUpdate varchar(50)
-  DECLARE @statusCCInbound smallint
-  select @descUpdate = ISNULL(@description, descripcion), @statusCCInbound = status from ccInbound where Inbound_id =@inboundId
-
-  IF NOT EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId=@inboundId) 
-    BEGIN
-        INSERT INTO contactMeanIn (meanContactTypeId, name, inboundId, isActive) 
-    values (5, @descUpdate, @inboundId, @statusCCInbound);
-    END
-
-  IF EXISTS (SELECT inboundId FROM contactMeanIn WHERE inboundId = @inboundId) 
-    BEGIN
-
-set @number = case when  @number is null or @number in('''',''0'') then '''' else @number end
-
-    UPDATE contactMeanIn set name=@descUpdate, conexionInfo=ISNULL(@number, conexionInfo)
-,connUser=ISNULL(@number, connUser)
-,ConnPass=ISNULL(@number, ConnPass) 
-    ,closeConversationTime = ISNULL(@maxAnswerTime, closeConversationTime),
-    answerTimeoutClient = ISNULL(@muTimeOutClient, 30)
-    where inboundId = @inboundId;
-
-update ccWhatsAppNumbers set inboundId=0 where inboundId=@inboundId
-if @number <> '''' begin
-update ccWhatsAppNumbers set inboundId=@inboundId where inboundId=0 and number=@number
-end
-
-    END
-
-  IF @frame IS NOT NULL
-  BEGIN
-    SELECT @graph_id = graphic_id from ccRIAGraphics where frame = @frame and [type_id] = 1
-    UPDATE ccRIAInboundGraph set graphic_id = ISNULL(@graph_id, graphic_id) where inbound_id = @inboundId
-  END
-
-  IF @showCalifWnd = 1
-    BEGIN
-    IF EXISTS(SELECT cam_id FROM ccCalifCamp WHERE cam_id = @inboundId AND tipo = 0)
-        BEGIN
-      UPDATE ccInbound SET ShowCalifWnd = ISNULL(@showCalifWnd, ShowCalifWnd)
-            WHERE inbound_id = @inboundId
-      SELECT 1 [Result]
-      RETURN(0)
-        END
-
-        SELECT -1 [Result]
-        RETURN(0)
-     END
-     ELSE
-   BEGIN
-    UPDATE ccInbound SET ShowCalifWnd = ISNULL(@ShowCalifWnd, ShowCalifWnd) WHERE inbound_id = @inboundId;
-   END
-
-   SELECT 1 [Result]
-   RETURN(0)
-
-  SET NOCOUNT OFF;
-END
-  '
-    EXEC(@sql)    
+  
 
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

5785f6253b88ab6d68942604269a133c69718904
Charls <cechavez@nuxiba.com>
2022-07-14 16:36:12 -0500

    Se agregan columnas en tabla Trec_repositorios

--- RecorderRIA/UpdateDB/20220714_v89_Julio.sql
+++ RecorderRIA/UpdateDB/20220714_v89_Julio.sql
@@ -6,17 +6,17 @@ declare @Sql varchar(max)
 declare @errorGenerated varchar(max)
 declare @process varchar(max)
 ---------------- VERSION ----------------
-	Set @Version = 88
+	Set @Version = 89
 	Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
 
 if @Version_Actual in(@Version, @Version -1) -- Aqui poner numero de nueva version
 begin
 	begin tran
 	begin try
-	
-	
-	
-	
+
+
+-----------------------------------------------------------------------------------------------------------------------
+
 	set @process = 'CW-6280 Add column ruta_repositorio_secundario'
 	set @Sql = 'IF NOT EXISTS (SELECT * FROM sys.columns WHERE NAME = N''ruta_repositorio_secundario'' AND Object_ID = Object_ID(N''TREC_REPOSITORIOS''))
 BEGIN
@@ -31,14 +31,10 @@ BEGIN
 END'
 	EXEC(@Sql)
 	
-	set @process = 'CW-6280 Add column id_nwCredential_secondary'
-	set @Sql = 'IF NOT EXISTS (SELECT * FROM sys.columns WHERE NAME = N''id_nwCredential_secondary'' AND Object_ID = Object_ID(N''TREC_REPO_NWCREDENTIALS''))
-BEGIN
-	ALTER TABLE TREC_REPO_NWCREDENTIALS ADD id_nwCredential_secondary int;
-END'
-	EXEC(@Sql)
-
-
+--------------------------------------------------------------------------------------------------
+		
+		
+		
 
  	update trec_parametros set par_valor = @Version where par_id = 30
  	set @Version_Actual=@Version_Actual+1

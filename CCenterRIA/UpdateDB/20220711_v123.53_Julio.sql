1e06a4eaf28a4d83e0099bfaef74cd32cb85d621
rodSalazar <rsalazar@nuxiba.com>
2022-07-15 08:56:08 -0500

    se agrega setting 233, cambios en ccsp_GalateaFinder y se crea sp ccsp_RecordsManagement

--- CCenterRIA/UpdateDB/20220711_v123.53_Julio.sql
+++ CCenterRIA/UpdateDB/20220711_v123.53_Julio.sql
@@ -621,6 +621,326 @@ END;'
     RETURN(0)
     SET NOCOUNT OFF'
      EXEC(@sql)
+
+	SET @process = 'CW-6672 Creacion de setting 233'
+
+	SET @sql = '
+	if not exists (select * from ccsettings where setting_id = 233)
+	begin
+		insert into ccsettings (setting_id, valor, descripcion, status, tipo, detalle, description, bloadSettings, validate) values
+		(233, ''30|10'', ''Parametros que el servicio NuxibaMangementRecordsRepository tomará'', 1, ''GRL'', ''Tiempo en días maximo de antiguedad de una grabacion|Tiempo en minutos para hacer copia las grabaciones de servidor local a repositorios principal y secundario'', ''Time in days maximum age of a recording|Time in minutes to make a copy of the recordings from the local server to the main and secondary repositories'', 1, ''.*'')
+	end '
+
+	EXEC(@sql)
+
+	SET @process = 'CW-6672 validación de sp ccsp_RecordsManagement'
+
+	SET @sql = '
+	if exists (select * from sys.procedures where name = N''ccsp_RecordsManagement'')
+	begin
+		DROP PROCEDURE ccsp_RecordsManagement;
+	end
+	'
+
+	EXEC(@sql)
+
+	SET @process = 'CW-6672 Creacion de sp ccsp_RecordsManagement'
+
+	SET @sql = '
+	CREATE PROCEDURE [dbo].[ccsp_RecordsManagement]
+	@action as tinyint,
+	@type as int = null,
+	@initialPort as int = null,
+	@finalPort as int = null,
+	@maxOldDays as int = null,
+	@callId as int = null,
+	@fileMoved as int = null
+
+	AS
+
+	declare @maxDays as int
+
+	SET NOCOUNT ON
+
+	if @action = 1
+	begin
+		set @maxDays = @maxOldDays * -1
+	
+		if @type = 0
+		begin 	
+			select cast(cal_id as int) as cal_id from ccoCallsOut where cal_Inicio < DATEADD(DAY, @maxDays, getDate()) and cal_puerto between @initialPort and @finalPort
+		end
+
+		else if @type = 1
+		begin
+			select cast(cal_id as int) as cal_id from ccCallsIn where cal_Inicio < DATEADD(DAY, @maxDays, getDate()) and cal_puerto between @initialPort and @finalPort
+		end
+	
+	end
+
+	if @action = 2
+	begin
+		declare @time int
+		if @type=0 
+		begin
+			select @time=cal_tDialog from ccoCallsOut with(nolock) where cal_id=@callId and file_moved is null
+		end
+		else 
+		begin
+			select @time=cal_tDialog from ccCallsIn with(nolock) where cal_id=@callId and file_moved is null 
+		end
+	
+		select case when @time > 0 then 1 else 0 end as result
+	end
+
+	if @action = 3 
+	begin
+		if @type=0 
+		begin
+			update ccoCallsOut with(rowlock) set file_moved=@fileMoved where cal_id=@callId
+		end
+		else 
+		begin
+			update ccCallsIn with(rowlock) set file_moved=@fileMoved where cal_id=@callId
+		end
+	end
+
+	if @action = 4 
+	begin
+		declare @result as int = 0
+		if @type = 0 
+		begin
+			if exists (select * from ccoCallsOut where cal_id = @callId and cal_puerto between @initialPort and @finalPort)
+			begin
+				set @result = 1
+			end
+		end
+	
+		else if @type = 1
+		begin
+			if exists (select * from cccallsin where cal_id = @callId and cal_puerto between @initialPort and @finalPort)
+			begin
+				set @result = 1
+			end
+		end
+
+		select @result as result
+	end'
+
+	EXEC(@sql)
+
+	SET @process = 'CW-6281 validación de sp ccspGalatea_Finder'
+
+	SET @sql = '
+	if exists (select * from sys.procedures where name = N''ccspGalatea_Finder'')
+	begin
+		DROP PROCEDURE ccspGalatea_Finder;
+	end'
+
+	EXEC(@sql)
+
+	SET @process = 'CW-6281 Creacion de sp ccspGalatea_Finder'
+
+	SET @sql = '
+	CREATE PROCEDURE [dbo].[ccspGalatea_Finder]
+	@action int,
+	@grabIds varchar(max)=null,
+	@grabId int =null,
+	@userId int =0,	
+	@markTime int=null,
+	@markId int=null,
+	@isSuperUser bit=0,
+	@dateStart datetime=null,
+	@dateEnd datetime=null
+	AS
+	BEGIN
+
+		SET NOCOUNT ON;
+		declare @sql varchar(max)
+		declare @camType table(Id int,camType tinyint)
+
+		if @action=0 begin			
+			if @isSuperUser =0 begin			
+					 SELECT WGCam.IdCampEsp AS [Id], 2 callType
+					 FROM ccRIAWorkGroupUsers Wguser
+						  INNER JOIN ccRIACampEspWG WGCam ON WGCam.IDWG = Wguser.IDWG
+						  INNER JOIN ccCamps c ON WGCam.IdCampEsp = c.cam_id
+												  AND WGCam.Tipo = 1
+					 WHERE Wguser.User_id = @userId
+					 UNION
+					 SELECT WGCam.IdCampEsp AS [Id], 1 AS callType
+					 FROM ccRIAWorkGroupUsers Wguser
+						  INNER JOIN ccRIACampEspWG WGCam ON WGCam.IDWG = Wguser.IDWG
+						  INNER JOIN ccInbound inb ON WGCam.IdCampEsp = inb.Inbound_id
+													  AND WGCam.Tipo = 0
+					 WHERE Wguser.User_id = @userId;
+				 end
+			else begin			 
+				SELECT c.cam_id as [Id], 2 AS callType FROM ccCamps c
+				UNION
+				SELECT inb.Inbound_id AS [Id], 1 AS callType FROM ccInbound inb;
+			end
+		
+		end
+
+		else if @action=1 begin
+			select id_repositorio as repositoryId,dirvirtual_audio as pathAudio,dirvirtual_video as pathVideo,ruta_repositorio as pathRepositoryAudio, 
+				ruta_repositorio_secundario as PathRepositoryAudioSecundario, dirvirtual_audio_secundario as PathAudioSecundario
+			from TREC_REPOSITORIOS
+		end
+		else if @action=2 begin
+			set @sql=''
+			;
+			with grab as (
+			select grab_id,cal_id,tipo_llamada from RIA_GRABACION where grab_id in(''+@grabIds+'')
+			union
+			select grab_id,cal_id,tipo_llamada from RIA_GRABACIONCONSULTA where grab_id in(''+@grabIds+'')
+			)
+
+			select mark.id_marca as markId, grab.grab_id as grabId,b.login as userName, mark.user_id as [userId],mark.marca as mark,convert(bit,case when b.TipoUser_id =1 then 0 else 1 end ) as IsAdmin
+			from RIA_MARCAS mark 
+			inner join grab on grab.cal_id=mark.call_id and grab.tipo_llamada=mark.tipo_llamada
+			inner join ccUsers b on b.user_id = mark.user_id
+			order by grab.grab_id,mark.tipo_marca 
+			''
+			--print @sql
+			exec (@sql)
+		end
+		else if @action =3 begin
+			select cast(case when par_valor =''1'' then 1 else 0 end as bit) as isEncrypt from TREC_PARAMETROS where par_id=15
+		end
+		else if @action =4 begin
+			set @sql=''declare @nameFolder table (callType int,nameFolder varchar(100),prefijo varchar(2))
+		insert into @nameFolder values(1,''''INBOUND'''',''''I_'''')
+		insert into @nameFolder values(2,''''OUTBOUND'''',''''O_'''')
+		declare @ext varchar(30)
+
+		select @ext = case when par_valor=''''1'''' then ''''.wav.enc'''' else ''''.wav'''' end from TREC_PARAMETROS where par_id=15
+			;
+			with grab as (
+			select grab_id,cal_id,tipo_llamada,id_repositorio,Prefijo as subFijo from RIA_GRABACION where grab_id in(''+@grabIds+'')
+			union
+			select grab_id,cal_id,tipo_llamada,id_repositorio,Prefijo as subFijo from RIA_GRABACIONCONSULTA where grab_id in(''+@grabIds+'')
+			)
+		
+			select grab.grab_id as grabId,grab.id_repositorio as repositoryId,rep.dirvirtual_audio as virtualAudio
+			,rep.ruta_repositorio+''''\''''+f.nameFolder+''''\''''+ cast(cal_id/10000 as varchar(100))+''''\'''' as pathRep,
+			rep.dirvirtual_audio_secundario as virtualAudioSecundario,
+			rep.ruta_repositorio_secundario +''''\''''+f.nameFolder+''''\''''+ cast(cal_id/10000 as varchar(100))+''''\'''' as pathRepSecundario,
+			f.prefijo+cast(cal_id as varchar(100))	+ case when subFijo<>'''''''' then ''''_''''+subFijo else '''''''' end + @ext as [fileAudio]
+			from grab 
+			inner join TREC_REPOSITORIOS rep on grab.id_repositorio=rep.id_repositorio
+			inner join @nameFolder f on f.callType=grab.tipo_llamada  
+			order by repositoryId
+			''
+			--print @sql
+			exec (@sql)
+		end
+		else if @action =5 begin
+			select top 1 id_repositorio as repositoryId,rep.ruta_repositorio as pathRep, Cred.domain, Cred.[user], Cred.[password],Rep.dirvirtual_audio as virtualAudio
+			from TREC_REPOSITORIOS Rep
+			inner join TREC_REPO_NWCREDENTIALS  RepCred on Rep.id_repositorio =repCred.id_repository
+			inner join RIA_NETWORKCREDENTIALS  Cred on RepCred.id_nwCredential=Cred.id
+			where Cred.type = 1 and status=1
+		end
+		else if @action=6 begin
+			declare @cal_id int,@tipo_llamada int
+
+			select @cal_id= cal_id,@tipo_llamada=tipo_llamada from RIA_GRABACION where grab_id=@grabId
+
+			if @cal_id is null and @tipo_llamada is null begin
+				select @cal_id= cal_id,@tipo_llamada=tipo_llamada from RIA_GRABACIONCONSULTA where grab_id=@grabId
+			end			   
+			insert RIA_MARCAS (grab_id,user_id,marca,tipo_marca,tipo_llamada,call_id) values (@grabId,@userId,CONVERT(varchar, DATEADD(ss, @markTime , 0), 8),2,@tipo_llamada,@cal_id )
+			select cast( @@IDENTITY  as int) as markId
+		end
+		else if @action=7 begin
+			delete from RIA_MARCAS  where id_marca=@markId
+		end
+		else if @action=8 begin	
+			select par_valor as hexKey from TREC_PARAMETROS where par_id=75
+		end
+		else if @action=9 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+			
+			select A.grab_id from RIA_GRABACION A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+			union
+			select A.grab_id from RIA_GRABACIONCONSULTA A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+		end
+		else if @action=10 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+			
+			select distinct A.cal_key from RIA_GRABACION A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+			union
+			select distinct A.cal_key from RIA_GRABACIONCONSULTA A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+		end
+		else if @action=11 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+			
+			select distinct A.ani as Phone from RIA_GRABACION A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+			union
+			select distinct A.ani as Phone from RIA_GRABACIONCONSULTA A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+		end
+		else if @action=12 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+			
+			select distinct A.dni from RIA_GRABACION A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+			union
+			select distinct A.dni from RIA_GRABACIONCONSULTA A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+		end
+		else if @action=13 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+			
+			select distinct convert(varchar(100), A.cal_extension) as cal_extension from RIA_GRABACION A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+			union
+			select distinct convert(varchar(100), A.cal_extension) from RIA_GRABACIONCONSULTA A
+			inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+			where A.finicio between @dateStart and @dateEnd
+		end
+
+		else if @action=14 begin					
+			insert into @camType
+			exec ccspGalatea_Finder @action=0,@userId=@userId,@isSuperUser=@isSuperUser
+
+		
+			select isnull(min(minDuration),0) as MinDuration, isnull(max(maxDuration),600) as MaxDuration from (
+				select max(duracion) as maxDuration,min(duracion) as minDuration from RIA_GRABACION A
+				inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+				where A.finicio between @dateStart and @dateEnd
+				union
+				select max(duracion) as maxDuration,min(duracion) as minDuration from RIA_GRABACIONCONSULTA A
+				inner join @camType B on A.cam_id=B.Id and A.tipo_llamada=B.camType
+				where A.finicio between @dateStart and @dateEnd
+			)X
+		end
+  
+	END'
+
+	EXEC(@sql)
+
 	 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */
 		--exec ccsp_getVersion 'BD', @version

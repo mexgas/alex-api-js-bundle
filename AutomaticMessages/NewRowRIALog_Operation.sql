6777c5c9fd4787972a4de6e3cd6ede9c74afd2d5
Jesus Gallardo <agallardo@nuxiba.com>
2022-07-28 16:02:42 -0500

    Se agrega mensajes automaticos

--- AutomaticMessages/NewRowRIALog_Operation.sql
+++
@@ -1,3 +0,0 @@
-if not exists(select * from ccRIALog_Operation where operationType=194) begin
-	INSERT INTO ccRIALog_Operation VALUES(194, 'EDITAR ORDEN DE AUDIO/VARIABLE|EDIT AUDIO/VARIABLE ORDER');
-end
--- AutomaticMessages/permisos.sql
+++
@@ -1,7 +0,0 @@
-
-insert into ccPermissions values (10028,'Gestionar Asociacion de campaña','RolesPermissionCampaignAssociation',0,0,0,'N/A',1)
-insert into ccPermissions values (10029,'Gestionar Mensajes Automaticos','RolesPermissionAutomaticMessages',0,0,0,'N/A',1)
-
-
-insert into ccRoles_Permissions values(1,10028)
-insert into ccRoles_Permissions values(1,10029)

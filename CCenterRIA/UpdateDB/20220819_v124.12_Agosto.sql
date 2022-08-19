265443414a3bad51ea790dd2142c748905473406
Gerardo_Zumaya <gmorales@nuxiba.com>
2022-08-19 17:06:23 -0500

    K002145-Historial se modifica option 1, se agrego parametro inboundId en los filtros de la consulta

---
+++ CCenterRIA/UpdateDB/20220819_v124.12_Agosto.sql
@@ -0,0 +1,255 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2022/02/15
+Description: Merge con los cambios de sorteos
+
+Database: CCenterRia
+Required version: 123.27
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+SET @version = 124 --**********actualizar a 123 sin fix
+SET @versionfix = 12
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD'
+
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 4;
+
+IF @actualVersion = @version and @actualVersionFix >= @versionfix - 1
+BEGIN
+	BEGIN TRAN
+
+	BEGIN TRY
+
+	    ----------------------------------GMZ | K002145-Historial --------------------------------------------------
+
+        set @process = 'K002145-Historial se modifica option 1, se agrego parametro inboundId en los filtros de la consulta'
+        set @sql = 'ALTER PROCEDURE [dbo].[ccsp_AgentHistoricalChat] @option SMALLINT, @clientNum VARCHAR(15) = '''', @conversationId AS INT = 0, @inboundId AS SMALLINT = 0, @serviceType AS SMALLINT = 0
+			AS
+			BEGIN
+				IF @option = 1 --whatsapp, get conversation ids
+				BEGIN
+					SELECT conversationId
+					FROM [CCenterRIA].[dbo].[ccWhatsAppConversations]
+					WHERE clientId = @clientNum AND
+						inboundId = @inboundId 
+					GROUP BY conversationId
+				END
+
+				IF @option = 2 --whatsapp, get acdId by conversation id
+				BEGIN
+					SELECT CAST(inboundId AS INT)
+					FROM [CCenterRIA].[dbo].[ccWhatsAppConversations]
+					WHERE conversationId = @conversationId
+				END
+
+				IF @option = 3 --get data conversation
+				BEGIN
+					DECLARE @OldAgentId INT = 0
+					DECLARE @OldConversationId INT = 0
+
+					SELECT @OldAgentId = conv.agentId, @OldConversationId = rel.conversationIdBefore
+					FROM ccWhatsAppConversationsRelationship rel
+					RIGHT JOIN ccWhatsAppConversations conv ON conv.conversationId = rel.conversationIdBefore
+					WHERE rel.conversationIdAfter = @conversationId
+
+					SELECT cast(i.chat AS INT) AS ServiceType, cast(c.conversationId AS INT) AS ConversationID, c.clientId AS ClientId, cm.conexionInfo AS [To], cast(i.Inbound_id AS INT) AS ACDId, i.descripcion AS ACDName, cast(g.
+							graphic_id AS INT) AS ACDGraphicId, cast(cm.closeConversationTime AS INT) AS [TimeOut], cast(cm.answerTimeOut AS INT) AS [TimeOutWarning], i.ExitWrapUpDisposition AS [ExitWrapUpDisposition], i.tNotas AS 
+						[WrapUpTime], i.ShowCalifWnd, cast(ISNULL(answerTimeoutClient, 30) AS INT) AS [AnswerTimeoutClient], ISNULL(DATEDIFF(ss, lm.timeStampLastMessageAgent, lm.desconnectionAgent), 0) AS 
+						[SecTimeOutLastMessageAgent], isnull(permission.AllowUnassign, 0) AS AllowUnassign, isnull(permission.AllowSpam, 0) AS AllowSpam, ISNULL(@OldAgentId, 0) AS OldAgentId, ISNULL(@OldConversationId, 0) AS 
+						OldConversationId, c.agentId AS AgentId
+					FROM ccInbound i
+					INNER JOIN contactMeanIn cm ON i.Inbound_id = cm.inboundId
+					INNER JOIN ccWhatsAppConversations c ON (
+							c.inboundId = i.Inbound_id
+							AND c.conversationId = @conversationId
+							)
+					INNER JOIN ccRIAInboundGraph g ON g.Inbound_id = i.Inbound_id
+					LEFT JOIN ccLastMessageAgentByConversation lm ON lm.conversationId = c.conversationId
+					LEFT JOIN ccRIAMultimediaUsersPermissions permission ON permission.AgentId = c.agentId
+					WHERE i.chat = @serviceType
+						AND i.Inbound_id = @inboundId
+
+				END
+
+				IF @option = 4 --get messages from conversation id
+				BEGIN
+					DECLARE @filetype AS VARCHAR(5)
+
+					SELECT messageId AS MessageId, messageStatus AS STATUS, originType AS Origin, CASE 
+							WHEN originType = ''Client''
+								THEN 3
+							WHEN originType = ''Agent''
+								THEN 2
+							WHEN originType = ''Admin''
+								THEN 1
+							ELSE 0
+							END AS OriginType, timeStampMessage AS [Timestamp], CASE 
+							WHEN typeMessage <> ''text''
+								THEN ''''
+							ELSE content
+							END AS Content, typeMessage AS Type, CASE 
+							WHEN typeMessage NOT IN (''text'', ''location'')
+								THEN content
+							ELSE ''''
+							END AS Caption, CASE 
+							WHEN originType = ''Client''
+								THEN CASE 
+										WHEN typeMessage = ''text''
+											OR typeMessage = ''location''
+											THEN ''''
+										ELSE CHAR(92) + CHAR(92) + ''WhatsApp'' + CHAR(92) + CHAR(92) + cast(conversationId / 1000 AS VARCHAR(30)) + CHAR(92) + CHAR(92) + cast(conversationId AS VARCHAR(20)) + CHAR(92) + CHAR(92) + 
+											typeMessage + CHAR(92) + CHAR(92) + messageId + ''.'' + CASE 
+												WHEN typeMessage = ''video''
+													THEN ''mp4''
+												WHEN typeMessage = ''image''
+													THEN ''jpg''
+												WHEN typeMessage = ''audio''
+													THEN ''mp3''
+												WHEN typeMessage = ''file''
+													THEN (
+															SELECT substring(content, CHARINDEX(''.'', content) + 1, len(content))
+															)
+												ELSE ''''
+												END
+										END
+							ELSE CASE 
+									WHEN typeMessage = ''text''
+										OR typeMessage = ''location''
+										THEN ''''
+									ELSE content
+									END
+							END AS [Url], CASE 
+							WHEN typeMessage = ''location''
+								THEN (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 1
+													), '':'')
+										WHERE id = 2
+										)
+							ELSE ''''
+							END AS [Address], CASE 
+							WHEN typeMessage = ''location''
+								THEN (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 2
+													), '':'')
+										WHERE id = 2
+										)
+							ELSE ''''
+							END AS [Lat], CASE 
+							WHEN typeMessage = ''location''
+								THEN (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 3
+													), '':'')
+										WHERE id = 2
+										)
+							ELSE ''''
+							END AS [Long], CASE 
+							WHEN typeMessage = ''location''
+								THEN (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 4
+													), '':'')
+										WHERE id = 2
+										)
+							ELSE ''''
+							END AS [Name], CASE 
+							WHEN typeMessage = ''location''
+								THEN ''https://www.google.com/maps/search/'' + (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 2
+													), '':'')
+										WHERE id = 2
+										) + '','' + (
+										SELECT value
+										FROM dbo.fn_RIASplitDelimited((
+													SELECT value
+													FROM dbo.fn_RIASplitDelimited(content, ''|'')
+													WHERE id = 3
+													), '':'')
+										WHERE id = 2
+										)
+							ELSE ''''
+							END AS [LocationURL]
+					FROM ccWAMessagesConversations
+					WHERE conversationId = @conversationId
+					ORDER BY TIMESTAMP ASC
+				END
+
+				IF @option = 5 --get if conversation is reassigned
+				BEGIN
+					SELECT CASE 
+							WHEN EXISTS (
+									SELECT *
+									FROM [CCenterRIA].[dbo].[ccWhatsAppConversationsRelationship]
+									WHERE conversationIdAfter = @conversationId
+									)
+								THEN CAST(1 AS BIT)
+							ELSE CAST(0 AS BIT)
+							END
+				END
+			END'
+        EXEC(@sql)
+------------------------------------------------------------  END  ----------------------------------------------------------------------------------------------------------------------------------
+
+		/* End script release */
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		EXEC ccsp_getVersion 'BDF', @versionFix
+
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		/* Error generated based on sintax */
+		SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+
+		RAISERROR (@errorGenerated, 11, 1)
+
+		ROLLBACK TRAN
+	END CATCH
+END

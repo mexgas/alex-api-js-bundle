2f21a68e7d396db45255b56de613fd5b0a4e2b2d
mexgas <mexicanlocos@gmail.com>
2022-09-14 11:08:51 -0500

    Se agrega para la tarea KR011000 Notificación recepción de llamada de entrada

--- CCenterRIA/UpdateDB/20220819_v124.12_Agosto.sql
+++ CCenterRIA/UpdateDB/20220819_v124.12_Agosto.sql
@@ -4764,6 +4764,176 @@ set nocount off'
 		EXEC(@sql)
         ------------------------------------------------------------  END  ANIRotative ---------------------------------------------------------------------
 
+        ------------------------------------------------------------  KR011000 Notificación recepción de llamada de entrada ---------------------------------------------------------------------
+
+set @process = 'CW-7276 KR011000 CREATE TABLE ccAgentMsgFiles'
+set @sql = 'if not exists(select * from sys.tables where name=''ccAgentMsgFiles'') begin
+CREATE TABLE [dbo].[ccAgentMsgFiles](
+        [MsgId] [int] primary key identity NOT NULL,
+        [MsgFile] [varchar](100) NOT NULL,
+        [Description] [varchar](40) NOT NULL,
+        [Duration] [smallint] NOT NULL,
+        [MsgName] [varchar](40) NOT NULL)
+end'
+EXEC(@sql)
+
+set @process = 'CW-7276 KR011000 CREATE TABLE ccAgentMsgRelationFiles'
+set @sql = 'if not exists(select * from sys.tables where name=''ccAgentMsgRelationFiles'') begin
+CREATE TABLE [dbo].[ccAgentMsgRelationFiles](
+        [MsgId] [int] NOT NULL FOREIGN KEY REFERENCES ccAgentMsgFiles(MsgId),
+        [CamId] [int] NOT NULL,
+        [CamType] [tinyint] NOT NULL,
+        primary key([MsgId],[CamId],[CamType])  
+        )               
+end'
+EXEC(@sql)
+
+set @process = 'CW-7276 KR011000 Add ccTipoMsgs 16'
+set @sql = 'if not exists(select * from ccTipoMsgs where tipomsg_id=16) begin
+        insert into ccTipoMsgs values(16,''Message Agent befor xfer'',''Message Agent befor xfer'')
+end'
+EXEC(@sql)
+
+set @process = 'CW-7276 KR011000 CREATE PROCEDURE ccsp_GalateaAgentAutomaticMessages'
+set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_GalateaAgentAutomaticMessages'')
+    begin
+        DROP PROCEDURE ccsp_GalateaAgentAutomaticMessages;
+    end'
+EXEC(@sql)
+
+set @process = 'CW-7276 KR011000 CREATE PROCEDURE ccsp_GalateaAgentAutomaticMessages'
+set @sql = 'CREATE PROCEDURE [dbo].[ccsp_GalateaAgentAutomaticMessages] 
+        @action as tinyint,
+        @msgName as varchar(40) = '''',
+        @msgFile as varchar(100) = null,
+        @Description as varchar(40) = '''',
+        @duration as int = -1,
+        @CampType tinyint = 0,
+        @msgIdLst varchar(8000) = null,
+        @camId int =null,
+        @MsgId int = null
+AS
+BEGIN
+        SET NOCOUNT ON
+        declare @tableMsgId table(MsgId int not null)
+        declare @campName varchar(70)
+
+        if @action in (3,7) begin --Assin/Unassign
+                if @CampType=0
+                        select @campName =descripcion from ccInbound where Inbound_id=@camId
+                else
+                        select @campName =cam_descripcion from ccCamps where cam_id=@camId
+        end
+
+        if @action = 1  -- GET_AUDIO_CATALOG
+        begin
+                select ISNULL(msgName, msgFile) [MsgName], [Description] [MsgDescription], [MsgId] [MsgId] from ccAgentMsgFiles         
+                return (0)
+        end
+        else if @action = 2 --CREATE_NEW_MSG
+        begin
+                if EXISTS(select msgName from ccAgentMsgFiles where msgName=@msgName)
+                begin
+                        select -1 as result
+                end
+                else
+                begin 
+                        insert into ccAgentMsgFiles (msgFile, [Description], Duration, msgName) 
+                        values (@msgFile, @Description, @duration, @msgName)
+                        select cast(@@identity as int) as result
+                end 
+    
+        end 
+        else IF @action = 3 -- Assing
+        begin   
+                if not exists(select MsgId from ccAgentMsgFiles where MsgId=@MsgId)
+                begin
+                        select ''0'' as result
+                        return(0)
+                end
+
+                if exists(select MsgId from [ccAgentMsgRelationFiles] where CamId=@camId and CamType=@CampType)
+                begin
+                        select ''-1'' as result
+                        return(0)
+                end
+
+                insert into [ccAgentMsgRelationFiles]    values(@MsgId,@camId,@CampType)
+
+                select @campName
+
+        end
+        
+        else IF @action = 4 -- GET_CAMP_MESSAGES_RELATION
+        begin   
+                select MsgId from [ccAgentMsgRelationFiles] where CamId=@camId and CamType=@CampType             
+        end
+        else IF @action = 5 -- DELETE_AUDIO_MSG
+        begin
+        
+                insert into @tableMsgId
+                select value from dbo.fn_RIASplitDelimited(@msgIdLst, '','')
+
+                if exists(select A.MsgId from [ccAgentMsgRelationFiles] A 
+                                  inner join @tableMsgId B on A.MsgId=B.MsgId
+                )
+                begin
+                        select 0 as result
+                        return(0)
+                end
+
+                 delete A from ccAgentMsgFiles A 
+                 inner join @tableMsgId B on A.MsgId=B.MsgId
+                 
+                 select 1 as result  
+                 return(0)
+        end
+                
+        else if @action = 6 --EDIT_AUDIO_MSG
+        BEGIN    
+                update ccAgentMsgFiles set [Description] = isnull(@Description,[Description]), MsgName = isnull(@msgName,MsgName),
+                MsgFile = isnull(@msgFile,MsgFile), Duration=case when @duration is null or @duration<=0 then Duration else @duration end
+                where MsgId = @MsgId    
+        END
+        else IF @action = 7 -- UnAssing
+        begin           
+                if not exists(select MsgId from [ccAgentMsgRelationFiles] where MsgId=@MsgId and CamId=@camId and CamType=@CampType)
+                begin
+                        select ''-1'' as result
+                        return(0)
+                end
+
+                delete from [ccAgentMsgRelationFiles] where MsgId=@MsgId and CamId=@camId and CamType=@CampType 
+                select @campName
+        end
+        
+        else IF @action = 8 -- list fileName
+        begin                           
+                insert into @tableMsgId
+                select value from dbo.fn_RIASplitDelimited(@msgIdLst, '','')
+                
+                select A.MsgFile from ccAgentMsgFiles A 
+                                  inner join @tableMsgId B on A.MsgId=B.MsgId
+        end
+        else IF @action = 9 -- Relation CampIn and MsgFile
+        begin                           
+                select A.CamId,B.MsgFile,B.Duration from [ccAgentMsgRelationFiles] A
+                inner join ccAgentMsgFiles B on A.MsgId=B.MsgId
+                where CamType=@CampType 
+
+        end
+        else IF @action = 10 -- Relation CampIn and MsgFile
+        begin
+                select MsgId,MsgFile ,Duration from ccAgentMsgFiles where MsgId=@MsgId
+
+        end
+        
+END
+'
+EXEC(@sql)
+        
+        ------------------------------------------------------------  END  KR011000 Notificación recepción de llamada de entrada ---------------------------------------------------------------------
+
 
 		/* End script release */
 		/* Upgrade database version (use your own script to do it) */

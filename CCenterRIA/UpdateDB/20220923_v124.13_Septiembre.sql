d04136c642cd109f3ac4f16cc464e352b6d0c1b5
EduardoF <eflores@nuxiba.com>
2022-09-22 20:06:31 -0500

    fix

--- CCenterRIA/UpdateDB/20220923_v124.13_Septiembre.sql
+++ CCenterRIA/UpdateDB/20220923_v124.13_Septiembre.sql
@@ -50,7 +50,7 @@ BEGIN
 	 ------------------------------------------------------------ DEV2-3_K004023-labels-reports---------------------------------------------------------------------
 		set @process = 'DEV2-17_K004022-Admin-Config_Tiempo_Preview add column cam_tPreview'
         set @sql = '
-		 if not exists(select typeProcess_id from ccTypeProcessPreview where typeProcess_id = 7)
+		 if not exists(select typeProcess_id from ccTypeProcessPreview where typeProcess_id = 8)
 					begin
 					insert into  ccTypeProcessPreview (typeProcess_id,descripcion,translatedDesc) values (8, ''systemTranslated_DiscardByMaxTimes'','''')
 		end		

fc875937cd931c3ae733917902f45ea50adb9c65
JCLares <jesus.castro@nuxiba.com>
2022-10-06 16:29:03 -0500

    Se regreso consulta original, remplazando INNER por LEFT a la tabla ccInbound

--- CCenterRIA/UpdateDB/20220819_v124.12_Agosto.sql
+++ CCenterRIA/UpdateDB/20220819_v124.12_Agosto.sql
@@ -6222,17 +6222,16 @@ BEGIN
   AND C.TipoUser_id = 1
     ORDER BY A.User_id;
 
-    INSERT INTO @tmpCamAgent SELECT DISTINCT campPerWg.IdCampEsp, wgUser.User_id,
-    CASE WHEN @Id = 0 AND @CampType = 0 THEN inbound.chat ELSE NULL END
-    FROM ccRIACampEspWG campPerWg
-    INNER JOIN @AdminWorkgroups wg ON wg.Id = campPerWg.IDWG
-    INNER JOIN ccRIAWorkGroupUsers wgUser ON wgUser.IDWG = wg.id
-    INNER JOIN ccUsers C ON wgUser.User_id = C.User_id
-    LEFT JOIN ccInbound inbound ON Inbound_id = campPerWg.IdCampEsp
-    LEFT JOIN ccCamps outbound ON outbound.cam_id = campPerWg.IdCampEsp 
-    AND C.TipoUser_id = 1
-    WHERE campPerWg.Tipo = @CampType
-    AND (@Id = 0 OR campPerWg.IdCampEsp = @Id);
+  INSERT INTO @tmpCamAgent SELECT DISTINCT campPerWg.IdCampEsp, wgUser.User_id,
+  CASE WHEN @Id = 0 AND @CampType = 0 THEN inbound.chat ELSE NULL END
+  FROM ccRIACampEspWG campPerWg
+  INNER JOIN @AdminWorkgroups wg ON wg.Id = campPerWg.IDWG
+  INNER JOIN ccRIAWorkGroupUsers wgUser (nolock) ON wgUser.IDWG = wg.id
+  INNER JOIN ccUsers C ON wgUser.User_id = C.User_id
+  LEFT JOIN ccInbound inbound ON Inbound_id = campPerWg.IdCampEsp 
+  AND C.TipoUser_id = 1
+  WHERE campPerWg.Tipo = @CampType
+  AND (@Id = 0 OR campPerWg.IdCampEsp = @Id);
   
   WITH lastState AS (
   SELECT A.user_id, MAX(A.fecha) AS fecha

6dfae3f4347fc0a5e5cb22fbfb0d9ee35ec8c203
Gerardo_Zumaya <gmorales@nuxiba.com>
2022-11-30 11:01:03 -0600

    Se crea sp  ccsp_RIAGetNotReadyHistoryToAdmin para obtener los no disponibles de los agentes

---
+++ CCenterRIA/UpdateDB/20221129_v124.22_Noviembre.sql
@@ -0,0 +1,139 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2022/02/15
+Description: Merge con los cambios de sorteos
+
+Database: CCenterRia
+Required version: 123.27
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+SET @version = 124 --**********actualizar a 123 sin fix
+SET @versionfix = 22
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD'
+
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 4;
+
+IF @actualVersion = @version and @actualVersionFix >= @versionfix - 1
+BEGIN
+	BEGIN TRAN
+
+	BEGIN TRY
+
+
+		set @process = 'KR027001_NoDisponibles - Se quita el SP si ya existe'
+    set @sql = 'if exists (select * from sys.procedures where name = N''ccsp_RIAGetNotReadyHistoryToAdmin'')
+            begin
+          		DROP PROCEDURE ccsp_RIAGetNotReadyHistoryToAdmin;
+            end'
+    EXEC(@sql)
+
+    set @process = 'KR027001_NoDisponibles ccsp_RIAGetNotReadyHistoryToAdmin se crea'
+    set @sql = 'CREATE PROCEDURE [dbo].[ccsp_RIAGetNotReadyHistoryToAdmin]
+		@action INT = 0,
+		@user_id INT = 0
+		AS
+		SET NOCOUNT ON
+		-- Para horarios depues de las 12 de la noche
+		DECLARE @fStart DATETIME, @fEnd DATETIME
+		DECLARE @inicioTurno INT, @AcumTime INT
+		DECLARE @fecha SMALLDATETIME
+
+		SET @inicioTurno = 2 --Cambio de dia a las 2 de la mañana
+		SET @fecha = getdate()
+
+		IF datepart(hh,@fecha)>@inicioTurno-1
+		 BEGIN	
+			SET @fStart=CONVERT(DATETIME, CONVERT(VARCHAR(11), @fecha, 121) + CAST(@inicioTurno AS VARCHAR) +'':00'', 121)
+			SET @fEnd=DATEADD(d,1,@fstart)
+		 END
+
+		ELSE
+		 BEGIN
+			SET @fEnd=CONVERT(DATETIME, CONVERT(VARCHAR(11), @fecha, 121) + CAST(@inicioTurno AS VARCHAR) +'':00'', 121)
+			SET @fStart=DATEADD(d,-1,@fEnd)
+		 END
+
+		IF @action = 1
+			BEGIN
+				SELECT 
+					l.user_id AS AgentId,
+					l.tiponotready_id AS TipoNotReady_id, 
+					Descripcion, 
+					Frame, 
+					CONVERT(CHAR(8),DATEADD(SECOND,SUM(tStatus),0),108) AS Tiempo,
+					COUNT(l.tiponotready_id) AS Veces
+					FROM ccLogAgentesNotReady l 
+					INNER JOIN ccTipoNotReady t ON l.tiponotready_id = t.tiponotready_id
+					INNER JOIN ccRIAnotreadyGraph a2 ON (t.tiponotready_id=a2.tiponotready_id)
+					INNER JOIN ccRIAGraphics a3 ON (a2.graphic_id=a3.graphic_id)
+					WHERE fecha BETWEEN @fStart AND @fEnd
+					GROUP BY l.user_id, t.descripcion, l.tiponotready_id, frame
+			END
+		ELSE IF @action = 2 and @user_id > 0
+			BEGIN
+				SELECT 
+					l.user_id AS AgentId,
+					l.tiponotready_id AS TipoNotReady_id, 
+					Descripcion, 
+					Frame, 
+					CONVERT(CHAR(8),DATEADD(SECOND,SUM(tStatus),0),108) AS Tiempo,
+					COUNT(l.tiponotready_id) AS Veces
+					FROM ccLogAgentesNotReady l 
+					INNER JOIN ccTipoNotReady t ON l.tiponotready_id = t.tiponotready_id
+					INNER JOIN ccRIAnotreadyGraph a2 ON (t.tiponotready_id=a2.tiponotready_id)
+					INNER JOIN ccRIAGraphics a3 ON (a2.graphic_id=a3.graphic_id)
+					WHERE fecha BETWEEN @fStart AND @fEnd AND user_id = @user_id
+					GROUP BY l.user_id, t.descripcion, l.tiponotready_id, frame
+			END
+
+		SET NOCOUNT OFF'
+    EXEC(@sql)
+
+
+		/* End script release */
+		/* Upgrade database version (use your own script to do it) */
+		--exec ccsp_getVersion 'BD', @version
+		EXEC ccsp_getVersion 'BDF', @versionFix
+
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		/* Error generated based on sintax */
+		SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+
+		RAISERROR (@errorGenerated, 11, 1)
+
+		ROLLBACK TRAN
+	END CATCH
+END
+

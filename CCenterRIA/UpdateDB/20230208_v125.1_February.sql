4ad4029f16d3470a90f857af7f7427a005512ff8
Marco García <marco.garcia@nuxiba.com>
2023-02-09 16:26:59 -0600

    Se agrego comentarios

--- CCenterRIA/UpdateDB/20230208_v125.1_February.sql
+++ CCenterRIA/UpdateDB/20230208_v125.1_February.sql
@@ -42,6 +42,7 @@ SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
 FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
 WHERE id = 4;
 
+--- Validación para cuando pasamos a una nueva versión LTS
 IF @version > @actualVersion 
 BEGIN 
 	SET @actualVersionFix = 0

994b92df59a7801ecfc4031267cc310347c447dd
Marco Antonio Chagolla <marco.chagolla@nuxiba.com>
2023-03-02 14:13:00 -0600

    Se cambian obtener el numero de WA asiciado a una campaña y para desaociar el numero cuando se elimina una campaña

--- CCenterRIA/UpdateDB/WhatsOut.sql
+++ CCenterRIA/UpdateDB/WhatsOut.sql
@@ -3265,6 +3265,226 @@ SET NOCOUNT OFF;
 '
 		EXEC(@sql)
 
+
+		SET @process = 'ccsp_MultimediaConfigurations se opcion 4 para obtener el numero de wa asiciado a la campaña'
+		SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_MultimediaConfigurations] 
+				@Option AS SMALLINT,
+				@ServiceType AS SMALLINT = 0,
+				@Number AS VARCHAR(25) = '''',
+				@Id AS INT = 0,
+				@CamType AS SMALLINT = 0
+				AS
+				BEGIN
+				    SET NOCOUNT ON;
+					BEGIN
+				    IF(@Option = 1) -- Get Vonage Configurations depending the Service Type and number 
+						BEGIN
+							SELECT config.applicationId AS ApplicationId,
+								   config.secretKey AS SecretKey,
+								   config.messagesUrl AS MessagesUrl
+							FROM ccVonageConfigurations config
+							INNER JOIN ccWhatsAppNumbers numbers ON config.vonageId = numbers.vonageId 
+							AND numbers.number = @Number 
+							AND config.serviceType = @ServiceType   -- 5 = WhatsApp
+						END 
+
+					IF(@Option = 2) -- Get WhatsApp registered numbers 
+						BEGIN
+							SELECT number AS AvailableNumbers FROM ccWhatsAppNumbers Numbers 
+							INNER JOIN ccVonageConfigurations Configurations 
+							ON Numbers.vonageId = Configurations.vonageId 
+							AND Numbers.inboundId = 0 
+							AND Numbers.status = 1 
+							AND Configurations.serviceType = 5
+						END 
+					IF(@Option = 3) -- Get WhatsApp registered numbers Outbound
+						BEGIN
+							SELECT number AS AvailableNumbers FROM ccWhatsAppNumbers Numbers 
+							INNER JOIN ccVonageConfigurations Configurations 
+							ON Numbers.vonageId = Configurations.vonageId 
+							AND (Numbers.camp_id  = 0 or Numbers.camp_id IS NULL)
+							AND Numbers.status = 1 
+							AND Configurations.serviceType = 5
+						END
+					IF(@Option = 4) -- Get associated whatsapp number
+						BEGIN
+							IF (@CamType = 0) BEGIN
+								SELECT @Number = number from ccWhatsAppNumbers WHERE inboundId = @Id
+							END
+							ELSE BEGIN
+								SELECT @Number = number from ccWhatsAppNumbers WHERE camp_id = @Id
+							END
+							SELECT ISNULL(@Number, '''') as number
+						END
+					END
+				END'
+		EXEC(@sql)
+
+
+		SET @process = 'ccsp_GalateaDeleteCampaignAndACD se agrega MediaType para validar si es una campañade WA'
+		SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaDeleteCampaignAndACD]
+			@userId           SMALLINT,
+			@DeleteCamId      VARCHAR(MAX),
+			@DeleteACDGroupId VARCHAR(MAX),
+			@moduleId         SMALLINT = 49
+		AS
+		BEGIN
+
+			IF OBJECT_ID(''tempdb..#CampsDelete'') IS NOT NULL DROP TABLE #CampsDelete
+				SELECT value As DeleteCamId, c.IDArea AS IDAreaCamp, 1 AS CampTypeCamp, ISNULL(wg.IDWG,0) as IDWG, ISNULL(m.meanContactTypeId, 0) AS MediaType
+				INTO #CampsDelete
+				FROM fn_RIASplitDelimited(@DeleteCamId, '','') a
+				inner join ccCamps c on  a.value = c.cam_id and c.IDArea IS NOT NULL
+				left join ccRIACampEspWG wg on a.Value = wg.IdCampEsp and wg.Tipo=1
+				left join contactMeanOut m on c.cam_id = m.camp_id
+			IF OBJECT_ID(''tempdb..#ACDDelete'') IS NOT NULL DROP TABLE #ACDDelete
+				SELECT value As DeleteACDId, c.IDArea AS IDAreaACD, 0 AS CampTypeACD, ISNULL(wg.IDWG,0) as IDWG, ISNULL(m.meanContactTypeId, 0) AS MediaType
+				INTO #ACDDelete
+				FROM fn_RIASplitDelimited(@DeleteACDGroupId, '','') a
+				inner join ccInbound c on  a.value = c.Inbound_id and c.IDArea IS NOT NULL
+				left join ccRIACampEspWG wg on a.Value = wg.IdCampEsp and wg.Tipo=0
+				left join contactMeanIn m on c.Inbound_id = m.inboundId
+
+			IF  not Exists (select * from #CampsDelete union select * from #ACDDelete )
+			begin
+				select ''-1'' AS Result
+				return
+			end
+
+			IF datalength(@DeleteCamId) > 0
+				BEGIN
+
+				if exists(select cam_id from ccInbound where cam_id in (select DeleteCamId from #CampsDelete)) begin
+					--Borra las calificacion con reprogramacion
+					delete ccCalifCamp from ccInbound A
+					inner join ccCalifCamp B on A.Inbound_id=B.cam_id and  B.tipo=0
+					inner join ccTipoCalif C on B.calif_id=C.calif_id and C.CanReprogram=1
+					where A.cam_id in (select DeleteCamId from #CampsDelete)
+					--Borra las subcalificacion con reprogramacion
+					delete rel from ccInbound A
+					inner join ccCalifCamp B on A.Inbound_id=B.cam_id and  B.tipo=0
+					inner join ccTipoCalif C on B.calif_id=C.calif_id
+					inner join cctipoSubCalifRel rel on rel.calif_id=C.calif_id and rel.tipoSubRel=1
+					inner join ccTipoCalifSub sb on rel.califsub_id=sb.califsub_id
+					where A.cam_id in (select DeleteCamId from #CampsDelete) and sb.canReprogram=1
+
+					update ccInbound set cam_id = null where cam_id in (select DeleteCamId from #CampsDelete)
+
+				end
+
+				insert into ccCampsAgenteBackUp(user_id,cam_id,prioridad,skill,rel_id,IDWG)
+				select A.user_id,A.cam_id,A.prioridad,A.skill,A.rel_id,A.IDWG from ccCampsAgente A left join ccCampsAgenteBackUp B on A.user_Id=B.user_id and A.cam_id=B.cam_id where B.User_id is null and A.cam_id in (select DeleteCamId from #CampsDelete)
+
+				delete from ccCampsAgente where cam_id in (select DeleteCamId from #CampsDelete)
+				insert into ccSupervisorCamBackup(user_id,cam_id,tipo,IDWG,monitored)
+				select A.user_id,A.cam_id,A.tipo,A.IDWG,A.monitored from ccSupervisorCam A left join ccSupervisorCam B on A.user_Id=B.user_id and A.cam_id=B.cam_id where B.User_id is null and A.cam_id in (select DeleteCamId from #CampsDelete) and A.tipo = 1
+
+				delete from ccSupervisorCam where cam_id in (select DeleteCamId from #CampsDelete) and tipo = 1
+				delete from ccRIACampEspWG where IdCampEsp in (select DeleteCamId from #CampsDelete) and tipo = 1
+
+				IF OBJECT_ID(''tempdb..#CampLog'') IS NOT NULL DROP TABLE #CampLog
+				SELECT ca.AreaName,
+					   GETDATE() operationDate,
+					   27 operationType,
+					   (SELECT Login FROM ccUsers WHERE User_Id = @userId) login,
+					   @moduleId module_id,
+					   c.cam_descripcion value,
+					   ca.AreaName AS target
+				INTO #CampLog
+				FROM ccRIACat_Areas ca
+				Inner join ccCamps c with(nolock) on ca.IDArea = c.IDArea
+				WHERE c.cam_id in (select DeleteCamId from #CampsDelete)
+
+				Update ccCamps set IDArea = null where cam_id in (select DeleteCamId from #CampsDelete)
+		
+				update contactMeanOut set name = '''', conexionInfo = '''', connUser = '''', isActive = 0
+				where camp_id in (SELECT DeleteCamId FROM #CampsDelete) and meanContactTypeId=5
+        
+				update ccWhatsAppNumbers set camp_id = 0 where camp_id in (select DeleteCamId from #CampsDelete)
+        
+
+			END
+			IF datalength(@DeleteACDGroupId) > 0
+				BEGIN
+
+				if exists(select top 1 cam_id from ccInbound where Inbound_id in (select DeleteACDId from #ACDDelete))
+					begin
+						update ccInbound set cam_id = null where Inbound_id in (select DeleteACDId from #ACDDelete)
+				end
+
+				IF OBJECT_ID(''tempdb..#AllWGACD'') IS NOT NULL DROP TABLE #AllWGACD
+				SELECT DISTINCT(IDWG)
+				INTO #AllWGACD
+				FROM ccRIACampEspWG ce
+				WHERE IDCampEsp in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+
+				insert into ccInboundAgentesBackup(user_id,Inbound_id,cli_id,prioridad,skill,rel_id,IDWG)
+				select A.user_id,A.Inbound_id,A.cli_id,A.prioridad,A.skill,A.rel_id,A.IDWG
+				from ccInboundAgentes A left join ccInboundAgentesBackup B on A.user_Id=B.user_id and A.Inbound_id=B.Inbound_id
+				where B.User_id is null and A.Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+				delete ccInboundHorarios Where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+				delete ccInboundMsgs Where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+				delete ccInboundDnis where inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+				insert into ccCampsAgenteBackUp(user_id,cam_id,prioridad,skill,rel_id,IDWG)
+				select A.user_id,A.cam_id,A.prioridad,A.skill,A.rel_id,A.IDWG
+				from ccCampsAgente A left join ccCampsAgenteBackUp B on A.user_Id=B.user_id and A.cam_id=B.cam_id
+				where B.User_id is null and A.cam_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+				delete ccSupervisorCam where cam_id in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+				delete ccInboundAgentes where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+				delete ccRIACampEspWG where IdCampEsp  in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+
+
+				IF OBJECT_ID(''tempdb..#ACDLog'') IS NOT NULL DROP TABLE #ACDLog
+				SELECT ca.AreaName,
+						GETDATE() operationDate,
+						28 operationType,
+						(SELECT Login FROM ccUsers WHERE User_Id = @userId) login,
+						@moduleId module_id,
+						i.descripcion value,
+						ca.AreaName AS target
+				INTO #ACDLog
+				FROM ccRIACat_Areas ca
+				inner join ccInbound i with(nolock) on ca.IDArea = i.IDArea
+				WHERE i.Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+				Update ccInbound set IDArea = null, status = 0 where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+				if exists(select * from ContactMeanIn where meanContactTypeId=2 and inboundId in (SELECT DeleteACDId FROM #ACDDelete))--Si encuentra un registro en contactMeanIn de tipo twitter asociado al ACD
+					begin
+						update ContactMeanIn set name = '''', conexionInfo = ''usuarioID|token|tokenSecret|1|0'', connUser = '''', isActive = 0
+						where inboundId in (SELECT DeleteACDId FROM #ACDDelete) and meanContactTypeId=2
+				end
+				if exists(select * from ContactMeanIn where meanContactTypeId=1 and inboundId in (SELECT DeleteACDId FROM #ACDDelete))--Si encuentra un registro en contactMeanIn de tipo twitter asociado al ACD
+					begin
+						update ContactMeanIn set name = '''', conexionInfo = '''', connUser = '''', connpass='''', isActive = 0 where inboundId in (SELECT DeleteACDId FROM #ACDDelete) and meanContactTypeId=1
+				end
+				update ccinbound set chatDomain = '''' where inbound_id in (SELECT DeleteACDId FROM #ACDDelete)--para desasociar el dominio del chat
+
+				if exists (SELECT inboundId FROM contactMeanIn WHERE inboundId in (select DeleteACDId from #ACDDelete))
+					begin
+						update contactMeanIn set isActive = 0 where inboundId in (select DeleteACDId from #ACDDelete)
+				end            
+				update ccWhatsAppNumbers set inboundId = 0 where inboundId in (select DeleteACDId from #ACDDelete)
+        
+			END
+
+			IF datalength(@DeleteCamId) > 0
+				Insert into ccRIALog Select * from #CampLog
+			IF datalength(@DeleteACDGroupId) > 0
+				Insert into ccRIALog Select * from #ACDLog
+
+			SELECT DeleteCamId AS DeleteId,IDAreaCamp AS IDArea,CampTypeCamp AS CampType,''1'' AS Result, cast(IDWG as smallint) IDWG, MediaType FROM #CampsDelete
+			UNION
+			SELECT DeleteACDId,IDAreaACD,CampTypeACD,''1'' AS Result, cast(IDWG as smallint) IDWG, MediaType FROM #ACDDelete
+			IF OBJECT_ID(''tempdb..#CampsDelete'') IS NOT NULL DROP TABLE #CampsDelete
+			IF OBJECT_ID(''tempdb..#ACDDelete'') IS NOT NULL DROP TABLE #ACDDelete
+			IF OBJECT_ID(''tempdb..#CampLog'') IS NOT NULL DROP TABLE #CampLog
+		END'
+		EXEC(@sql)
+
 		------------------------------------------------- END BEGIN Alter Store ----------------------------------------------------------------------
 
 		/* End script release */

5dae42c3d5b34b17e276ce4bdbe073f30500e008
ivanmartinenciso <imartin@nuxiba.com>
2023-04-14 06:27:49 -0600

    Se agregan Sps con modificaciones para Templates de salida

--- CCenterRIA/UpdateDB/IM-K020099_Create_Outbound_Templates_Tables.sql
+++ CCenterRIA/UpdateDB/IM-K020099_Create_Outbound_Templates_Tables.sql
@@ -58,17 +58,6 @@ BEGIN
 
 	BEGIN TRY
 		-------------------------------------------- BEGIN IVAN K020099 OUTBOUND TEMPLATES ------------------------------
-		SET @process = 'IM-K020099 Create reference table for languages (ccWhatsAppOutboundTemplateLanguages)'
-		SET @sql = 'IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = N''ccWhatsAppOutboundTemplateLanguages'')
-					BEGIN
-						CREATE TABLE ccWhatsAppOutboundTemplateLanguages (
-							LanguageId INT NOT NULL,
-							Language varchar(100) NOT NULL,
-							LanguageCode varchar(50)NOT NULL
-							PRIMARY KEY (LanguageId)
-						);
-					END'
-		EXEC(@sql)
 
 		SET @process = 'IM-K020099 Create table for templates (ccWhatsAppOutboundTemplates)'
 		SET @sql = 'IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = N''ccWhatsAppOutboundTemplates'')
@@ -76,12 +65,109 @@ BEGIN
 						CREATE TABLE ccWhatsAppOutboundTemplates (
 							TemplateId INT NOT NULL,
 							Category VARCHAR(25) NOT NULL,
-							TableName VARCHAR(500),
-							LanguageId INT,
-							State BIT,
-							AsociatedNumber VARCHAR(30)
+							TemplateName VARCHAR(500),
+							LanguageCode VARCHAR(10),
+							Status BIT,
+							AsociatedNumber VARCHAR(30),
+							Type VARCHAR(50),
+							Format VARCHAR(15),
+							Body VARCHAR(MAX),
 							PRIMARY KEY (TemplateId)
 						);
+					END'
+		EXEC(@sql)
+
+		SET @process = 'IM-K020099 Drop procedure if exists'
+		SET @sql = 'IF EXISTS(SELECT * FROM sys.procedures WHERE name = N''ccsp_WhatsAppOutboundTemplates'')
+					BEGIN
+						DROP PROCEDURE ccsp_WhatsAppOutboundTemplates;
+					END'
+		EXEC(@sql)
+
+		SET @process = 'IM-K020099 Create new procedure for WhatsApp Outbound Templates'
+		SET @sql = 'CREATE PROCEDURE [dbo].[ccsp_WhatsAppOutboundTemplates] 
+					@Action SMALLINT, 
+					@TemplateName VARCHAR(500) = '''' 
+					AS  
+					SET NOCOUNT ON;  
+					IF @Action = 0  -- Get all template information
+					BEGIN
+						SELECT TemplateName, LanguageCode, Type, Format, Body FROM ccWhatsAppOutboundTemplates WHERE TemplateName = @TemplateName
+					END
+					IF @Action = 1  -- Get template body 
+					BEGIN
+						SELECT Body FROM ccWhatsAppOutboundTemplates WHERE TemplateName = @TemplateName
+					END
+					RETURN(0)
+					SET NOCOUNT OFF'
+		EXEC(@sql)
+
+		SET @process = 'IM-K020099 Add action 2 which gets template information to display in Agent UI (lines 163 to 171) '
+		SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_ConversationOutWASave] 
+					@action             INT
+					, @conversationId     INT         = 0
+					, @campId			  INT		  = NULL        
+					, @phoneCamp          VARCHAR(50) = NULL
+					, @clientId           VARCHAR(25) = NULL
+					, @conversationStatus SMALLINT    = 0
+					, @tChatting          FLOAT       = 0
+					, @tWrapUp            SMALLINT    = 0
+					, @finishedBy         TINYINT     = 0
+					, @onQueue            BIT         = NULL
+					, @tQueue             SMALLINT    = 0
+					, @tTimeout           INT         = 0
+					, @disposition        SMALLINT    = 0
+					, @subDisposition     SMALLINT    = 0
+					, @agentId            INT         = 0
+
+					AS
+					BEGIN
+						SET NOCOUNT ON;
+					    
+						declare @conversationIdTemporal     INT;
+
+					IF @action = 1 BEGIN --new Conversation
+					    select @phoneCamp= number from ccWhatsAppNumbers where camp_id= @campId
+							
+						if @phoneCamp is null or @phoneCamp='''' begin
+							select 0 as [ConversationId],0 as [MessageId]
+							return(0)
+						end
+
+						if not exists (select * from ccWhatsAppConversationsOut where phoneCamp = @phoneCamp and clientId = @clientId and DATEDIFF(hh,requestDate,getdate()) <= 23 and finishedBy = 0) begin
+							if not exists (select * from ccWhatsAppConversations where phoneACD = @phoneCamp and clientId = @clientId and DATEDIFF(hh,requestDate,getdate()) <= 23 and finishedBy = 0) begin
+								INSERT INTO [ccWhatsAppConversationsOut]
+								([camId] , [phoneCamp], clientId, conversationStatus, tChatting
+								, tWrapUp, finishedBy, onQueue, tQueue, requestDate
+								, tTimeout, disposition, subDisposition, agentId)
+								VALUES(@campId, @phoneCamp, @clientId, @conversationStatus, @tChatting, @tWrapUp, @finishedBy, 
+								@onQueue, @tQueue, GETDATE(), @tTimeout, @disposition, @subDisposition, @agentId);
+					    
+								SELECT @conversationIdTemporal = SCOPE_IDENTITY();    
+								SELECT @conversationIdTemporal AS [ConversationId],0 as [MessageId]
+							end
+							else begin
+								select A.descripcion CamDescription, B.requestDate RequestDate, B.agentId UserId, C.Login Username 
+								FROM ccInbound A INNER JOIN ccWhatsAppConversations B 
+								ON B.clientId = @clientId AND B.finishedBy = 0 and B.inboundId=A.Inbound_id
+								INNER JOIN ccUsers C ON B.agentId = C.User_id;
+							end  
+						end
+						else begin
+							select A.cam_descripcion CamDescription, B.requestDate RequestDate, B.agentId UserId, C.Login Username
+							FROM ccCamps A INNER JOIN ccWhatsAppConversationsOut B 
+							ON B.clientId = @clientId AND B.finishedBy = 0 and B.camId=A.cam_id
+							INNER JOIN ccUsers C ON B.agentId = C.User_id;
+						end  
+					END 
+					IF @action = 2 -- Get Outbound Templates
+					BEGIN
+						IF @campId IS NOT NULL
+						BEGIN
+							DECLARE @AsociatedNumber VARCHAR(30) = (SELECT number from ccWhatsAppNumbers WHERE @campId = camp_id);
+							SELECT * FROM ccWhatsAppOutboundTemplates WHERE AsociatedNumber = @AsociatedNumber AND Status = 1;
+						END
+					END
 					END'
 		EXEC(@sql)
 

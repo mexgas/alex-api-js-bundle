c1aab5a3c0b6f7db5760d8475fd4d47f1f2e471d
Ivan Martin <imartin@nuxiba.com>
2023-05-16 19:58:28 +0200

    Se cambia archivo de CCenterRia a RecorderRIA

--- RecorderRIA/UpdateDB/20230515_v92_Mayo.sql
+++ RecorderRIA/UpdateDB/20230515_v92_Mayo.sql
@@ -1,62 +1,19 @@
-/*******************************/
-/***** NUXIBA TECHNOLOGIES *****/
-/*******************************/
-/*
-Author:
+set nocount on
+declare @Version int
+declare @Version_Actual int
 
+declare @Sql varchar(max)
+declare @errorGenerated varchar(max)
+declare @process varchar(max)
+---------------- VERSION ----------------
+    Set @Version = 92
+    Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
 
-Date: 2022/11/19
-Description: Cambios para estados de email
+if @Version_Actual in(@Version, @Version -1) -- Aqui poner numero de nueva version
+begin
+    begin tran
+    begin try
 
-Database: CCenterRia
-Required version: 124
-
-IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
-*/
-SET NOCOUNT ON
-
-DECLARE @version INT, @versionFix INT
-DECLARE @actualVersion INT, @actualVersionFix INT
-DECLARE @sql VARCHAR(max)
-DECLARE @errorGenerated VARCHAR(max)
-DECLARE @process VARCHAR(max)
-DECLARE @versionALL VARCHAR(max);
-
-/* Version to release (use the version of your own databse)*/
-/*******************************************************************************************************
-Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
-set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
-sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
-SET @version = 125 --**********actualizar a 124 sin fix
-SET @versionfix = 22
-/* Actual version (use your own script to do it)*/
-EXEC @actualVersion = ccsp_getVersion 'BD'
-
-EXEC @actualVersionFix = ccsp_getVersion 'BDF'
-
-SELECT @versionALL = valor
-FROM ccsettings
-WHERE setting_id = 77;
-
-SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
-FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
-WHERE id = 4;
-
---- Validaci?n para cuando pasamos a una nueva versi?n LTS
-declare @versioMajer int= case when @version > @actualVersion then 1 else 0 end
-
-
-IF @version > @actualVersion 
-BEGIN 
-    SET @actualVersionFix = 0
-    select @version,@actualVersion,@versioMajer
-END
-
-IF @version >= @actualVersion  and @versionfix >= @actualVersionFix 
-BEGIN
-    BEGIN TRAN
-
-    BEGIN TRY
         -------------------------------------------- BEGIN IVAN MARTIN feature/IM-DEV1-239_Send_cal_ids_list_to_front ------------------------------
         SET @process = 'Addition of action 17 to get cal_ids list for outbout and inbound'
         SET @sql = 'ALTER PROCEDURE [dbo].[ccspGalatea_Finder] 
@@ -284,20 +241,22 @@ BEGIN
         EXEC(@sql)
 
         -------------------------------------------- END IVAN MARTIN feature/IM-DEV1-239_Send_cal_ids_list_to_front -------------------------------------------------------------------------------
-        /* End script release */
-        /* Upgrade database version (first and the last number of setting 77) */
-        EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
-        EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
+        
 
-        COMMIT TRAN
-    END TRY
+    update trec_parametros set par_valor = @Version where par_id = 30
+    set @Version_Actual=@Version_Actual+1
 
-    BEGIN CATCH
-        /* Error generated based on sintax */
-        SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+    select par_valor from trec_parametros where par_id = 30
 
-        RAISERROR (@errorGenerated, 11, 1)
+    commit tran
 
-        ROLLBACK TRAN
-    END CATCH
-END
+    end try
+    begin catch
+        select @errorGenerated = 'DB Script Version: ' + cast(@Version as nvarchar) + ' Error Process: ' + @process + ' Line: ' + cast(error_line() as nvarchar) + ' Number: ' + cast(@@error as nvarchar) + ' Message: ' + error_message()
+        RAISERROR(@errorGenerated, 11, 1)
+    rollback tran
+    end catch
+ end
+ else begin
+    select par_valor,'This version is incorrect, need version '+ convert(varchar(max),@Version-1) from trec_parametros where par_id = 30
+ end

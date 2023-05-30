27a53a413a83aee1e7a4b5abf0f46946f51045f7
GabyGuevara <eguevara@nuxiba.com>
2023-05-30 15:09:12 -0600

    historial llamadas por agente

--- CCenterRIA/UpdateDB/20230530_v125.23_May.sql
+++ CCenterRIA/UpdateDB/20230530_v125.23_May.sql
@@ -548,6 +548,94 @@ BEGIN
     EXEC(@sql)
 
 	---------------------------------------END Ricardo ---------------------------------------------------------
+	---------------------------------------BEGIN Gabriela ---------------------------------------------------------
+	SET @process = 'add module calls by agent'
+	SET @sql = 'if not exists (select * from ccGalateaModules where ModuleId=4)
+	begin
+	  insert into ccGalateaModules(ModuleId,MTagEs,MTagEn,MTagPt) values (4,''Factor de marcación fijo'',''Fixed dialing rate'',''Fator de discagem fixo'')
+	end';
+	EXEC(@sql);
+
+
+	SET @process = 'Add operation calls by agent'
+	SET @sql = 'if not exists (select * from ccGalateaOperations where OperationId=62)
+	begin
+	  insert into ccGalateaOperations(OperationId,OpTagEs,OpTagEn,OpTagPt) values (62,''Editar llamadas por agente'',''Edit calls by agent'',''Editar chamadas por agente'')
+	  insert into ccGalateaModOpRelation (ModuleId,OperationId) values(4,62)
+	end';
+	EXEC(@sql);
+
+	
+	SET @process = 'K049003 Add identifier calls by agent'
+	SET @sql = 'if not exists (select * from ccGalateaIdentifiers where Description like ''%T$NUMBER_CALLS%'')
+	begin
+	  insert into ccGalateaIdentifiers(Description,TagEs,TagEn,TagPt) values (''T$NUMBER_CALLS'',''Número de llamadas'',''Number of calls'',''Número de chamadas'')
+	end';
+	EXEC(@sql);
+
+
+	SET @process = 'Drop SP ccsp_RIAUpdateCamConfigExtend'
+	SET @sql = 'IF EXISTS(SELECT 1 FROM sys.procedures WHERE Name = ''ccsp_RIAUpdateCamConfigExtend'')
+			BEGIN
+				DROP PROCEDURE [dbo].[ccsp_RIAUpdateCamConfigExtend]
+			END'
+	EXEC(@sql);
+
+
+	SET @process = 'K049003 Add identifier calls by agent'
+	SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_GalateaConfAggrFct]
+@Type tinyint,    -- 1:Muestra | 2:Actualiza Camp | 3:Actualiza Todas por Usuario
+@cam_id varchar(255) = null,
+@User_id int = null,
+@aggressionFactor float = null
+AS
+set nocount on
+if @Type=1
+ begin
+	if @user_id > 0 and not exists (select * from ccUsers_Roles where User_id = @user_id and Rol_id = (select Rol_id from ccRoles where Level = 7)) begin
+			select cam_id, cam_Descripcion, aggressionFactor
+			from ccCamps where cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+			order by cam_descripcion
+		end
+		else begin
+			select cam_id, cam_Descripcion, aggressionFactor from ccCamps where cam_activo <> 0 and IDArea is not null
+		end
+    return(0)
+ end
+
+if @Type=2
+ begin
+    UPDATE ccCamps SET aggressionFactor = isnull(@aggressionFactor,aggressionFactor) 
+    Where cam_id in (select value from dbo.fn_RIASplitDelimited(@cam_id, '',''))
+
+	if @@ROWCOUNT > 0
+		select cam_id, aggressionFactor, cam_descripcion as cam_description from ccCamps Where cam_id in (select value from dbo.fn_RIASplitDelimited(@cam_id, '',''))
+    return(0)
+ end
+
+if @Type=3
+ begin
+	if @user_id > 0 and not exists (select * from ccUsers_Roles where User_id = @user_id and Rol_id = (select Rol_id from ccRoles where Level = 7)) begin
+			UPDATE ccCamps SET aggressionFactor = isnull(@aggressionFactor,aggressionFactor)
+			Where cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+
+			if @@ROWCOUNT > 0
+				select cam_id, aggressionFactor, cam_descripcion as cam_description from ccCamps where cam_id in (select cam_id from dbo.fGet_CampAcd_Area (@User_id, 1))
+		end
+		else begin
+			UPDATE ccCamps SET aggressionFactor = isnull(@aggressionFactor,aggressionFactor)
+			where cam_activo <> 0 and IDArea is not null
+
+			if @@ROWCOUNT > 0
+				select cam_id, aggressionFactor, cam_descripcion as cam_description from ccCamps where cam_activo <> 0 and IDArea is not null
+		end
+    return(0)
+ end
+
+set nocount off';
+	EXEC(@sql);
+	---------------------------------------END Gabriela ---------------------------------------------------------
+
 
 	
 		/* End script release */

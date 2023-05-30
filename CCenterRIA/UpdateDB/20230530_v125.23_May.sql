9b370b17b56b8cc574d96526ed6e64d053a5c097
ricardomendoza21 <ricardo.mendoza@nuxiba.com>
2023-05-30 13:56:09 -0600

    Nuevo documento K023000-Preview

---
+++ CCenterRIA/UpdateDB/20230530_v125.23_May.sql
@@ -0,0 +1,104 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2022/11/19
+Description: Cambios para estados de email
+
+Database: CCenterRia
+Required version: 124
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+SET @version = 125 --**********actualizar a 124 sin fix
+SET @versionfix = 22
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD'
+
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 4;
+
+--- Validaci�n para cuando pasamos a una nueva versi�n LTS
+declare @versioMajer int= case when @version > @actualVersion then 1 else 0 end
+
+
+IF @version > @actualVersion 
+BEGIN 
+	SET @actualVersionFix = 0
+	select @version,@actualVersion,@versioMajer
+END
+
+IF @version >= @actualVersion and @versionfix >= @actualVersionFix 
+BEGIN
+	BEGIN TRAN
+	BEGIN TRY
+
+	---------------------------------------BEGIN Ricardo ---------------------------------------------------------
+
+	set @process = 'KR082000 Drop SP ccspGalatea_Finder'
+	set @sql = 'IF NOT EXISTS (SELECT * FROM ccSettings WHERE setting_id = 248) INSERT INTO [dbo].[ccSettings]
+           ([setting_id]
+           ,[valor]
+           ,[descripcion]
+           ,[Status]
+           ,[Tipo]
+           ,[detalle]
+           ,[description]
+           ,[bLoadSettings]
+           ,[validate])
+     VALUES
+           (248
+           ,0
+           ,''Permite determinar la forma en que deben ser asignados los registros al tener configurada la campaña con vista simultánea de registros >1''
+           ,1
+           ,''X''
+           ,''0: Asignar registros hasta que el agente termine de atender los registros que tiene asignados por el sistema (valor default). 1: Asignar registros conforme el agente libere registros previamente asignados (dependiendo de cómo tenga configurado el parámetro vista simultánea de registros la campaña que está trabajando el agente)''
+           ,''It allows determining the way records should be assigned when the campaign is configured with simultaneous viewing of records >1''
+           ,0
+           ,''.*'')'
+    EXEC(@sql)
+
+	---------------------------------------END Ricardo ---------------------------------------------------------
+
+	
+		/* End script release */
+		/* Upgrade database version (first and the last number of setting 77) */
+		EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
+		EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
+
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		/* Error generated based on sintax */
+		SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+
+		RAISERROR (@errorGenerated, 11, 1)
+
+		ROLLBACK TRAN
+	END CATCH
+END

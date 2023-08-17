11354af28a16f66c98d677d43cb67da720428ccc
FriAleOm <frida.orta@nuxiba.com>
2023-08-17 11:08:44 -0600

    modificaciones para información de llamadas

---
+++ CCenterRIA/UpdateDB/20230816_v125.35_August.sql
@@ -0,0 +1,143 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2023/07/04
+Description: K053000
+
+Database: CCenterRia
+Required version: 125.31
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+SET @version = 125 --**********actualizar a 124 sin fix
+SET @versionfix = 35
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD'
+
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 4;
+
+--- Validacion para cuando pasamos a una nueva version LTS
+declare @versioMajer int= case when @version > @actualVersion then 1 else 0 end
+
+
+IF @version > @actualVersion 
+BEGIN 
+	SET @actualVersionFix = 0
+	select @version,@actualVersion,@versioMajer
+END
+
+IF @version >= @actualVersion and @versionfix >= @actualVersionFix 
+BEGIN
+	BEGIN TRAN
+	BEGIN TRY
+
+	
+	SET @process = 'DEV2-253 se agrega tradcucción (cancelada en diálogo) a la tabla ccTypeProcessPreview '
+	SET @sql = '
+	if not exists(select typeProcess_id from ccTypeProcessPreview where typeProcess_id = 13)
+		begin
+			insert into ccTypeProcessPreview (typeProcess_id,descripcion,translatedDesc) values (13, ''CancelledByEngaged'',''systemTranslated_CancelledByEngaged'')
+		end'
+	EXEC(@sql)
+
+	SET @process = 'DEV2-253 se agrega tradcucción (cancelada) a la tabla ccTypeProcessPreview '
+	SET @sql = '
+	if not exists(select typeProcess_id from ccTypeProcessPreview where typeProcess_id = 14)
+		begin
+			insert into ccTypeProcessPreview (typeProcess_id,descripcion,translatedDesc) values (14, ''Cancelled'',''systemTranslated_Cancelled'')
+		end'
+	EXEC(@sql)
+
+	SET @process = 'DEV2-253 DROP PROCEDURE ccsp_GetPreviewHistory '
+	SET @sql = '
+	if exists (select * from sys.procedures where name = N''ccsp_GetPreviewHistory'')
+    begin
+        DROP PROCEDURE ccsp_GetPreviewHistory;
+    end'
+	EXEC(@sql)
+	
+	SET @process = 'DEV2-253 CREATE PROCEDURE ccsp_GetPreviewHistory '
+	SET @sql = '
+	CREATE Proc [dbo].[ccsp_GetPreviewHistory]( @callOut_Id int ,@initialRow smallint,@finalRow smallint)
+		AS
+		declare @initialDate datetime, @finalDate datetime
+		set @finalDate= GETDATE()
+		set @initialDate = (select DATEDIFF(day,30,@finalDate))
+
+		declare @temTable table (callOut_id int, dialResult varchar(50),disposition varchar(100),date datetime)
+		insert into @temTable 
+					select co.callout_id as callOut_id, 
+					trd.descTranslate dialResult,
+					ISNULL( tco.Description,'''') as calificacion,
+					ld.fecha as fecha
+					from ccoCallsOut co 
+					left join ccoLogDials ld on co.callout_id = ld.callout_id
+					left join cctipoResultadoDial trd ON ld.tipoResDial_id = trd.tiporesdial_id
+					LEFT JOIN cctipocalifout tco ON tco.calif_id = co.calif_id
+					where co.callout_id = @callOut_Id and ld.fecha >= @initialDate and ld.fecha <=@finalDate and ld.tipoResDial_id != 13
+					and cast(co.cal_Inicio as varchar) = cast(ld.fecha as varchar)
+
+					union 
+					select rppr.callout_id,
+					tpp.descripcion,
+					'''',
+					rppr.reg_date
+					from RegProcessPreviewRecord rppr
+					join ccTypeProcessPreview tpp on rppr.process= tpp.typeProcess_id
+					where rppr.process NOT IN (1,5,7) and rppr.callout_id = @callOut_Id and rppr.reg_date >= @initialDate and rppr.reg_date <=@finalDate
+			
+
+		SELECT  * FROM    
+				( SELECT    ROW_NUMBER() OVER ( ORDER BY date ) AS RowNum, *
+				  FROM      @temTable 
+				) AS RowConstrainedResult
+		WHERE   RowNum >= @initialRow
+			AND RowNum <= @finalRow 
+		ORDER BY RowNum	'
+	EXEC(@sql)
+	
+	
+	
+	/* End script release */
+	/* Upgrade database version (first and the last number of setting 77) */
+	EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
+	EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
+
+	COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		/* Error generated based on sintax */
+		SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+
+		RAISERROR (@errorGenerated, 11, 1)
+
+		ROLLBACK TRAN
+	END CATCH
+END

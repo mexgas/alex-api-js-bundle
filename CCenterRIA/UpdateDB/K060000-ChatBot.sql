a8b114c48b90a401408a4a2291910da8aff43458
Marco Antonio Chagolla <marco.chagolla@nuxiba.com>
2023-10-10 13:45:45 -0600

    Se agrega tabla ChatBotRelation

--- CCenterRIA/UpdateDB/K060000-ChatBot.sql
+++ CCenterRIA/UpdateDB/K060000-ChatBot.sql
@@ -57,7 +57,7 @@ BEGIN
 	BEGIN TRAN
 	BEGIN TRY
 
-		SET @process = 'K060000-Se crea Tabla ccssetings 2'
+	SET @process = 'K060000-Se crea Tabla ccssetings 2'
 	SET @sql = 'if not exists(select * from sys.tables where name=''ccSettings2'') begin
 	CREATE TABLE ccSettings2(
 	setting_id smallint NOT NULL,
@@ -73,22 +73,29 @@ BEGIN
 	end'
 	EXEC(@sql)
 
+	SET @process = 'K060000-Se crea Tabla ChatBotRelation'
+	SET @sql = 'if not exists(select * from sys.tables where name=''ChatBotRelation'') begin
+	CREATE TABLE ChatBotRelation (
+	AzureKnowledgeId int,
+	ContactName varchar(255)
+	);
+	end'
+	EXEC(@sql)
+
 	SET @process = 'K060000-Se inserta setting 256 Ruta para guardar conversaciones de chatbot'
-	SET @sql = 'if not exists(select * ccSettings2 where setting_id=256) begin
+	SET @sql = 'if not exists(select * from ccSettings2 where setting_id=256) begin
 	INSERT INTO ccSettings2 (setting_id, valor, descripcion, Status, Tipo, detalle, description, bLoadSettings, validate)
 	VALUES (256, ''C:\Multimedia\Conversations\Chatbot'', ''Ruta donde se guardarán las conversaciones de Chatbot'', 1, ''GRL'', ''Se guardan los archivos .json separados por carpetas'', ''Path for saving Chatbot conversations'', 1, ''.{0,99}'');
 	end'
 	EXEC(@sql)
 
 	SET @process = 'K060000-Se crea vista de los settings'
-	SET @sql = 'IF NOT EXISTS(select * from sys.views where name=''VIEW_SETTINGS'')
-	BEGIN
-		CREATE VIEW [dbo].[VIEW_SETTINGS]
-		AS
-		select setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate from ccSettings 
-		union
-		select setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate from ccSettings2
-	END'
+	SET @sql = '
+	CREATE OR ALTER VIEW [dbo].[VIEW_SETTINGS]
+	AS
+	select setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate from ccSettings 
+	union
+	select setting_id,valor,descripcion,Status,Tipo,detalle,description,bLoadSettings,validate from ccSettings2'
 	EXEC(@sql)
 
 	SET @process = 'K060013-Buscador-Conversaciones ChatBot Create Table ccChatBotNode'

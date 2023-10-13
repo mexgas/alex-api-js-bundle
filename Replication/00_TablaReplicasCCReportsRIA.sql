2b46abfc186b6346055d8aa1a550d13b35b735d9
Ivan Martin Enciso <imartin@nuxiba.com>
2023-10-13 19:15:48 +0200

    Se agregan archivos para las tablas de las replicas

--- Replication/00_TablaReplicasCCReportsRIA.sql
+++ Replication/00_TablaReplicasCCReportsRIA.sql
@@ -35,6 +35,7 @@ insert into publicationTableCCenterRIA(publicationName,status) values(N'Conversa
 insert into publicationTableCCenterRIA(publicationName,status) values(N'Conversationtweet',0)
 insert into publicationTableCCenterRIA(publicationName,status) values(N'AVRSGraphs',0)
 insert into publicationTableCCenterRIA(publicationName,status) values(N'ConversationWhatsApp',0)
+insert into publicationTableCCenterRIA(publicationName,status) values(N'ConversationWhatsAppOut',0)
 insert into publicationTableCCenterRIA(publicationName,status) values(N'SMS',0)
 
 

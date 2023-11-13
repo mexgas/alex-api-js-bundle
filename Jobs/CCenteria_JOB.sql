7aa8299b0602ab36cdf432f0da5a973b4fdddc2d
Jesus Gallardo <agallardo@nuxiba.com>
2023-11-13 12:38:20 -0600

    Se modifica para truncar tabla con los resultados de marcacion y calificaciones

--- Jobs/CCenteria_JOB.sql
+++ Jobs/CCenteria_JOB.sql
@@ -883,6 +883,74 @@ EndSave:
 	'
     EXEC(@sql)
 
+     set @process = 'DEV1-409 Create Job TruncateccDispositionDashboardResult'
+    set @sql = 'USE [msdb]
+if exists(select * from  [msdb].[dbo].[sysjobs] AS [sJOB] where [name]=N''TruncateccDispositionDashboardResult'') begin
+    EXEC msdb.dbo.sp_delete_job @job_name=N''TruncateccDispositionDashboardResult'', @delete_unused_schedule=1
+end
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [Nuxiba]    Script Date: 20/09/2023 10:42:30 a. m. ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Nuxiba'' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Nuxiba''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''TruncateccDispositionDashboardResult'', 
+        @enabled=1, 
+        @notify_level_eventlog=0, 
+        @notify_level_email=0, 
+        @notify_level_netsend=0, 
+        @notify_level_page=0, 
+        @delete_level=0, 
+        @description=N''No description available.'', 
+        @category_name=N''Nuxiba'', 
+        @owner_login_name=N''sa'', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [TruncateSaveDispositionResult]    Script Date: 20/09/2023 10:42:30 a. m. ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''TruncateSaveDispositionResult'', 
+        @step_id=1, 
+        @cmdexec_success_code=0, 
+        @on_success_action=1, 
+        @on_success_step_id=0, 
+        @on_fail_action=2, 
+        @on_fail_step_id=0, 
+        @retry_attempts=0, 
+        @retry_interval=0, 
+        @os_run_priority=0, @subsystem=N''TSQL'', 
+        @command=N''exec ccspSaveDispositionResult @action=0'', 
+        @database_name=N''CCenterRIA'', 
+        @flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''SchTruncateSaveDispositionResult'', 
+        @enabled=1, 
+        @freq_type=4, 
+        @freq_interval=1, 
+        @freq_subday_type=1, 
+        @freq_subday_interval=0, 
+        @freq_relative_interval=0, 
+        @freq_recurrence_factor=0, 
+        @active_start_date=20230920, 
+        @active_end_date=99991231, 
+        @active_start_time=1000, 
+        @active_end_time=235959     
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+'
+    EXEC(@sql)
+
     set @process = 'CREATE JOB '
     set @sql = ''
     EXEC(@sql)

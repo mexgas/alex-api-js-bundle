ba5954ac602987556ae8b61c80e9a3437069274a
Jesus Gallardo <agallardo@nuxiba.com>
2023-11-16 17:43:14 -0600

    Correcion de assembis

--- CCenterRIA/UpdateDB/20231108_v125.44_Oct.sql
+++ CCenterRIA/UpdateDB/20231108_v125.44_Oct.sql
@@ -3644,7 +3644,89 @@ SET @process = 'CW-8180 CREATE PROCEDURE ccsp_OUTUpdateDialJob se agrega validac
 	EXEC(@sql)
 	-------------------------------------End Omar Mejia hotfix/125.20230719.0.9-----------------------------------------------------------
 
+    
+    SET @process = 'DEV1-444 Asembis Alter SP ccsp_LoadGraphics se cambia inner a left join ccCampsExtend a4 on (a1.cam_id = a4.cam_id)';
+    SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_LoadGraphics]
+@Id as smallint,
+@callType as smallint,
+@UserId as smallint,
+@phone varchar(50)=null
+AS
+BEGIN
+        
+    SET NOCOUNT ON  
+    DECLARE @realValue int      
+    exec @realValue= ccsp_AgentGetStartStopPermission @age_id=@UserId, @cam_id=@Id, @call_type=@callType,@phone=@phone
+    
+    if (@callType=1)
+    begin
+        DECLARE @canReprogram bit  
+        create table #canReprogram (canReprogram bit)
+        insert into #canReprogram
+        exec ccsp_AgentGetCampReprogramData @Id, @callType
+        select @canReprogram = canReprogram from #canReprogram
+        drop table #canReprogram
 
+        select a1.Inbound_id id, a2.descripcion description, a1.graphic_id, a3.type_id, a3.frame, a2.EditableCallKey, 0 as leaveRecMessage , a2.EditableContactData,
+        case when isnull(a4.callsBySurvey,0) > 0 then 1 else 0 end isRelationSurvey ,
+        isnull(a2.callBackSurveyAgent,1) callBackSurveyAgent,isnull(a2.callBackSurveyClient,1) callBackSurveyClient,
+        a2.ShowCalifWnd as ShowDisposition,
+        isnull(a2.startStopRecording,0) as StartStopRecording,
+        @realValue as IsStartStopRecording,
+        isnull(a2.editableDtmf, 0) as isEditDtmf,
+        @canReprogram  CanReprogram
+        from ccRIAInboundGraph a1 
+        inner join ccInbound a2 on (a1.inbound_id=a2.inbound_id)
+         inner join ccRIAGraphics a3 on (a1.graphic_id=a3.graphic_id) 
+         left join ccCamps a4 on a4.cam_id=a2.cam_id  where a1.inbound_id=@Id and type_id in(1,2,3) order by type_id        
+     end    
+     else
+     begin
+        select a1.cam_id Id, a2.cam_descripcion description, a1.graphic_id, a3.type_id, a3.frame, a2.EditableCallKey,
+        case when msgFile <> '''' and leaveRecMessage = 1 then 1 else 0 end as leaveRecMessage, 
+        case when isnull(a2.surveyCamId,0) >0 then 1 else 0 end isRelationSurvey ,
+        a2.cam_ShowCalifWnd as ShowDisposition,
+        a2.callBackSurveyAgent,a2.callBackSurveyClient,
+        isnull(a2.startStopRecording,0) as StartStopRecording,
+        @realValue as IsStartStopRecording,
+        ISNULL( a4.EditableContactData,0) as EditableContactData
+        from ccRIACampsGraph a1 
+        inner join ccCamps a2 on (a1.cam_id=a2.cam_id)
+        inner join ccRIAGraphics a3 on (a1.graphic_id=a3.graphic_id)
+        left join ccCampsExtend a4 on (a1.cam_id = a4.cam_id)
+        left outer join (select top 1 M.cam_id, coalesce(msgFile+'''','''','''') as msgFile 
+        from ccCampsMsgs M join ccMsgFiles T on M.Msg_id=T.msg_id 
+        where M.cam_id = @Id and type = 8) b 
+        on (a2.cam_id = b.cam_id) 
+        where a1.cam_id=@Id and type_id in(1,2,3) order by type_id
+     end    
+END
+    ';
+    EXEC(@sql);
+
+    SET @process = 'DEV1-444 Asembis Alter SP ccsp_RIAACDCallParams se cambia para dar un valor fijo @RecordCalls';
+    SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAACDCallParams]
+@option int,
+@campId int = 0
+AS
+BEGIN
+    SET NOCOUNT ON;
+declare @RecordCalls tinyint
+set @RecordCalls=0
+if(@option = 1)
+begin
+    select @RecordCalls= RecordCalls from ccInboundExtend where Inbound_id = @campId                
+end
+
+else if(@option = 2)
+begin
+    select @RecordCalls= RecordCalls from ccCampsExtend where cam_id = @campId              
+end
+select @RecordCalls RecordCalls
+
+    SET NOCOUNT OFF;
+END';
+    EXEC(@sql);
 
 
 

eb801e0ea80eeb6043942e972c1b80e73e2da6cc
Jesus Gallardo <agallardo@nuxiba.com>
2023-12-14 19:36:57 -0600

    Se modifica para poder usar los intervaloes del tiempo

--- Jobs/CCReportsRIA_JOB.sql
+++ Jobs/CCReportsRIA_JOB.sql
@@ -1,3 +1,19 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+
+
+Date: 2023/12/13
+Description:
+
+Database: CCReportsRIA
+Required version: 104
+
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+
 SET NOCOUNT ON
 
 DECLARE @version INT
@@ -37,7 +53,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterProcess'', 
-        @enabled=1, 
+        @enabled=0, 
         @notify_level_eventlog=0, 
         @notify_level_email=0, 
         @notify_level_netsend=0, 
@@ -117,7 +133,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterProcessPublicationHighLoad'', 
-        @enabled=1, 
+        @enabled=0, 
         @notify_level_eventlog=0, 
         @notify_level_email=0, 
         @notify_level_netsend=0, 
@@ -166,73 +182,7 @@ QuitWithRollback:
 EndSave:'
     EXEC(@sql)
 
-    set @process = 'CREATE JOB ReportsMasterProcessPublicationLowLoad'
-    set @sql = 'USE [msdb]
-if exists(select * from  [msdb].[dbo].[sysjobs] AS [sJOB] where [name]=N''ReportsMasterProcessPublicationLowLoad'') begin
-    EXEC msdb.dbo.sp_delete_job @job_name=N''ReportsMasterProcessPublicationLowLoad'', @delete_unused_schedule=1
-end
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Nuxiba]    Script Date: 03/09/2021 07:55:33 p. m. ******/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Nuxiba'' AND category_class=1)
-BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Nuxiba''
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
-END
-
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterProcessPublicationLowLoad'', 
-        @enabled=1, 
-        @notify_level_eventlog=0, 
-        @notify_level_email=0, 
-        @notify_level_netsend=0, 
-        @notify_level_page=0, 
-        @delete_level=0, 
-        @description=N''ReportsMasterProcess'', 
-        @category_name=N''Nuxiba'', 
-        @owner_login_name=N''replication'', @job_id = @jobId OUTPUT
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Generate Reports]    Script Date: 03/09/2021 07:55:33 p. m. ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''Generate Reports'', 
-        @step_id=1, 
-        @cmdexec_success_code=0, 
-        @on_success_action=1, 
-        @on_success_step_id=0, 
-        @on_fail_action=2, 
-        @on_fail_step_id=0, 
-        @retry_attempts=0, 
-        @retry_interval=0, 
-        @os_run_priority=0, @subsystem=N''TSQL'', 
-        @command=N''EXEC ReportsMasterProcessPublicationLowLoad'', 
-        @database_name=N''CCReportsRIA'', 
-        @flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''RepotsMasterProcess'', 
-        @enabled=1, 
-        @freq_type=4, 
-        @freq_interval=1, 
-        @freq_subday_type=4, 
-        @freq_subday_interval=15, 
-        @freq_relative_interval=0, 
-        @freq_recurrence_factor=0, 
-        @active_start_date=20130912, 
-        @active_end_date=99991231, 
-        @active_start_time=0, 
-        @active_end_time=235959
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:'
-    EXEC(@sql)
-
+    
     set @process = 'CREATE JOB ReportsMasterProcessYesterday'
     set @sql = 'USE [msdb]
 
@@ -327,7 +277,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterSubProcess'', 
-        @enabled=1, 
+        @enabled=0, 
         @notify_level_eventlog=0, 
         @notify_level_email=0, 
         @notify_level_netsend=0, 
@@ -384,7 +334,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportMasterProcessGenerateLow'', 
-        @enabled=1, 
+        @enabled=0, 
         @notify_level_eventlog=0, 
         @notify_level_email=0, 
         @notify_level_netsend=0, 
@@ -447,14 +397,12 @@ EndSave:
 '
     EXEC(@sql)
 
-     set @process = 'CREATE JOB ReportsMasterProcessPublicationLowLoad'
+    
+set @process = 'CREATE JOB ReportsMasterProcessPublicationLowLoad'
     set @sql = 'USE [msdb]
-
 if exists(select * from  [msdb].[dbo].[sysjobs] AS [sJOB] where [name]=N''ReportsMasterProcessPublicationLowLoad'') begin
     EXEC msdb.dbo.sp_delete_job @job_name=N''ReportsMasterProcessPublicationLowLoad'', @delete_unused_schedule=1
 end
-
-
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
@@ -468,7 +416,7 @@ END
 
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''ReportsMasterProcessPublicationLowLoad'', 
-        @enabled=1, 
+        @enabled=0, 
         @notify_level_eventlog=0, 
         @notify_level_email=0, 
         @notify_level_netsend=0, 
@@ -500,7 +448,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''RepotsMa
         @freq_type=4, 
         @freq_interval=1, 
         @freq_subday_type=8, 
-        @freq_subday_interval=2, 
+        @freq_subday_interval=4, 
         @freq_relative_interval=0, 
         @freq_recurrence_factor=0, 
         @active_start_date=20130912, 
@@ -517,6 +465,7 @@ QuitWithRollback:
 EndSave:'
     EXEC(@sql)
 
+    
      set @process = 'CREATE JOB '
     set @sql = ''
     EXEC(@sql)
@@ -527,7 +476,7 @@ EndSave:'
     	
 
 		
-			COMMIT TRAN
+	COMMIT TRAN
 	END TRY
 
 	BEGIN CATCH

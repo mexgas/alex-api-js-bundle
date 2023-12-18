260e56f5494cd35984332543d0e75d492753e019
JCLares <jesus.castro@nuxiba.com>
2023-12-18 13:31:45 -0600

    Se hace correcion del archivo

--- CCenterRIA/UpdateDB/20231211_v125.46_Dic.sql
+++ CCenterRIA/UpdateDB/20231211_v125.46_Dic.sql
@@ -216,6 +216,289 @@ BEGIN
 
         -----------------------------------------------------END K042023-Indicador de creditos Ivan Martin ----------------------------------------------------------------
 
+        	    -----------------------------------------------------BEGIN fix JCL ----------------------------------------------------------------
+
+        SET @process = 'fix historial de cambios identifier Idioma'
+        SET @sql = 'IF NOT EXISTS(SELECT 1 FROM ccGalateaIdentifiers WHERE Description = ''T&SET_TOOLSTRANSFER'')
+                    BEGIN
+                        insert into ccGalateaIdentifiers(Description,TagEs,TagEn,TagPt)
+                        values(''T&SET_TOOLSTRANSFER'', ''Mostrar datos del contacto y controles de llamada en transferencias'', ''Show contact’s data and call controls on transfers'', ''Exibir dados do contato e controles de chamada em transferências'')          
+                    END'
+        EXEC(@sql);
+
+        SET @process = 'fix historial de cambios relacion identifier'
+        SET @sql = 'IF NOT EXISTS(SELECT 1 FROM relationTableColumnIdentifiers WHERE Identifiers = ''T&SET_TOOLSTRANSFER'')
+                    BEGIN
+                        insert into relationTableColumnIdentifiers(Identifiers,tableName,colunName)
+                        values(''T&SET_TOOLSTRANSFER'', ''ccRIACat_Areas'', ''ToolsTransfer'')
+                    END'
+        EXEC(@sql);
+
+        SET @process = 'fix historial de cambios tipo de dato'
+        SET @sql = 'ALTER procedure [dbo].[ccsp_GalateaAreas] 
+    @option int = 2,
+    @IDArea smallint = 0,
+    @Descripcion varchar(40) = NULL,
+    @maxMails smallint = 3,
+    @maxChats smallint = 3,
+    @maxTweets smallint = 3,
+    @defCampaing smallint = 0,
+    @movesfromArea bit = 0,
+    @userId int = NULL,
+    @groupAreas varchar (MAX) = NULL,
+	@toolsTransfer tinyint = NULL
+AS
+
+SET NOCOUNT ON;
+    
+    declare @opt int = @option -1
+    
+    DECLARE @userLogin as varchar(40);
+    SET @userLogin = (SELECT [Login] FROM ccUsers WHERE User_id = @userId);
+
+    if @option = 1 --Superuser info
+    begin
+        create table #campsIds(
+            id int,
+            cadena varchar(max)
+        )
+            
+        declare @sql varchar(max),@idPivots varchar(max),@idConcat varchar(max)
+            
+        set @idPivots =''''
+        set @idConcat=''''
+            
+        select @idPivots=@idPivots+Id+'','',
+            @idConcat=@idConcat+''case when ''+id+'' is not null then convert(varchar(max),''+ id+'') + '''','''' else '''''''' end + 
+            ''
+            from (
+            select distinct ''[''+convert(varchar(max),cam_id)+'']'' as Id from ccCamps   
+            )x
+            
+        set @idPivots =SUBSTRING(@idPivots,0,len(@idPivots))
+        set @idConcat =SUBSTRING(@idConcat,0,len(@idConcat)-7)
+            
+        set @sql=''
+            select IDArea,''+@idConcat+'' from 
+            (   select IDArea, cam_id from ccCamps) as T
+            PIVOT (
+            max(cam_id) for cam_id in (''+@idPivots+'') ) as P''
+
+        insert into #campsIds
+        exec(@sql)
+            
+        select a.IDArea Id, 
+            a.AreaName Name, 
+            a.StatusArea Status, 
+            a.maxMails Mails, 
+            a.maxChats Chats, 
+            a.maxTweets Tweets, 
+            a.CreateDate as CreateDate,         
+            ISNULL(b.cadena, 0) as CampaignIds  
+        from ccRIACat_Areas a --Falta el datetime 
+        left join #campsIds b on a.IDArea = b.id
+
+        drop table #campsIds
+    end
+    if @option = 2 -- Select de las areas
+    begin
+        IF OBJECT_ID(''tempdb..#Areas'') IS NOT NULL DROP TABLE #Areas;
+        Create table #Areas(
+            IDArea smallint,
+            AreaName varchar(MAX),
+            maxChats tinyint ,
+            maxMails tinyint ,
+            users int,
+            admins int,
+            camps int,
+            acds int,
+            maxTweets tinyint,
+			toolsTransfer tinyint
+        )
+        insert into #Areas
+        EXECUTE ccsp_RIA_ABCAreas @option = @opt, @IDArea=@IDArea,@Descripcion=@Descripcion,@maxMails=@maxMails,@maxChats=@maxChats,@maxTweets=@maxTweets,@defCampaing=@defCampaing, @isKolob=1
+        select a.*,rca.CreateDate,Isnull(rca.defCampaing,0) as defCampaing
+        from #Areas a
+        inner join ccRIACat_Areas rca with(nolock) on a.IDArea = rca.IDArea
+    end
+    if @option = 3 -- Insert new area
+    begin
+    IF OBJECT_ID(''tempdb..#InsertAreas'') IS NOT NULL DROP TABLE #InsertAreas;
+        Create table #InsertAreas(
+            result int,
+            idAreas decimal
+        )
+        insert into #InsertAreas
+        EXEC ccsp_RIA_ABCAreas 
+            @option = @opt,
+            @IDArea=@IDArea,
+            @Descripcion=@Descripcion,
+            @maxMails=@maxMails,
+            @maxChats=@maxChats,
+            @maxTweets=@maxTweets,
+            @defCampaing=@defCampaing,
+			@toolsTransfer=@toolsTransfer
+        if (select result from #InsertAreas) = 1
+            begin
+
+                --INSERTA UN REGISTRO EN EL HISTORIAL DE ACTIVIDAD AL CREAR UN AREA
+                INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target) VALUES (@Descripcion, getDate(), @userLogin, 17, 3, '''', '''', @Descripcion);
+
+                if(@movesfromArea = 1) begin
+                    Update ccUsers set IDArea = (select idAreas from #InsertAreas), status = 1 where User_id = @userId
+                end
+            end
+        Select * from #InsertAreas
+    end
+    if @option = 4 -- Delete Areas
+    begin
+        IF OBJECT_ID(''tempdb..#AreasDelete'') IS NOT NULL DROP TABLE #AreasDelete;
+        SELECT value As IDArea into #AreasDelete FROM fn_RIASplitDelimited(@groupAreas, '','')
+        
+        
+        if (exists(select IDArea from ccUsers where IDArea=(Select top 1 IDArea from #AreasDelete)) or exists(select IDArea from ccCamps where IDArea = (Select top 1 IDArea from #AreasDelete))
+          or exists(select IDArea from ccInbound where IDArea=(Select top 1 IDArea from #AreasDelete))) and (select valor from ccSettings where setting_id=95)<>1
+        BEGIN
+            Select -1 as result
+        END
+        ELSE
+        BEGIN
+            declare @DWorkGroups as varchar(500)
+            insert into ccCampsAgenteBackUp(user_id,cam_id,prioridad,skill,rel_id,IDWG)
+            select user_id,cam_id,prioridad,skill,rel_id,IDWG
+            from ccCampsAgente
+            where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+
+            insert into ccInboundAgentesBackup(user_id,Inbound_id,cli_id,prioridad,skill,rel_id,IDWG)
+            select user_id,Inbound_id,cli_id,prioridad,skill,rel_id,IDWG
+            from ccInboundAgentes where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+
+            Delete ccCampsAgente where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+            Delete ccInboundAgentes where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+
+            insert into ccSupervisorCamBackup(user_id,cam_id,tipo,IDWG,monitored)
+            select user_id,cam_id,tipo,IDWG,monitored
+            from ccSupervisorCam
+            where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+
+            Delete ccSupervisorCam where user_id in (select user_id from ccusers with(index(PK_ccUsers)) where IDArea in (Select IDArea from #AreasDelete))
+
+            delete ccoDialerCamp where cam_id in (select cam_id from ccCamps with(index(PK_ccCamps)) where IDArea in (Select IDArea from #AreasDelete))
+            delete ccoWorkingTable where cam_id in (select cam_id from ccCamps with(index(PK_ccCamps)) where IDArea in (Select IDArea from #AreasDelete))
+            delete ccoWorkingTable where callout_id in (select callout_id from ccoCallsOutSource with(index(IX_ccoCallsOutSource_1))
+            where cam_id in (select cam_id from ccCamps where IDArea in (Select IDArea from #AreasDelete)))
+
+            Delete ccInboundHorarios Where Inbound_id in (select Inbound_id from ccInbound with(index(PK_ccInbound)) where IDArea in (Select IDArea from #AreasDelete))
+            Delete ccInboundMsgs Where Inbound_id in (select Inbound_id from ccInbound with(index(PK_ccInbound)) where IDArea in (Select IDArea from #AreasDelete))
+
+            Delete from ccRIAWorkGroupUsers where IDWG in (select IDWG from ccRIAAreaWorkGroup where IDArea in (Select IDArea from #AreasDelete))
+            Delete from ccRIACat_WorkGroup where IDWG in (select IDWG from ccRIAAreaWorkGroup where IDArea in (Select IDArea from #AreasDelete))
+            Delete from ccRIACampEspWG where IDWG in (select IDWG from ccRIAAreaWorkGroup where IDArea in (Select IDArea from #AreasDelete))
+
+            select @DWorkGroups = coalesce(@DWorkGroups + '''','''', '''') + CAST(IDWG as varchar(40)) FROM ccRIAAreaWorkGroup where IDArea in (Select IDArea from #AreasDelete)
+            Delete from ccRIAAreaWorkGroup where IDArea in (Select IDArea from #AreasDelete)
+
+            if (select valor from ccSettings where setting_id=95)=1
+            begin
+            Update ccInbound set IDArea=NULL, status=0 where IDArea in (Select IDArea from #AreasDelete)
+            Update ccCamps set IDArea=NULL where IDArea in (Select IDArea from #AreasDelete)
+            Update ccUsers set IDArea=NULL where IDArea in (Select IDArea from #AreasDelete)
+            end
+
+            Update ccRIACat_Areas set StatusArea=0 where IDArea in (Select IDArea from #AreasDelete)
+
+            --INSERTA UN REGISTRO EN EL HISTORIAL DE ACTIVIDAD POR CADA AREA ELIMINADA
+            INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target)
+            SELECT AreaName, getDate(), @userLogin, 19, 3, '''', '''', AreaName
+            FROM ccRIACat_Areas 
+            WHERE IDArea in (Select IDArea from #AreasDelete);
+
+            select 1 as result
+        END
+    end
+    if @option = 5 -- update Areas
+    begin
+        if exists(Select AreaName from ccRIACat_Areas where StatusArea=1 and AreaName=@Descripcion and IDArea <> @IDArea)
+            begin
+                select -1 as result
+                return
+            end
+        else
+            begin
+
+                --INICIO - INSERTA UN REGISTRO EN EL HISTORIAL DE ACTIVIDAD POR CADA PROPIEDAD EDITADA*******
+
+                DECLARE @PrevDescription AS VARCHAR(50);
+                DECLARE @SelectedArea AS VARCHAR(10) = CAST(@IDArea AS varchar(10));
+
+                SELECT @PrevDescription = AreaName
+                FROM ccRIACat_Areas 
+                WHERE IDArea = @IDArea;
+
+                EXEC InsertLogAdminGalatea @action=1, @tableName=''ccRIACat_Areas'', @columnNameId=''IDArea'', @valueId=@SelectedArea, @userId= @userId
+
+                DECLARE @AreasTable TABLE 
+                (
+                    columnInfo VARCHAR(255),
+                    dataInfo VARCHAR(255),
+                    identifierInfo VARCHAR(255)
+                )
+
+                update ccRIACat_Areas set AreaName= isnull(@Descripcion,AreaName),maxMails=isnull(@maxMails,maxMails),maxChats=isnull(@maxChats,maxChats),maxTweets=isnull(@maxTweets,maxTweets),defCampaing=isnull(@defCampaing, 0), ToolsTransfer=case when @toolsTransfer = 3 then ToolsTransfer else @toolsTransfer end where IDArea=@IDArea
+
+                INSERT INTO @AreasTable EXEC InsertLogAdminGalatea @action=2, @tableName=''ccRIACat_Areas'', @columnNameId=''IDArea'', @valueId=@SelectedArea, @userId= @userId;
+
+                INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target) 
+                SELECT 
+                    CASE WHEN AT.identifierInfo IS NOT NULL THEN
+                        CASE 
+                            WHEN AT.identifierInfo = ''T&EDIT_NAME'' THEN @PrevDescription ELSE isNull(@Descripcion, @PrevDescription) END
+                    ELSE '''' END,
+                    getDate(), 
+                    @userLogin, 
+                    18, 
+                    3, 
+                    AT.identifierInfo,
+                    CASE WHEN AT.identifierInfo IS NOT NULL THEN
+                        CASE 
+                            WHEN AT.identifierInfo = ''T&EDIT_NAME'' THEN @Descripcion
+                            WHEN AT.identifierInfo = ''T&SET_CAMPAIGN'' THEN 
+                                CASE 
+                                    WHEN @defCampaing IS NOT NULL AND @defCampaing <> 0 THEN
+                                        (SELECT [cam_descripcion] FROM ccCamps WHERE cam_id = @defCampaing)
+                                    ELSE ''T&COMMON_NONE'' END
+							WHEN AT.identifierInfo = ''T&SET_TOOLSTRANSFER'' THEN
+								CASE
+									WHEN @toolsTransfer = 1 THEN ''COMMON_ENABLED''
+									ELSE ''COMMON_DISABLED'' END
+                            ELSE AT.dataInfo END
+                    ELSE '''' END, 
+                    CASE WHEN AT.identifierInfo IS NOT NULL THEN
+                        CASE 
+                            WHEN AT.identifierInfo = ''T&EDIT_NAME'' THEN @PrevDescription ELSE isNull(@Descripcion, @PrevDescription) END
+                    ELSE '''' END
+                FROM @AreasTable AS AT;
+
+                EXEC InsertLogAdminGalatea @action=3, @tableName=''ccRIACat_Areas'', @columnNameId=''IDArea'', @valueId=@SelectedArea, @userId= @userId
+
+                --FIN - INSERTA UN REGISTRO EN EL HISTORIAL DE ACTIVIDAD POR CADA PROPIEDAD EDITADA*******
+
+            end
+        if @maxChats is not null
+            begin
+                Update ccinbound set maxChats=@maxChats where IDArea=@IDArea
+            end
+        if @movesfromArea = 1
+        Begin
+            Update ccUsers set IDArea = @IDArea, status = 1 where User_id = @userId
+        End
+        select 1 as result
+    end
+SET NOCOUNT ON;'
+        EXEC(@sql);
+
+        -----------------------------------------------------END fix JCL ----------------------------------------------------------------
+
 
         /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
         EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)

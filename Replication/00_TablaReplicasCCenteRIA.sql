5bccfcbbd64528f0622ececeafc368bcf13bd3d6
Ulises Espinosa <uespinosa@nuxiba.com>
2024-04-24 15:45:55 -0600

    Se cambia el formato del archivo

--- Replication/00_TablaReplicasCCenteRIA.sql
+++ Replication/00_TablaReplicasCCenteRIA.sql
@@ -198,7 +198,7 @@ insert into publicationTableCCenterRIA(publicationName,status) values(N'SMS',0)
 insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'smsccoLogDial',@idInt,0)
 insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'smsOutSource',@idInt,0)
 insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'smsoutSourceMessage',@idInt,0)
-insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'SmsRemesasMuñozDay',@idInt,0)
+insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'SmsRemesasMuÃ±ozDay',@idInt,0)
 insert into articleTableCCenterRIA(articleName,publicationId,status) values(N'ccSmsSegments',@idInt,0)
 
 set @idInt=@idInt+1

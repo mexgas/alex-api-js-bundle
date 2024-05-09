c08f4fd54047e27d7947c74e2cf371747425c2e6
FriAleOm <frida.orta@nuxiba.com>
2024-05-09 15:39:41 -0600

    se asigna null a la columna Cam_Id de la tabla ccMetaWhatsAppNumbers

---
+++ CCenterRIA/UpdateDB/20240509_v127_May.sql
@@ -0,0 +1,267 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author:
+Date: 2023/07/04
+Description: K089000
+Database: CCenterRia
+Required version: 125.37
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+DECLARE @version INT, @versionFix INT
+DECLARE @actualVersion INT, @actualVersionFix INT
+DECLARE @sql VARCHAR(max)
+DECLARE @errorGenerated VARCHAR(max)
+DECLARE @process VARCHAR(max)
+DECLARE @versionALL VARCHAR(max);
+/* Version to release (use the version of your own databse)*/
+/*******************************************************************************************************
+Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
+set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
+sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
+--SET @version = 126 --**********actualizar a 124 sin fix
+SET @versionfix = 6
+/* Actual version (use your own script to do it)*/
+EXEC @actualVersion = ccsp_getVersion 'BD'
+EXEC @actualVersionFix = ccsp_getVersion 'BDF'
+SELECT @versionALL = valor
+FROM ccsettings
+WHERE setting_id = 77;
+SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
+FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
+WHERE id = 5;
+--- Validacion para cuando pasamos a una nueva version LTS
+declare @versioMajer int= case when @version > @actualVersion then 1 else 0 end
+IF @version > @actualVersion 
+BEGIN 
+    SET @actualVersionFix = 0
+    select @version,@actualVersion,@versioMajer
+END
+IF @version >= @actualVersion and @versionfix >= @actualVersionFix 
+BEGIN
+    BEGIN TRAN
+    BEGIN TRY
+    	
+
+	
+
+
+
+	-----------------------------------------------Frida Orta Begin ----------------------------------------------------------------------------------
+	set @process = 'Drop SP ccsp_GalateaDeleteCampaignAndACD '
+	set @sql = '
+	if exists (select * from sys.procedures where name = N''ccsp_GalateaDeleteCampaignAndACD'')
+    begin
+        DROP PROCEDURE ccsp_AgentDataACD;
+    end'
+	EXEC(@sql)
+
+	set @process = 'Create ccsp_GalateaDeleteCampaignAndACD, campañas de salida de WA, se asigna null a la columna Cam_Id de la tabla ccMetaWhatsAppNumbers '
+	set @sql = '
+	CREATE PROCEDURE ccsp_GalateaDeleteCampaignAndACD
+            @userId           SMALLINT,
+            @DeleteCamId      VARCHAR(MAX),
+            @DeleteACDGroupId VARCHAR(MAX),
+            @moduleId         SMALLINT = 49
+        AS
+        BEGIN
+
+            IF OBJECT_ID(''tempdb..#CampsDelete'') IS NOT NULL DROP TABLE #CampsDelete
+				SELECT value As DeleteCamId, c.IDArea AS IDAreaCamp, 1 AS CampTypeCamp, ISNULL(wg.IDWG,0) as IDWG, ISNULL(c.CampType, 0) AS MediaType
+                INTO #CampsDelete
+                FROM fn_RIASplitDelimited(@DeleteCamId, '','') a
+                inner join ccCamps c on  a.value = c.cam_id and c.IDArea IS NOT NULL
+                left join ccRIACampEspWG wg on a.Value = wg.IdCampEsp and wg.Tipo=1
+            IF OBJECT_ID(''tempdb..#ACDDelete'') IS NOT NULL DROP TABLE #ACDDelete
+				SELECT value As DeleteACDId, c.IDArea AS IDAreaACD, 0 AS CampTypeACD, ISNULL(wg.IDWG,0) as IDWG, cast(ISNULL(chat, 0) as int) AS MediaType
+                INTO #ACDDelete
+                FROM fn_RIASplitDelimited(@DeleteACDGroupId, '','') a
+                inner join ccInbound c on  a.value = c.Inbound_id and c.IDArea IS NOT NULL
+                left join ccRIACampEspWG wg on a.Value = wg.IdCampEsp and wg.Tipo=0
+
+            IF  not Exists (select * from #CampsDelete union select * from #ACDDelete )
+            begin
+                select ''-1'' AS Result
+                return
+            end
+
+            IF datalength(@DeleteCamId) > 0
+                BEGIN
+
+                if exists(select cam_id from ccInbound where cam_id in (select DeleteCamId from #CampsDelete)) begin
+                    --Borra las calificacion con reprogramacion
+                    delete ccCalifCamp from ccInbound A
+                    inner join ccCalifCamp B on A.Inbound_id=B.cam_id and  B.tipo=0
+                    inner join ccTipoCalif C on B.calif_id=C.calif_id and C.CanReprogram=1
+                    where A.cam_id in (select DeleteCamId from #CampsDelete)
+                    --Borra las subcalificacion con reprogramacion
+                    delete rel from ccInbound A
+                    inner join ccCalifCamp B on A.Inbound_id=B.cam_id and  B.tipo=0
+                    inner join ccTipoCalif C on B.calif_id=C.calif_id
+                    inner join cctipoSubCalifRel rel on rel.calif_id=C.calif_id and rel.tipoSubRel=1
+                    inner join ccTipoCalifSub sb on rel.califsub_id=sb.califsub_id
+                    where A.cam_id in (select DeleteCamId from #CampsDelete) and sb.canReprogram=1
+
+                    update ccInbound set cam_id = null where cam_id in (select DeleteCamId from #CampsDelete)
+
+                end
+
+                insert into ccCampsAgenteBackUp(user_id,cam_id,prioridad,skill,rel_id,IDWG)
+                select A.user_id,A.cam_id,A.prioridad,A.skill,A.rel_id,A.IDWG from ccCampsAgente A left join ccCampsAgenteBackUp B on A.user_Id=B.user_id and A.cam_id=B.cam_id where B.User_id is null and A.cam_id in (select DeleteCamId from #CampsDelete)
+
+                delete from ccCampsAgente where cam_id in (select DeleteCamId from #CampsDelete)
+                insert into ccSupervisorCamBackup(user_id,cam_id,tipo,IDWG,monitored)
+                select A.user_id,A.cam_id,A.tipo,A.IDWG,A.monitored from ccSupervisorCam A left join ccSupervisorCam B on A.user_Id=B.user_id and A.cam_id=B.cam_id where B.User_id is null and A.cam_id in (select DeleteCamId from #CampsDelete) and A.tipo = 1
+
+                delete from ccSupervisorCam where cam_id in (select DeleteCamId from #CampsDelete) and tipo = 1
+                delete from ccRIACampEspWG where IdCampEsp in (select DeleteCamId from #CampsDelete) and tipo = 1
+
+                IF OBJECT_ID(''tempdb..#CampLog'') IS NOT NULL DROP TABLE #CampLog
+                SELECT ca.AreaName,
+                       GETDATE() operationDate,
+                       27 operationType,
+                       (SELECT Login FROM ccUsers WHERE User_Id = @userId) login,
+                       @moduleId module_id,
+                       c.cam_descripcion value,
+                       ca.AreaName AS target
+                INTO #CampLog
+                FROM ccRIACat_Areas ca
+                Inner join ccCamps c with(nolock) on ca.IDArea = c.IDArea
+                WHERE c.cam_id in (select DeleteCamId from #CampsDelete)
+
+                INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target)
+				SELECT A.AreaName, GETDATE(), (SELECT [Login] FROM ccUsers WHERE User_id = @userid),
+				CASE 
+					WHEN CampType = 6 THEN 45
+					WHEN CampType = 5 THEN 47
+					WHEN CampType = 4 THEN 49
+					WHEN CampType = 7 THEN 51
+				ELSE 43 END, 
+				3, 
+				'''',
+				'''', 
+				c.cam_descripcion
+				FROM ccRIACat_Areas A INNER JOIN ccCamps c on A.IDArea = c.IDArea
+				where c.cam_id in (select DeleteCamId from #CampsDelete)
+
+                Update ccCamps set IDArea = null where cam_id in (select DeleteCamId from #CampsDelete)
+        
+                update contactMeanOut set name = '''', conexionInfo = '''', connUser = '''', isActive = 0
+                where camp_id in (SELECT DeleteCamId FROM #CampsDelete) and meanContactTypeId=5
+        
+                update ccWhatsAppNumbers set camp_id = 0 where camp_id in (select DeleteCamId from #CampsDelete)
+
+			    update ccMetaWhatsAppNumbers set Cam_Id = null where Cam_Id in (select DeleteCamId from #CampsDelete)
+        
+
+            END
+            IF datalength(@DeleteACDGroupId) > 0
+                BEGIN
+
+                if exists(select top 1 cam_id from ccInbound where Inbound_id in (select DeleteACDId from #ACDDelete))
+                begin
+                        update ccInbound set cam_id = null where Inbound_id in (select DeleteACDId from #ACDDelete)
+                end
+
+                IF OBJECT_ID(''tempdb..#AllWGACD'') IS NOT NULL DROP TABLE #AllWGACD
+                SELECT DISTINCT(IDWG)
+                INTO #AllWGACD
+                FROM ccRIACampEspWG ce
+                WHERE IDCampEsp in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+
+                insert into ccInboundAgentesBackup(user_id,Inbound_id,cli_id,prioridad,skill,rel_id,IDWG)
+                select A.user_id,A.Inbound_id,A.cli_id,A.prioridad,A.skill,A.rel_id,A.IDWG
+                from ccInboundAgentes A left join ccInboundAgentesBackup B on A.user_Id=B.user_id and A.Inbound_id=B.Inbound_id
+                where B.User_id is null and A.Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                delete ccInboundHorarios Where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+                delete ccInboundMsgs Where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+                delete ccInboundDnis where inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                insert into ccCampsAgenteBackUp(user_id,cam_id,prioridad,skill,rel_id,IDWG)
+                select A.user_id,A.cam_id,A.prioridad,A.skill,A.rel_id,A.IDWG
+                from ccCampsAgente A left join ccCampsAgenteBackUp B on A.user_Id=B.user_id and A.cam_id=B.cam_id
+                where B.User_id is null and A.cam_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                delete ccSupervisorCam where cam_id in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+                delete ccInboundAgentes where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+                delete ccRIACampEspWG where IdCampEsp  in (SELECT DeleteACDId FROM #ACDDelete) and tipo = 0
+
+
+                IF OBJECT_ID(''tempdb..#ACDLog'') IS NOT NULL DROP TABLE #ACDLog
+                SELECT ca.AreaName,
+                        GETDATE() operationDate,
+                        28 operationType,
+                        (SELECT Login FROM ccUsers WHERE User_Id = @userId) login,
+                        @moduleId module_id,
+                        i.descripcion value,
+                        ca.AreaName AS target
+                INTO #ACDLog
+                FROM ccRIACat_Areas ca
+                inner join ccInbound i with(nolock) on ca.IDArea = i.IDArea
+                WHERE i.Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target)
+				SELECT a.AreaName, GETDATE(), (SELECT [Login] FROM ccUsers WHERE User_id = @userId),
+				CASE 
+					WHEN chat = 5 THEN 41
+					WHEN chat = 1 THEN 65
+					ELSE 61 END, 
+				3, 
+				'''', 
+				'''', 
+				i.descripcion
+				FROM ccRIACat_Areas a inner join ccInbound i on a.IDArea = i.IDArea
+				WHERE i.Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                Update ccInbound set IDArea = null, status = 0 where Inbound_id in (SELECT DeleteACDId FROM #ACDDelete)
+
+                if exists(select * from ContactMeanIn where meanContactTypeId=2 and inboundId in (SELECT DeleteACDId FROM #ACDDelete))--Si encuentra un registro en contactMeanIn de tipo twitter asociado al ACD
+                    begin
+                        update ContactMeanIn set name = '''', conexionInfo = ''usuarioID|token|tokenSecret|1|0'', connUser = '''', isActive = 0
+                        where inboundId in (SELECT DeleteACDId FROM #ACDDelete) and meanContactTypeId=2
+                end
+                if exists(select * from ContactMeanIn where meanContactTypeId=1 and inboundId in (SELECT DeleteACDId FROM #ACDDelete))--Si encuentra un registro en contactMeanIn de tipo email asociado al ACD
+                    begin
+                        update ContactMeanIn set name = '''', conexionInfo = '''', connUser = '''', connpass='''', isActive = 0 where inboundId in (SELECT DeleteACDId FROM #ACDDelete) and meanContactTypeId=1
+                end
+                update ccinbound set chatDomain = '''' where inbound_id in (SELECT DeleteACDId FROM #ACDDelete)--para desasociar el dominio del chat
+
+                if exists (SELECT inboundId FROM contactMeanIn WHERE inboundId in (select DeleteACDId from #ACDDelete))
+                    begin
+                        update contactMeanIn set isActive = 0 where inboundId in (select DeleteACDId from #ACDDelete)
+                end            
+                update ccWhatsAppNumbers set inboundId = 0 where inboundId in (select DeleteACDId from #ACDDelete)
+        
+            END
+
+            IF datalength(@DeleteCamId) > 0
+                Insert into ccRIALog Select * from #CampLog
+            IF datalength(@DeleteACDGroupId) > 0
+                Insert into ccRIALog Select * from #ACDLog
+
+            SELECT DeleteCamId AS DeleteId,IDAreaCamp AS IDArea,CampTypeCamp AS CampType,''1'' AS Result, cast(IDWG as smallint) IDWG, MediaType FROM #CampsDelete
+            UNION
+            SELECT DeleteACDId,IDAreaACD,CampTypeACD,''1'' AS Result, cast(IDWG as smallint) IDWG, MediaType FROM #ACDDelete
+            IF OBJECT_ID(''tempdb..#CampsDelete'') IS NOT NULL DROP TABLE #CampsDelete
+            IF OBJECT_ID(''tempdb..#ACDDelete'') IS NOT NULL DROP TABLE #ACDDelete
+            IF OBJECT_ID(''tempdb..#CampLog'') IS NOT NULL DROP TABLE #CampLog
+        END'
+	EXEC(@sql)
+
+	-------------------------------------------------- Frida Orta End -----------------------------------------------------------------------------------
+ 	
+        /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
+        EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
+        EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
+        COMMIT TRAN
+    END TRY
+    BEGIN CATCH
+        /* Error generated based on sintax */
+        SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
+        RAISERROR (@errorGenerated, 11, 1)
+        ROLLBACK TRAN
+    END CATCH
+END 

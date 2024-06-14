a2e3f23256e7d1d036fd752530b1a5e9c7bd7a92
Marco García <marco.garcia@nuxiba.com>
2024-06-13 18:24:04 -0600

    Cambio

--- CCenterRIA/UpdateDB/whatsAppMasivo.sql
+++ CCenterRIA/UpdateDB/whatsAppMasivo.sql
@@ -21,7 +21,7 @@ DECLARE @versionALL VARCHAR(max);
 Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
 set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */--
-SET @version = 127 --**********actualizar a 124 sin fix
+SET @version = 127 --**********actualizar a 124 sin fixISNULL(contact.maxLimitQueueConversations, 99) maxLimitQueueConversations
 SET @versionfix = 1
 /* Actual version (use your own script to do it)*/
 EXEC @actualVersion = ccsp_getVersion 'BD'
@@ -739,6 +739,7 @@ SET @process = 'K020148-Carga BD WhatsApp salida-Detalle INSERT INTO tableLangue
 			,isnull(campsExtention.simultaneousRecs, 1) simultaneousRecs
 			,isnull(campsExtention.EditableContactData, 0) EditableContactData
 			,@intenationalDialingPorts intenationalDialingPorts 
+			,isnull(campsExtention.AssignConversationSameAgent, 0) AssignConversationSameAgent
 			,ISNULL(contact.maxLimitQueueConversations, 99) maxLimitQueueConversations
 		FROM ccCamps a1
 		INNER JOIN ccRIACampsGraph a2 ON (a1.cam_id = a2.cam_id)

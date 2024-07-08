c1e3d1cbe7b2466a3ed38e436f91bea092e9428c
Uriel Cabrera Salinas <cabrera.cs.personal@outlook.com>
2024-07-08 11:35:31 -0600

    chore: Create job to clean reconnectMsg column in ccUsers

--- Jobs/CCenteria_JOB.sql
+++ Jobs/CCenteria_JOB.sql
@@ -985,6 +985,77 @@ QuitWithRollback:
 EndSave:
 '
     EXEC(@sql)
+    	----------------------------------------------------------- Uriel Cabrera  Inicio Job de limpieza para la columna reconnectMsg en caso de que el usuario ya no se conecte en el dia -------------------------------------------------------------------------------
+	set @process = 'Create Job to clean reconnectMsg column in ccUsers'
+	set @sql = '
+		USE [msdb]
+			GO
+
+			BEGIN TRANSACTION
+			DECLARE @ReturnCode INT
+			SELECT @ReturnCode = 0
+
+			IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''Nuxiba'' AND category_class=1)
+			BEGIN
+			EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''Nuxiba''
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+			END
+
+			DECLARE @jobId BINARY(16)
+			EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''CleanReconnectMsgForAgent'', 
+					@enabled=1, 
+					@notify_level_eventlog=0, 
+					@notify_level_email=0, 
+					@notify_level_netsend=0, 
+					@notify_level_page=0, 
+					@delete_level=0, 
+					@description=N''Job for cleaning reconnectMsg Column in ccUsers day by day'', 
+					@category_name=N''Nuxiba'', 
+					@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''CleanReconnectMsgForAgent'', 
+					@step_id=1, 
+					@cmdexec_success_code=0, 
+					@on_success_action=1, 
+					@on_success_step_id=0, 
+					@on_fail_action=2, 
+					@on_fail_step_id=0, 
+					@retry_attempts=0, 
+					@retry_interval=0, 
+					@os_run_priority=0, @subsystem=N''TSQL'', 
+					@command=N''update ccUsers set reconnectMsg = 0'', 
+					@database_name=N''CCenterRIA'', 
+					@flags=0
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''CleanReconnectMsgForAgent'', 
+					@enabled=1, 
+					@freq_type=4, 
+					@freq_interval=1, 
+					@freq_subday_type=1, 
+					@freq_subday_interval=0, 
+					@freq_relative_interval=0, 
+					@freq_recurrence_factor=0, 
+					@active_start_date=20240604, 
+					@active_end_date=99991231, 
+					@active_start_time=30000, 
+					@active_end_time=235959, 
+					@schedule_uid=N''600d6078-80e5-4230-84a9-06f77f14264a''
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
+			IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+			COMMIT TRANSACTION
+			GOTO EndSave
+			QuitWithRollback:
+				IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+			EndSave:
+			GO
+			'
+	EXEC(@sql)
+	----------------------------------------------------------- Uriel Cabrera  Fin Job de limpieza para la columna reconnectMsg en caso de que el usuario ya no se conecte en el dia -------------------------------------------------------------------------------
 
     set @process = 'CREATE JOB '
     set @sql = ''

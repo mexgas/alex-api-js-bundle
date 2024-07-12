bbaac7210ed25bcaabb7323fc0faa1ee8ccf68c3
FriAleOm <frida.orta@nuxiba.com>
2024-07-12 09:41:41 -0600

    Se agrega la cláusula disctinct para no mostrar agentes duplicados

--- CCenterRIA/UpdateDB/20240624_v126.11_Jun.sql
+++ CCenterRIA/UpdateDB/20240624_v126.11_Jun.sql
@@ -4674,6 +4674,95 @@ CREATE PROCEDURE [dbo].[ccsp_RIAConfCamp] @User_id SMALLINT, @campID INT = NULL
 
 EXEC(@sql)
 
+SET @process = 'Corrección sp ccsp_GalateaAdminGetPermissions borrar sp'
+SET @sql = '
+	IF EXISTS (SELECT * FROM sysobjects WHERE name=''ccsp_GalateaAdminGetPermissions'')
+	BEGIN
+		DROP PROCEDURE ccsp_GalateaAdminGetPermissions
+	END'
+
+EXEC(@sql)
+
+SET @process = 'Corrección sp ccsp_GalateaAdminGetPermissions crear sp'
+SET @sql = '
+
+CREATE PROCEDURE ccsp_GalateaAdminGetPermissions
+    @user_id varchar(255),
+    @Type int
+    AS
+    set nocount on
+
+    declare @isRoot int;
+
+    if exists (Select Rol_id from ccUsers A join ccUsers_Roles B on A.User_id = B.User_id where A.User_id = @user_id and rol_id = 7) set @isRoot = 1 else set @isRoot = 0;
+    print @isRoot
+
+    IF @isRoot = 1
+    BEGIN
+        Select 
+        User_id as AgentId, 
+        Login as Username, Nombres + '' '' + isNull(apellidoPaterno,'''') + '' '' + isNull(ApellidoMaterno, '''') as FullName, 
+        1-cast(dialMask & 1 as int) as AllowCellPhoneCalls,
+        1-cast( (dialMask & 2) /2 as int) as AllowLongDistanceCalls, 
+        1-cast((dialMask & 4) / 4 as int) as AllowLocalCalls,
+        cast( xfermask as int) as AllowTransferCalls, 
+        cast(CanChangeStatus as tinyint) CanChangeStatus,
+        cast(XferAgents as tinyint) XferAgents,
+        ISNULL(cast(startStopRecording as tinyint), 0) startStopRecording,
+        cast(AllowChangeDialingMode as int) as AgentPermissionDailing,
+        ISNULL(cast( DialingMode & 1 as int), 0) as DailingMode,
+        ISNULL(agentsPermissions.AllowUnassign, 0) AS AllowUnassign,
+        ISNULL(agentsPermissions.AllowSpam, 0 ) AS AllowSpam,
+        ISNULL(agentsPermissions.AllowPlayRecordsOnCallHistory, 0) AS AllowPlayRecordsOnCallHistory,
+        cast(AllowDeleteRecord as int) as AgentPermissionDelete,
+        AllowMarks as AllowMarks,
+        ISNULL(allowSelectCamp,0) as AllowSelectCamp
+    from 
+        ccUsers users
+        left join ccRIAAgentsPermissions agentsPermissions on
+        users.User_id = agentsPermissions.AgentId
+    where 
+       tipoUser_id = 1
+    return(0)
+    END
+    ELSE
+    BEGIN
+        Select distinct
+        A.User_id as AgentId, 
+        Login as Username, Nombres + '' '' + isNull(apellidoPaterno,'''') + '' '' + isNull(ApellidoMaterno, '''') as FullName, 
+        1-cast(dialMask & 1 as int) as AllowCellPhoneCalls,
+        1-cast( (dialMask & 2) /2 as int) as AllowLongDistanceCalls, 
+        1-cast((dialMask & 4) / 4 as int) as AllowLocalCalls,
+        cast( xfermask as int) as AllowTransferCalls, 
+        cast(CanChangeStatus as tinyint) CanChangeStatus,
+        cast(XferAgents as tinyint) XferAgents,
+        ISNULL(cast(startStopRecording as tinyint), 0) startStopRecording,
+        cast(AllowChangeDialingMode as int) as AgentPermissionDailing,
+        ISNULL(cast( DialingMode & 1 as int), 0) as DailingMode,
+        ISNULL(agentsPermissions.AllowUnassign, 0) AS AllowUnassign,
+        ISNULL(agentsPermissions.AllowSpam, 0 ) AS AllowSpam,
+        ISNULL(agentsPermissions.AllowPlayRecordsOnCallHistory, 0) AS AllowPlayRecordsOnCallHistory,
+        cast(AllowDeleteRecord as int) as AgentPermissionDelete,
+        AllowMarks as AllowMarks,
+        ISNULL(allowSelectCamp,0) as AllowSelectCamp
+    from 
+        ccUsers A
+    join ccRIAWorkGroupUsers B on 
+        A.user_id = B.user_id
+    left join ccRIAAgentsPermissions agentsPermissions on
+        A.User_id = agentsPermissions.AgentId
+    where 
+        tipoUser_id = 1 and 
+        IDWG in (select IDWG from ccRIAWorkGroupUsers where user_id = @user_id)
+    return(0)
+    END
+set nocount off
+    
+	'
+
+EXEC(@sql)
+
+
 		------------------------------------------------------END Rod Salazar ---------------------------------------------------------------------
 
 

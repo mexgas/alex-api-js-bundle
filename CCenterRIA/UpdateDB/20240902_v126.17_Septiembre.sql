0102fa6f75c3fa0f57064eac095472d27321fcbf
Charls <cechavez@nuxiba.com>
2024-09-03 00:07:36 -0600

    Se modifica sp ccsp_RIAUpdateCamConfig para agregar validacion de nombre repetido en campana

--- CCenterRIA/UpdateDB/20240902_v126.17_Septiembre.sql
+++ CCenterRIA/UpdateDB/20240902_v126.17_Septiembre.sql
@@ -3,62 +3,51 @@
 /*******************************/
 /*
 Author:
-
-
-Date: 02/02/2024
-Description: K089000
-
+Date: 2024/07/04
+Description: KR140000
 Database: CCenterRia
-Required version: 125.48
-
+Required version: 126.16
 IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
 */
 SET NOCOUNT ON
-
 DECLARE @version INT, @versionFix INT
 DECLARE @actualVersion INT, @actualVersionFix INT
 DECLARE @sql VARCHAR(max)
 DECLARE @errorGenerated VARCHAR(max)
 DECLARE @process VARCHAR(max)
 DECLARE @versionALL VARCHAR(max);
-
 /* Version to release (use the version of your own databse)*/
 /*******************************************************************************************************
 Importante:la variable @version puede tener 2 valores dependiendo la necesidad que se tenga el primer ejemplo
 set @version = 118  y  ccsp_getVersion ''BD'' se utilizara para cambiar de 117 a 118 en caso de que se tenga la version 119 y se vaya a agragar un fix
 sera necesario poner solo el fix es decir @version = 01 y ccsp_getVersion ''BDF'' se tendra que tener cuidado con las versiones ya que */
-SET @version = 125 --**********actualizar a 124 sin fix
-SET @versionfix = 48
+SET @version = 126 --**********actualizar a 124 sin fix
+SET @versionfix = 17
 /* Actual version (use your own script to do it)*/
 EXEC @actualVersion = ccsp_getVersion 'BD'
-
 EXEC @actualVersionFix = ccsp_getVersion 'BDF'
-
 SELECT @versionALL = valor
 FROM ccsettings
 WHERE setting_id = 77;
-
 SELECT @actualVersionFix = cast(isnull(max(value), '0') AS INT)
 FROM dbo.fn_RIASplitDelimited(@versionALL, '.')
 WHERE id = 5;
-
 --- Validacion para cuando pasamos a una nueva version LTS
 declare @versioMajer int= case when @version > @actualVersion then 1 else 0 end
-
-
 IF @version > @actualVersion 
 BEGIN 
     SET @actualVersionFix = 0
     select @version,@actualVersion,@versioMajer
 END
-
 IF @version >= @actualVersion and @versionfix >= @actualVersionFix 
 BEGIN
     BEGIN TRAN
-    BEGIN TRY    
-
-        SET @process = 'Alter sp ccsp_RIAUpdateCamConfig - se agrega validacion para nombre duplicado'
-        SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig]
+    BEGIN TRY
+        
+	
+	
+	SET @process = 'TT11673 TT11674 Alter sp ccsp_RIAUpdateCamConfig - se agrega validacion para nombre duplicado'
+    SET @sql = 'ALTER PROCEDURE [dbo].[ccsp_RIAUpdateCamConfig]
 @cam_id smallint,
 @cam_descripcion varchar(40) = null,
 @cam_tnotas smallint = null,
@@ -469,26 +458,24 @@ SELECT 2
 RETURN(0)
 
 set nocount off'
-        EXEC(@sql);
+    EXEC(@sql);
+	
 
-       
+	
 
-     
+
+
 
         /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
-        EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
-        EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
+       EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
+       EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)
 
         COMMIT TRAN
     END TRY
-
     BEGIN CATCH
         /* Error generated based on sintax */
         SELECT @errorGenerated = 'DB script version: ' + cast(@version AS NVARCHAR) + '''.''' + cast(@versionfix AS NVARCHAR) + ''' Error process: ''' + @process + ''' Line: ''' + cast(error_line() AS NVARCHAR) + ''' Number: ''' + cast(@@error AS NVARCHAR) + ''' Message: ''' + error_message()
-
         RAISERROR (@errorGenerated, 11, 1)
-
         ROLLBACK TRAN
     END CATCH
-END
-
+END 

a712d05881894fe9f0e6828a6540e98fe016e7a0
CarlosEdu710 <carlos.carbajal@nuxiba.com>
2024-09-12 14:47:12 -0600

    Espacio

--- CCenterRIA/UpdateDB/20240911_v126.19_September.sql
+++ CCenterRIA/UpdateDB/20240911_v126.19_September.sql
@@ -1915,7 +1915,8 @@ BEGIN
 				END;
         '
         EXEC(@sql)
-
+        ------------------------------------------------- Termina Carlos Muñoz ----------------------------------------------------------------------------------
+        
         /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
         EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
         EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)

f32cd1bb10b775c6fcca201e9abb56d19187df7b
agallardonuxiba <agallardo@nuxiba.com>
2024-09-22 20:38:39 -0600

    Se modifica para pruebas pruebas en el main

---
+++ DatabaseUpdateValidator/CopyMdfAndLdFMain.bat
@@ -0,0 +1,61 @@
+@echo off
+rem Copiar archivos .mdf y .ldf de una ruta relativa a otra
+
+rem Definir las rutas relativas de origen y destino
+set source=.\CCenterRIA\
+set destination=..\DataMain
+
+rem Crear el destino si no existe
+if not exist %destination% (
+    mkdir %destination%
+)
+
+rem Copiar todos los archivos .mdf y .ldf
+for %%f in ("%source%\*.mdf" "%source%\*.ldf") do (
+    if exist "%%~f" (
+        copy "%%~f" "%destination%\"
+        echo Archivo copiado: %%~f
+    ) else (
+        echo No se encontraron archivos .mdf o .ldf en la ruta de origen.
+    )
+)
+
+set source=.\ccReportsRia\
+set destination=.\Data
+
+rem Crear el destino si no existe
+if not exist %destination% (
+    mkdir %destination%
+)
+
+rem Copiar todos los archivos .mdf y .ldf
+for %%f in ("%source%\*.mdf" "%source%\*.ldf") do (
+    if exist "%%~f" (
+        copy "%%~f" "%destination%\"
+        echo Archivo copiado: %%~f
+    ) else (
+        echo No se encontraron archivos .mdf o .ldf en la ruta de origen.
+    )
+)
+
+set source=.\RecorderRIA\Database
+set destination=.\Data
+
+rem Crear el destino si no existe
+if not exist %destination% (
+    mkdir %destination%
+)
+
+rem Copiar todos los archivos .mdf y .ldf
+for %%f in ("%source%\*.mdf" "%source%\*.ldf") do (
+    if exist "%%~f" (
+        copy "%%~f" "%destination%\"
+        echo Archivo copiado: %%~f
+    ) else (
+        echo No se encontraron archivos .mdf o .ldf en la ruta de origen.
+    )
+)
+
+
+echo Todos los archivos .mdf y .ldf han sido procesados.
+

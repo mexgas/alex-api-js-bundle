2994338e346532ef019bd0f27d1a51004c35dc45
agallardonuxiba <agallardo@nuxiba.com>
2024-10-04 22:49:37 -0600

    Correcion reporte

--- schemaspy/ReportGenerate.bat
+++ schemaspy/ReportGenerate.bat
@@ -1,5 +1,5 @@
 cls
-set pathReport=D:/schemaspy
+set pathReport=C:/schemaspy
 java -jar schemaspy-6.1.0.jar -configFile ./CCenterRia.config.file -norows -o "%pathReport%/CCenterRia"
 
 rem start "" "%pathReport%/CCenterRia/index.html"

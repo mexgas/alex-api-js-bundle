847fcc4b4c20da7acb0e0b30bd19a3563cba8865
agallardonuxiba <agallardo@nuxiba.com>
2024-10-04 22:18:16 -0600

    Se modifica para pruebas

--- schemaspy/ReportGenerate.bat
+++ schemaspy/ReportGenerate.bat
@@ -12,6 +12,6 @@ java -jar schemaspy-6.1.0.jar -configFile ./CCRecorderRIA.config.file -norows -o
 
 rem start "" "%pathReport%/CCRecorderRIA/index.html"
 
-java -jar schemaspy-6.1.0.jar -configFile ./CW_CenterScript.config.file -norows -o "%pathReport%/CW_CenterScript"
+@REM java -jar schemaspy-6.1.0.jar -configFile ./CW_CenterScript.config.file -norows -o "%pathReport%/CW_CenterScript"
 
 rem start "" "/%pathReport%/CW_CenterScript/index.html"

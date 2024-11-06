9c92a48877bc2dab80dc937d95a3d255cc8cf810
agallardonuxiba <agallardo@nuxiba.com>
2024-11-06 13:45:55 -0600

    Se valida los cambios

--- CCenterRIA/UpdateDB/20231211_v125.48_Feb.sql
+++ CCenterRIA/UpdateDB/20231211_v125.48_Feb.sql
@@ -13491,116 +13491,113 @@ SET @process = 'ccsp_GalateaLoadUsersForManagement - SP Edited, Editado para el 
 Se asigna el LastName a @ApellidoPaterno = @LastName, y NombreOpcionalExtra a  @ApellidoMaterno = @NombreOpcionalExtra'
 SET @sql = '
 ALTER PROCEDURE [dbo].[ccsp_GalateaLoadUsersForManagement]
-	 @option SMALLINT,
-	 @AreaId SMALLINT,
-	 @UserType INT = null,
-	 @Username VARCHAR(200)=null,
-	 @userId INT = 0
-	as
+ @option SMALLINT,
+ @AreaId SMALLINT,
+ @UserType INT = null,
+ @Username VARCHAR(200)=null,
+ @userId INT = 0
+as
 
-	--Obtiene el idioma de de Centerware
-	Declare @lenguageXion varchar
-	select @lenguageXion= valor from ccsettings where setting_id=27 --  0 para espanol, 1 para ingles, 2 para portugues
+        --Obtiene el idioma de de Centerware
+        Declare @lenguageXion varchar
+        select @lenguageXion= valor from ccsettings where setting_id=27 --  0 para espanol, 1 para ingles, 2 para portugues
 
-	IF @option = 1 --Agentes/supervisores de un Area
-	BEGIN
-	  SELECT  TipoUser_id as UserType,
-	  User_id as UserId,
-	  LOGIN as Username,
-	  Nombres as Names,
-	  ApellidoPaterno as LastName,
-	  ApellidoMaterno as OptionalExtraName,
-	  Password as Password,
-	  Sexo as IsMan,
-	  CanChangeStatus as EnableNotReady,
-	  isnull(IDArea, 0) as AreaId,
-	  notificationEmail
-	  FROM ccusers
-	  WHERE isnull(IDArea, 0) = isnull(@AreaId, 0) AND TipoUser_id & 2 = CASE @UserType WHEN 1 THEN 0 ELSE 2 END AND STATUS = 1
-		AND DATEDIFF(dd, LastLoginAttempt, getdate()) < 60
-	  ORDER BY LOGIN, Nombres, ApellidoPaterno,Sexo, User_id
+IF @option = 1 --Agentes/supervisores de un Area
+BEGIN
+  SELECT  TipoUser_id as UserType,
+  User_id as UserId,
+  LOGIN as Username,
+  Nombres as Names,
+  ApellidoPaterno as LastName,
+  ApellidoMaterno as OptionalExtraName,
+  Password as Password,
+  Sexo as IsMan,
+  CanChangeStatus as EnableNotReady,
+  isnull(IDArea, 0) as AreaId
+  FROM ccusers
+  WHERE isnull(IDArea, 0) = isnull(@AreaId, 0) AND TipoUser_id & 2 = CASE @UserType WHEN 1 THEN 0 ELSE 2 END AND STATUS = 1
+        AND DATEDIFF(dd, LastLoginAttempt, getdate()) < 60
+  ORDER BY LOGIN, Nombres, ApellidoPaterno,Sexo, User_id
 
-	  RETURN (0)
-	END
+  RETURN (0)
+END
 
-	IF @option = 2 -- obtiene Agente o supervisor en base a su nombre de usuario
-	BEGIN
-	  SELECT  TipoUser_id as UserType,
-	  User_id as UserId,
-	  LOGIN as Username,
-	  Nombres as Names,
-	  ApellidoPaterno as LastName,
-	  ApellidoMaterno as OptionalExtraName,
-	  Password as Password,
-	  Sexo as IsMan,
-	  CanChangeStatus as EnableNotReady,
-	  isnull(IDArea, 0) as AreaId,
-	  notificationEmail
-	  FROM ccusers
-	  WHERE Login=@Username
+IF @option = 2 -- obtiene Agente o supervisor en base a su nombre de usuario
+BEGIN
+  SELECT  TipoUser_id as UserType,
+  User_id as UserId,
+  LOGIN as Username,
+  Nombres as Names,
+  ApellidoPaterno as LastName,
+  ApellidoMaterno as OptionalExtraName,
+  Password as Password,
+  Sexo as IsMan,
+  CanChangeStatus as EnableNotReady,
+  isnull(IDArea, 0) as AreaId
+  FROM ccusers
+  WHERE Login=@Username
 
-	  RETURN (0)
-	END
+  RETURN (0)
+END
 
-	IF @option = 3 -- obtiene Agente o supervisor en base a su ID de usuario
-	BEGIN
-	  SELECT  TipoUser_id as UserType,
-	  User_id as UserId,
-	  LOGIN as Username,
-	  Nombres as Names,
-	  ApellidoPaterno as LastName,
-	  ApellidoMaterno as OptionalExtraName,
-	  Password as Password,
-	  Sexo as IsMan,
-	  CanChangeStatus as EnableNotReady,
-	  isnull(IDArea, 0) as AreaId,
-	  notificationEmail
-	  FROM ccusers
-	  WHERE user_id=@userId
+IF @option = 3 -- obtiene Agente o supervisor en base a su ID de usuario
+BEGIN
+  SELECT  TipoUser_id as UserType,
+  User_id as UserId,
+  LOGIN as Username,
+  Nombres as Names,
+  ApellidoPaterno as LastName,
+  ApellidoMaterno as OptionalExtraName,
+  Password as Password,
+  Sexo as IsMan,
+  CanChangeStatus as EnableNotReady,
+  isnull(IDArea, 0) as AreaId
+  FROM ccusers
+  WHERE user_id=@userId
 
-	  RETURN (0)
-	END
+  RETURN (0)
+END
 
 
 
 
-	IF @option = 4 -- supervisores en Area/Sistema
-	BEGIN
-		DECLARE @Admins TABLE (UserId smallint, Username varchar(50), Names varchar(50), LastName varchar(50), OptionalExtraName varchar(50), AreaId smallint, primary key(UserId))
-		INSERT INTO @Admins
-		SELECT User_id as UserId,
-		LOGIN as Username,
-		Nombres as Names,
-		ApellidoPaterno as LastName,
-		ApellidoMaterno as OptionalExtraName,
+IF @option = 4 -- supervisores en Area/Sistema
+BEGIN
+        DECLARE @Admins TABLE (UserId smallint, Username varchar(50), Names varchar(50), LastName varchar(50), OptionalExtraName varchar(50), AreaId smallint, primary key(UserId))
+        INSERT INTO @Admins
+        SELECT User_id as UserId,
+        LOGIN as Username,
+        Nombres as Names,
+        ApellidoPaterno as LastName,
+        ApellidoMaterno as OptionalExtraName,
 
-		isnull(IDArea, 0) as AreaId
-		FROM ccusers
-		WHERE TipoUser_id = 2 AND STATUS = 1
+        isnull(IDArea, 0) as AreaId
+        FROM ccusers
+        WHERE TipoUser_id = 2 AND STATUS = 1
 
 
-		IF NOT EXISTS(SELECT * FROM ccUsers_Roles WHERE User_id=@userId and Rol_id=7) BEGIN
-			SELECT UserId, Username, Names, LastName, OptionalExtraName
-			FROM @Admins
-			WHERE AreaId = (SELECT IDArea FROM ccUsers WHERE User_id=@userId)
-			ORDER BY Username, Names, LastName, UserId
-		END
-		ELSE BEGIN
-			SELECT UserId, Username, Names, LastName, OptionalExtraName
-			FROM @Admins
-			ORDER BY Username, Names, LastName, UserId
-		END
-		Return(0)
-	END
+        IF NOT EXISTS(SELECT * FROM ccUsers_Roles WHERE User_id=@userId and Rol_id=7) BEGIN
+                SELECT UserId, Username, Names, LastName, OptionalExtraName
+                FROM @Admins
+                WHERE AreaId = (SELECT IDArea FROM ccUsers WHERE User_id=@userId)
+                ORDER BY Username, Names, LastName, UserId
+        END
+        ELSE BEGIN
+                SELECT UserId, Username, Names, LastName, OptionalExtraName
+                FROM @Admins
+                ORDER BY Username, Names, LastName, UserId
+        END
+        Return(0)
+END
 
-	IF @option = 5 --Usuarios inactivos por mas de 60 dias por area
-		BEGIN
-			SELECT [User_id] as UserId,
-			LOGIN as Username
-			FROM CCUSERS WHERE DATEDIFF(dd, LastLoginAttempt, getdate()) >= 60
-			AND @AreaId = IDArea
-			RETURN 0;
-		END
+IF @option = 5 --Usuarios inactivos por mas de 60 dias por area
+BEGIN
+        SELECT [User_id] as UserId,
+        LOGIN as Username
+        FROM CCUSERS WHERE DATEDIFF(dd, LastLoginAttempt, getdate()) >= 60
+        AND @AreaId = IDArea
+        RETURN 0;
+END
 '
 EXEC(@sql)
 ----------------------------------------------------------- End Luis Miguel Zamora Nuñez -------------------------------------------------------------------------

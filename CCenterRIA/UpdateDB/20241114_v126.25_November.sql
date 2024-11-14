827f26b2d81890030f7e4b889198f71208f2d5fd
Marco García <marco.garcia@nuxiba.com>
2024-11-14 12:49:59 -0600

    Se agregaron scripts para la HU K066005 y K066006

--- CCenterRIA/UpdateDB/20241114_v126.25_November.sql
+++ CCenterRIA/UpdateDB/20241114_v126.25_November.sql
@@ -1177,11 +1177,454 @@ BEGIN
 END'
         EXEC(@sql);
 ------------------------------------------- END MACL -------------------------------------------------------------
-		
-		SET @process = 'Alter SP ccsp_ConversationOutWASave'
-        SET @sql = ''
-        EXEC(@sql);
+------------------------------------------- BEGIN MAGV K066005 y K066006-------------------------------------------------------------
+	SET @process = 'K066005 y K066006- delete store procedure [ccsp_RIALoadAgents]'
+SET @sql = 'IF EXISTS (SELECT * FROM sys.procedures where name= N''ccsp_RIALoadAgents'')
+		BEGIN
+			DROP PROCEDURE ccsp_RIALoadAgents
+		END'
+EXEC(@sql)
 
+SET @process = 'K066005 y K066006 CREATE store procedure [ccsp_RIALoadAgents]
+Se agregó el @option = 20 para obtener los agentes por id'
+SET @sql = 'CREATE PROCEDURE [dbo].[ccsp_RIALoadAgents] @option SMALLINT, @AreaId SMALLINT, @Sup SMALLINT, @UserType SMALLINT, @IDWG SMALLINT = NULL, @IDCampACD VARCHAR(max) = NULL, 
+@IDUser VARCHAR(max) = NULL
+AS
+SET NOCOUNT ON
+
+DECLARE @IDArea INT
+DECLARE @loginDays INT
+
+SET @loginDays = 0
+
+IF @option IN (1, 7) --1:Todos los agentes/supervisores | 7:UN solo agente/supervisor
+BEGIN
+	SELECT User_id, LOGIN, TipoLlamadas, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name, isnull(IDArea, 0) IDArea, Sexo
+	FROM ccUsers WITH (READPAST)
+	WHERE TipoUser_id & 2 = CASE @UserType WHEN 1 THEN 0 ELSE 2 END AND STATUS > 0 AND user_id = CASE @option WHEN 7 THEN isnull(@sup, user_id) ELSE user_id END
+	ORDER BY IDArea, Nombres, ApellidoPaterno, User_id
+
+	RETURN (0)
+END
+
+IF @option = 2 --Agentes/supervisores de un Area
+BEGIN
+	SELECT User_id, LOGIN, TipoLlamadas, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name, isnull(IDArea, 0) IDArea, Sexo
+	FROM ccusers
+	WHERE isnull(IDArea, 0) = isnull(@AreaId, 0) AND TipoUser_id & 2 = CASE @UserType WHEN 1 THEN 0 ELSE 2 END AND STATUS = 1
+	ORDER BY Sexo, Nombres, ApellidoPaterno, User_id
+
+	RETURN (0)
+END
+
+IF @option = 3 --Agentes por Supervisor
+BEGIN
+	SELECT a3.user_id, a3.LOGIN, a3.TipoLlamadas, a3.Nombres + isnull('' '' + a3.ApellidoPaterno, '''') + isnull('' '' + a3.ApellidoMaterno, '''') name, isnull(a3.IDArea, 0) IDArea, a3.Sexo
+	FROM ccsupervisorcam a1
+	JOIN cccampsagente a2 ON a1.cam_id = a2.cam_id
+	JOIN ccusers a3 ON a2.user_id = a3.user_id
+	WHERE a1.tipo = ''1'' AND a1.user_id = @Sup AND a3.TipoUser_id = 1 AND a3.STATUS > 0
+	
+	UNION
+	
+	SELECT a3.user_id, a3.LOGIN, a3.TipoLlamadas, a3.Nombres + isnull('' '' + a3.ApellidoPaterno, '''') + isnull('' '' + a3.ApellidoMaterno, '''') name, isnull(a3.IDArea, 0) IDArea, a3.Sexo
+	FROM ccsupervisorcam a1
+	JOIN ccInboundagentes a2 ON a1.cam_id = a2.Inbound_id
+	JOIN ccusers a3 ON a2.user_id = a3.user_id
+	WHERE a1.tipo = ''0'' AND a1.user_id = @Sup AND a3.TipoUser_id = 1 AND a3.STATUS > 0
+	ORDER BY 5, 4, 1
+
+	RETURN (0)
+END
+
+IF @option = 4 --Load All Supervisors
+BEGIN
+	SELECT User_id, LOGIN, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name
+	FROM ccUsers
+	WHERE TipoUser_id IN (2, 6) AND STATUS > 0
+
+	RETURN (0)
+END
+
+IF @option = 5 --Agentes por Supervisor de sus WG
+BEGIN
+	SELECT @loginDays = valor
+	FROM ccSettings
+	WHERE setting_id = 211 --Numero dias que cargara las relaciones
+
+	SELECT @IDArea = IDArea
+	FROM ccUsers
+	WHERE User_id = @Sup
+
+	SELECT User_id, LOGIN, TipoLlamadas, max(name) name, IDArea, Sexo, IP, sum(sumMultimedia) sumMultimedia
+	FROM (
+		SELECT DISTINCT A.User_id, A.LOGIN, a.TipoLLamadas, A.Nombres + isnull('' '' + A.ApellidoPaterno, '''') + isnull('' '' + A.ApellidoMaterno, '''') name, isnull(A.IDArea, 0) IDArea, A.Sexo, isnull(C.IP, ''0.0.0.0'') IP, (CASE isnull(E.chat, 0) WHEN 3 THEN POWER(2, 0) WHEN 4 THEN POWER(2, 1) ELSE 0 END) AS sumMultimedia --, E.chat mode,E.Inbound_id
+		FROM ccUsers A
+		INNER JOIN ccRIAWorkGroupUsers B ON A.User_id = B.User_id
+		LEFT JOIN ccPosicion C ON C.user_id = A.User_id
+		INNER JOIN ccRIACampEspWG D ON D.IDWG = B.IDWG
+		LEFT JOIN ccInbound E ON D.IdCampEsp = E.Inbound_id AND E.IDArea = @IDArea
+		WHERE A.TipoUser_id = 1 AND (@loginDays = 0 OR DATEDIFF(dd, LastLoginAttempt, getdate()) <= @loginDays) AND B.IDWG IN (
+				SELECT IDWG
+				FROM ccRIAWorkGroupUsers
+				WHERE user_id = @Sup
+				)
+		) x
+	GROUP BY user_id, LOGIN, TipoLlamadas, IDArea, Sexo, IP
+
+	RETURN (0)
+END
+
+IF @option = 6 --Agentes por Supervisor de sus WG
+BEGIN
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.TipoLlamadas, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name
+	FROM ccusers a1
+	JOIN ccRIAWorkGroupUsers a2 ON a1.user_id = a2.user_id
+	WHERE tipouser_id IN (2, 6) AND IDWG IN (
+			SELECT IDWG
+			FROM ccRIAWorkGroupUsers
+			WHERE user_id = @Sup
+			)
+
+	RETURN (0)
+END
+
+IF @option IN (8, 9) --8:Agentes de un WG | 9:Supervisores de un WG
+BEGIN
+	DECLARE @wgUsers AS VARCHAR(500)
+
+	SELECT @wgUsers = coalesce(@wgUsers + '','', '''') + CAST(A.user_id AS VARCHAR(40))
+	FROM ccRIAWorkGroupUsers A
+	JOIN ccUsers B ON A.user_id = B.user_id
+	WHERE IDWG = @IDWG AND TipoUser_id = CASE @option WHEN 8 THEN 1 ELSE 2 END
+
+	SELECT @wgUsers wgUsers
+
+	RETURN (0)
+END
+
+IF @option = 10 --Todos los agentes/supervisores
+BEGIN
+	SELECT User_id, LOGIN, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name, isnull(IDArea, 0) IDArea, Sexo
+	FROM ccUsers WITH (READPAST)
+	WHERE TipoUser_id IN (/*2,*/ 6) AND STATUS > 0
+	ORDER BY LOGIN, IDArea, Nombres, ApellidoPaterno, User_id
+
+	RETURN (0)
+END
+
+IF @option = 11 -- Agentes por ACD
+BEGIN
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name, a1.Sexo, CAST( a2.prioridad AS tinyint ) AS prioridad, a2.skill
+	FROM ccusers a1
+	JOIN ccInboundAgentes a2 ON a1.user_id = a2.user_id
+	WHERE a1.tipouser_id = 1 AND a2.Inbound_id IN (
+			SELECT value
+			FROM dbo.fn_RIASplitDelimited(@IDCampACD, '','')
+			)
+
+	RETURN (0)
+END
+
+IF @option = 12 -- Agentes por Camp
+BEGIN
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name, a1.Sexo, a2.prioridad, a2.skill
+	FROM ccusers a1
+	JOIN ccCampsAgente a2 ON a1.user_id = a2.user_id
+	WHERE a1.tipouser_id = 1 AND a2.cam_id IN (
+			SELECT value
+			FROM dbo.fn_RIASplitDelimited(@IDCampACD, '','')
+			)
+
+	RETURN (0)
+END
+
+IF @option = 13 -- Sups por ACD
+BEGIN
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name
+	FROM ccusers a1
+	JOIN ccSupervisorCam a2 ON a1.user_id = a2.user_id
+	WHERE a1.tipouser_id & 2 = 2 AND tipo = 0 AND a2.cam_id IN (
+			SELECT value
+			FROM dbo.fn_RIASplitDelimited(@IDCampACD, '','')
+			)
+
+	RETURN (0)
+END
+
+IF @option = 14 -- Sups por Camp
+BEGIN
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name
+	FROM ccusers a1
+	JOIN ccSupervisorCam a2 ON a1.user_id = a2.user_id
+	WHERE a1.tipouser_id & 2 = 2 AND tipo = 1 AND a2.cam_id IN (
+			SELECT value
+			FROM dbo.fn_RIASplitDelimited(@IDCampACD, '','')
+			)
+
+	RETURN (0)
+END
+
+DECLARE @sxML AS VARCHAR(max), @xml AS XML, @action AS INT
+
+IF @option = 15 -- Info Agentes
+BEGIN
+	SET @action = @option - 9
+	SET @xml = cast(''<?xml version="1.0"?> <AgentData/>'' AS XML)
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT 1 AS tag, NULL AS parent, User_id "Agent!1!id", LOGIN "Agent!1!login", Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') "Agent!1!name", Sexo "Agent!1!gender", isnull(IDArea, 0) "Agent!1!areaID"
+					FROM ccUsers WITH (READPAST)
+					WHERE TipoUser_id = 1 AND STATUS > 0 AND user_id = @sup
+					) AS x
+				ORDER BY tag, "Agent!1!areaID", "Agent!1!name", "Agent!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<AgentData/>'')
+
+	SET @xml.modify(''insert element Workgroups {""} as last into (/AgentData/Agent)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT 1 AS tag, NULL AS parent, W.IDWG "Workgroup!1!id", W.WGName "Workgroup!1!description"
+					FROM ccRIACat_WorkGroup W
+					JOIN ccRIAWorkGroupUsers U ON W.IDWG = U.IDWG
+					WHERE user_id = @sup
+					) AS x
+				ORDER BY tag, "Workgroup!1!description"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<Workgroups/>'')
+
+	SET @xml.modify(''insert element Campaigns {""} as last into (/AgentData/Agent)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT DISTINCT 1 AS tag, NULL AS parent, a1.cam_id "Campaign!1!id", a1.cam_descripcion "Campaign!1!description", a3.frame "Campaign!1!frame", a1.cam_procesando "Campaign!1!processing"
+					FROM ccCamps a1
+					JOIN ccRIACampsGraph a2 ON a1.cam_id = a2.cam_id
+					JOIN ccRIAGraphics a3 ON a2.graphic_id = a3.graphic_id
+					JOIN ccCampsAgente a4 ON a1.cam_id = a4.cam_id
+					WHERE a3.type_id = 1 AND a4.user_id = @Sup
+					) AS x
+				ORDER BY tag, "Campaign!1!processing", "Campaign!1!description", "Campaign!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<Campaigns/>'')
+
+	SET @xml.modify(''insert element ACDs {""} as last into (/AgentData/Agent)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT DISTINCT 1 AS tag, NULL AS parent, a1.inbound_id "ACD!1!id", descripcion "ACD!1!description", frame "ACD!1!frame"
+					FROM ccinbound a1
+					JOIN ccRIAinboundGraph a2 ON a1.inbound_id = a2.inbound_id
+					JOIN ccRIAGraphics a3 ON a2.graphic_id = a3.graphic_id
+					JOIN ccInboundAgentes a4 ON a1.Inbound_id = a4.Inbound_id
+					WHERE a3.type_id = 1 AND a4.user_id = @Sup
+					) AS x
+				ORDER BY tag, "ACD!1!description", "ACD!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<ACDs/>'')
+
+	SET @xml.modify(''insert element action {""} as last into (/AgentData)[1]'')
+	SET @xml.modify(''insert attribute value {sql:variable("@action")} as last into (/AgentData/action)[1]'')
+
+	SELECT @xML
+
+	RETURN (0)
+END
+
+IF @option = 16 -- Info Sups
+BEGIN
+	SET @action = @option - 9
+	SET @xml = cast(''<?xml version="1.0"?> <SuperData/>'' AS XML)
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT 1 AS tag, NULL AS parent, User_id "Super!1!id", LOGIN "Super!1!login", Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') "Super!1!name", Sexo "Super!1!gender", isnull(IDArea, 0) "Super!1!areaID"
+					FROM ccUsers WITH (READPAST)
+					WHERE TipoUser_id & 2 = 2 AND STATUS > 0 AND user_id = @sup
+					) AS x
+				ORDER BY tag, "Super!1!areaID", "Super!1!name", "Super!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<SuperData/>'')
+
+	SET @xml.modify(''insert element Workgroups {""} as last into (/SuperData/Super)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT 1 AS tag, NULL AS parent, W.IDWG "Workgroup!1!id", W.WGName "Workgroup!1!description"
+					FROM ccRIACat_WorkGroup W
+					JOIN ccRIAWorkGroupUsers U ON W.IDWG = U.IDWG
+					WHERE user_id = @sup
+					) AS x
+				ORDER BY tag, "Workgroup!1!description"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<Workgroups/>'')
+
+	SET @xml.modify(''insert element Campaigns {""} as last into (/SuperData/Super)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT DISTINCT 1 AS tag, NULL AS parent, a1.cam_id "Campaign!1!id", a1.cam_descripcion "Campaign!1!description", a3.frame "Campaign!1!frame", a1.cam_procesando "Campaign!1!processing"
+					FROM ccCamps a1
+					JOIN ccRIACampsGraph a2 ON a1.cam_id = a2.cam_id
+					JOIN ccRIAGraphics a3 ON a2.graphic_id = a3.graphic_id
+					JOIN ccSupervisorCam a4 ON a1.cam_id = a4.cam_id
+					WHERE a3.type_id = 1 AND a4.user_id = @Sup AND a4.tipo = 1
+					) AS x
+				ORDER BY tag, "Campaign!1!processing", "Campaign!1!description", "Campaign!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<Campaigns/>'')
+
+	SET @xml.modify(''insert element ACDs {""} as last into (/SuperData/Super)[1]'')
+
+	SELECT @sxML = cast((
+				SELECT *
+				FROM (
+					SELECT DISTINCT 1 AS tag, NULL AS parent, a1.inbound_id "ACD!1!id", descripcion "ACD!1!description", frame "ACD!1!frame"
+					FROM ccinbound a1
+					JOIN ccRIAinboundGraph a2 ON a1.inbound_id = a2.inbound_id
+					JOIN ccRIAGraphics a3 ON a2.graphic_id = a3.graphic_id
+					JOIN ccSupervisorCam a4 ON a1.inbound_id = a4.cam_id
+					WHERE a3.type_id = 1 AND a4.user_id = @Sup AND a4.tipo = 0
+					) AS x
+				ORDER BY tag, "ACD!1!description", "ACD!1!id"
+				FOR XML explicit, type
+				) AS VARCHAR(max))
+
+	SELECT @xml = dbo.xmlAppend(@xml, @sxML, ''<ACDs/>'')
+
+	SET @xml.modify(''insert element action {""} as last into (/SuperData)[1]'')
+	SET @xml.modify(''insert attribute value {sql:variable("@action")} as last into (/SuperData/action)[1]'')
+
+	SELECT @xML
+
+	RETURN (0)
+END
+
+IF @option = 17 -- Load all agents
+BEGIN
+	SELECT @loginDays = valor
+	FROM ccSettings
+	WHERE setting_id = 211 --Numero dias que cargara las relaciones
+
+	SELECT DISTINCT user_id, LOGIN, TipoLlamadas, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name, isnull(IDArea, 0) IDArea, Sexo, ''0.0.0.0'' IP, 0 AS flagMine
+	INTO #allAgents
+	FROM ccusers
+	WHERE tipouser_id = 1 AND (@loginDays = 0 OR DATEDIFF(dd, LastLoginAttempt, getdate()) <= @loginDays)
+
+	SELECT DISTINCT a1.user_id, a1.LOGIN, a1.TipoLlamadas, a1.Nombres + isnull('' '' + a1.ApellidoPaterno, '''') + isnull('' '' + a1.ApellidoMaterno, '''') name, isnull(a1.IDArea, 0) IDArea, Sexo, isnull(IP, ''0.0.0.0'') IP
+	INTO #myAgents
+	FROM ccusers a1
+	JOIN ccRIAWorkGroupUsers a2 ON a1.user_id = a2.user_id
+	LEFT JOIN ccPosicion a3 ON a1.user_id = a3.user_id
+	JOIN ccRIACampEspWG a4 ON a2.idwg = a4.idwg
+	WHERE a1.tipouser_id = 1 AND (@loginDays = 0 OR DATEDIFF(dd, LastLoginAttempt, getdate()) <= @loginDays) AND a2.IDWG IN (
+			SELECT IDWG
+			FROM ccRIAWorkGroupUsers
+			WHERE user_id = @Sup
+			)
+
+	UPDATE #allAgents
+	SET flagMine = 1
+	FROM #allAgents a, #myAgents b
+	WHERE a.user_id = b.user_id
+
+	SELECT *
+	FROM #allAgents
+
+	DROP TABLE #allAgents
+
+	DROP TABLE #myAgents
+
+	RETURN (0)
+END
+
+IF @option = 18 -- View Agents
+BEGIN
+	SELECT isnull(viewAgents, 0) AS viewAgents
+	FROM ccusers
+	WHERE tipouser_id = 2 AND user_id = @Sup
+
+	RETURN (0)
+END
+IF @option = 19 -- Load Just One Supervisor
+BEGIN
+    SELECT User_id, Login, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') name
+    FROM ccUsers
+    WHERE TipoUser_id IN (2, 6) AND STATUS > 0 AND User_id = @Sup
+
+    RETURN (0)
+END
+IF @option = 20 -- Load Agents by user id
+BEGIN
+	SELECT User_id AS Id, LOGIN AS Login, Nombres + isnull('' '' + ApellidoPaterno, '''') + isnull('' '' + ApellidoMaterno, '''') Name
+	FROM ccUsers WITH (READPAST)
+	WHERE  User_id IN (
+			SELECT value
+			FROM dbo.fn_RIASplitDelimited(@IDUser, '','')
+			)
+	ORDER BY LOGIN, Nombres, ApellidoPaterno, User_id
+
+END
+
+SET NOCOUNT OFF'
+EXEC(@sql)
+
+SET @process = 'K066005 y K066006 column descripcion alter to varchar(50)'
+SET @sql = '
+    if EXISTS(select column_name from information_schema.columns  where table_name = ''ccTipoStatusAgente'' AND column_name = ''descripcion'')
+    BEGIN
+		ALTER TABLE dbo.ccTipoStatusAgente ALTER COLUMN descripcion VARCHAR(50)
+    END'
+EXEC(@sql)
+
+SET @process = 'K066005 y K066006 - Add Transfer Agent Unified Media to ccTipoStatusAgente table'
+SET @sql = '
+    declare  @language int;
+    select @language = valor from ccSettings where setting_id = 27; 
+    declare @descripTransferUnifiedMedia varchar(200);
+    if @language = 2 begin
+        set @descripTransferUnifiedMedia=''Transferência de agente de mídia unificada''
+    end
+    else if @language = 0 begin
+        set @descripTransferUnifiedMedia=''Transferencia Agente Medios Unificados''
+    end
+    else begin
+        set @descripTransferUnifiedMedia=''Transfer Agent Unified Media''
+    end 
+
+    if not exists (select 1 from ccTipoStatusAgente where TipoStatusAge_id = 39)
+    begin
+        insert into ccTipoStatusAgente values (39, @descripTransferUnifiedMedia)
+    end
+'
+
+EXEC(@sql)
+
+------------------------------------------- END MAGV -------------------------------------------------------------		
+	
         /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
         EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
         EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)

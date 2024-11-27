d33fbf686409dc7a6fc1ee1eea1cda5d1497892e
David Medina <david.medina@nuxiba.com>
2024-11-26 20:34:36 -0600

    Se cambia subconsulta por joins

--- CCenterRIA/UpdateDB/20241114_v126.25_November.sql
+++ CCenterRIA/UpdateDB/20241114_v126.25_November.sql
@@ -2553,16 +2553,10 @@ BEGIN
         INNER JOIN #TmpConversationIds tci ON tci.Id = cwc.conversationId
         LEFT JOIN ccTipoCalif ctc ON ctc.calif_id = cwc.disposition
         LEFT JOIN ccTipoCalifSub ctcs ON ctcs.califSub_id = cwc.subDisposition
-        LEFT JOIN ccMetaWhatsAppNumbers mwn 
-            ON mwn.number = (
-                SELECT phoneACD
-                FROM ccWhatsAppConversations
-                WHERE conversationId = tci.Id
-            )
-        LEFT JOIN cccamps ccc 
-            ON ccc.cam_Id = mwn.Cam_Id
+        LEFT JOIN ccMetaWhatsAppNumbers mwn ON mwn.Number = cwc.phoneACD
+        LEFT JOIN cccamps ccc ON ccc.cam_Id = mwn.Cam_Id 
     END
-    ELSE
+	ELSE
     BEGIN
         SELECT 
             cwo.conversationId AS ConversationId, 
@@ -2591,18 +2585,18 @@ BEGIN
 					OR cu.User_id IS NULL THEN ''''
 				ELSE cu.Nombres
 			END) AS AgentName,
-            ISNULL(cwo.camId, 0) AS ReopenWithTemplateOutboundCamId,
-            (SELECT cam_descripcion 
-             FROM cccamps 
-             WHERE cam_id = cwo.camId) AS ReopenWithTemplateOutboundCamName
+			ISNULL(CAST(ccc.Cam_Id AS SMALLINT), 0) AS ReopenWithTemplateOutboundCamId,
+            ccc.cam_descripcion AS ReopenWithTemplateOutboundCamName
         FROM ccWhatsAppConversationsOut cwo
         INNER JOIN ccCamps cc ON cc.cam_id = cwo.camId 
         LEFT JOIN ccUsers cu ON cu.User_id = cwo.agentId
         INNER JOIN #TmpConversationIds tci ON tci.Id = cwo.conversationId
         LEFT JOIN ccTipoCalifOUT ctco ON ctco.calif_id = cwo.disposition
         LEFT JOIN ccTipoCalifSubOUT ctcso ON ctcso.califSub_id = cwo.subDisposition
+		LEFT JOIN ccMetaWhatsAppNumbers mwn ON mwn.Number = cwo.phoneCamp
+		LEFT JOIN ccCamps ccc on ccc.cam_id = mwn.Cam_Id 
     END
-END;
+END
 
     DECLARE @MaxWhatsAllowed INT;
     DECLARE @ConversationCount INT;

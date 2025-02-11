46938c7904669bb6636b940fc067c1517ebde24b
IsaacCortes <isaac.godinez@nuxiba.com>
2025-02-11 16:43:18 -0600

    quitando comillas extras

--- CCenterRIA/UpdateDB/20250129_v127.99_MiniLuis.sql
+++ CCenterRIA/UpdateDB/20250129_v127.99_MiniLuis.sql
@@ -2504,7 +2504,7 @@ if @action= 0 begin
                 ,ISNULL(cc.CamCanceled, 4) AS CamCanceled
                 ,ISNULL(ex.SimultaneousRecs, 0) AS SimultaneousRecs
                 ,ISNULL(cva.idAgent, 0) AS IdAgentVirtual
-				,ISNULL(cva.nameAgent, '''''''') AS NameAgentVirtual
+				,ISNULL(cva.nameAgent, '''') AS NameAgentVirtual
                 ,ISNULL(cva.concurrentSessionsLimit, 0) AS NumberSessions
                 FROM ccCamps cc (NOLOCK) 
                 LEFT JOIN ccCampsExtend ex (NOLOCK) ON ex.cam_id = cc.cam_id

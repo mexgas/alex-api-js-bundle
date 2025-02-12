6598206187aec58483a44f0e93edd32929bb545f
FriAleOm <frida.orta@nuxiba.com>
2025-02-12 15:49:27 -0600

    Se agrega option 9 al sp ccsp_RIARegistryLists para actualizar el orden de las listas

--- CCenterRIA/UpdateDB/20250129_v127.99_MiniLuis.sql
+++ CCenterRIA/UpdateDB/20250129_v127.99_MiniLuis.sql
@@ -2730,6 +2730,203 @@ END
     '
     EXEC(@sql);
 ---------------------------------------------------- END Isaac --------------------------------------------------------------
+---------------------------------------------------- BEGIN Frida --------------------------------------------------------------
+
+	SET @process = 'DEV2-844-Listas Drop procedure ccsp_RIARegistryLists'
+    SET @sql = 'IF EXISTS (SELECT * FROM sysobjects WHERE name=''ccsp_RIARegistryLists'')
+		BEGIN
+			DROP PROCEDURE ccsp_RIARegistryLists
+		END'
+    EXEC(@sql);
+	SET @process = 'DEV2-844-Listas Create procedure ccsp_RIARegistryLists'
+    SET @sql = '
+	CREATE Procedure ccsp_RIARegistryLists
+@action tinyint = 0, 
+@list_id int = 0,
+@cam_id smallint = 0,
+@name varchar(80) = '''',
+@status tinyint = 0,
+@sequence smallint = 0,
+@load_id int = 0,
+@listIds VARCHAR(MAX) = '''',
+@sequences VARCHAR(MAX) = ''''
+
+AS
+
+--Status lista 0: inactiva, 1:pausa, 2:procesar
+
+--Insert
+IF @action = 1 begin
+
+    IF @cam_id <> 0 begin
+        select @sequence = isnull(max( sequence ),0) from ccRIARegistryLists where cam_id = @cam_id
+        set @sequence = @sequence + 1
+        Insert into ccRIARegistryLists(cam_id,name,status,sequence) values (@cam_id, @name, 2, @sequence)
+        select max(list_id) from ccRIARegistryLists
+    end
+end
+
+--Update sequence
+IF @action = 2 begin
+    
+    declare @oldSeq as int
+    select @oldSeq = sequence, @cam_id = cam_id from ccRIARegistryLists where list_id = @list_id
+
+    if @oldSeq <> @sequence begin
+        
+        if @oldSeq > @sequence begin
+            update ccRIARegistryLists set sequence = sequence + 1 where cam_id = @cam_id and sequence >= @sequence and sequence < @oldSeq
+        end
+
+        if @oldSeq < @sequence begin
+            update ccRIARegistryLists set sequence = sequence - 1 where cam_id = @cam_id and sequence <= @sequence and sequence > @oldSeq
+        end
+
+        update ccRIARegistryLists set sequence = @sequence where list_id = @list_id
+
+    end
+
+end
+
+--Change status
+IF @action = 3 begin
+    
+    update ccRIARegistryLists set status = @status where list_id = @list_id
+    SELECT 200 as ReturnValue
+
+end
+
+-- lista campañas y listas de registros
+IF @action = 4 begin
+    select a.cam_id, b.cam_descripcion, count(list_id) as NoListas, c.graphic_id as Frame 
+    from ccRIARegistryLists a  with(nolock)
+    left join cccamps b on a.cam_id = b.cam_id
+    left join ccRIACampsGraph c on a.cam_id = c.cam_id
+    where b.cam_activo = 1 and a.cam_id in ( select distinct(cam_id) from ccRIARegistryLists ) 
+    group by a.cam_id,b.cam_descripcion,c.graphic_id order by a.cam_id asc
+
+end
+
+-- listas de registros y no. registros
+IF @action = 5 
+begin
+    select a.list_id,a.name,count(b.list_id) as NoRegistros,a.sequence   
+    from ccRIARegistryLists a with(index(IX_ccRIARegistryLists_1),nolock) 
+    left join ccocallsoutsource b with(index(IX_ccoCallsOutSource_13),nolock) 
+    on b.cam_id = @cam_id and a.list_id = b.list_id 
+    where a.status > 0 and a.cam_id = @cam_id and status > 0 
+    group by a.list_id,a.name,a.sequence 
+    order by a.sequence
+end
+
+-- borrar lista
+IF @action = 6 begin
+
+    select @cam_id = cam_id from ccRIARegistryLists where list_id = @list_id
+    select @sequence = max(sequence) from ccRIARegistryLists where cam_id = @cam_id
+    exec ccsp_RIARegistryLists @action = 3, @status = 0, @list_id = @list_id
+    exec ccsp_RIARegistryLists @action = 2, @sequence = @sequence, @list_id = @list_id
+    SELECT 200 as ReturnValue
+
+end
+
+-- Detalle de numero de registros
+IF @action = 7 begin
+
+    declare @total as int
+
+    select @total = count(*) from ccocallsoutsource where list_id = @list_id
+    select @total = (@total - count(*)) from ccoworkingtable where list_id = @list_id
+
+    if exists(select list_id from ccoWorkingTable where list_id = @list_id) begin
+        select @status = status from ccRIARegistryLists where list_id = @list_id
+        select @list_id as list_id,cast(cam_id as smallint) as cam_id, @status as status,
+            count(case cal_status when 0 then 1 else null end) as New,
+            count(case cal_status when 1 then 1 else null end) as CB,
+            count(case cal_status when 2 then 1 else null end) as Pro, 
+            @total as Fin
+        from ccoWorkingTable where list_id = @list_id group by cam_id
+    end
+    ELSE begin
+        select list_id, cam_id, status, 
+        0 as New,
+        0 as CB,
+        0 as Pro,
+        0 as Fin
+        from ccRIARegistryLists where list_id = @list_id
+    end
+
+
+end
+
+-- Cambia de nombre a la lista
+IF @action = 8 begin
+    
+    update ccRIARegistryLists set name = @name where list_id = @list_id
+
+end
+
+-- Borra listas sin registros y reordena las listas
+IF @action = 9 begin
+
+    Create table #TempRegs(
+        list_id int,
+        [name] varchar(100),
+        NoRegistros int,
+        sequence int)
+
+    insert into #TempRegs 
+        select a.list_id,a.name,count(b.list_id) as NoRegistros,a.sequence 
+        from ccRIARegistryLists a with(index(IX_ccRIARegistryLists_1),nolock)
+        left join ccocallsoutsource b with(index(IX_ccoCallsOutSource_14),nolock)
+        on a.list_id = b.list_id
+        where a.status > 0 and a.cam_id = @cam_id and status > 0 
+        group by a.list_id,a.name,a.sequence,a.status order by a.sequence
+
+    while ( exists( select list_id from #TempRegs where NoRegistros = 0 ) ) begin
+        declare @listToDelete as int
+        select top 1 @listToDelete = list_id from #TempRegs where NoRegistros = 0
+        exec ccsp_RIARegistryLists @action = 6, @list_id = @listToDelete
+        delete from #TempRegs where list_id =  @listToDelete
+    end
+
+    drop table #TempRegs
+    
+    select @sequence=min(sequence) from ccRIARegistryLists where  cam_id = @cam_id and status = 0 
+
+    select @list_id= list_id from ccRIARegistryLists where sequence =(
+    select  max(sequence) as sequence from ccRIARegistryLists where  cam_id = @cam_id and status > 0 ) and cam_id = @cam_id
+    update ccRIALoading set list_id = @list_id where load_id=@load_id
+    exec ccsp_RIARegistryLists @action=2,@list_id=@list_id,@sequence=@sequence
+
+    end
+	--- Actualizar orden de las listas ------------
+	IF @action = 9 begin
+
+
+	DECLARE @list_Ids TABLE (i int, ListId int)
+	insert @list_Ids select * from dbo.fn_RIASplitDelimited (@listIds, '','') 
+
+	DECLARE @list_sequence TABLE (i int, ListSecuence int)
+	insert @list_sequence  select * from dbo.fn_RIASplitDelimited (@sequences, '','') 
+
+
+	declare @i int, @n int, @idList int, @secuence int 
+	select @i = 1 , @n = COUNT(ListId) from @list_Ids
+	while (@i <= @n)
+		begin
+			select @secuence = ListSecuence from @list_sequence where i = @i 
+			select @idList = ListId from @list_Ids where i = @i 
+			update ccRIARegistryLists set sequence=@secuence where list_id=@idList 
+			set @i = @i + 1
+		end
+	select 1
+	end'
+    EXEC(@sql);
+---------------------------------------------------- END Frida --------------------------------------------------------------
+
+
+
  
 
 

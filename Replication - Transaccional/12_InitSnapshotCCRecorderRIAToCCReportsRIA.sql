d365fa47aa6e0b577c33e7ee654bef5797503b69
agallardonuxiba <agallardo@nuxiba.com>
2025-02-23 23:37:50 -0600

    Se hacen pruebas replicas

---
+++ Replication - Transaccional/12_InitSnapshotCCRecorderRIAToCCReportsRIA.sql
@@ -0,0 +1,186 @@
+set nocount on
+use [CCRecorderRIA]
+declare @Version int, @Version_Actual int
+---------------- VERSION ----------------
+Set @Version = '9'
+
+select @Version_Actual = par_valor from TREC_PARAMETROS where par_id = 30 
+
+if @Version_Actual >= @Version
+ begin
+	declare @Sql nvarchar(max)
+
+	---------------- INICIO SCRIPT ----------------
+
+	set @Sql = 'IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE [name] = ''MigrationAVRSReports'')
+	BEGIN
+		CREATE TABLE [dbo].[MigrationAVRSReports](
+		[id] [int] NOT NULL,
+		[description] [varchar](255) NOT NULL,
+		[status] [int] NOT NULL,
+		[error] [nvarchar](max) NOT NULL,
+		[dateStart] datetime NOT NULL,
+		[dateEnd] datetime NOT NULL
+		) ON [PRIMARY]		
+	END
+	truncate table MigrationAVRSReports;
+
+	insert into MigrationAVRSReports
+SELECT ROW_NUMBER() OVER(ORDER BY name desc)+99 AS id, P.name as [description],0 as status,'''' as error,''1901-01-01'' as dateStart,''1901-01-01'' as dateEnd FROM dbo.sysmergepublications P
+where  P.publisher_db=''CCRecorderRIA'' 
+	'
+
+		EXEC(@Sql)		
+
+			set @Sql='USE [msdb]
+
+
+/****** Object:  Job [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+if exists( select * from msdb.dbo.sysjobs where name=''AVRSReports Merge Replication'')
+EXEC msdb.dbo.sp_delete_job @job_name=N''AVRSReports Merge Replication'', @delete_unused_schedule=1
+
+/****** Object:  Job [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''[Uncategorized (Local)]'' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''[Uncategorized (Local)]''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''AVRSReports Merge Replication'', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N''No description available.'', 
+		@category_name=N''[Uncategorized (Local)]'', 
+		@owner_login_name=N''sa'', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [AVRSReports Merge Replication]    Script Date: 23/06/2018 11:08:46 a.m. ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N''AVRSReports Merge Replication'', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N''TSQL'', 
+		@command=N''
+declare @id int,@i int,@count int
+declare @publicationName varchar(max)
+declare @dateStart datetime,@dateNow datetime
+declare @status int
+declare @maxId int,@minId int
+
+--delete from MigrationAVRSReports
+
+select @maxId=isnull(max(id),99),@dateNow =getdate(),@i=0 FROM MigrationAVRSReports
+
+insert into MigrationAVRSReports
+SELECT ROW_NUMBER() OVER(ORDER BY name desc)+@maxId AS id, P.name as [description],0 as status,'''''''' as error,''''1901-01-01'''' as dateStart,''''1901-01-01'''' as dateEnd FROM dbo.sysmergepublications P
+left join MigrationAVRSReports M on P.name=M.[description]
+where  P.publisher_db=''''CCRecorderRIA'''' and M.[description] is null 
+
+select @minId=ISNULL(min(id),99), @maxId=isnull(max(id),99),@count=COUNT(*) FROM MigrationAVRSReports
+
+select * FROM MigrationAVRSReports
+
+if exists(select * FROM MigrationAVRSReports where status in(0,1)) begin
+
+	while @i<@count and DATEDIFF(ss,@dateNow,getdate())<59 begin
+		select @publicationName=[description],  @dateStart  = dateStart, @status = status from MigrationAVRSReports  with (nolock) where id=@minId+@i
+		
+		if @status = 2 begin		 
+		 set @i=@i+1
+		 continue;
+		end
+
+		if (not exists( select * from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+			where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+			and a.runstatus = 2 and b.publication = @publicationName and a.start_time < convert(datetime,convert(varchar(10),@dateStart,121)))	   
+			) begin
+				 update MigrationAVRSReports with (rowlock) set [status] = 1, [dateStart] = getdate() where id=@minId+@i     
+				 
+				 exec sp_startpublication_snapshot @publication = @publicationName
+
+				 WAITFOR DELAY ''''00:00:01''''
+
+				while not exists(
+					select *
+					from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+					where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+					and a.runstatus = 2
+					and b.publication = @publicationName and a.start_time > convert(datetime,convert(varchar(10),@dateStart,121))	
+				)
+				begin
+					WAITFOR DELAY ''''00:00:01''''
+					if DATEDIFF(ss,@dateNow,getdate())>=59 begin
+						break
+					end
+				end
+				update MigrationAVRSReports with (rowlock) set [status] = 2, [dateEnd] = getdate() where id=@minId+@i
+		end
+		else if(@status = 1 and
+				exists( select * from distribution.dbo.MSsnapshot_history a ,distribution.dbo.MSsnapshot_agents b
+						where b.publisher_db = ''''CCRecorderRIA'''' and b.id = a.agent_id 
+						and a.runstatus = 2 and b.publication = @publicationName and a.start_time > convert(datetime,convert(varchar(10),@dateStart,121))
+						)
+			) begin 	
+			update MigrationAVRSReports with (rowlock) set [status] = 2, [dateEnd] = getdate() where id=@minId+@i  and status=1
+		end		
+		set @i=@i+1
+		
+	end
+
+end
+'', 
+		@database_name=N''CCRecorderRIA'', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N''replication'', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=1, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20130625, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N''(local)''
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+'
+
+		EXEC(@Sql)
+
+	------------------ FIN SCRIPT @Sql ------------------
+
+	select 'Merge Snapshots Finished'
+ end
+
+else
+ begin
+	select 'Version incorrecta de base de datos, version actual: ' 
+	+ cast(@Version_Actual as varchar(5))
+	+ ', version que desea ingresar: ' + cast(@Version as varchar(5))
+ end
+set nocount off

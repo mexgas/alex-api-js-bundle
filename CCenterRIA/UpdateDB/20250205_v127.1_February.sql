3b2665b797bb22d62393e7b0b0709aca115fdd75
agallardonuxiba <agallardo@nuxiba.com>
2025-03-05 21:06:06 -0600

    se agrega para los reportes

--- CCenterRIA/UpdateDB/20250205_v127.1_February.sql
+++ CCenterRIA/UpdateDB/20250205_v127.1_February.sql
@@ -1290,6 +1290,78 @@ select @prefix as sDialPrefix, @tNoContesta as tNoContesta,@ani as ani, @detectA
 		
  ------------------------------------------------------ End Ulises Espinosa ------------------------------------------------------------------
 
+	SET @process = 'DROP PROCEDURE GetReportMenus'
+    SET @sql = '
+	if exists (select * from sys.procedures where name = N''GetReportMenus'')
+	begin
+		DROP PROCEDURE GetReportMenus
+	end'
+    EXEC(@sql)
+
+    SET @process = 'CREATE PROCEDURE [dbo].[GetReportMenus]'
+    SET @sql = 'CREATE PROCEDURE [dbo].[GetReportMenus]
+@userId int,
+@activeChat tinyint,
+@activeAVRS tinyint,
+@activeCRM tinyint=0,
+@activeEmail tinyint=0,
+@activeTwitter tinyint=0
+AS
+BEGIN
+
+select menu_id,
+	substring(menu_descrip, charindex(''|'', menu_descrip) + 1, len(menu_descrip)) as menu_descrip,
+	nullif(parent,menu_id) as parent,Nivel,ordengral,release
+	into #tempCCMenus
+	from ccMenus with(nolock)
+	where type = 3 and menu_id >= 2000 and(
+		(menu_id not in (
+		3130,3131,3132,3133,3134,3135,3136,
+		8050,8060,8061,8062,8063,8070,8071,8072,8080,
+		9000,9010,
+		10000,10010,10020,10030,10040,
+		11000,11010,11020,11030,11040
+		))
+		or  (@activeChat = 1 and menu_id in (3130,3131,3132,3133,3134,3135,3136))
+		or  (@activeAVRS = 1 and menu_id in (8050,8060,8061,8062,8063,8070,8071,8072,8080) )
+		or  (@activeCRM = 1 and menu_id in (9000,9010) )
+		or  (@activeEmail = 1 and menu_id in (10000,10010,10020,10030,10040) )
+		or (@activeTwitter = 1 and menu_id in (11000,11010,11020,11030,11040))
+		)
+		order by menu_id
+
+
+;WITH ccMenusUserRec(Nivel, menu_descrip, menu_id, ordengral, parent,release)
+AS
+(
+	select
+		distinct b.Nivel as Nivel,
+		b.menu_descrip as menu_descrip,
+		b.menu_id as menu_id,
+		b.ordengral as ordengral,
+		b.parent as parent,b.release
+		from #tempCCMenus as b
+		inner join ccMenuUser as a with(nolock) on a.id_menu = b.menu_id and a.id_User = @userId and b.menu_id<>b.parent and a.type = 3
+	UNION ALL
+
+
+--RECURSIViDAD
+	select a.Nivel, a.menu_descrip, a.menu_id, a.ordengral, a.parent,a.release
+		from #tempCCMenus a inner join ccMenusUserRec b on a.menu_id=b.parent
+)
+
+select distinct Nivel,menu_descrip,menu_id,ordengral,parent,release into #tempCCMenusUser from ccMenusUserRec order by ordengral,menu_id
+
+select distinct A.Nivel, A.menu_descrip, A.menu_id, A.ordengral,5 filtersType,A.release,parent from #tempCCMenusUser A
+where  menu_id not in
+	(select distinct parent from  #tempCCMenus where Nivel=''C'' and parent not in (select distinct  A.parent from  #tempCCMenusUser A where A.Nivel=''C''))
+order by ordengral,menu_id
+
+drop table #tempCCMenus
+drop table #tempCCMenusUser
+
+END '
+    EXEC(@sql)
 	
         /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
         EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)

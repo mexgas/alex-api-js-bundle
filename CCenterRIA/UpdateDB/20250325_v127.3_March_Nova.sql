fe30c24830901e7d7791189b54b759b19bd54f48
IsaacCortes <isaac.godinez@nuxiba.com>
2025-03-25 10:55:40 -0600

    cambios para insertar activity log

--- CCenterRIA/UpdateDB/20250325_v127.3_March_Nova.sql
+++ CCenterRIA/UpdateDB/20250325_v127.3_March_Nova.sql
@@ -2552,7 +2552,172 @@ BEGIN
 END'
         EXEC(@sql)
 
+    SET @process = 'CW-9302 Create operation 135'
+    SET @sql = '
+    IF NOT EXISTS(SELECT OperationId FROM ccGalateaOperations WHERE OperationId=135)
+    BEGIN
+        INSERT INTO ccGalateaOperations (OperationId, OpTagEs, OpTagEn, OpTagPt)
+        VALUES (135, ''Asignar puerto'', ''Assign port'', ''Atribuir porta'')
+    END
+    '
+    EXEC(@sql)
 
+    SET @process = 'CW-9302 Create relation module - operation 135'
+    SET @sql = '
+    IF NOT EXISTS(SELECT OperationId FROM ccGalateaModOpRelation WHERE OperationId=135)
+    BEGIN
+        INSERT INTO ccGalateaModOpRelation (ModuleId, OperationId)
+        VALUES (14, 135)
+    END
+    '
+    EXEC(@sql)
+
+    SET @process = 'CW-9302 Create operation 136'
+    SET @sql = '
+    IF NOT EXISTS(SELECT OperationId FROM ccGalateaOperations WHERE OperationId=136)
+    BEGIN
+        INSERT INTO ccGalateaOperations (OperationId, OpTagEs, OpTagEn, OpTagPt)
+        VALUES (136, ''Desasignar puerto'', ''Unassign port'', ''Cancelar atribuição de porta'')
+    END
+    '
+    EXEC(@sql)
+
+    SET @process = 'CW-9302 Create relation module - operation 136'
+    SET @sql = '
+    IF NOT EXISTS(SELECT OperationId FROM ccGalateaModOpRelation WHERE OperationId=136)
+    BEGIN
+        INSERT INTO ccGalateaModOpRelation (ModuleId, OperationId)
+        VALUES (14, 136)
+    END
+    '
+    EXEC(@sql)
+
+    SET @process = 'CW-9302 Drop sp ccsp_GalateaAdminPortsManagement'
+    SET @sql = '
+    IF EXISTS (SELECT 1 FROM sys.procedures WHERE name = N''ccsp_GalateaAdminPortsManagement'')
+    BEGIN
+        DROP PROCEDURE ccsp_GalateaAdminPortsManagement;
+    END
+    '
+    EXEC(@sql)
+
+    SET @process = 'CW-9302 Create sp ccsp_GalateaAdminPortsManagement'
+    SET @sql = '
+CREATE PROCEDURE [dbo].[ccsp_GalateaAdminPortsManagement]
+@action SMALLINT,
+@dialer_id INT = 0,
+@cam_id SMALLINT = 0,
+@list_dialier_id varchar(max) ='''',
+@user_id SMALLINT = 0
+AS
+SET NOCOUNT ON;
+DECLARE @transtate BIT
+IF @@TRANCOUNT = 0
+BEGIN
+    SET @transtate = 1
+BEGIN TRANSACTION transtate
+END
+BEGIN TRY
+	
+	IF @action IN (3,4) -- activity log
+	BEGIN
+		DECLARE @login VARCHAR(50) = '''',
+				@camp VARCHAR(40) = ''''
+		DECLARE @dialerSource TABLE (dialer_id INT)
+
+		IF @list_dialier_id = ''''
+			INSERT INTO @dialerSource VALUES (@dialer_id)
+		ELSE
+			INSERT INTO @dialerSource
+			SELECT Value FROM fn_RIASplitDelimited(@list_dialier_id, '','')
+			
+		SELECT 
+			@login = cu.Login
+		FROM ccUsers cu
+		WHERE cu.User_id = @user_id
+
+		SELECT 
+			@camp = cc.cam_descripcion
+		FROM ccCamps cc WHERE cc.cam_id = @cam_id
+
+		-- activity log
+		INSERT INTO ccGalateaActivityLog (Area, ActivityDate, Login, OperationId, ModuleId, Identifier, Value, Target)
+		SELECT
+			''Default'',
+			GETDATE(),
+			@login,
+			CASE WHEN @action = 3 THEN 135 ELSE 136 END,
+			14,
+			cd.Descripcion,
+			'''',
+			@camp
+		FROM ccoDialers cd
+		WHERE cd.dialer_id IN (SELECT dialer_id FROM @dialerSource)
+	END
+
+    IF @action = 1 --return all ports
+    BEGIN
+        SELECT Dialers.dialer_id AS DialerId, Dialers.Descripcion AS PortDescription, Provedor.Descrip AS ProviderDescription, Dialers.Puerto, XferType, DialingType, case when DialingType = 1 then 0 else IdCode end AS DialingCode
+        FROM [CCenterRIA].[dbo].[ccoDialers] AS Dialers INNER JOIN [CCenterRIA].[dbo].[cstoProvedor] AS Provedor 
+        ON Dialers.provedor_id = Provedor.provedor_id
+    END;
+    IF @action = 2 --return ports for camp
+    BEGIN
+        SELECT dialer_id AS DialerId, cam_id AS CampId FROM [CCenterRIA].[dbo].[ccoDialerCamp] ORDER BY cam_id
+    END;
+    IF @action = 3 --insert port
+    BEGIN
+        IF @list_dialier_id = ''''
+        BEGIN
+            IF NOT EXISTS (SELECT dialer_id, cam_id FROM [CCenterRIA].[dbo].[ccoDialerCamp]
+                WHERE dialer_id=@dialer_id AND cam_id=@cam_id)
+            BEGIN
+                INSERT INTO [CCenterRIA].[dbo].[ccoDialerCamp](dialer_id, cam_id) VALUES (@dialer_id, @cam_id)
+            END;
+        END
+        ELSE
+        BEGIN
+			INSERT INTO [CCenterRIA].[dbo].[ccoDialerCamp](dialer_id, cam_id)
+			Select dialer_id,@cam_id from ccoDialers where dialer_id not in (SELECT dialer_id FROM [CCenterRIA].[dbo].[ccoDialerCamp]
+			WHERE dialer_id in (select Value FROM fn_RIASplitDelimited(@list_dialier_id, '','') where [value] > 0) AND cam_id=@cam_id) and
+					dialer_id in (select Value FROM fn_RIASplitDelimited(@list_dialier_id, '',''))
+        END;
+    END;
+    IF @action = 4 --delete port
+    BEGIN
+        IF @list_dialier_id = ''''
+		BEGIN
+            DELETE FROM [CCenterRIA].[dbo].[ccoDialerCamp] WITH(ROWLOCK) WHERE cam_id = @cam_id AND dialer_id = @dialer_id
+		END
+        ELSE
+        BEGIN
+            DELETE FROM [CCenterRIA].[dbo].[ccoDialerCamp] WITH(ROWLOCK) WHERE cam_id = @cam_id AND dialer_id in (select Value FROM fn_RIASplitDelimited(@list_dialier_id, '','') where [value] > 0)
+        END;
+    END;
+
+    IF @action = 5 --return ports for single camp
+    BEGIN
+        SELECT dialer_id AS DialerId, cam_id AS CampId FROM [CCenterRIA].[dbo].[ccoDialerCamp] WHERE cam_id = @cam_id ORDER BY cam_id
+    END;
+
+    IF @transtate = 1 AND XACT_STATE() = 1
+    BEGIN
+        COMMIT TRANSACTION transtate
+    END;
+END TRY
+BEGIN CATCH
+DECLARE @error INT, @message VARCHAR(4000), @xstate INT;
+SELECT @error = ERROR_NUMBER(), @message = ERROR_MESSAGE(), @xstate = XACT_STATE();
+IF @xstate = -1
+    ROLLBACK;
+IF @xstate = 1
+    ROLLBACK
+IF @xstate = 1
+    ROLLBACK TRANSACTION ccsp_GalateaAdminPortsManagement;
+RAISERROR (''ccsp_GalateaAdminPortsManagement: %d: %s'', 16, 1, @error, @message) ;
+END CATCH;
+    '
+    EXEC(@sql)
 
         -------------------------------------------  END Isaac  ----------------------------------------
 

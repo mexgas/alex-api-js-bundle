f481fe7d8cd9dd6fd4b11fb1488625861a534dec
agallardonuxiba <agallardo@nuxiba.com>
2025-07-01 22:31:48 -0600

    Se corrige los datos

--- CCenterRIA/UpdateDB/20250325_v127.3_March_Nova.sql
+++ CCenterRIA/UpdateDB/20250325_v127.3_March_Nova.sql
@@ -15309,35 +15309,41 @@ CREATE PROCEDURE [dbo].[ccsp_GalateaAdminCampaigns]
 
 
 	SET @process = 'CW-9795 Validar Function  - Create function hashList'
-    SET @sql ='
+    SET @sql ='CREATE FUNCTION [dbo].[hashList] (@calKey varchar(255)) 
+        RETURNS BIGINT
+AS
+BEGIN
+    DECLARE @codigo VARCHAR(100) = ''
+    DECLARE @hash BIGINT = 0
+    DECLARE @i INT
+    DECLARE @len INT
 
+    -- Validación inicial
+    IF @calKey IS NULL OR LTRIM(RTRIM(@calKey)) = ''
+        RETURN NULL
 
-        CREATE FUNCTION [dbo].[hashList] (@calKey varchar(255)) 
-        RETURNS bigint AS
+    SET @i = 1
+    SET @len = LEN(@calKey)
+
+    WHILE @i <= @len
+    BEGIN
+        SET @codigo = @codigo + CONVERT(VARCHAR(3), ASCII(SUBSTRING(@calKey, @i, 1)))
+
+        IF @i % 5 = 0
         BEGIN
-
-            IF @calKey IS NULL OR @calKey = ''''
-                RETURN NULL;
-
-            declare @codigo varchar(max)
-            declare @hash bigint
-    
-            set @codigo=''''
-            set @hash=0
-            declare @i int,@len int
-            select @i=1,@len=len(@calKey)
-            while @i<=@len begin
-    	        select @codigo=@codigo+convert(varchar(max), ASCII(SUBSTRING(@calKey,@i,1)))
-	            if @i%5=0 begin
-		        set @hash=@hash+cast(@codigo as bigint)
-		        set @codigo=''''
-	       end	
-	     set @i=@i+1
-        end
-        if @codigo<>''''
-            set @hash=@hash+cast(@codigo as bigint)
-            return @hash % 99999999999973
+            SET @hash = @hash + CAST(ISNULL(@codigo, '0') AS BIGINT)
+            SET @codigo = ''
         END
+
+        SET @i = @i + 1
+    END
+
+    IF @codigo <> ''
+        SET @hash = @hash + CAST(ISNULL(@codigo, '0') AS BIGINT)
+
+    RETURN @hash % 99999999999973
+END
+
 '
 
     EXEC(@sql);

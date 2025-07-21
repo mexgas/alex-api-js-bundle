9490f5fb09c71108b64c48e0e082921a6b1f6c3b
Marco Antonio Chagolla <marco.chagolla@nuxiba.com>
2025-07-21 12:29:29 -0600

    Cambios para el sprint 2 julio

--- RecorderRIA/UpdateDB/20250721_v101_July.sql
+++ RecorderRIA/UpdateDB/20250721_v101_July.sql
@@ -6,7 +6,7 @@ declare @Sql varchar(max)
 declare @errorGenerated varchar(max)
 declare @process varchar(max)
 ---------------- VERSION ----------------
-    Set @Version = 100
+    Set @Version = 101
     Set @Version_Actual = (select par_valor from trec_parametros where par_id = 30)
 
 if @Version_Actual in(@Version, @Version -1) -- Aqui poner numero de nueva version

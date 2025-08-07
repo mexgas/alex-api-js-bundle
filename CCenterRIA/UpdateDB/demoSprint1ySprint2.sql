9f780380eafab1937f246b53a9d38eee67307036
Marco García <marco.garcia@nuxiba.com>
2025-08-07 14:41:06 -0600

    Se agregó el archivo para los cambios de base de datos

---
+++ CCenterRIA/UpdateDB/demoSprint1ySprint2.sql
@@ -0,0 +1,105 @@
+/*******************************/
+/***** NUXIBA TECHNOLOGIES *****/
+/*******************************/
+/*
+Author: Marco Antonio García
+Date: 2025/06/23
+Description: Demo/Sprint2
+Database: CCenterRia
+Required version: 127.2
+IMPORTANT: In order to write the scripts to release in database go to the las part of this one to obtain guide and help to do it
+*/
+SET NOCOUNT ON
+
+DECLARE @sql VARCHAR(max)
+DECLARE @process VARCHAR(max)
+------------------- BEGIN MAGV --------------------------------
+/*DEV3-1182*/
+SET @process = 'K0248 - Se realiza cambio de tags, para portugues ya que estaba mal la etiqueta para el historial de actividad'
+SET @sql = '
+IF EXISTS (
+    SELECT 1 FROM dbo.ccGalateaIdentifiers
+    WHERE Description = ''COMMON_NONE_O''
+)
+BEGIN
+    UPDATE dbo.ccGalateaIdentifiers
+	SET TagEs = ''Ninguna'', TagPt = ''Nenhuma''
+	WHERE Description = ''COMMON_NONE_O''
+END
+'
+EXEC(@sql)
+
+
+/*K070254*/
+-- Para ccoCallsOutDispositionIA
+SET @process = 'K070254 - add column disposition_id in ccoCallsOutDispositionIA table to save CW dispositions'
+SET @sql = 'IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = ''ccoCallsOutDispositionIA''
+      AND COLUMN_NAME = ''disposition_id''
+)
+BEGIN
+	ALTER TABLE ccoCallsOutDispositionIA ADD 
+	disposition_id SMALLINT NULL
+END;'
+
+EXEC(@sql)
+
+-- Para ccCallsInDispositionIA
+SET @process = 'K070254 - add column disposition_id in ccCallsInDispositionIA table to save CW dispositions'
+SET @sql = 'IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = ''ccCallsInDispositionIA''
+      AND COLUMN_NAME = ''disposition_id''
+)
+BEGIN
+	ALTER TABLE ccCallsInDispositionIA ADD 
+	disposition_id SMALLINT NULL
+END;'
+EXEC(@sql)
+
+SET @process = 'K070254 - delete store procedure [SaveDispositionsAI]'
+SET @sql = 'IF EXISTS (SELECT * FROM sys.procedures where name= N''SaveDispositionsAI'')
+		BEGIN
+			DROP PROCEDURE SaveDispositionsAI
+		END'
+EXEC(@sql)
+
+SET @process = 'K070254 - CREATE store procedure [SaveDispositionsAI]
+1.- Se le agregó al sp el parametro @disposition_Id para recibir la calificación de CW
+2.- Se modificaron las opciones 1 y 3, para agregarles el @disposition_Id y se pueda registrar '
+SET @sql = 'CREATE PROCEDURE [dbo].[SaveDispositionsAI]
+		@action smallint = null,
+		@call_Id int = null,
+		@Qualification varchar(max) = null,
+		@result VARCHAR(MAX) = null,
+		@Observations VARCHAR(MAX) = null,
+		@Transcription VARCHAR(MAX) = null,
+		@CamType bit = 0,
+		@disposition_Id SMALLINT = null
+
+		AS
+		IF @action = 1  --Outbound
+		BEGIN
+			insert into ccoCallsOutDispositionIA (call_id, Qualification, result, Observations, disposition_id) values (@call_Id, @Qualification, @result, @Observations, @disposition_Id)
+		END
+		
+		IF @action = 2 --Outbound
+		BEGIN
+			insert into ccoCallsOutTranscriptionIA (call_id, Transcription) values (@call_Id, @Transcription)
+		END
+
+		IF @action = 3 --Inbound
+		BEGIN
+			insert into ccCallsInDispositionIA (call_id, Qualification, result, Observations, disposition_id) values (@call_Id, @Qualification, @result, @Observations, @disposition_Id)
+		END
+
+		IF @action = 4 --Inbound
+		BEGIN
+			insert into ccCallsInTranscriptionIA (call_id, Transcription) values (@call_Id, @Transcription)
+		END'
+EXEC(@sql)
+
+------------------- END MAGV  ----------------------------------------

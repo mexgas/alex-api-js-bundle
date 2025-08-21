2adfbd4a4d1a28a448aacefe6772ce78d3f8112e
Juan Medina <juan.medina@nuxiba.com>
2025-08-20 18:54:34 -0600

    Modificacion para K070248 - K070248 - K070108

--- CCenterRIA/UpdateDB/20250814_v127.6_August.sql
+++ CCenterRIA/UpdateDB/20250814_v127.6_August.sql
@@ -44,7 +44,7 @@ BEGIN
     BEGIN TRAN
     BEGIN TRY
 
-	--------------------------------- BEGIN MACL ------------------------------------------------
+	--------------------------------- BEGIN ALTERS AND ADD ------------------------------------------------
 	SET @process = 'K070253 - Inserting setting 290'
 	SET @sql = 'IF NOT EXISTS(SELECT 1 FROM ccSettings2 WHERE setting_id = 290)
 	BEGIN
@@ -53,6 +53,387 @@ BEGIN
 	END'
 		EXEC(@sql)
 
+	SET @process = 'K070248 - add setting for transcription 289'
+	SET @sql = '
+		IF NOT EXISTS (
+			SELECT 1
+			FROM [CCenterRIA].[dbo].[ccSettings2]
+			WHERE setting_id = 289
+		)
+		BEGIN
+			INSERT INTO [CCenterRIA].[dbo].[ccSettings2] (
+				[setting_id],
+				[valor],
+				[descripcion],
+				[Status],
+				[Tipo],
+				[detalle],
+				[description],
+				[bLoadSettings],
+				[validate]
+			)
+			VALUES (
+				289,
+				''1|521|12'',
+				''Permite configurar el estado, peso máximo y tiempo de retención de la transcripción de llamadas.'',
+				1,
+				''ADM'',
+				''0:Disabled,1:Enabled|Maximum information weight|Retention time'',
+				''Allows configuring the status, maximum weight, and retention time of the call transcription.'',
+				1,
+				''.*''
+			);
+		END
+	'
+
+	EXEC(@sql)
+
+		-- Para ccoCallsOutDispositionIA
+	SET @process = 'K070254 - add column disposition_id in ccoCallsOutDispositionIA table to save CW dispositions'
+	SET @sql = 'IF NOT EXISTS (
+		SELECT 1
+		FROM INFORMATION_SCHEMA.COLUMNS
+		WHERE TABLE_NAME = ''ccoCallsOutDispositionIA''
+		  AND COLUMN_NAME = ''disposition_id''
+	)
+	BEGIN
+		ALTER TABLE ccoCallsOutDispositionIA ADD 
+		disposition_id SMALLINT NULL
+	END;'
+
+	EXEC(@sql)
+
+	-- Para ccCallsInDispositionIA
+	SET @process = 'K070254 - add column disposition_id in ccCallsInDispositionIA table to save CW dispositions'
+	SET @sql = 'IF NOT EXISTS (
+		SELECT 1
+		FROM INFORMATION_SCHEMA.COLUMNS
+		WHERE TABLE_NAME = ''ccCallsInDispositionIA''
+		  AND COLUMN_NAME = ''disposition_id''
+	)
+	BEGIN
+		ALTER TABLE ccCallsInDispositionIA ADD 
+		disposition_id SMALLINT NULL
+	END;'
+	EXEC(@sql)
+
+	--------------------------------- END ALTERS AND ADD ------------------------------------------------
+
+			--------------------------------- BEGIN JUAN MEDINA ------------------------------------------------
+
+
+	-- K070248 AND K070108
+
+	SET @process = 'K070248 & K070108 - Drop SP SaveDispositionsAI'
+
+    SET @sql = 'if exists (select * from sys.procedures where name = N''SaveDispositionsAI'')
+    BEGIN
+        DROP PROCEDURE dbo.SaveDispositionsAI;
+    END'
+    EXEC(@sql);
+
+	 SET @process = 'K070248 & K070108- CREATE SP SaveDispositionsAI'
+
+    SET @sql = '
+		
+	CREATE PROCEDURE SaveDispositionsAI
+    @action        smallint    = NULL,
+    @call_Id       int         = NULL,
+    @Qualification varchar(MAX)= NULL,
+    @result        varchar(MAX)= NULL,
+    @Observations  varchar(MAX)= NULL,
+	@CallbackAT    DATETIME = NULL,
+    @Transcription varchar(MAX)= NULL,
+    @CamType       bit         = 0,
+	@disposition_Id SMALLINT = null
+	AS
+	BEGIN
+		SET NOCOUNT ON;
+		--Variables para devolución de llamada 
+		DECLARE @cal_key varchar(40) ='''';
+		DECLARE @cam_id smallint;
+		DECLARE @cal_telefono varchar(19);
+		DECLARE @inbound_id smallint = NULL;
+		DECLARE @CanReprogram smallint  = null
+
+		-- Validacion del Status del Setting 289
+		DECLARE @trans_status BIT = NULL;
+		
+		DECLARE @valor  NVARCHAR(15) = NULL;
+
+		SELECT @valor = TRY_CAST(valor AS NVARCHAR(15))	
+		FROM ccSettings2
+		WHERE setting_id = 289;
+
+		DECLARE @status NVARCHAR(5);
+		DECLARE @sep    INT;
+
+		SET @sep = CHARINDEX(''|'', ISNULL(@valor, ''''));
+		SET @status = CASE
+						WHEN @sep > 0 THEN SUBSTRING(@valor, 1, @sep - 1)
+						ELSE ISNULL(@valor, '''')
+					  END;
+
+		IF @action = 1  -- Outbound
+		BEGIN
+			INSERT INTO ccoCallsOutDispositionIA (call_id, Qualification, result, Observations,disposition_id)
+			VALUES (@call_Id, @Qualification, @result, @Observations,@disposition_Id);
+		END
+
+		IF @action = 2 AND @status = ''1''   -- Outbound
+		BEGIN
+			--Se deja pendiente para el siguiente Sprint 
+			--DECLARE @cam_id smallint = NULL;
+
+			--Select @cam_id = cam_id 
+			--From ccoCallsOut
+			--Where cal_id = @call_Id
+
+			--Select @trans_status = IsCallTranscriptionEnabled
+			--From ccCampsExtend
+			--Where cam_id  = @cam_id
+
+			--IF @trans_status = 1
+			--BEGIN
+			--	INSERT INTO ccoCallsOutTranscriptionIA (call_id, Transcription)
+			--	VALUES (@call_Id, @Transcription);
+			--END
+
+			INSERT INTO ccoCallsOutTranscriptionIA (call_id, Transcription)
+				VALUES (@call_Id, @Transcription);
+		END
+
+		IF @action = 3  -- Inbound
+		BEGIN
+			Select @CanReprogram = CanReprogram from ccTipoCalif where calif_id = @disposition_Id
+			
+			IF (@CallbackAT IS NOT NULL  
+				AND CONVERT(datetime, @CallbackAT, 120) IS NOT NULL 
+				AND CONVERT(datetime, @CallbackAT, 120) > GETDATE()  
+				AND @CanReprogram <> 0)
+			BEGIN
+				INSERT INTO ccCallsInDispositionIA (call_id, Qualification, result, Observations, CallbackAT,disposition_id)
+				VALUES (@call_Id, @Qualification, @result, @Observations,@CallbackAT,@disposition_Id);
+
+				SELECT @inbound_id = Inbound_id, @cal_telefono = cal_ANI
+					FROM ccCallsIn 
+					WHERE cal_id = @call_Id;
+
+				SELECT @cam_id = cam_id
+					FROM ccInbound
+					WHERE Inbound_id  = @inbound_id
+
+				EXEC ccsp_INInsertaCallBack
+					@cal_key = @call_Id,
+					@cam_id = @cam_id,
+					@cal_telefono = @cal_telefono,
+					@fechadial = @CallbackAT,
+					@dato4 = @result,
+					@dato5 = @Observations
+			END
+
+			ELSE BEGIN
+				INSERT INTO ccCallsInDispositionIA (call_id, Qualification, result, Observations,disposition_id)
+				VALUES (@call_Id, @Qualification, @result, @Observations,@disposition_Id);
+			END
+
+		END
+
+		IF @action = 4 AND @status = ''1''   -- Inbound
+		BEGIN
+			
+			Select @inbound_id = Inbound_id 
+				From ccCallsIn 
+				Where cal_id = @call_Id
+			
+			Select @trans_status = IsCallTranscriptionEnabled
+				From ccInboundExtend
+				Where Inbound_id  = @inbound_id
+
+			IF @trans_status = 1
+			BEGIN
+				INSERT INTO ccCallsInTranscriptionIA (call_id, Transcription)
+				VALUES (@call_Id, @Transcription);
+			END
+		END
+	END
+	
+	'
+
+	EXEC(@sql);
+
+
+	SET @process = 'K070108 - Drop SP ccsp_INInsertaCallBack'
+
+    SET @sql = 'if exists (select * from sys.procedures where name = N''ccsp_INInsertaCallBack'')
+    BEGIN
+        DROP PROCEDURE dbo.ccsp_INInsertaCallBack;
+    END'
+    EXEC(@sql);
+
+	 SET @process = 'K070108 - CREATE SP ccsp_INInsertaCallBack'
+
+    SET @sql = '
+	
+	CREATE PROCEDURE ccsp_INInsertaCallBack
+	@cal_key varchar(40) ='''',
+	@cam_id smallint,
+	@cal_telefono varchar(19),
+	@fechadial varchar(17),
+	@dato1 varchar(255) = '''',
+	@dato2 varchar(255) = '''',
+	@dato3 varchar(255) = '''',
+	@dato4 varchar(255) = '''',
+	@dato5 varchar(255) = '''',
+	@TelReprograma smallint = -1,
+	@user_id int=0,
+	@isAuto bit=0
+	AS
+	set nocount on
+	declare @TelOriginal as varchar(15)
+	declare @FechaOriginal as datetime
+
+	if len(@cal_telefono)<=3
+		return(0)
+
+	if isnull(@cal_key,'''') = ''''
+	 begin
+		  -- Generamos cal_key aleatorio para casos de reprogramacion inbound --
+		  Genera_cal_key:
+		  select @cal_key = right(newID(), 10)
+		  if exists (select cal_key from ccoCallsOutSource where cal_key=@cal_key)
+				goto Genera_cal_key
+	 end
+
+	declare @bIsDaylight as bit
+	declare @idioma as int
+	declare @country_id as varchar(3)
+
+	select @country_id = valor from ccsettings where setting_id = 104
+
+	select @bIsDaylight = dbo.fnIsDayLight (@country_id, getdate())
+
+	declare @difference as int
+	declare @Fecha smalldatetime, @callout_id int, @iZonaHoraria int,@iZonaHoraria_verano int, @cal_statusTemp tinyint
+	declare @iZonaHoraria2 int,@iZonaHoraria_verano2 int
+	declare @iZonaHoraria3 int,@iZonaHoraria_verano3 int
+	declare @iZonaHoraria4 int,@iZonaHoraria_verano4 int
+	declare @iZonaHoraria5 int,@iZonaHoraria_verano5 int
+	if @isAuto=0
+		select @difference = isNull(dbo.fnGetTimeDifference(dbo.fnGetTimeZone(@cal_telefono,@bIsDaylight)),0)
+	else
+		set @difference = 0
+	select @Fecha= dateadd(hh,@difference,convert(datetime,@FechaDial,101))
+
+	if exists(select cal_Key, cam_id from ccoCallsOutSource where cal_Key = @cal_key and cam_id =@cam_id)
+	 begin
+		select @callout_id=callout_id,@cal_statusTemp =cal_status,
+		@iZonaHoraria=case when len(cal_telefono)>0 then iZonaHoraria else null end,@iZonaHoraria_verano=case when len(cal_telefono)>0 then iZonaHoraria_verano else null end, 
+		@iZonaHoraria2=case when len(cal_telefono2)>0 then iZonaHoraria2 else null end,@iZonaHoraria_verano2=case when len(cal_telefono2)>0 then iZonaHoraria_verano2 else null end, 
+		@iZonaHoraria3=case when len(cal_telefono3)>0 then iZonaHoraria3 else null end,@iZonaHoraria_verano3=case when len(cal_telefono3)>0 then iZonaHoraria_verano3 else null end, 
+		@iZonaHoraria4=case when len(cal_telefono4)>0 then iZonaHoraria4 else null end,@iZonaHoraria_verano4=case when len(cal_telefono4)>0 then iZonaHoraria_verano4 else null end, 
+		@iZonaHoraria5=case when len(cal_telefono5)>0 then iZonaHoraria5 else null end,@iZonaHoraria_verano5=case when len(cal_telefono5)>0 then iZonaHoraria_verano5 else null end, 
+		@TelOriginal = cal_telefono, @FechaOriginal = cal_fechadial from ccoCallsOutSource 
+		where cal_Key = @cal_key and cam_id = @cam_id
+
+		update ccoCallsOutSource set cal_status = ''2'',cal_telefono=@cal_telefono where cal_key = @cal_key and cam_id = @cam_id
+
+		if exists(select callout_id from ccoWorkingTable where callout_id=@callout_id)
+			UPDATE ccoWorkingTable SET cal_telefono=@cal_telefono,cam_id=@cam_id,cal_fechaDial=@Fecha,cal_status=1,nTryingContact=3,prioridad_cb=1,[user_id]=@user_id,cal_keyw=@cal_key WHERE callout_id=@callout_id
+		else
+			INSERT ccoWorkingTable(callout_id,cal_telefono,cam_id,cal_fechaDial,cal_status,nTryingContact,prioridad_cb,[user_id],cal_keyw
+			,iZonaHoraria,iZonaHoraria_verano
+			,iZonaHoraria2,iZonaHoraria_verano2
+			,iZonaHoraria3,iZonaHoraria_verano3
+			,iZonaHoraria4,iZonaHoraria_verano4
+			,iZonaHoraria5,iZonaHoraria_verano5)
+			select @callout_id,@cal_telefono,@cam_id,@Fecha,1,3,1,@user_id,@cal_key
+			,@iZonaHoraria,@iZonaHoraria_verano
+			,@iZonaHoraria2,@iZonaHoraria_verano2
+			,@iZonaHoraria3,@iZonaHoraria_verano3
+			,@iZonaHoraria4,@iZonaHoraria_verano4
+			,@iZonaHoraria5,@iZonaHoraria_verano5
+
+			if not exists (select callout_id from ccoCallBacks with(nolock) where callout_id = @callout_id)
+				begin
+					insert into ccoCallBacks (callout_id, user_id, cam_id, cal_key, cal_telefono, cal_telCB, cal_fecha, cal_fusercallback, cal_fcallback, status, schedulerStatus)
+					values (@callout_id,@user_id,@cam_id,@cal_key,@TelOriginal,@cal_telefono,@FechaOriginal,@Fecha,NULL,0,1)
+				end
+			else
+				begin
+					update ccoCallBacks
+					set user_id = @user_id, cam_id = @cam_id, cal_key = @cal_key, cal_telefono = @TelOriginal, cal_telCB = @cal_telefono, cal_fecha = @FechaOriginal, cal_fusercallback = @Fecha, cal_fcallback = NULL, status = 0, schedulerStatus = 1
+					where callout_id = @callout_id
+				end
+	  end
+
+	else
+	 begin
+		select @FechaOriginal = getdate()
+
+		insert into ccoCallsOutSource (cal_key,cam_id,cal_telefono,cal_fechadial,Dato1,Dato2,Dato3,Dato4,Dato5,cal_status)
+		values (@cal_key,@cam_id,@cal_telefono,@FechaOriginal,@dato1,@dato2,@dato3,@dato4,@dato5,''2'')
+
+		select @TelOriginal = @cal_telefono
+
+		select @callout_id = scope_identity()
+		select @iZonaHoraria=iZonaHoraria,@iZonaHoraria_verano=iZonaHoraria_verano from ccocallsoutsource where callout_id=@callout_id
+
+		select @callout_id=callout_id,
+		@iZonaHoraria=case when len(cal_telefono)>0 then iZonaHoraria else null end,@iZonaHoraria_verano=case when len(cal_telefono)>0 then iZonaHoraria_verano else null end, 
+		@iZonaHoraria2=case when len(cal_telefono2)>0 then iZonaHoraria2 else null end,@iZonaHoraria_verano2=case when len(cal_telefono2)>0 then iZonaHoraria_verano2 else null end, 
+		@iZonaHoraria3=case when len(cal_telefono3)>0 then iZonaHoraria3 else null end,@iZonaHoraria_verano3=case when len(cal_telefono3)>0 then iZonaHoraria_verano3 else null end, 
+		@iZonaHoraria4=case when len(cal_telefono4)>0 then iZonaHoraria4 else null end,@iZonaHoraria_verano4=case when len(cal_telefono4)>0 then iZonaHoraria_verano4 else null end, 
+		@iZonaHoraria5=case when len(cal_telefono5)>0 then iZonaHoraria5 else null end,@iZonaHoraria_verano5=case when len(cal_telefono5)>0 then iZonaHoraria_verano5 else null end
+		from ccoCallsOutSource 
+		where callout_id=@callout_id
+
+
+		 if exists(select callout_id from ccoWorkingTable where callout_id=@callout_id)
+			UPDATE ccoWorkingTable SET cal_telefono=@cal_telefono,cam_id=@cam_id,cal_fechaDial=@Fecha,cal_status=1,nTryingContact=3,prioridad_cb=1,[user_id]=@user_id,cal_keyw=@cal_key WHERE callout_id=@callout_id
+		 else
+			INSERT ccoWorkingTable(callout_id,cal_telefono,cam_id,cal_fechaDial,cal_status,nTryingContact,prioridad_cb,[user_id],cal_keyw
+			,iZonaHoraria,iZonaHoraria_verano
+			,iZonaHoraria2,iZonaHoraria_verano2
+			,iZonaHoraria3,iZonaHoraria_verano3
+			,iZonaHoraria4,iZonaHoraria_verano4
+			,iZonaHoraria5,iZonaHoraria_verano5)
+			select @callout_id,@cal_telefono,@cam_id,@Fecha,1,3,1,@user_id,@cal_key
+			,@iZonaHoraria,@iZonaHoraria_verano
+			,@iZonaHoraria2,@iZonaHoraria_verano2
+			,@iZonaHoraria3,@iZonaHoraria_verano3
+			,@iZonaHoraria4,@iZonaHoraria_verano4
+			,@iZonaHoraria5,@iZonaHoraria_verano5
+
+			if not exists (select callout_id from ccoCallBacks with(nolock) where callout_id = @callout_id)
+				begin
+					insert into ccoCallBacks (callout_id, user_id, cam_id, cal_key, cal_telefono, cal_telCB, cal_fecha, cal_fusercallback, cal_fcallback, status, schedulerStatus)
+					values (@callout_id,@user_id,@cam_id,@cal_key,@TelOriginal,@cal_telefono,@FechaOriginal,@Fecha,NULL,0,1)
+				end
+			else
+				begin
+					update ccoCallBacks
+					set user_id = @user_id, cam_id = @cam_id, cal_key = @cal_key, cal_telefono = @TelOriginal, cal_telCB = @cal_telefono, cal_fecha = @FechaOriginal, cal_fusercallback = @Fecha, cal_fcallback = NULL, status = 0, schedulerStatus = 1
+					where callout_id = @callout_id
+				end
+	end
+
+	if exists(select callbacks from ccRIAcallbacks where año=year(@Fecha)and mes=month(@Fecha)and dia=day(@Fecha)and hora=datepart(hh,@Fecha)and cam_id=@cam_id)
+		update ccRIAcallbacks set callbacks=callbacks+1 where año=year(@Fecha)and mes=month(@Fecha)and dia=day(@Fecha)and hora=datepart(hh,@Fecha)and cam_id=@cam_id
+	else
+		insert ccRIAcallbacks select year(@Fecha),month(@Fecha),day(@Fecha),datepart(hh,@Fecha),''1'',@cam_id
+
+	return(@callout_id)
+	set nocount off
+	
+	'
+
+	EXEC(@sql);
+
+		--------------------------------- END JUAN MEDINA ------------------------------------------------
+
+	--------------------------------- BEGIN MACL ------------------------------------------------
+	
 	SET @process = 'K070253 - delete store procedure [ccsp_ManageQuantumDispositions]'
 	SET @sql = 'IF EXISTS (SELECT * FROM sys.procedures where name= N''ccsp_ManageQuantumDispositions'')
 			BEGIN
@@ -440,79 +821,6 @@ BEGIN
 		WHERE Description = ''COMMON_NONE_O''
 	END
 	'
-	EXEC(@sql)
-
-
-	/*K070254*/
-	-- Para ccoCallsOutDispositionIA
-	SET @process = 'K070254 - add column disposition_id in ccoCallsOutDispositionIA table to save CW dispositions'
-	SET @sql = 'IF NOT EXISTS (
-		SELECT 1
-		FROM INFORMATION_SCHEMA.COLUMNS
-		WHERE TABLE_NAME = ''ccoCallsOutDispositionIA''
-		  AND COLUMN_NAME = ''disposition_id''
-	)
-	BEGIN
-		ALTER TABLE ccoCallsOutDispositionIA ADD 
-		disposition_id SMALLINT NULL
-	END;'
-
-	EXEC(@sql)
-
-	-- Para ccCallsInDispositionIA
-	SET @process = 'K070254 - add column disposition_id in ccCallsInDispositionIA table to save CW dispositions'
-	SET @sql = 'IF NOT EXISTS (
-		SELECT 1
-		FROM INFORMATION_SCHEMA.COLUMNS
-		WHERE TABLE_NAME = ''ccCallsInDispositionIA''
-		  AND COLUMN_NAME = ''disposition_id''
-	)
-	BEGIN
-		ALTER TABLE ccCallsInDispositionIA ADD 
-		disposition_id SMALLINT NULL
-	END;'
-	EXEC(@sql)
-
-	SET @process = 'K070254 - delete store procedure [SaveDispositionsAI]'
-	SET @sql = 'IF EXISTS (SELECT * FROM sys.procedures where name= N''SaveDispositionsAI'')
-			BEGIN
-				DROP PROCEDURE SaveDispositionsAI
-			END'
-	EXEC(@sql)
-
-	SET @process = 'K070254 - CREATE store procedure [SaveDispositionsAI]
-	1.- Se le agregó al sp el parametro @disposition_Id para recibir la calificación de CW
-	2.- Se modificaron las opciones 1 y 3, para agregarles el @disposition_Id y se pueda registrar '
-	SET @sql = 'CREATE PROCEDURE [dbo].[SaveDispositionsAI]
-			@action smallint = null,
-			@call_Id int = null,
-			@Qualification varchar(max) = null,
-			@result VARCHAR(MAX) = null,
-			@Observations VARCHAR(MAX) = null,
-			@Transcription VARCHAR(MAX) = null,
-			@CamType bit = 0,
-			@disposition_Id SMALLINT = null
-
-			AS
-			IF @action = 1  --Outbound
-			BEGIN
-				insert into ccoCallsOutDispositionIA (call_id, Qualification, result, Observations, disposition_id) values (@call_Id, @Qualification, @result, @Observations, @disposition_Id)
-			END
-		
-			IF @action = 2 --Outbound
-			BEGIN
-				insert into ccoCallsOutTranscriptionIA (call_id, Transcription) values (@call_Id, @Transcription)
-			END
-
-			IF @action = 3 --Inbound
-			BEGIN
-				insert into ccCallsInDispositionIA (call_id, Qualification, result, Observations, disposition_id) values (@call_Id, @Qualification, @result, @Observations, @disposition_Id)
-			END
-
-			IF @action = 4 --Inbound
-			BEGIN
-				insert into ccCallsInTranscriptionIA (call_id, Transcription) values (@call_Id, @Transcription)
-			END'
 	EXEC(@sql)
 	------------------- END MAGV  ----------------------------------------
 	------------------- Begin DMM  ----------------------------------------

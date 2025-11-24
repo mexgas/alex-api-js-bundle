87eeb4442af3b3a21a79b05fe6f4a675d3f299fa
octavioortiz <octavio.ortiz@nuxiba.com>
2025-11-24 12:59:10 -0600

    Se adicionan los alter table faltantes

--- ccReportsRia/UpdateDB/20251124_nr148_Novi_Nova.sql
+++ ccReportsRia/UpdateDB/20251124_nr148_Novi_Nova.sql
@@ -39,6 +39,57 @@ BEGIN
 	BEGIN TRY
 
 --------------------------------------------------------BEGIN Octavio Ortiz----------------------------------------------------------------------------------------
+
+---Alter Table--
+
+-- RepAgentSummary
+IF COL_LENGTH('RepAgentSummary', 'areaId') IS NULL
+    ALTER TABLE RepAgentSummary 
+    ADD areaId INT NOT NULL DEFAULT(0);
+
+IF COL_LENGTH('RepAgentSummary', 'area') IS NULL
+    ALTER TABLE RepAgentSummary 
+    ADD area VARCHAR(100) NULL;
+GO
+
+-- RepInCallsDetail
+IF COL_LENGTH('RepInCallsDetail', 'areaId') IS NULL
+    ALTER TABLE RepInCallsDetail 
+    ADD areaId INT NOT NULL DEFAULT(0);
+
+IF COL_LENGTH('RepInCallsDetail', 'area') IS NULL
+    ALTER TABLE RepInCallsDetail 
+    ADD area VARCHAR(100) NULL;
+GO
+
+-- RepOutCallsDetail
+IF COL_LENGTH('RepOutCallsDetail', 'areaId') IS NULL
+    ALTER TABLE RepOutCallsDetail 
+    ADD areaId INT NOT NULL DEFAULT(0);
+
+IF COL_LENGTH('RepOutCallsDetail', 'area') IS NULL
+    ALTER TABLE RepOutCallsDetail 
+    ADD area VARCHAR(100) NULL;
+GO
+-- RepOutDialDetail
+IF COL_LENGTH('RepOutDialDetail', 'areaId') IS NULL
+    ALTER TABLE RepOutDialDetail 
+    ADD areaId INT NOT NULL DEFAULT(0);
+
+IF COL_LENGTH('RepOutDialDetail', 'area') IS NULL
+    ALTER TABLE RepOutDialDetail 
+    ADD area VARCHAR(100) NULL;
+GO
+-- RepSpecialAbndCamp
+IF COL_LENGTH('RepSpecialAbndCamp', 'areaId') IS NULL
+    ALTER TABLE RepSpecialAbndCamp 
+    ADD areaId INT NOT NULL DEFAULT(0);
+
+IF COL_LENGTH('RepSpecialAbndCamp', 'area') IS NULL
+    ALTER TABLE RepSpecialAbndCamp 
+    ADD area VARCHAR(100) NULL;
+GO
+
 -------Inserts-------
     SET @process = 'Insert into ReportsFilters - KR201001';
 

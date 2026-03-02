75abef0dd7451decd9c2ef193609185e847d6631
Marco García <marco.garcia@nuxiba.com>
2026-03-02 10:52:52 -0600

    Se realizó cambio para que solo obtenga los valores distintos de null

--- CCenterRIA/UpdateDB/20250905_v127.7_September.sql
+++ CCenterRIA/UpdateDB/20250905_v127.7_September.sql
@@ -11847,9 +11847,11 @@ SELECT s.call_id, s.tipo, s.disposition_id
 FROM (
     SELECT DISTINCT call_id, CONVERT(bit, 0) AS tipo, disposition_id
     FROM dbo.ccCallsInDispositionIA
+	WHERE disposition_id IS NOT NULL
     UNION ALL
     SELECT DISTINCT call_id, CONVERT(bit, 1) AS tipo, disposition_id
     FROM dbo.ccoCallsOutDispositionIA
+	WHERE disposition_id IS NOT NULL
 ) AS s
 WHERE NOT EXISTS (
     SELECT 1

946e83216a2f8a65e9890aa4f87660494f08a9c8
Marco Antonio Diaz Luna <marco.diaz@nuxiba.com>
2026-03-06 19:21:42 -0600

    obtener el último estado del agente desde ccLogAgentesDiaLast, permitiendo que el dashboard muestre correctamente los agentes y sus estados.

--- CCenterRIA/UpdateDB/20250905_v127.7_September.sql
+++ CCenterRIA/UpdateDB/20250905_v127.7_September.sql
@@ -24300,6 +24300,379 @@ Para posiciones ip, by ODC
  EXEC(@sql);
 -----------------------------End Ulises Espinosa ------------------------------
 
+------------------------ BEGIN Marco Antonio Díaz------------------------
+
+SET @process = 'Drop procedure ccsp_GalateaAdminGetAgentCounters if exists'
+SET @sql = 'IF EXISTS (SELECT * FROM sys.procedures WHERE name = N''ccsp_GalateaAdminGetAgentCounters'')
+BEGIN
+DROP PROCEDURE ccsp_GalateaAdminGetAgentCounters;
+END'
+EXEC(@sql);
+
+SET @process = ' obtener el último estado del agente desde ccLogAgentesDiaLas ccsp_GalateaAdminGetAgentCounters'
+SET @sql = '
+
+CREATE PROCEDURE [dbo].[ccsp_GalateaAdminGetAgentCounters]
+@type AS INT,
+@sup_id AS INT = 0,
+@agent_id AS INT = 0,
+@WG AS INT = 0,
+@AgentsIds AS VARCHAR(MAX) = '''',
+@campId AS INT = 0,
+@CampType AS SMALLINT = 1,
+@workgroupIds varchar(max)=''0''
+AS
+
+SET NOCOUNT ON;
+
+DECLARE @dateStart DATETIME;
+
+IF @type = 1
+BEGIN
+
+WITH TableUserAgent(userId)
+AS
+(
+SELECT DISTINCT
+wgAgt.User_id AS Id
+FROM ccriaworkgroupusers wgAdmin
+INNER JOIN ccriaworkgroupusers wgAgt ON wgAdmin.IDWG = wgAgt.IDWG
+INNER JOIN ccUsers usr ON usr.User_id = wgAgt.User_id
+AND usr.TipoUser_id = 1
+WHERE wgAdmin.User_id = @sup_id
+)
+
+SELECT
+CAST(a.User_id AS INT) Id,
+a.login AS Username,
+a.Nombres + '' '' + a.ApellidoPaterno + '' '' + a.ApellidoMaterno AS Name
+FROM ccusers a WITH(NOLOCK)
+INNER JOIN TableUserAgent b ON a.User_id = b.userId
+ORDER BY a.Login ASC;
+
+RETURN 0;
+
+END;
+
+IF @type = 2
+BEGIN
+
+SELECT
+CAST(u.User_id AS INT) Id,
+Login Username,
+Nombres + '' '' + ApellidoPaterno + '' '' + ApellidoMaterno Name,
+CASE
+WHEN p.publicIp IS NULL
+OR p.publicIp = ''''
+THEN ''000.000.000.000''
+ELSE p.publicIp
+END IP
+FROM ccUsers u
+LEFT JOIN ccPosicion p ON p.user_id = @agent_id
+WHERE u.User_id = @agent_id;
+
+RETURN 0;
+
+END;
+
+IF @type = 3
+BEGIN
+
+DECLARE @table2 TABLE(userId INT PRIMARY KEY NOT NULL);
+
+INSERT INTO @table2
+SELECT DISTINCT
+wg.User_id
+FROM ccRIAWorkGroupUsers wg
+LEFT JOIN ccUsers us ON wg.User_id = us.User_id
+WHERE us.TipoUser_id = 1
+AND wg.IDWG IN
+(
+SELECT IDWG
+FROM ccRIAWorkGroupUsers
+WHERE User_id = @sup_id
+AND IDWG <> @WG
+);
+
+SELECT CAST(B.User_id AS INT) AS Id
+FROM @table2 A
+RIGHT JOIN
+(
+SELECT DISTINCT
+wg.User_id
+FROM ccRIAWorkGroupUsers wg
+LEFT JOIN ccUsers us ON wg.User_id = us.User_id
+WHERE wg.IDWG = @WG
+AND us.TipoUser_id = 1
+) B ON A.userId = B.User_id
+WHERE A.userId IS NULL;
+
+RETURN 0;
+
+END;
+
+IF @type = 4
+BEGIN
+
+SELECT
+CAST(wg.User_id AS INT) Id
+FROM ccRIAWorkGroupUsers wg
+JOIN CCUsers u ON u.user_id = wg.user_id
+AND u.TipoUser_id = 1
+WHERE IDWG = @WG;
+
+RETURN 0;
+
+END;
+
+IF @type = 5
+BEGIN
+
+SELECT DISTINCT
+(CAST(U.User_id AS INT)) Id
+FROM ccRIACampEspWG camp
+JOIN ccRIAWorkGroupUsers wg ON camp.IDWG = wg.IDWG
+JOIN ccUsers U ON U.User_id = WG.User_id
+AND U.TipoUser_id = 1
+WHERE IdCampEsp = @campId
+AND TIPO = @CampType;
+
+RETURN 0;
+
+END;
+
+IF @type = 6
+BEGIN
+
+WITH UserMaxFecha(User_id, fecha)
+AS
+(
+SELECT User_id, MAX(fecha) AS fecha
+FROM ccLogAgentesDia
+WHERE fecha >= CONVERT(DATE, DATEADD(HOUR,-3,GETDATE()))
+GROUP BY User_id
+)
+
+SELECT
+CASE
+WHEN CurrentState.currentStatus IS NULL
+OR CurrentState.currentStatus < 0 THEN 0
+ELSE CAST(CurrentState.currentStatus AS INT)
+END CurrentState
+FROM ccUsers u
+LEFT JOIN
+(
+SELECT A.User_id, B.currentStatus
+FROM UserMaxFecha A
+INNER JOIN ccLogAgentesDia B
+ON A.User_id = B.User_id
+AND A.fecha = B.fecha
+) CurrentState
+ON u.User_id = CurrentState.User_id
+WHERE u.TipoUser_id = 1
+AND u.User_id = @agent_id;
+
+RETURN 0;
+
+END;
+
+IF @type = 7
+BEGIN
+
+DECLARE @superuserId AS INT;
+
+SET @superuserId =
+(
+SELECT Rol_id
+FROM ccRoles
+WHERE Level = 7
+);
+
+SELECT CAST(cr.User_id AS INT) User_id
+FROM ccUsers_Roles cr
+WHERE Rol_id = @superuserId;
+
+RETURN 0;
+
+END;
+
+IF @type = 8
+BEGIN
+
+SET @dateStart = CONVERT(DATE, GETDATE());
+
+DECLARE @wgIds table (wgId int primary key)
+
+insert into @wgIds
+select distinct value from dbo.fn_RIASplitDelimited(@workgroupIds,'','')
+
+;WITH wgAgt AS
+(
+SELECT DISTINCT
+A.User_id,
+us.Login AS Username,
+us.Nombres + '' '' + us.ApellidoPaterno + '' '' + us.ApellidoMaterno AS Name
+FROM ccRIAWorkGroupUsers A
+INNER JOIN ccUsers us
+ON A.User_id = us.User_id
+INNER JOIN @wgIds w
+ON w.wgId = A.IDWG
+WHERE us.TipoUser_id = 1
+)
+
+SELECT
+CONVERT(INT, us.User_id) AS Id,
+us.Username AS Username,
+us.Name,
+LastStateId =
+CASE
+WHEN B.currentStatus IS NULL OR B.currentStatus < 0 THEN 0
+ELSE B.currentStatus
+END
+FROM wgAgt us
+LEFT JOIN ccLogAgentesDiaLast B
+ON B.User_id = us.User_id;
+
+RETURN 0;
+
+END;
+
+IF @type = 9
+BEGIN
+
+SELECT publicIp
+FROM ccPosicion WITH(NOLOCK)
+WHERE user_id = @agent_id;
+
+RETURN 0;
+
+END;
+
+IF @type = 10
+BEGIN
+
+SELECT
+CAST(user_id AS INT) AgentId,
+publicIp Ip
+FROM ccPosicion
+WHERE user_id <> 0;
+
+RETURN 0;
+
+END;
+
+IF @type = 11
+BEGIN
+
+WITH UserMaxFecha(User_id, fecha)
+AS
+(
+SELECT User_id, MAX(fecha) AS fecha
+FROM ccLogAgentesDia
+WHERE fecha >= CONVERT(DATE, GETDATE())
+GROUP BY User_id
+)
+
+SELECT
+CAST(u.User_id AS INT) AS UserId,
+CASE
+WHEN CurrentState.currentStatus IS NULL
+OR CurrentState.currentStatus < 0 THEN 0
+ELSE CAST(CurrentState.currentStatus AS INT)
+END CurrentState
+FROM ccUsers u
+LEFT JOIN
+(
+SELECT A.User_id, B.currentStatus
+FROM UserMaxFecha A
+INNER JOIN ccLogAgentesDia B
+ON A.User_id = B.User_id
+AND A.fecha = B.fecha
+) CurrentState
+ON u.User_id = CurrentState.User_id
+WHERE u.TipoUser_id = 1
+AND u.User_id IN
+(
+SELECT value
+FROM dbo.fn_RIASplitDelimited(@AgentsIds,'','')
+);
+
+RETURN 0;
+
+END;
+
+IF @type = 12
+BEGIN
+
+SET @dateStart = CONVERT(DATE, GETDATE());
+
+WITH lastState AS
+(
+SELECT user_id, MAX(fecha) dateStart
+FROM ccLogAgentesDia WITH(NOLOCK)
+WHERE fecha > @dateStart
+GROUP BY user_id
+),
+currentState AS
+(
+SELECT
+A.User_id,
+CASE
+WHEN B.currentStatus IS NULL
+OR B.currentStatus < 0 THEN 0
+ELSE B.currentStatus
+END AS LastStateId
+FROM lastState A
+INNER JOIN ccLogAgentesDia B
+ON A.User_id = B.User_id
+AND A.dateStart = B.fecha
+)
+
+SELECT
+CONVERT(INT, us.User_id) AS Id,
+us.Login Username,
+us.Nombres + '' '' + us.ApellidoPaterno + '' '' + us.ApellidoMaterno AS Name,
+ISNULL(B.LastStateId,0) LastStateId,
+ISNULL(c.publicIp,''0.0.0.0'') as [Ip]
+FROM ccusers us
+LEFT JOIN currentState B
+ON us.User_id = B.User_id
+LEFT JOIN ccposicion C
+ON C.user_id=us.user_id
+WHERE us.TipoUser_id = 1;
+
+RETURN 0;
+
+END;
+
+IF @type = 13
+BEGIN
+
+declare @wgIdsList table (wgId int primary key)
+
+insert into @wgIdsList
+select distinct value from dbo.fn_RIASplitDelimited(@workgroupIds,'','')
+
+SELECT
+CAST(wg.User_id AS INT) Id,
+IDWG AS IdWg
+FROM ccRIAWorkGroupUsers wg
+inner join @wgIdsList w on w.wgId=wg.IDWG
+JOIN CCUsers u
+ON u.user_id = wg.user_id
+AND u.TipoUser_id = 1
+
+RETURN 0;
+
+END;
+
+SET NOCOUNT ON;
+'
+
+EXEC(@sql);
+
+------------------------ END Marco Antonio Díaz KM28002------------------------
+
     /* End script release */        /* Upgrade database version (first and the last number of setting 77) */
         EXEC ccsp_getVersion 'BD', @version --- Update first number (Version)
         EXEC ccsp_getVersion 'BDF', @versionFix --- Update last number (FIX)

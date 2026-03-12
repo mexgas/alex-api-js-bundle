9bb46ed5070f438833e567c58c1f3ba3f9870f47
Esme <dalia.garcia@nuxiba.com>
2026-03-12 10:10:25 -0600

    se corrigen comillas Los cambios que agregue fueron en la linea 24559-Agregue el parametro faltante y 24661 se modifica la condicion para que la validacion por cp se omita al recibir un valor de 1

--- CCenterRIA/UpdateDB/20250905_v127.7_September.sql
+++ CCenterRIA/UpdateDB/20250905_v127.7_September.sql
@@ -24553,232 +24553,397 @@ END'
 EXEC(@sql);
 
 SET @process = 'Se agrega parámetro para omitir la validación de zona horaria cuando la bandera está activa'
-SET @sql = 'CREATE PROCEDURE [dbo].[ccsp_Limpia]
-@tel VARCHAR(50), @Camp INT = 0, @calKey VARCHAR(20) = '', @dato1 VARCHAR(10)='', @ForceInsertWithDefaultTimezone BIT = 0
-AS
-SET NOCOUNT ON
+SET @sql = '
+CREATE PROCEDURE [dbo].[ccsp_Limpia]
 
-DECLARE @lon TINYINT, @cldLocal VARCHAR(7), @pais VARCHAR(3), @extLen SMALLINT, @specialDialPlan SMALLINT, @validateTel SMALLINT, @ld VARCHAR(7)
-DECLARE @checkLd_In_ANILst SMALLINT = 0
-/***
- 4  as res lista Negra
- 2 as res Digitos incorrectos Prefijo Marcacion 01,044,045,001
- 3 as res Number notExists
- 1 as res Longitud invalida
- 0 as res Numero correcto
+	@tel VARCHAR(50), @Camp INT = 0, @calKey VARCHAR(20) = '''', @dato1 VARCHAR(10)='''', @ForceInsertWithDefaultTimezone BIT = 0
+	AS
+	SET NOCOUNT ON
 
-***/
-SELECT @tel = dbo.limpia(@tel)
+	DECLARE @lon TINYINT, @cldLocal VARCHAR(7), @pais VARCHAR(3), @extLen SMALLINT, @specialDialPlan SMALLINT, @validateTel SMALLINT, @ld VARCHAR(7)
+	DECLARE @checkLd_In_ANILst SMALLINT = 0
+	/***
+	 4  as res lista Negra
+	 2 as res Digitos incorrectos Prefijo Marcacion 01,044,045,001
+	 3 as res Number notExists
+	 1 as res Longitud invalida
+	 0 as res Numero correcto
 
-SELECT @lon = len(@tel)
+	***/
+	SELECT @tel = dbo.limpia(@tel)
 
-SELECT @pais = valor
-FROM ccSettings WITH (NOLOCK)
-WHERE setting_id = 104
+	SELECT @lon = len(@tel)
 
-SELECT @cldLocal = valor
-FROM ccSettings WITH (NOLOCK)
-WHERE setting_id = 17
+	SELECT @pais = valor
+	FROM ccSettings WITH (NOLOCK)
+	WHERE setting_id = 104
 
-SELECT @extLen = valor
-FROM ccsettings WITH (NOLOCK)
-WHERE setting_id = 108
+	SELECT @cldLocal = valor
+	FROM ccSettings WITH (NOLOCK)
+	WHERE setting_id = 17
 
-SELECT @validateTel = valor
-FROM ccsettings WITH (NOLOCK)
-WHERE setting_id = 206
+	SELECT @extLen = valor
+	FROM ccsettings WITH (NOLOCK)
+	WHERE setting_id = 108
 
-SELECT @checkLd_In_ANILst = valor FROM ccsettings WITH (NOLOCK) WHERE setting_id = 213
+	SELECT @validateTel = valor
+	FROM ccsettings WITH (NOLOCK)
+	WHERE setting_id = 206
 
-IF @lon > 1
-BEGIN
+	SELECT @checkLd_In_ANILst = valor FROM ccsettings WITH (NOLOCK) WHERE setting_id = 213
 
-	IF @validateTel = 2
-		BEGIN --Setting 206 only validates blacklist
+	IF @lon > 1
+	BEGIN
 
-			IF (SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)) = 1
+		IF @validateTel = 2
+			BEGIN --Setting 206 only validates blacklist
+
+				IF (SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)) = 1
+				BEGIN
+					SELECT 4 AS res, @tel AS tel --blackList
+					RETURN (0)
+				END
+				SELECT 0 AS res, @tel AS tel
+
+				RETURN (0)
+
+		END
+		IF @validateTel = 1
+		BEGIN --Setting 206 para no validar longitud ni listas negras
+			SELECT 0 AS res, @tel AS tel
+
+			RETURN (0)
+		END
+
+		IF @extLen = @lon
+		BEGIN -- Setting 108 validar el tamaño longitud del telefono
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
 			BEGIN
 				SELECT 4 AS res, @tel AS tel --blackList
+
 				RETURN (0)
 			END
-			SELECT 0 AS res, @tel AS tel
+
+			SELECT 0 AS res, @tel AS tel -- Extension
 
 			RETURN (0)
-
-	END
-	IF @validateTel = 1
-	BEGIN --Setting 206 para no validar longitud ni listas negras
-		SELECT 0 AS res, @tel AS tel
-
-		RETURN (0)
+		END
 	END
 
-	IF @extLen = @lon
-	BEGIN -- Setting 108 validar el tamaño longitud del telefono
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
+	DECLARE @telTemp AS VARCHAR(15)
+
+	SELECT @telTemp = @tel
+
+	IF @pais = 1
+	BEGIN ---Mexico
+		IF @lon = 3 AND @tel = ''911''
 		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
+			SELECT 4 AS res, @tel AS tel --Lista Negra
 
 			RETURN (0)
 		END
 
-		SELECT 0 AS res, @tel AS tel -- Extension
+		IF (@lon < 10)
+		BEGIN
+			SELECT 1 AS res, @tel AS tel --Longitud invalida
 
-		RETURN (0)
-	END
-END
-
-DECLARE @telTemp AS VARCHAR(15)
-
-SELECT @telTemp = @tel
-
-IF @pais = 1
-BEGIN ---Mexico
-	IF @lon = 3 AND @tel = '911'
-	BEGIN
-		SELECT 4 AS res, @tel AS tel --Lista Negra
-
-		RETURN (0)
-	END
-
-	IF (@lon < 10)
-	BEGIN
-		SELECT 1 AS res, @tel AS tel --Longitud invalida
-
-		RETURN (0)
-	END
-		   IF EXISTS (
-				SELECT 1
-				FROM ccCampsExtend
-				WHERE cam_id = @Camp
-				  AND ZipCodeSchedule = 1
-			)
-			BEGIN
-				IF (@ForceInsertWithDefaultTimezone = 0) AND (@dato1 = '' OR NOT EXISTS (SELECT 1 FROM ccTimeZoneAreaCP WHERE ZipCode = LTRIM(RTRIM(@dato1))))
+			RETURN (0)
+		END
+			   IF EXISTS (
+					SELECT 1
+					FROM ccCampsExtend
+					WHERE cam_id = @Camp
+					  AND ZipCodeSchedule = 1
+				)
 				BEGIN
-					SELECT 6 AS res, @tel AS tel; -- No tiene codigo postal
-					RETURN (0);
+					IF (@ForceInsertWithDefaultTimezone = 0) AND (@dato1 = '''' OR NOT EXISTS (SELECT 1 FROM ccTimeZoneAreaCP WHERE ZipCode = LTRIM(RTRIM(@dato1))))
+					BEGIN
+						SELECT 6 AS res, @tel AS tel; -- No tiene codigo postal
+						RETURN (0);
+					END
 				END
+
+		IF @lon = 12 AND left(@tel, 2) <> ''01'' OR @lon = 13 AND left(@tel, 3) NOT IN (''044'', ''045'') AND left(@tel, 3) <> ''001''
+		BEGIN
+			SELECT 2 AS res, @tel AS tel --Digitos incorrectos
+
+			RETURN (0)
+		END
+
+		IF left(@tel, 3) = ''001''
+		BEGIN
+			SELECT 0 AS res, @tel AS tel
+
+			RETURN (0)
+		END
+
+		SELECT @tel = right(@tel, 10)
+
+		IF (
+				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+				) = 1
+		BEGIN
+			SELECT 4 AS res, @tel AS tel --blackList
+
+			RETURN (0)
+		END
+
+		If (@Camp > 0 AND @checkLd_In_ANILst = 1)
+		BEGIN
+			If(SELECT len(ani) FROM ccCamps WHERE cam_id = @Camp) > 0  --Permitir todos los telefonos a 10 digitos cuando existe un ani configurado en la campana.
+			BEGIN
+				SELECT 0 AS res, @tel AS tel
+				RETURN (0)
 			END
 
-	IF @lon = 12 AND left(@tel, 2) <> '01' OR @lon = 13 AND left(@tel, 3) NOT IN ('044', '045') AND left(@tel, 3) <> '001'
-	BEGIN
-		SELECT 2 AS res, @tel AS tel --Digitos incorrectos
+			IF exists (SELECT TOP 1 area FROM ccCamps c WITH (NOLOCK) inner join ccEdoAniList l WITH (NOLOCK) on c.id_anilist = l.id_AniList
+					  inner join ccEstadosAni e WITH (NOLOCK) on l.id_AniList = e.id_AniList
+					  WHERE cam_id = @Camp and telAni <> '''' and area = left(@tel, 3))
+			BEGIN
+				SELECT 0 AS res, @tel AS tel
+				RETURN (0)
+			END
+			ELSE IF exists (SELECT TOP 1 area FROM ccCamps c WITH (NOLOCK) inner join ccEdoAniList l WITH (NOLOCK) on c.id_anilist = l.id_AniList
+					  inner join ccEstadosAni e WITH (NOLOCK) on l.id_AniList = e.id_AniList
+					  WHERE cam_id = @Camp and telAni <> '''' and area = left(@tel, 2))
+			BEGIN
+				SELECT 0 AS res, @tel AS tel
+				RETURN (0)
+			END
+		END
 
-		RETURN (0)
-	END
+		SELECT @tel = dbo.Verifica2(@tel, 1, @cldLocal, DEFAULT, DEFAULT)
 
-	IF left(@tel, 3) = '001'
-	BEGIN
+		IF LEFT(@tel, 1) = ''E''
+		BEGIN
+			SELECT 3 AS res, @telTemp AS tel --No encontrado
+
+			RETURN (0)
+		END
+
 		SELECT 0 AS res, @tel AS tel
 
 		RETURN (0)
 	END
+	ELSE IF @pais = 2
+	BEGIN --Argentina
+		SET @tel = dbo.completa(@tel, @pais, @cldLocal)
 
-	SELECT @tel = right(@tel, 10)
-
-	IF (
-			SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-			) = 1
-	BEGIN
-		SELECT 4 AS res, @tel AS tel --blackList
-
-		RETURN (0)
-	END
-
-	If (@Camp > 0 AND @checkLd_In_ANILst = 1)
-	BEGIN
-		If(SELECT len(ani) FROM ccCamps WHERE cam_id = @Camp) > 0  --Permitir todos los telefonos a 10 digitos cuando existe un ani configurado en la campana.
+		IF left(@tel, 1) = ''E''
 		BEGIN
-			SELECT 0 AS res, @tel AS tel
+			SELECT 1 AS res, @telTemp AS tel --Longitud Invalida
+
 			RETURN (0)
 		END
 
-		IF exists (SELECT TOP 1 area FROM ccCamps c WITH (NOLOCK) inner join ccEdoAniList l WITH (NOLOCK) on c.id_anilist = l.id_AniList
-				  inner join ccEstadosAni e WITH (NOLOCK) on l.id_AniList = e.id_AniList
-				  WHERE cam_id = @Camp and telAni <> '' and area = left(@tel, 3))
+		SELECT @tel = dbo.fnClearPhoneArg(@tel)
+
+		IF (len(@tel) = 10 OR len(@cldLocal + @tel) = 10) AND left(@tel, 1) <> ''E''
 		BEGIN
-			SELECT 0 AS res, @tel AS tel
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
+
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
+		END
+		ELSE
+		BEGIN
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
+		END
+
+		RETURN (0)
+	END
+	ELSE IF @pais = 3
+	BEGIN --Colombia
+		IF @lon < 7 OR @lon = 9 OR (@lon = 10 AND left(@telTemp, 1) <> ''3'') OR (@lon = 11 AND left(@telTemp, 2) <> ''03'')
+		BEGIN
+			SELECT 1 AS res, @telTemp AS tel --Longitud Invalida
+
 			RETURN (0)
 		END
-		ELSE IF exists (SELECT TOP 1 area FROM ccCamps c WITH (NOLOCK) inner join ccEdoAniList l WITH (NOLOCK) on c.id_anilist = l.id_AniList
-				  inner join ccEstadosAni e WITH (NOLOCK) on l.id_AniList = e.id_AniList
-				  WHERE cam_id = @Camp and telAni <> '' and area = left(@tel, 2))
+
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
+
+		IF (len(@tel) IN (8, 10)) AND left(@tel, 1) <> ''E''
 		BEGIN
-			SELECT 0 AS res, @tel AS tel
-			RETURN (0)
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
+
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
 		END
-	END
-
-	SELECT @tel = dbo.Verifica2(@tel, 1, @cldLocal, DEFAULT, DEFAULT)
-
-	IF LEFT(@tel, 1) = 'E'
-	BEGIN
-		SELECT 3 AS res, @telTemp AS tel --No encontrado
+		ELSE
+		BEGIN
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
+		END
 
 		RETURN (0)
 	END
-
-	SELECT 0 AS res, @tel AS tel
-
-	RETURN (0)
-END
-ELSE IF @pais = 2
-BEGIN --Argentina
-	SET @tel = dbo.completa(@tel, @pais, @cldLocal)
-
-	IF left(@tel, 1) = 'E'
-	BEGIN
-		SELECT 1 AS res, @telTemp AS tel --Longitud Invalida
+	ELSE IF @pais = 4
+	BEGIN --USA
+		EXEC ccsp_LimpiaUsa @tel, @Camp, @calKey
 
 		RETURN (0)
 	END
+	ELSE IF @pais = 5
+	BEGIN --Chile
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
 
-	SELECT @tel = dbo.fnClearPhoneArg(@tel)
-
-	IF (len(@tel) = 10 OR len(@cldLocal + @tel) = 10) AND left(@tel, 1) <> 'E'
-	BEGIN
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
+		IF len(@tel) IN (8, 9) AND left(@tel, 1) <> ''E''
 		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
+
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
 		END
 		ELSE
 		BEGIN
-			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-			IF left(@tel, 1) = 'E'
-			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
-			END
-
-			SELECT 0 AS res, @tel AS tel
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
 		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
-
-	RETURN (0)
-END
-ELSE IF @pais = 3
-BEGIN --Colombia
-	IF @lon < 7 OR @lon = 9 OR (@lon = 10 AND left(@telTemp, 1) <> '3') OR (@lon = 11 AND left(@telTemp, 2) <> '03')
-	BEGIN
-		SELECT 1 AS res, @telTemp AS tel --Longitud Invalida
 
 		RETURN (0)
 	END
+	ELSE IF @pais = 6
+	BEGIN --Venezuela
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
 
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
+		IF len(@tel) = 10 AND left(@tel, 1) <> ''E''
+		BEGIN
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
 
-	IF (len(@tel) IN (8, 10)) AND left(@tel, 1) <> 'E'
-	BEGIN
-		IF (
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
+		END
+		ELSE
+		BEGIN
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
+		END
+
+		RETURN (0)
+	END
+	ELSE IF @pais = 7
+	BEGIN --Reino Unido
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
+
+		IF (len(@tel) IN (9, 10)) AND left(@tel, 1) <> ''E''
+		BEGIN
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
+
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
+		END
+		ELSE
+		BEGIN
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
+		END
+
+		RETURN (0)
+	END
+	ELSE IF @pais = 8
+	BEGIN --Arabia saudita
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
+
+		IF (len(@tel) IN (9, 10, 11))
+		BEGIN
+			IF (
+					SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
+					) = 1
+			BEGIN
+				SELECT 4 AS res, @tel AS tel --blackList
+			END
+			ELSE
+			BEGIN
+				SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
+
+				IF left(@tel, 1) = ''E''
+				BEGIN
+					SELECT 3 AS res, @telTemp --Not existsFound
+				END
+
+				SELECT 0 AS res, @tel AS tel
+			END
+		END
+		ELSE
+		BEGIN
+			SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
+		END
+
+		RETURN (0)
+	END
+	ELSE IF @pais IN (9, 10, 11, 12, 13, 14, 15, 16)
+	BEGIN --9: Australia, 10:Brasil, 11:Guatemala, 12:Costa Rica, 13:Salvador, 14:España 15:Peru, 16: Panama
+		SELECT @tel = dbo.Completa_ListaNegra(@tel)
+
+		IF left(@tel, 1) = ''E''
+		BEGIN
+			SELECT 1 AS res, @telTemp --Longitud Invalida
+		END
+		ELSE IF (
 				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
 				) = 1
 		BEGIN
@@ -24788,179 +24953,16 @@ BEGIN --Colombia
 		BEGIN
 			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
 
-			IF left(@tel, 1) = 'E'
+			IF left(@tel, 1) = ''E''
 			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
+				SELECT 2 AS res, @telTemp --Digitos Incorrectos ??? debe ser numero no existe
 			END
 
 			SELECT 0 AS res, @tel AS tel
 		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
 
-	RETURN (0)
-END
-ELSE IF @pais = 4
-BEGIN --USA
-	EXEC ccsp_LimpiaUsa @tel, @Camp, @calKey
-
-	RETURN (0)
-END
-ELSE IF @pais = 5
-BEGIN --Chile
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
-
-	IF len(@tel) IN (8, 9) AND left(@tel, 1) <> 'E'
-	BEGIN
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
-		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
-		END
-		ELSE
-		BEGIN
-			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-			IF left(@tel, 1) = 'E'
-			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
-			END
-
-			SELECT 0 AS res, @tel AS tel
-		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
-
-	RETURN (0)
-END
-ELSE IF @pais = 6
-BEGIN --Venezuela
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
-
-	IF len(@tel) = 10 AND left(@tel, 1) <> 'E'
-	BEGIN
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
-		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
-		END
-		ELSE
-		BEGIN
-			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-			IF left(@tel, 1) = 'E'
-			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
-			END
-
-			SELECT 0 AS res, @tel AS tel
-		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
-
-	RETURN (0)
-END
-ELSE IF @pais = 7
-BEGIN --Reino Unido
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
-
-	IF (len(@tel) IN (9, 10)) AND left(@tel, 1) <> 'E'
-	BEGIN
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
-		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
-		END
-		ELSE
-		BEGIN
-			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-			IF left(@tel, 1) = 'E'
-			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
-			END
-
-			SELECT 0 AS res, @tel AS tel
-		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
-
-	RETURN (0)
-END
-ELSE IF @pais = 8
-BEGIN --Arabia saudita
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
-
-	IF (len(@tel) IN (9, 10, 11))
-	BEGIN
-		IF (
-				SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-				) = 1
-		BEGIN
-			SELECT 4 AS res, @tel AS tel --blackList
-		END
-		ELSE
-		BEGIN
-			SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-			IF left(@tel, 1) = 'E'
-			BEGIN
-				SELECT 3 AS res, @telTemp --Not existsFound
-			END
-
-			SELECT 0 AS res, @tel AS tel
-		END
-	END
-	ELSE
-	BEGIN
-		SELECT 2 AS res, @telTemp AS tel --Digitos incorrectos
-	END
-
-	RETURN (0)
-END
-ELSE IF @pais IN (9, 10, 11, 12, 13, 14, 15, 16)
-BEGIN --9: Australia, 10:Brasil, 11:Guatemala, 12:Costa Rica, 13:Salvador, 14:España 15:Peru, 16: Panama
-	SELECT @tel = dbo.Completa_ListaNegra(@tel)
-
-	IF left(@tel, 1) = 'E'
-	BEGIN
-		SELECT 1 AS res, @telTemp --Longitud Invalida
-	END
-	ELSE IF (
-			SELECT dbo.ValidateBlackListPhone(@tel, @Camp, @calKey)
-			) = 1
-	BEGIN
-		SELECT 4 AS res, @tel AS tel --blackList
-	END
-	ELSE
-	BEGIN
-		SELECT @tel = dbo.verifica2(@tel, @pais, @cldLocal, DEFAULT, DEFAULT)
-
-		IF left(@tel, 1) = 'E'
-		BEGIN
-			SELECT 2 AS res, @telTemp --Digitos Incorrectos ??? debe ser numero no existe
-		END
-
-		SELECT 0 AS res, @tel AS tel
-	END
-
-	RETURN (0)
-END'
+		RETURN (0)
+	END'
 
 EXEC(@sql);
 -----------------------END Esmeralda Garcia------------------------------

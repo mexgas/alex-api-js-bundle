62454ff9c02441e545fcafc76d3c69812a94c35a
Pavel Martinez <carlos.martinez@nuxiba.com>
2026-03-12 11:59:00 -0600

    cambios para las calificaciones (porcentajes) en el finder que dio producto

--- RecorderRIA/UpdateDB/20250905_v103_September.sql
+++ RecorderRIA/UpdateDB/20250905_v103_September.sql
@@ -90,3 +90,252 @@ END'
  else begin
     select par_valor,'This version is incorrect, need version '+ convert(varchar(max),@Version-1) from trec_parametros where par_id = 30
  end
+
+ 	-----------------------------------BEGIN Pavel Martinez---------------------------------------
+        
+    SET @process = 'Drop procedure trsp_InsertRecNodeGrabIds'
+	SET @sql = '
+		IF EXISTS (SELECT * FROM sys.procedures WHERE name = N''trsp_InsertRecNodeGrabIds'')
+		BEGIN
+			DROP PROCEDURE trsp_InsertRecNodeGrabIds;
+		END
+	'
+	EXEC(@sql);
+
+
+ set @process = 'Se Hace el cambio para regresar la calificacion con los cambios de producto'
+     set @sql='    CREATE PROCEDURE trsp_InsertRecNodeGrabIds
+        @grabIds varchar(8000),
+        @rateEvaluationFormatKolob int = -1
+        AS
+        BEGIN
+            declare @grabIdTmp table(grabId bigint primary key)
+            declare @tEvaluationTmp table(grabId bigint primary key,total float)    
+            DECLARE @XMLTable TABLE (grab_id INT, dateIn datetime, [node] xml, [status] bit);
+            DECLARE @InboudAI int = 11, @OutboundAI int = 10
+            
+            DECLARE @RecNodeTable TABLE (
+            Date datetime,
+            CDATE VARCHAR(23),  -----> Date Generic
+            CID int,            -----> CamId
+            CType int,          -----> Tipo de llamada
+            C01 bigint,         -----> grab_id
+            C02 VARCHAR(50),    -----> Type of Recording (Inbound/Outbound)
+            C03 VARCHAR(250),   -----> Camp/ACD descripcion
+            C04 INT,            -----> ShoutLevel
+            C05 VARCHAR(250),   -----> Agent Login
+            C06 VARCHAR(50),    -----> Formated date
+            C07 VARCHAR(50),    -----> Position Computer
+            C08 VARCHAR(50),    -----> Duration
+            C09 VARCHAR(50),    -----> Ani
+            C10 VARCHAR(50),    -----> Dnis
+            C11 VARCHAR(100),   -----> Calkey
+            C12 VARCHAR(50),    -----> Manual
+            C13 INT,            -----> User ID
+            C14 int,            -----> cal ID
+            C15 INT,            -----> Cam /ACD ID
+            C16 VARCHAR(12),    -----> Duration Reco@rding as 00:00:00
+            C17 INT,            -----> Position Extension
+            C18 VARCHAR(50),    -----> rating(Scoring Template)
+            C19 INT,            -----> Reposiory ID
+            C20 VARCHAR(250),   -----> Disposition
+            C21 INT,            -----> Disposition ID
+            C22 TINYINT,        -----> Has Video
+            C23 VARCHAR(300),   -----> Agent Full Name
+            C24 VARCHAR(300),   -----> Supervisor Name
+            C25 VARCHAR(250),   -----> Score Template
+            C26 INT,            -----> graphic_id
+            C27 VARCHAR(250),   -----> Prefix recording
+            C28 VARCHAR(250),   -----> subDisposition
+            C29 BIT,            -----> LLamada Grabada --@extraInfo 1 Record, 0 Dont Record
+            C30 BIT,            ------> LLamada VoiceMail @extraInfo 2
+            C31 VARCHAR(250),    ------> AI Model Name
+            C32 VARCHAR(250)    ------> Fecha y Hora Final
+        );
+        insert into @grabIdTmp
+        select value from  dbo.fn_RIASplitDelimited(@grabIds,'','')
+
+
+        if @rateEvaluationFormatKolob = 0 begin
+            ;with maxFechaCalif as(
+            select max(fecha_calif) as fecha_calif,B.grabId from ria_formacalif A 
+            inner join @grabIdTmp B on A.id_grabacion=B.grabId
+            group by B.grabId
+            ), ria_formacalifTop as(
+            select A.grabId,B.total_forma from maxFechaCalif A
+            inner join ria_formacalif B on A.grabId=B.id_grabacion and A.fecha_calif=B.fecha_calif
+            )
+            insert into @tEvaluationTmp
+            select * from ria_formacalifTop
+        end
+        else if @rateEvaluationFormatKolob = 1 begin
+            insert into @tEvaluationTmp
+            SELECT 
+                A.grab_id,
+                CAST(ROUND(AVG((CAST(A.totalPoints AS float) / NULLIF(CAST(A.generalQualification AS float), 0)) * 100), 0) AS INT)
+            FROM RECORDERRIA_RECORDINGEVALUATION A
+            inner join @grabIdTmp T on A.grab_id = T.grabId
+            WHERE A.deleted != 1 
+            group by A.grab_id
+        end
+
+        ;with GrabacionSource AS (
+            SELECT rec.grab_id, rec.tipo_llamada, rec.Prefijo, rec.cal_manual, rec.id_nivel_grito, 
+            rec.cal_id, rec.finicio,rec.ffin, rec.extra_info, rec.extra_info2, rec.video, 
+            isnull(E.total,0) AS total_forma
+            ,calif_id,cam_id,duracion,ani,dni,cal_key,id_repositorio,califSub_id,age_id
+            ,cal_extension, rec.virtualAgentId
+            FROM ria_grabacion rec with(nolock)
+            inner join @grabIdTmp t on rec.grab_id=t.grabId
+            left join @tEvaluationTmp E on t.grabId=E.grabId    
+            UNION ALL
+            SELECT rec.grab_id, rec.tipo_llamada, '''' AS Prefijo, rec.cal_manual, rec.id_nivel_grito, 
+            rec.cal_id, rec.finicio,rec.ffin, rec.extra_info, rec.extra_info2, NULL AS video,
+            isnull(E.total,0) AS total_forma
+            ,calif_id,cam_id,duracion,ani,dni,cal_key,id_repositorio,califSub_id,age_id
+            ,cal_extension, rec.virtualAgentId
+            FROM RIA_GRABACIONCONSULTA rec with(nolock)
+            inner join @grabIdTmp t on rec.grab_id=t.grabId
+            left join @tEvaluationTmp E on t.grabId=E.grabId
+        )
+        INSERT INTO @RecNodeTable
+        SELECT 
+        rec.finicio,
+        CONVERT(VARCHAR(23), rec.finicio, 126) AS ''@CDATE'',
+        rec.cam_id AS ''@CID'',
+
+        CASE WHEN rec.tipo_llamada = 1 THEN
+            CASE WHEN rec.virtualAgentId > 0 THEN @InboudAI ELSE rec.tipo_llamada END
+            ELSE CASE WHEN rec.virtualAgentId > 0 THEN @OutboundAI ELSE rec.tipo_llamada END END
+        AS ''@CType'',
+
+        rec.grab_id AS ''@C01'',
+        CASE WHEN rec.tipo_llamada = 1 THEN ''Inbound'' ELSE ''Outbound'' END AS ''@C02'',
+        CASE WHEN rec.tipo_llamada = 1 THEN inb.descripcion ELSE outb.cam_descripcion END AS ''@C03'',
+        ISNULL(rec.id_nivel_grito, -1) AS ''@C04'',
+        CASE WHEN ISNULL(rec.virtualAgentId, 0) > 0 THEN '''' ELSE
+        ISNULL(usr.LOGIN, ''N/A'') END AS ''@C05'',
+        CONVERT(VARCHAR(23), rec.finicio, 126) AS ''@C06'',
+        pos.Computer AS ''@C07'',
+        CONVERT(NVARCHAR(10), rec.duracion) AS ''@C08'',
+        rec.ani AS ''@C09'',
+        rec.dni AS ''@C10'',
+        rec.cal_key AS ''@C11'',
+        CASE WHEN rec.cal_manual = 0 THEN ''N/A'' ELSE ''Manual'' END AS ''@C12'',
+        ISNULL(usr.[User_id], 0) AS ''@C13'',
+        rec.cal_id AS ''@C14'',
+        rec.cam_id AS ''@C15'',
+        CONVERT(CHAR(8), DATEADD(SECOND, rec.duracion, 0), 108) AS ''@C16'',
+        ISNULL(CASE WHEN pos.ext_id = 0 THEN pos.pos_id ELSE pos.ext_id END, -1) AS ''@C17'',
+        ISNULL(rec.total_forma, 0) AS ''@C18'',
+        rec.id_repositorio AS ''@C19'',
+        case when rec.calif_id<=0 then ''N/A'' when rec.tipo_llamada = 1 then e.description  when rec.tipo_llamada = 2 then eOut.Description end  AS ''@C20'',
+        rec.calif_id AS ''@C21'',
+        rec.video AS ''@C22'',
+        ISNULL(usr.Nombres + '' '' + usr.ApellidoPaterno + '' '' + usr.ApellidoMaterno, CASE WHEN ISNULL(rec.virtualAgentId, 0) > 0 THEN '''' ELSE ''N/A'' END) AS ''@C23'',
+        '''' AS ''@C24'',
+        '''' AS ''@C25'',
+        isnull(grap.graphic_id,1) AS ''@C26'',
+        rec.Prefijo AS ''@C27'',    
+        case when  rec.califSub_id<=0 then ''N/A'' when rec.tipo_llamada = 1 then subDisposition.califSubDesc  when rec.tipo_llamada = 2 then subDispositionOut.califSubDesc end  AS ''@C28'',    
+        CONVERT(BIT, ISNULL(rec.extra_info, ''1'')) AS ''@C29'',
+        CONVERT(BIT, ISNULL(rec.extra_info2, ''0'')) AS ''@C30'',
+        CASE WHEN ISNULL(rec.virtualAgentId, 0) > 0 THEN va.nameAgent ELSE '''' END AS ''@C31'',
+        CASE WHEN rec.ffin IS NULL THEN '''' ELSE CONVERT(VARCHAR(23), rec.ffin, 126)END AS ''@C32''
+        FROM GrabacionSource rec
+        LEFT JOIN ccinbound inb ON rec.cam_id = inb.Inbound_id AND rec.tipo_llamada = 1
+        LEFT JOIN cccamps outb ON rec.cam_id = outb.cam_id AND rec.tipo_llamada = 2
+        LEFT JOIN ccUsers usr ON usr.User_id = rec.age_id
+        LEFT JOIN ccPosicion pos ON pos.pos_id = rec.cal_extension * -1
+        LEFT JOIN ccTipoCalif AS e ON rec.calif_id = e.calif_id AND rec.tipo_llamada = 1
+        LEFT JOIN ccTipoCalifOUT AS eOut ON rec.calif_id = eOut.calif_id AND rec.tipo_llamada = 2
+        LEFT JOIN cctipocalifsub AS subDisposition ON rec.califSub_id = subDisposition.califSub_id
+        LEFT JOIN cctipocalifsubout AS subDispositionOut ON rec.califSub_id = subDispositionOut.califSub_id AND rec.tipo_llamada = 2
+        LEFT JOIN ccRIAInboundGraph grap ON grap.Inbound_id = inb.Inbound_id AND rec.tipo_llamada = 1
+        LEFT JOIN ccRIACampsGraph grapOut ON grapOut.cam_id = outb.cam_id AND rec.tipo_llamada = 2
+        LEFT JOIN ccVirtualAgent va ON rec.virtualAgentId = va.idAgent
+
+
+        insert into @XMLTable (grab_id,status, dateIn, node)       
+        SELECT  
+            C01,0 [status],[date],
+            case when C03 is null then null 
+            when (C30=0 and C05 is not null and C20 is not null and C28 is not null) or  (C30=1) then   
+            (
+                SELECT 
+                    CDATE AS "@CDATE",
+                    CID As "@CID",
+                    CType As "@CType",
+                    C01 AS "@C01",
+                    C02 AS "@C02",
+                    C03 AS "@C03",
+                    C04 AS "@C04",
+                    C05 AS "@C05",
+                    C06 AS "@C06",
+                    C07 AS "@C07",
+                    C08 AS "@C08",
+                    C09 AS "@C09",
+                    C10 AS "@C10",
+                    C11 AS "@C11",
+                    C12 AS "@C12",
+                    C13 AS "@C13",
+                    C14 AS "@C14",
+                    C15 AS "@C15",
+                    C16 AS "@C16",
+                    C17 AS "@C17",
+                    C18 AS "@C18",
+                    C19 AS "@C19",
+                    C20 AS "@C20",
+                    C21 AS "@C21",
+                    C22 AS "@C22",
+                    C23 AS "@C23",
+                    C24 AS "@C24",
+                    C25 AS "@C25",
+                    C26 AS "@C26",
+                    C27 AS "@C27",
+                    C28 AS "@C28",
+                    C29 AS "@C29",
+                    C30 AS "@C30",
+                    C31 AS "@C31",
+                    C32 AS "@C32"
+                FROM @RecNodeTable AS innerTable
+                WHERE innerTable.C01 = outerTable.C01       
+                FOR XML PATH(''R02''), TYPE
+            ) 
+            end AS node
+        FROM @RecNodeTable AS outerTable;
+
+
+        INSERT INTO ria_RecNode (grab_id, node, dateIn, [status]) 
+        select X.grab_id,X.[node],X.dateIn,-1 from @XMLTable X
+        left join ria_RecNode R on R.grab_id=X.grab_id
+        where X.node is null and R.grab_id is null
+
+        delete  from @XMLTable where node is null
+
+        update h set h.node=t.node,h.status=2
+        from RIA_RecNodeHistory h with(nolock)
+        inner join @XMLTable t on h.grab_id=t.grab_id and t.node is not null
+
+        update t set t.status=1
+        from RIA_RecNodeHistory h with(nolock)
+        inner join @XMLTable t on h.grab_id=t.grab_id and t.node is not null
+
+        if exists(select 1 from @XMLTable where status=0) begin
+            update h set h.node=t.node,h.status=2
+            from ria_RecNode h with(nolock)
+            inner join @XMLTable t on h.grab_id=t.grab_id
+
+            update t set t.status=1
+            from ria_RecNode h with(nolock)
+            inner join @XMLTable t on h.grab_id=t.grab_id
+        end
+        if exists(select 1 from @XMLTable where status=0) begin
+            INSERT INTO ria_RecNode (grab_id, node, dateIn, [status]) 
+            select grab_id,[node],dateIn,0 from @XMLTable where status=0    
+        end'
+
+      EXEC(@sql)
+
+    ------------------------------------END Pavel Martinez----------------------------------------
+

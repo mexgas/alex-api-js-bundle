3667d48ff55efec4cd8a7c2d7961dc91619a7c02
jesusGallardo <agallardo@nuxiba.com>
2026-03-24 23:42:48 -0600

    Se agrega para aliminar tabla ccBaseXDB

--- RecorderRIA/UpdateDB/20250905_v103_September.sql
+++ RecorderRIA/UpdateDB/20250905_v103_September.sql
@@ -322,7 +322,35 @@ END'
 
     ------------------------------------END Pavel Martinez----------------------------------------
 
+    SET @process = 'CW-9760 DROP TABLE ccBaseXDB;'
+    SET @sql = '
+       IF EXISTS (
+        SELECT 1
+        FROM sys.columns c
+        INNER JOIN sys.tables t ON c.object_id = t.object_id
+        WHERE t.name = N''ccBaseXDB''
+          AND c.name = N''id''
+          AND c.is_identity = 1
+    )
+    BEGIN
+     DROP TABLE ccBaseXDB;
+    END'
+    EXEC(@sql)
 
+    SET @process = 'CW-9760 create table  ccsp_BaseXmngr'
+    SET @sql = '
+    if not exists (select * from sys.tables where name = N''ccBaseXDB'')
+    begin
+         CREATE TABLE ccBaseXDB (
+            id INT primary key NOT NULL,
+            serviceId INT NULL,
+            dateStart DATETIME NOT NULL,
+            dateEnd DATETIME NULL,
+            Xname VARCHAR(25) NOT NULL,
+            isFull BIT NULL
+        );
+    end'
+    EXEC(@sql)
 
 
     update trec_parametros set par_valor = @Version where par_id = 30

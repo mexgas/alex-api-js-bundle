4023e60060a7414b2025d0cb25e690364d67fbd7
Marco García <marco.garcia@nuxiba.com>
2026-05-12 16:39:40 -0600

    Se quitó comentario de rollback

--- ServicesPack/CenterRia/37 ALTER TABLE dbo.ccVirtualAgent.sql
+++ ServicesPack/CenterRia/37 ALTER TABLE dbo.ccVirtualAgent.sql
@@ -7,13 +7,3 @@ ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN ReplyNoUnderstanding NVARCHAR(1000);
 
 ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN rules VARCHAR(6000);
 ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN instructions VARCHAR(MAX);
-
-
---ROLLBACK
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN ReplyGreeting NVARCHAR(350);
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN ReplyFarewell NVARCHAR(350);
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN ReplySystemFailure NVARCHAR(350);
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN ReplyNoUnderstanding NVARCHAR(350);
-
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN rules VARCHAR(1200);
---ALTER TABLE dbo.ccVirtualAgent ALTER COLUMN instructions VARCHAR(8000);

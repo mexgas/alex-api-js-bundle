2d537689b1cffe9d7380ae443d0071e43fe6f776
jesusGallardo <agallardo@nuxiba.com>
2026-05-14 15:57:35 -0600

    se quita por que va en reportes

--- CCenterRIA/UpdateDB/20250905_v127.7.1_ServicesPack9.sql
+++ CCenterRIA/UpdateDB/20250905_v127.7.1_ServicesPack9.sql
@@ -9017,16 +9017,6 @@ WHERE id = @camp_id
 SET NOCOUNT OFF';
     EXEC(@sql);
 
-
-    SET @process = 'INSERT ReportsFiltersMenus 2140'
-    SET @sql = 'INSERT INTO ReportsFiltersMenus(idReport,filterMenuName,showFilter) VALUES(2140,''date'',1),(2140,''filterby'',1)'
-    exec (@sql)
-    
-
-    SET @process = 'INSERT ReportsFilters 2140'
-    SET @sql = 'INSERT INTO ReportsFilters(reportName,filterName,id) VALUES(''Agent History'',''users'',2140),(''Agent History'',''areas'',2140)'
-    exec (@sql)
-
     
     SET @process = 'INSERT ccMenus 2140'
     SET @sql = 'INSERT INTO ccMenus (menu_id,menu_descrip,parent,Nivel,ordengral,type,HelpSWF,release) VALUES (2140,''Historico de Agentes|Agent History'',2000,''B'',2,3,'''',''5b93455cb709b0d0e530e8c721d1a8d4e39ec732890faa5e326f078f203e910e96bf09cfe811ecf0150db2af792c3401'');'

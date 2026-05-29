cc308f82d95bd840bf04f9585b581bfd01deab8f
jesusGallardo <agallardo@nuxiba.com>
2026-05-29 17:56:41 -0600

    se modifica para pruebas

--- DatabaseUpdateValidator/DatabaseUpdateValidator/Nuxiba/StartUp/BusinessService/Impl/NuxibaWorkerService.cs
+++ DatabaseUpdateValidator/DatabaseUpdateValidator/Nuxiba/StartUp/BusinessService/Impl/NuxibaWorkerService.cs
@@ -40,7 +40,7 @@ namespace DatabaseUpdateValidator.Nuxiba.StartUp.BusinessService.Impl
                 Password = password,
                 Server = server,
                 DirectoryPath = Path.Combine(path, "./CCenterRIA/UpdateDB/"),
-                Pattern = @"^.+_v(?<version>\d+)(?:\.(?<versionFix>\d+))?(?:\.(?<patch>\d+))?_.+\.sql$",
+                Pattern = @"^.+_nr(?<version>\d+)(?:\.(?<versionFix>\d+))?(?:\.(?<patch>\d+))?_.+\.sql$",
                 VersionQuery = @"ccsp_getVersion 'BD'",
                 VersionQueryFix = @"declare @version varchar(max);
                     select @version =valor from ccsettings where setting_id=77;
@@ -63,7 +63,8 @@ EXEC sp_detach_db 'CCenterRIA';"
                 Password = password,
                 Server = server,
                 DirectoryPath = Path.Combine(path, "./RecorderRIA/UpdateDB/"),
-                Pattern = @"^.+_nr(?<version>\d+)(?:\.(?<versionFix>\d+))?(?:\.(?<patch>\d+))?_.+\.sql$",
+                Pattern = @"^.+_v(?<version>\d+)(?:\.(?<versionFix>\d+))?(?:\.(?<patch>\d+))?_.+\.sql$",
+
                 VersionQuery = "select cast(par_valor as int) from trec_parametros where par_id = 30",
                 QueryAttaach = @"
 if not exists(select * from sys.databases where name='CCRecorderRIA') begin

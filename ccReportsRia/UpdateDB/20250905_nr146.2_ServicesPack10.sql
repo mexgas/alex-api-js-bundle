5f1a05f466e091e7f12be7fb049fa24b39cc1658
Uriel Martinez <uriel.martinez@nuxiba.com>
2026-06-10 16:46:40 -0600

    modificacion en menu de cola virtual

--- ccReportsRia/UpdateDB/20250905_nr146.2_ServicesPack10.sql
+++ ccReportsRia/UpdateDB/20250905_nr146.2_ServicesPack10.sql
@@ -7678,7 +7678,7 @@ IF OBJECT_ID(''dbo.ccspRepTwitterGeneral'', ''P'') IS NOT NULL
     SET @process = 'INSERT ccMenus 3230'
     SET @sql = 'IF NOT EXISTS (SELECT 1 FROM ccMenus WHERE menu_id = 3230)
     BEGIN
-        INSERT INTO ccMenus (menu_id,menu_descrip,parent,Nivel,ordengral,type,HelpSWF,release) VALUES (3230,''Cola virtual|Virtual queue'',3000,''B'',2,3,''Error'',''3f2db252ad1e8747e879159ed8498ccf30092c85618c7a5edde85033f5b8cfac'');
+        INSERT INTO ccMenus (menu_id,menu_descrip,parent,Nivel,ordengral,type,HelpSWF,release) VALUES (3230,''Cola virtual|Virtual queue'',3000,''B'',3,3,''Error'',''3f2db252ad1e8747e879159ed8498ccf30092c85618c7a5edde85033f5b8cfac'');
     END'
     exec (@sql)
 

ecc59dfd36917469bdaf9abec2237fd49d57f571
jesusGallardo <agallardo@nuxiba.com>
2026-07-11 21:25:39 -0600

    se agrega para login xion CREATE PROCEDURE [dbo].[ccsp_TwitterInitialStatistics]

--- CCenterRIA/UpdateDB/20250905_v127.7.3_ServicesPack11.sql
+++ CCenterRIA/UpdateDB/20250905_v127.7.3_ServicesPack11.sql
@@ -1012,12 +1012,66 @@ END'
     exec (@sql)
     
 
-    SET @process = ''
-    SET @sql = ''
+   
+    SET @process = ' DROP PROCEDURE dbo.ccsp_TwitterInitialStatistics,ccsp_TwitterSave,ccspADMaddConversationTweet'
+    SET @sql = '
+IF OBJECT_ID(''dbo.ccsp_TwitterInitialStatistics'', ''P'') IS NOT NULL
+    DROP PROCEDURE dbo.ccsp_TwitterInitialStatistics;
+'
     exec (@sql)
 
-    SET @process = ''
-    SET @sql = ''
+    SET @process = 'CREATE PROCEDURE [dbo].[ccsp_TwitterInitialStatistics] se agrega para xion para login'
+    SET @sql = 'CREATE PROCEDURE [dbo].[ccsp_TwitterInitialStatistics]
+@inboundId int=0,
+@Option AS SMALLINT=0,
+@User_id AS SMALLINT=0
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    if(@Option=0)
+    begin
+        select
+        count(*) received,
+        count(case when messageStatusId = 1 then 1 else null end) pending,
+        count(case when messageStatusId in (2,3) then 1 else null end) assigned,
+        count(case when messageStatusId = 4 then 1 else null end) unassigned,
+        count(case when messageStatusId in (5,6,10,11) then 1 else null end) sent,
+        count(case when messageStatusId = 7 then 1 else null end) rejected,
+        count(case when messageStatusId = 8 then 1 else null end) programFwd,
+        count(case when messageStatusId = 9 then 1 else null end) forwarding,
+        count(case when messageStatusId in (10,11) then 1 else null end) closed,
+        count(case when messageStatusId = 3 then 1 else null end) active ,
+        isnull(AVG(msg.twait + msg.tretention + msg.tresponse),0) avgtAtention,
+        isnull(AVG(msg.twait),0) avgtWait,
+        isnull(MAX(msg.twait),0) maxtWait
+        from messageOutTwitter msg(nolock) join conversationTwitter con (nolock) on con.conversationTwitterId=msg.conversationTwitterId
+        where inboundId=@inboundId and [date] between convert(varchar(10),getdate(),121) and convert(varchar(10),getdate()+1,121)
+    end
+    if @Option = 1
+    BEGIN
+        select
+        count(*) received,
+        count(case when messageStatusId = 1 then 1 else null end) pending,
+        count(case when messageStatusId in (2,3) then 1 else null end) assigned,
+        count(case when messageStatusId = 4 then 1 else null end) unassigned,
+        count(case when messageStatusId in (5,6,10,11) then 1 else null end) sent,
+        count(case when messageStatusId = 7 then 1 else null end) rejected,
+        count(case when messageStatusId = 8 then 1 else null end) programFwd,
+        count(case when messageStatusId = 9 then 1 else null end) forwarding,
+        count(case when messageStatusId in (10,11) then 1 else null end) closed,
+        count(case when messageStatusId = 3 then 1 else null end) active ,
+        isnull(AVG(msg.twait + msg.tretention + msg.tresponse),0) avgtAtention,
+        isnull(AVG(msg.twait),0) avgtWait,
+        isnull(MAX(msg.twait),0) maxtWait,
+        InboundId inboundId
+        from messageOutTwitter msg (nolock) join conversationTwitter con (nolock) on con.conversationTwitterId=msg.conversationTwitterId
+        where inboundId in (select inbound_id from ccInbound where inbound_id in (SELECT cam_id FROM ccSupervisorCam WHERE user_id = @User_id AND tipo = 0) and chat = 4)
+        and [date] between convert(varchar(10),getdate(),121) and convert(varchar(10),getdate()+1,121)
+        GROUP BY InboundId
+    END
+END'
     exec (@sql)
     
 
